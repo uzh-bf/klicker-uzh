205ad6a62b0ad84ba657938617c4fc06ce432f13
Roland Schläfli <rolandschlaefli@gmail.com>
2017-06-17 17:17:21 +0200

    Setup a basic application structure with graphql

---
+++ components/QuestionList.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import { gql, graphql } from 'react-apollo'
+
+class QuestionList extends Component {
+  render() {
+    const { data } = this.props
+
+    if (data.loading) {
+      return <div>Loading</div>
+    }
+
+    return (
+      <div>
+        <ul>
+          {data.allQuestions.map(question =>
+            <li key={question.id}>{question.title}</li>
+          )}
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default graphql(gql`
+  {
+    allQuestions {
+      id
+      title
+    }
+  }
+`)(QuestionList)
---
+++ pages/questions.js
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import withData from '../lib/withData'
+import App from '../components/App'
+import QuestionList from '../components/QuestionList'
+
+export default withData(() => {
+  return (
+    <App>
+      All questions in our database:
+      <QuestionList />
+      <Link href="/">
+        <a>Back to main</a>
+      </Link>
+    </App>
+  )
+})
