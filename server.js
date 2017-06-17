4fd82c376502284db2a09e2777c4ef01ebb993ed
Roland Schläfli <rolandschlaefli@gmail.com>
2017-06-17 22:56:22 +0200

    restructure into src folder, install storybook

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const next = require('next')
 
 const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
+const app = next({ dir: './src', dev })
 const handle = app.getRequestHandler()
 
 app
