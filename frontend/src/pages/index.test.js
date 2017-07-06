a29692fa0d355cfda80ee12c6928c61db8df2487
Roland Schläfli <rolandschlaefli@gmail.com>
2017-07-06 16:07:36 +0200

    Setup jest for testing (#2)
    Setup jest for testing

---
+++ frontend/src/pages/index.test.js
@@ -0,0 +1,14 @@
+/* eslint-env jest */
+
+import React from 'react'
+import renderer from 'react-test-renderer'
+
+import Index from './index'
+
+describe('Snapshot-Testing', () => {
+  it('Works', () => {
+    const component = renderer.create(<Index />)
+    const tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
