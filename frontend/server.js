5a54b66a49781da3c26ca60fbbb273807aa3f07b
Roland Schläfli <rolandschlaefli@gmail.com>
2017-07-06 17:59:46 +0200

    Add internationalization with react-intl (#1)

--- frontend/server.js
+++ frontend/server.js
@@ -1,20 +1,60 @@
+// Polyfill Node with `Intl` that has data for all locales.
+// See: https://formatjs.io/guides/runtime-environments/#server
+const IntlPolyfill = require('intl')
+
+Intl.NumberFormat = IntlPolyfill.NumberFormat
+Intl.DateTimeFormat = IntlPolyfill.DateTimeFormat
+
+const { basename } = require('path')
+const { readFileSync } = require('fs')
+const glob = require('glob')
+
+const accepts = require('accepts')
 const express = require('express')
 const next = require('next')
 
 const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dir: './src', dev })
+const app = next({ dev, dir: './src' })
 const handle = app.getRequestHandler()
+
+// Get the supported languages by looking for translations in the `lang/` dir.
+const languages = glob.sync('./lang/*.json').map(f => basename(f, '.json'))
+
+// We need to expose React Intl's locale data on the request for the user's
+// locale. This function will also cache the scripts by lang in memory.
+const localeDataCache = new Map()
+const getLocaleDataScript = (locale) => {
+  const lang = locale.split('-')[0]
+  if (!localeDataCache.has(lang)) {
+    const localeDataFile = require.resolve(`react-intl/locale-data/${lang}`)
+    const localeDataScript = readFileSync(localeDataFile, 'utf8')
+    localeDataCache.set(lang, localeDataScript)
+  }
+  return localeDataCache.get(lang)
+}
+
+// We need to load and expose the translations on the request for the user's
+// locale. These will only be used in production, in dev the `defaultMessage` in
+// each message description in the source code will be used.
+const getMessages = locale => require(`./lang/${locale}.json`)
 
 app
   .prepare()
   .then(() => {
     const server = express()
 
-    server.get('*', (req, res) => handle(req, res))
+    server.get('*', (req, res) => {
+      const accept = accepts(req)
+      const locale = accept.language(dev ? ['en'] : languages)
+      req.locale = locale
+      req.localeDataScript = getLocaleDataScript(locale)
+      req.messages = dev ? {} : getMessages(locale)
+      handle(req, res)
+    })
 
     server.listen(3000, (err) => {
       if (err) throw err
-      console.log('Ready on http://localhost:3000')
+      console.log('> Ready on http://localhost:3000')
     })
   })
   .catch((err) => {
