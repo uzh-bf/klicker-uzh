fc775e5097a3d059b25e2586845b6308cbce0976
Roland Schläfli <rolandschlaefli@gmail.com>
2017-07-07 16:42:26 +0200

    (fix) Env variables not being replaced correctly

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,12 @@
 set -e
 
 # embed environment variables
-SENTRY=${SENTRY:-null}
-LOGROCKET=${LOGROCKET:-null}
+SENTRY=${SENTRY:-'__SENTRY__'}
+LOGROCKET=${LOGROCKET:-'__LOGROCKET__'}
 
 echo "Replacing environment variables..."
-sed -i 's;__SENTRY__;"$SENTRY";g' ./src/lib/env.js
-sed -i 's;__LOGROCKET__;"$LOGROCKET";g' ./src/lib/env.js
+sed -i 's;__SENTRY__;'"$SENTRY"';g' ./src/lib/env.js
+sed -i 's;__LOGROCKET__;'"$LOGROCKET"';g' ./src/lib/env.js
 
 echo "Building application sources..."
 yarn run build
