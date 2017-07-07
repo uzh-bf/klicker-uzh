a613b4c5c8e0906b6d7708346229f429e3935d95
Roland Schläfli <rolandschlaefli@gmail.com>
2017-07-07 17:32:48 +0200

    Initialize variable lockfiles if they don't yet exist

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,10 @@ set -e
 SENTRY=${SENTRY:-'__SENTRY__'}
 LOGROCKET=${LOGROCKET:-'__LOGROCKET__'}
 
+# initialize variable lockfiles if they don't yet exist
+if [[ ! -f /tmp/klicker_sentry ]]; then echo "__SENTRY__" >> /tmp/klicker_sentry fi
+if [[ ! -f /tmp/klicker_logrocket ]]; then echo "__LOGROCKET__" >> /tmp/klicker_logrocket fi
+
 # ensure that the application has not been built already
 # take into account any changed environment variables
 # => prevent build from rerunning on crash, restart etc.
@@ -22,7 +26,7 @@ if [[ ! -f /tmp/klicker.lock || !SENTRY_SAME || !LOGROCKET_SAME ]]; then
   echo "Building application sources..."
   yarn run build
 
-  echo "Saving lockfile..."
+  echo "Saving lockfiles..."
   touch /tmp/klicker.lock
   echo "$SENTRY" >> /tmp/klicker_sentry
   echo "$LOGROCKET" >> /tmp/klicker_logrocket
