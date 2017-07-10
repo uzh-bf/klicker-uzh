3d83c50d4d1b656dac7328b5b3eec627109ef1bb
Roland Schläfli <rolandschlaefli@gmail.com>
2017-07-10 11:16:59 +0200

    (fix) Lint errors

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,26 +1,22 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { gql, graphql } from 'react-apollo'
 
-class QuestionList extends Component {
-  render() {
-    const { data } = this.props
-
-    if (data.loading) {
-      return <div>Loading</div>
-    }
-
-    return (
-      <div>
-        <ul>
-          {data.allQuestions.map(question =>
-            (<li key={question.id}>
-              {question.title}
-            </li>),
-          )}
-        </ul>
-      </div>
-    )
+const QuestionList = ({ data }) => {
+  if (data.loading) {
+    return <div>Loading</div>
   }
+
+  return (
+    <div>
+      <ul>
+        {data.allQuestions.map(question =>
+          (<li key={question.id}>
+            {question.title}
+          </li>),
+        )}
+      </ul>
+    </div>
+  )
 }
 
 export default graphql(
