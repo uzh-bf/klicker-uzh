05e80a97744f3bd682786cbf0dd4fb75b5c9fb6e
Roland Schläfli <rolandschlaefli@gmail.com>
2017-07-17 17:49:50 +0200

    Extend WithCSS with displayName

--- src/lib/withCSS.js
+++ src/lib/withCSS.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 // take in a ComposedComponent and a list of semantic ui css file links
 // links=[dropdown] will lead to inclusion of dropdown.min.css from CDN
-export default (ComposedComponent, links) => {
+const WithCSS = (ComposedComponent, links) => {
   // construct next/head by mapping over all requested links
   // TODO: extension => check whether link is semantic or full
   // HACK: this might lead to duplicate link declarations in <head>
@@ -22,3 +22,7 @@ export default (ComposedComponent, links) => {
   // return the enhanced component, passing head and remaining props
   return props => <ComposedComponent head={head} {...props} />
 }
+
+WithCSS.displayName = `WithCSS(${WithCSS.displayName})`
+
+export default WithCSS
