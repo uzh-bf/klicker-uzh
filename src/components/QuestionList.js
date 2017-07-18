22797cd6660c10f019da5f722e0a9bc494317007
Roland Schläfli <rolandschlaefli@gmail.com>
2017-07-18 12:34:11 +0200

    Setup basic layout with TagList and QuestionList components, install lodash

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,33 +1,41 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { gql, graphql } from 'react-apollo'
+import { Segment } from 'semantic-ui-react'
 
-const QuestionList = ({ data }) => {
+import withCSS from '../lib/withCSS'
+
+const QuestionList = ({ data, head }) => {
   if (data.loading) {
     return <div>Loading</div>
   }
 
   return (
     <div>
-      <ul>
-        {data.allQuestions.map(question =>
-          (<li key={question.id}>
-            {question.title}
-          </li>),
-        )}
-      </ul>
+      {head}
+
+      {data.allQuestions.map(question =>
+        (<Segment key={question.id}>
+          {question.title}
+        </Segment>),
+      )}
     </div>
   )
 }
 
 QuestionList.propTypes = {
   data: PropTypes.shape({
-    allQuestions: PropTypes.shape({
-      id: PropTypes.number,
-      title: PropTypes.string,
-    }),
+    allQuestions: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+      }),
+    ),
   }).isRequired,
+  head: PropTypes.node.isRequired,
 }
+
+const QuestionListWithCSS = withCSS(QuestionList, ['segment'])
 
 export default graphql(
   gql`
@@ -38,4 +46,4 @@ export default graphql(
       }
     }
   `,
-)(QuestionList)
+)(QuestionListWithCSS)
---
+++ src/components/TagList.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import _sortBy from 'lodash/sortBy'
+import { gql, graphql } from 'react-apollo'
+import { List } from 'semantic-ui-react'
+
+import withCSS from '../lib/withCSS'
+
+const TagList = ({ activeTags, data, head, handleTagClick }) => {
+  if (data.loading) {
+    return <div>Loading</div>
+  }
+
+  // sort the tags by name
+  const sortedByName = _sortBy(data.allTags, ['name'])
+
+  return (
+    <List size="big">
+      {head}
+
+      {sortedByName.map(tag =>
+        (<List.Item key={tag.id}>
+          <List.Icon name="folder" />
+          <List.Content onClick={() => handleTagClick(tag.id)}>
+            <a className={activeTags.includes(tag.id) ? 'active' : 'inactive'} role="presentation">
+              {tag.name}
+            </a>
+          </List.Content>
+        </List.Item>),
+      )}
+
+      <style jsx>{`
+        .active {
+          font-weight: bold;
+        }
+      `}</style>
+    </List>
+  )
+}
+
+TagList.propTypes = {
+  activeTags: PropTypes.arrayOf(PropTypes.string),
+  data: PropTypes.shape({
+    loading: PropTypes.bool.isRequired,
+    allTags: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+      }),
+    ),
+  }).isRequired,
+  handleTagClick: PropTypes.func.isRequired,
+  head: PropTypes.node.isRequired,
+}
+
+TagList.defaultProps = {
+  activeTags: [],
+}
+
+const TagListWithCSS = withCSS(TagList, ['list'])
+
+export default graphql(gql`
+  {
+    allTags {
+      id
+      name
+    }
+  }
+`)(TagListWithCSS)
