1b95fad515b5c4dbf905726233e3f389ae09d2a3
Pascal Zehnder <pascal.zehnder@bf.uzh.ch>
2017-07-18 18:04:47 +0200

    Basic grid setting

--- src/components/questions/Question.js
+++ src/components/questions/Question.js
@@ -1,25 +1,31 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Segment } from 'semantic-ui-react'
+import { Grid, Segment } from 'semantic-ui-react'
 
 import withCSS from '../../lib/withCSS'
 
 const Question = ({ head, id, title }) =>
   (
-    <Segment.Group>
-      <Segment.Group horizontal>
-        <Segment>Hi</Segment>
-        <Segment>Ho</Segment>
-        <Segment>Ha</Segment>
-      </Segment.Group>
-      <Segment.Group horizontal>
-        <Segment inverted secondary>
-          {head}
-          <strong>{id}</strong> - {title}
-        </Segment>
-        <Segment inverted color="pink">Zuletzt</Segment>
-      </Segment.Group>
-    </Segment.Group>
+    <Segment>
+      <Grid>
+        <Grid.Row>
+          <Grid.Column floated="left" width="12">
+            {head}
+            <strong>{id}</strong> - {title}
+          </Grid.Column>
+          <Grid.Column floated="right" width="1">MC</Grid.Column>
+          <Grid.Column floated="right" width="1">CAPM</Grid.Column>
+          <Grid.Column floated="right" width="1">Risk</Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column width="11">Test</Grid.Column>
+          <Grid.Column width="5">
+           Zuletzt
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Segment>
+
   )
 
 Question.propTypes = {
@@ -28,4 +34,4 @@ Question.propTypes = {
   title: PropTypes.string.isRequired,
 }
 
-export default withCSS(Question, ['segment'])
+export default withCSS(Question, ['grid', 'segment'])
