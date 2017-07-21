f22f14fe4d5f19674b6b8ea666c5b159fbbd8734
Roland Schläfli <rolandschlaefli@gmail.com>
2017-07-21 13:30:29 +0200

    Setup storybook for question

--- stories/index.js
+++ stories/index.js
@@ -4,4 +4,6 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import Question from '../src/components/questions/Question'
 
+import '../node_modules/semantic-ui-css/semantic.min.css'
+
 storiesOf('Question', module).add('basic', () => <Question id="abcd" title="Hello world" />)
