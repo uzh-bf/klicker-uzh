4fe3162ea71bfad34ddc83468f6bb2dd353f617d
Roland Schläfli <rolandschlaefli@gmail.com>
2017-07-21 13:33:58 +0200

    Wrap story with grid row and column

--- stories/index.js
+++ stories/index.js
@@ -2,8 +2,17 @@
 
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import Question from '../src/components/questions/Question'
+import { Grid } from 'semantic-ui-react'
 
+import Question from '../src/components/questions/Question'
 import '../node_modules/semantic-ui-css/semantic.min.css'
 
-storiesOf('Question', module).add('basic', () => <Question id="abcd" title="Hello world" />)
+storiesOf('Question', module).add('basic', () =>
+  (<Grid padded stackable>
+    <Grid.Row>
+      <Grid.Column>
+        <Question id="abcd" title="Hello world" />
+      </Grid.Column>
+    </Grid.Row>
+  </Grid>),
+)
