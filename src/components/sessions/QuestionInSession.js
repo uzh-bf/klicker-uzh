75e32f65a4e51ab2441c341d49224167bfc037b1
Pascal Zehnder <pascal.zehnder@bf.uzh.ch>
2017-07-24 13:27:25 +0200

    TitleRow formatted

--- src/components/sessions/QuestionInSession.js
+++ src/components/sessions/QuestionInSession.js
@@ -5,18 +5,25 @@ import { Divider, Grid, Segment } from 'semantic-ui-react'
 import withCSS from '../../lib/withCSS'
 
 const QuestionInSession = ({ head, id, title, type }) =>
-  (<Segment as={Grid}>
+  (<Segment as={Grid} className="questionInSession">
     {head}
-    <Grid.Row columns="2">
-      <Grid.Column><strong>{id}</strong></Grid.Column>
-      <Grid.Column>{type}</Grid.Column>
+    <Grid.Row className="titleRow" columns="2">
+      <Grid.Column floated="left" width="8"><strong>{id}</strong></Grid.Column>
+      <Grid.Column floated="right" width="1">{type}</Grid.Column>
     </Grid.Row>
     <Divider />
     <Grid.Row>
       {title}
     </Grid.Row>
     <style jsx global>{`
-
+      .ui.grid.questionInSession > .row.titleRow {
+        padding-top: 0;
+        padding-bottom: 0;
+      }
+      .ui.grid.questionInSession > .row.titleRow > .column {
+        padding-left: 0;
+        padding-right: 0;
+      }
     `}</style>
   </Segment>)
 
