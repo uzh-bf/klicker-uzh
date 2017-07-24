94ea591cf872af42698136cd6f30975e5c911b5e
Pascal Zehnder <pascal.zehnder@bf.uzh.ch>
2017-07-24 10:30:02 +0200

    Add one session to stories

--- stories/index.js
+++ stories/index.js
@@ -3,5 +3,8 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import Question from '../src/components/questions/Question'
+import Session from '../src/components/sessions/Session'
 
 storiesOf('Question', module).add('basic', () => <Question id="abcd" title="Hello world" />)
+
+storiesOf('Session', module).add('basic', () => <Session id="abcd" title="Hello world" />)
