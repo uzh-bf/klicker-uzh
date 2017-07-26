8aa8012b8b32c8f45eb1cb7e054abd3ef0e3a754
Roland Schläfli <rolandschlaefli@gmail.com>
2017-07-26 13:29:29 +0200

    Add QuestionTags component with responsive flex layout

---
+++ src/components/questions/question/QuestionTags.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const QuestionTags = ({ tags, type }) =>
+  (<div className="container">
+    <div className="type tag">
+      {type}
+    </div>
+    {tags.map(tag =>
+      (<div className="tag">
+        {tag}
+      </div>),
+    )}
+
+    <style jsx>{`
+      .container {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+      }
+      .tag {
+        border-top: solid 1px;
+        padding: 0.5rem 1rem;
+        text-align: center;
+      }
+      .tag:last-child {
+        border-bottom: solid 1px;
+      }
+      .type {
+        font-weight: bold;
+      }
+
+      @media all and (min-width: 768px) {
+        .container {
+          flex-direction: row;
+          justify-content: flex-end;
+        }
+        .tag {
+          border-left: solid 1px;
+        }
+        .tag:last-child {
+          border-bottom: 0;
+          border-right: solid 1px;
+        }
+      }
+    `}</style>
+  </div>)
+
+QuestionTags.propTypes = {
+  tags: PropTypes.arrayOf(PropTypes.string).isRequired,
+  type: PropTypes.string.isRequired,
+}
+
+export default QuestionTags
