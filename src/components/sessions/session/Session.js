086fd2f69778392a081ff17d28f2f7f791b1ab76
Roland Schläfli <rolandschlaefli@gmail.com>
2017-07-27 12:43:21 +0200

    Rework flexbox styling for session (mobile-first)

--- src/components/sessions/session/Session.js
+++ src/components/sessions/session/Session.js
@@ -2,40 +2,21 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import moment from 'moment'
 import { FormattedMessage } from 'react-intl'
-import { Button, Grid, Icon, Segment } from 'semantic-ui-react'
+import { Button, Icon } from 'semantic-ui-react'
 
 import QuestionBlock from './QuestionBlock'
 import withCSS from '../../../lib/withCSS'
 
-const Session = ({ createdAt, head, name, blocks, id, status }) => {
-  let buttonContent = ''
-  let buttonIcon = ''
-  switch (status) {
-    case 'RUNNING': {
-      buttonContent = (
-        <FormattedMessage id="session.button.running.content" defaultMessage="Running" />
-      )
-      buttonIcon = 'play' // TODO which icon here?
-      break
-    }
-    case 'COMPLETED': {
-      buttonContent = (
-        <FormattedMessage id="session.button.completed.content" defaultMessage="Copy" />
-      )
-      buttonIcon = 'copy'
-      break
-    }
-    default: {
-      buttonContent = (
-        <FormattedMessage id="session.button.created.content" defaultMessage="Start" />
-      )
-      buttonIcon = 'play'
-      break
-    }
+const Session = ({ createdAt, name, blocks, id, status }) => {
+  const statusCases = {
+    completed: { disabled: false, icon: 'copy', message: <FormattedMessage id="session.button.completed.content" defaultMessage="Copy" /> },
+    created: { disabled: false, icon: 'play', message: <FormattedMessage id="session.button.created.content" defaultMessage="Start" /> },
+    running: { disabled: true, icon: 'play', message: <FormattedMessage id="session.button.running.content" defaultMessage="Running" /> },
   }
+  const buttonStatus = statusCases[status]
 
   return (
-    <div className="container">
+    <div className="session">
       <h2 className="title">
         {id.slice(0, 7)} - {name}
       </h2>
@@ -43,125 +24,108 @@ const Session = ({ createdAt, head, name, blocks, id, status }) => {
         <FormattedMessage id="sessionHistory.string.createdOn" defaultMessage="Created on" />{' '}
         {moment(createdAt).format('DD.MM.YYYY HH:MM:SS')}
       </div>
+
       <div className="details">
-        <div className="blocks">
-          {blocks.map(block =>
-            (<div className="block">
-              <QuestionBlock
-                questions={block.questions.map(question => ({
-                  id: question.id,
-                  title: question.questionDefinition.title,
-                  type: question.questionDefinition.type,
-                }))}
-                showSolutions={block.showSolutions}
-                timeLimit={block.timeLimit}
-              />
-            </div>),
-          )}
+        {blocks.map(block =>
+          (<div className="block">
+            <QuestionBlock
+              questions={block.questions.map(question => ({
+                id: question.id,
+                title: question.questionDefinition.title,
+                type: question.questionDefinition.type,
+              }))}
+              showSolutions={block.showSolutions}
+              timeLimit={block.timeLimit}
+            />
+          </div>),
+        )}
+        <div className="actionArea">
+          <Button icon disabled={buttonStatus.disabled} labelPosition="left">
+            <Icon name={buttonStatus.icon} />
+            {buttonStatus.message}
+          </Button>
         </div>
       </div>
 
       <style jsx>{`
-        .container {
+        .session, .details {
           display: flex;
-          flex-flow: row wrap;
+          flex-direction: column;
         }
-        .title {
-          margin: 0;
-          margin-bottom: 0.5rem;
-          font-size: 1.2rem;
-          text-align: center;
-          width: 100%;
-        }
+        .title,
         .date {
-          text-align: center;
-          width: 100%;
-        }
-        .details {
-          background-color: lightgrey;
-          width: 100%;
-        }
-        .blocks {
-          display: flex;
-          margin: 0.5rem;
+          margin: auto;
+          margin-bottom: 0.5rem;
         }
         .block {
-          flex-grow: 1;
-          flex-shrink: 1;
+          margin: 0 0.5rem;
+          margin-bottom: 0.5rem;
+        }
+        .actionArea {
+          margin: auto;
         }
 
         @media all and (min-width: 768px) {
+          .session, .details {
+            flex-flow: row wrap;
+          }
+          .details {
+            padding: 0.5rem;
+          }
+          .title,
+          .date {
+            flex: 0 0 50%;
+            margin: 0;
+          }
           .title {
-            text-align: left;
-            width: 70%;
+            font-size: 1.2rem;
+            margin-bottom: 0.5rem;
           }
           .date {
+            align-self: center;
             text-align: right;
-            width: 30%;
+          }
+          .details {
+            border: 1px solid lightgrey;
+          }
+          .block {
+            flex: 1;
+            margin: 0;
+          }
+          .block:not(:first-child) {
+            margin-left: 0.5rem;
+          }
+          .actionArea {
+            align-self: flex-end;
+            margin: 0;
+            margin-left: 1rem;
           }
         }
       `}</style>
     </div>
   )
-
-  return (
-    <Grid padded stackable className="session">
-      {head}
-
-      {/* TODO Possibility for more than two columns */}
-      <Segment as={Grid.Row} className="questionsRow">
-        {/* TODO desturcture questions array and pass title and type */
-        blocks.map(({ id: questionId, title, type }) =>
-          /* TODO Possibility for more than two columns, depends on long id */
-          (<Grid.Column key={questionId} width="3">
-            <QuestionInSession
-              id={id} // HACK Correct short ID
-              title={title}
-              type={type}
-              /* TODO pass type and id as props */
-            />
-          </Grid.Column>),
-        )}
-        <Grid.Column
-          className="buttonColumn"
-          floated="right"
-          textAlign="right"
-          verticalAlign="bottom"
-          width="3"
-        >
-          <Button icon labelPosition="left">
-            <Icon name={buttonIcon} />
-            {buttonContent}
-          </Button>
-        </Grid.Column>
-      </Segment>
-
-      <style jsx>{`
-        .container {
-        }
-      `}</style>
-      <style jsx global>{`
-        .ui.grid.session > .row.questionsRow {
-          margin-top: 0;
-        }
-      `}</style>
-    </Grid>
-  )
 }
 
 Session.propTypes = {
+  blocks: PropTypes.shape({
+    questions: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      questionDefinition: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        type: PropTypes.string.isRequired,
+      }).isRequired,
+      showSolutions: PropTypes.bool.isRequired,
+      timeLimit: PropTypes.number.isRequired,
+    }),
+  }).isRequired,
   createdAt: PropTypes.string.isRequired,
-  head: PropTypes.node.isRequired,
   id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   status: PropTypes.string.isRequired,
-  questions: PropTypes.shape({
-    questionDefinition: PropTypes.shape({
-      createdAt: PropTypes.string.isRequired,
-      title: PropTypes.string.isRequired,
-      type: PropTypes.string.isRequired,
-    }).isRequired,
-  }).isRequired,
+}
+
+Session.defaultProps = {
+  status: 'created',
 }
 
 export default withCSS(Session, ['segment'])
