ce50891d81cd7628a528811de7aad1224ef4278c
Pascal Zehnder <pascal.zehnder@bf.uzh.ch>
2017-07-28 14:48:37 +0200

    Session Progress Box initial design

--- src/components/sessions/activeSession/SessionProgress.js
+++ src/components/sessions/activeSession/SessionProgress.js
@@ -1,20 +1,56 @@
 import React from 'react'
-import { Button } from 'semantic-ui-react'
+import { Button, Icon } from 'semantic-ui-react'
 
 const SessionProgress = () => (
-  <div className="flex">
-    <div className="container">Container</div>
-    <Button className="cancel" content="Abbrechen" icon="close" labelPosition="left" />
-    <Button className="evaluation" content="Auswertung" icon="play" labelPosition="left" />
+  <div className="container">
+    <div className="sessionContainer">
+      <div className="topRow">
+        <div><Icon name="time" /> Start</div>
+        <div><Icon name="play circle" /> Laufzeit</div>
+        <div>Sessions</div>
+      </div>
+      <div className="content">Content</div>
+    </div>
+    <div className="buttonSection">
+      <Button className="cancel" content="Abbrechen" icon="close" labelPosition="left" />
+      <Button className="evaluation" content="Auswertung" icon="play" labelPosition="left" />
+    </div>
 
     <style jsx>{`
-        .flex {
+        .container {
           display: flex;
+          flex-direction: row;
+          flex-wrap: wrap;
+        }
+        .sessionContainer {
+          background-color: lightgrey;
+          border: 1px solid;
+          flex: 0 0 100%;
+          margin-bottom: 0.5rem;
+        }
+        .sessionContainer > .topRow {
+          display: flex;
+          border-bottom: 1px solid;
+          padding: 0.5rem;
+        }
+        .sessionContainer > .topRow > div {
+          margin: 0 0.5rem;
+        }
+        .sessionContainer > .content {
+          display: flex;
+          padding: 0.5rem;
+        }
+        .buttonSection {
+          display: flex;
+          flex-direction: row;
+          flex-wrap: wrap;
         }
         .cancel {
+          flex: 0 0 20%
           justify-content: flex-start;
         }
         .evaluation {
+          flex: 0 0 20%
           justify-content: flex-end;
         }
       `}</style>
