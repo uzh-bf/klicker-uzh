26b1c5bfa1231872755d13a8c860b9893f952a2d
Roland Schläfli <rolandschlaefli@gmail.com>
2017-08-05 16:53:06 +0200

    (fix) Storybook working with react-intl

--- stories/index.js
+++ stories/index.js
@@ -4,6 +4,7 @@
 
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import { IntlProvider } from 'react-intl'
 
 import Question from '../src/components/questions/question/Question'
 import QuestionSingle from '../src/components/sessions/session/QuestionSingle'
@@ -13,20 +14,40 @@ import './base.css'
 
 storiesOf('Question', module)
   .add('SC with a single version', () =>
-    (<Question id="1"
-               lastUsed={['2015-02-08 14:32:11', '2016-09-09 15:22:09']}
-               tagList={['CAPM', 'Risk']}
-               title="Hello world" type="SC"
-    />))
-  .add('MC with multiple versions', () =>
-    (<Question id="1" lastUsed={['2017-08-08 14:30:22', '2016-09-09 15:22:09']} tagList={['Beta']} title="Good question"
-               type="MC" version="2"
-    />),
+    (<IntlProvider>
+      <Question
+        id="1"
+        lastUsed={['2015-02-08 14:32:11', '2016-09-09 15:22:09']}
+        tags={['CAPM', 'Risk']}
+        title="Hello world"
+        type="SC"
+      />
+    </IntlProvider>),
   )
-
-storiesOf('QuestionSingle', module).add('default', () =>
-  <QuestionSingle id="abc" title="hello world this is a long long question" type="SC" />,
-)
+  .add('MC with multiple versions', () =>
+    (<IntlProvider>
+      <Question
+        id="1"
+        lastUsed={['2017-08-08 14:30:22', '2016-09-09 15:22:09']}
+        tags={['Beta']}
+        title="Good question"
+        type="MC"
+        version="2"
+      />
+    </IntlProvider>),
+  )
+  .add('MC without tags', () =>
+    (<IntlProvider>
+      <Question
+        id="1"
+        lastUsed={['2017-08-08 14:30:22', '2016-09-09 15:22:09', '2015-10-09 15:22:09']}
+        tags={[]}
+        title="Good question"
+        type="MC"
+        version="1"
+      />
+    </IntlProvider>),
+  )
 
 storiesOf('QuestionBlock', module)
   .add('default', () =>
@@ -53,3 +74,7 @@ storiesOf('QuestionBlock', module)
     />),
   )
   .add('empty', () => <QuestionBlock questions={[]} showSolutions={false} timeLimit={60} />)
+
+storiesOf('QuestionSingle', module).add('default', () =>
+  <QuestionSingle id="abc" title="hello world this is a long long question" type="SC" />,
+)
