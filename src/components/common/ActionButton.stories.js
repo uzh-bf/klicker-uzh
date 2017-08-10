1e9d5fe19750ef327f3d304177e2170967056ceb
Roland Schläfli <rolandschlaefli@gmail.com>
2017-08-10 17:49:44 +0200

    Add stories for the new components

---
+++ src/components/common/ActionButton.stories.js
@@ -0,0 +1,15 @@
+// @flow
+
+/* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, import/extensions */
+/* eslint-disable react/jsx-max-props-per-line, react/jsx-indent-props */
+/* eslint-disable react/jsx-first-prop-new-line */
+/* eslint-disable react/prop-types */
+
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+
+import ActionButton from './ActionButton'
+
+storiesOf('ActionButton', module).add('default', () =>
+  <ActionButton items={[{ label: 'abcd' }, { label: 'cdef' }]} />,
+)
