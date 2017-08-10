c444566c2e6b717a509037d270c753c78ca2be49
Roland Schläfli <rolandschlaefli@gmail.com>
2017-08-10 19:14:11 +0200

    Add fixtures for a single session

--- .storybook/fixtures.js
+++ .storybook/fixtures.js
@@ -15,12 +15,25 @@ const questionBlock = {
   timeLimit: 60,
 }
 
+const question2 = {
+  id: question.id,
+  questionDefinition: {
+    title: question.title,
+    type: question.type,
+  }
+}
+
+const questionBlock2 = {
+  ...questionBlock,
+  questions: [question2, question2, question2]
+}
+
 const session = {
-  blocks: [{ id: '0940-4a24-b31a', questions: [], showSolutions: false, timeLimit: 60 }],
+  blocks: [questionBlock2],
   createdAt: '2015-02-08 14:32:11',
   id: '4c821db7-0940-4a24-b31a-7969104bbc9f',
   name: 'Long session',
-  status: 'created',
+  status: 'CREATED',
 }
 
 export default {
