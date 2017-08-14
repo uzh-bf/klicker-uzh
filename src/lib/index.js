efba39fe576cb2345e023e4ba145448b64f61e8c
Roland Schläfli <rolandschlaefli@gmail.com>
2017-08-14 13:18:53 +0200

    Fully remove WithCSS

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,7 +1,6 @@
 export { default as initApollo } from './initApollo'
 export { default as initLogging } from './initLogging'
 export { default as pageWithIntl } from './pageWithIntl'
-export { default as withCSS } from './withCSS'
 export { default as withData } from './withData'
 export { default as initRedux } from './initRedux'
 export * from './utils/css'
