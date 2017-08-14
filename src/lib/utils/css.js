3c095c3e643a39a71ff5fc6aeab3d384c241a20b
Roland Schläfli <rolandschlaefli@gmail.com>
2017-08-14 13:17:48 +0200

    Add css util function for expanding list of links

---
+++ src/lib/utils/css.js
@@ -0,0 +1,22 @@
+// @flow
+
+import React from 'react'
+
+function createLinks(
+  linksFull?: Array<string> = [],
+  linksSemantic?: Array<string> = [],
+): Array<any> {
+  const links = [
+    ...linksFull,
+    ...linksSemantic.map(
+      file =>
+        `https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.11/components/${file}.min.css`,
+    ),
+  ]
+
+  const elements = links.map(link => <link key={link} rel="stylesheet" href={link} />)
+
+  return elements
+}
+
+export { createLinks }
