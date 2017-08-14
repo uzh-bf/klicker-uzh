94a2d87e64cfb901993955f6baaa650e943ae885
Roland Schläfli <rolandschlaefli@gmail.com>
2017-08-14 11:01:24 +0200

    Integrate dotenv (#19)

--- frontend/src/lib/initLogging.js
+++ frontend/src/lib/initLogging.js
@@ -1,23 +1,21 @@
 export default function initLogging() {
   // restrict code execution for trackers to clientside
   if (typeof window !== 'undefined') {
-    const ENV = require('./env').default
-
     // embed logrocket if enabled
-    if (ENV.LOGROCKET !== '__LOGROCKET__') {
+    if (process.env.LOGROCKET !== '__LOGROCKET__') {
       const LogRocket = require('logrocket')
       const LogRocketReact = require('logrocket-react')
-      LogRocket.init(ENV.LOGROCKET)
+      LogRocket.init(process.env.LOGROCKET)
       LogRocketReact(LogRocket)
     }
 
     // embed sentry if enabled
-    if (ENV.SENTRY !== '__SENTRY__') {
+    if (process.env.SENTRY !== '__SENTRY__') {
       const Raven = require('raven-js')
 
-      Raven.config(ENV.SENTRY).install()
+      Raven.config(process.env.SENTRY).install()
 
-      if (ENV.LOGROCKET !== '__LOGROCKET__') {
+      if (process.env.LOGROCKET !== '__LOGROCKET__') {
         Raven.setDataCallback(data =>
           Object.assign({}, data, {
             extra: {
