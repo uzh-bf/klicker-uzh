a8546804cccc79a9890c5ed04e9ed2eef0490bcb
Roland Schläfli <rolandschlaefli@gmail.com>
2017-08-15 11:00:06 +0200

    Modify tests of createLinks util to use snapshots

--- src/lib/utils/css.test.js
+++ src/lib/utils/css.test.js
@@ -1,28 +1,11 @@
-import React from 'react'
-
 import { createLinks } from './css'
 
 describe('createLinks', () => {
   it('works with full urls', () => {
-    expect(createLinks(['http://www.google.com'])).toEqual([
-      <link rel="stylesheet" href="http://www.google.com" />,
-    ])
+    expect(createLinks(['http://www.google.com'])).toMatchSnapshot()
   })
 
   it('works with semantic components', () => {
-    expect(createLinks(['dropdown', 'header', 'menu'])).toEqual([
-      <link
-        rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.11/components/dropdown.min.css"
-      />,
-      <link
-        rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.11/components/header.min.css"
-      />,
-      <link
-        rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.11/components/menu.min.css"
-      />,
-    ])
+    expect(createLinks(['dropdown', 'header', 'menu'])).toMatchSnapshot()
   })
 })
