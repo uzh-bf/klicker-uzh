b29bb00e77f594f3cf33dba51cb84ab85578243d
Roland Schläfli <rolandschlaefli@gmail.com>
2017-08-15 12:51:01 +0200

    Add basic mutation for registration

--- src/queries/mutations.js
+++ src/queries/mutations.js
@@ -1,1 +1,17 @@
-// TODO: mutations here
+// @flow
+
+import { gql } from 'react-apollo'
+
+type RegistrationMutationType = {}
+
+const RegistrationMutation = gql`
+  mutation CreateUser($email: string!, $password: string!) {
+    createUser(authProvider: { email: { $email, $password } }) {
+      email
+    }
+  }
+
+`
+
+export { RegistrationMutation }
+export type { RegistrationMutationType }
