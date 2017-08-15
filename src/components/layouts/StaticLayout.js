b13a9d8f06f831dc0aabfcc8be17f4a2354961dd
Roland Schläfli <rolandschlaefli@gmail.com>
2017-08-15 10:52:53 +0200

    Add a static layout

---
+++ src/components/layouts/StaticLayout.js
@@ -0,0 +1,61 @@
+// @flow
+
+import React, { Component } from 'react'
+import { Helmet } from 'react-helmet'
+
+import { createLinks, initLogging } from '../../lib'
+
+class StaticLayout extends Component {
+  props: {
+    children: any,
+    pageTitle: string,
+  }
+
+  state = {}
+
+  componentWillMount() {
+    // initialize sentry and logrocket (if appropriately configured)
+    initLogging()
+  }
+
+  render() {
+    const { children, pageTitle } = this.props
+
+    return (
+      <div className="staticLayout">
+        <Helmet>
+          {createLinks(['https://fonts.googleapis.com/css?family=Open Sans'], ['reset'])}
+          <title>
+            {pageTitle}
+          </title>
+        </Helmet>
+
+        <div className="content">
+          {children}
+        </div>
+
+        <style jsx global>{`
+          * {
+            font-family: 'Open Sans', sans-serif;
+          }
+          html {
+            font-size: 16px;
+          }
+          body {
+            font-size: 1rem;
+          }
+        `}</style>
+
+        <style jsx>{`
+          .staticLayout {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+        `}</style>
+      </div>
+    )
+  }
+}
+
+export default StaticLayout
