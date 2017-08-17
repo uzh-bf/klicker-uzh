c248a6eb8cdd3e9ee2ef79afb6fe95661ff23a39
Roland Schläfli <rolandschlaefli@gmail.com>
2017-08-17 12:54:36 +0200

    Run flow-typed install

---
+++ flow-typed/npm/isomorphic-fetch_v2.x.x.js
@@ -0,0 +1,7 @@
+// flow-typed signature: 10750ebd5135c6c432ef2f5279b2379d
+// flow-typed version: ec28077c25/isomorphic-fetch_v2.x.x/flow_>=v0.25.x
+
+
+declare module 'isomorphic-fetch' {
+    declare module.exports: (input: string | Request | URL, init?: RequestOptions) => Promise<Response>;
+}
