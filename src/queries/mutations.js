ff99a7b164914f1bb22c47917b5fbb2cf77f4020
Roland Schläfli <rolandschlaefli@gmail.com>
2017-08-29 12:47:51 +0200

    Update login and registration for the new API

--- src/queries/mutations.js
+++ src/queries/mutations.js
@@ -9,10 +9,7 @@ type RegistrationMutationType = {
 }
 const RegistrationMutation = gql`
   mutation CreateUser($email: String!, $password: String!, $shortname: String!) {
-    createUser(
-      authProvider: { email: { email: $email, password: $password } }
-      shortname: $shortname
-    ) {
+    createUser(user: { email: $email, password: $password, shortname: $shortname }) {
       id
       email
       shortname
@@ -21,7 +18,6 @@ const RegistrationMutation = gql`
 `
 
 type LoginMutationType = {
-  token: string,
   user: {
     id: string,
     email: string,
@@ -29,14 +25,11 @@ type LoginMutationType = {
   },
 }
 const LoginMutation = gql`
-  mutation LoginUser($email: String!, $password: String!) {
-    signinUser(email: { email: $email, password: $password }) {
-      token
-      user {
-        id
-        email
-        shortname
-      }
+  mutation Login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      id
+      email
+      shortname
     }
   }
 `
