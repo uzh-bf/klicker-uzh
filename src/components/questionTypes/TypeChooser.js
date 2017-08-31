485c5ee8859130e1e5d652df894b366b70db488c
Roland Schläfli <rolandschlaefli@gmail.com>
2017-08-31 20:09:34 +0200

    Create a simple type chooser component

---
+++ src/components/questionTypes/TypeChooser.js
@@ -0,0 +1,52 @@
+// @flow
+
+import React from 'react'
+import classNames from 'classnames'
+
+type Props = {
+  activeType: string,
+  types: Array<{
+    name: string,
+    value: string,
+  }>,
+  handleChange: string => () => void,
+}
+
+const TypeChooser = ({ activeType, types, handleChange }: Props) => (
+  <div className="typeChooser">
+    {types.map(({ name, value }) => (
+      <button
+        key={value}
+        className={classNames('type', { active: value === activeType })}
+        onClick={handleChange(value)}
+      >
+        {name}
+      </button>
+    ))}
+
+    <style jsx>{`
+      button {
+        background-color: white;
+        border: 1px solid lightgrey;
+        cursor: pointer;
+        padding: 1rem;
+        outline: none;
+      }
+
+      button.active {
+        border-color: orange;
+      }
+
+      button:not(:last-child) {
+        margin-bottom: 0.5rem;
+      }
+
+      .typeChooser {
+        display: flex;
+        flex-direction: column;
+      }
+    `}</style>
+  </div>
+)
+
+export default TypeChooser
