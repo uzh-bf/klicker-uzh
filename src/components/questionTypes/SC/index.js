96ee379ace86c2117f79cd7dd0af6374361499fb
Roland Schläfli <rolandschlaefli@gmail.com>
2017-09-07 17:55:23 +0200

    Start setup for a draggable list of options

--- src/components/questionTypes/SC/index.js
+++ src/components/questionTypes/SC/index.js
@@ -1,4 +1,4 @@
 export { default as SCAnswerOptions } from './answer/Options'
-export { default as SCCreationOptions } from './creation/Options'
+export { default as SCCreationOptions } from './creation/OptionsDraggable'
 export { default as SCCreationOption } from './creation/Option'
 export { default as SCCreationPlaceholder } from './creation/Placeholder'
