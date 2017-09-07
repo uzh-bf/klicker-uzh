879d561b8731326a0d53959e1c0ea1c3005ef8f9
Roland Schläfli <rolandschlaefli@gmail.com>
2017-09-07 17:19:56 +0200

    Implement a basic responsive form for question creation, refetch data and redirections

--- src/pages/questions/create.js
+++ src/pages/questions/create.js
@@ -1,14 +1,19 @@
 // @flow
+// semantic: button, form, input
 
 import React, { Component } from 'react'
 import TagsInput from 'react-tagsinput'
+import Router from 'next/router'
 import { graphql } from 'react-apollo'
+import { FormattedMessage } from 'react-intl'
+import { Helmet } from 'react-helmet'
 
 import TeacherLayout from '../../components/layouts/TeacherLayout'
 import TypeChooser from '../../components/questionTypes/TypeChooser'
-import { pageWithIntl, withData } from '../../lib'
+import { createLinks, pageWithIntl, withData } from '../../lib'
 import { SCCreationOptions } from '../../components/questionTypes/SC'
 import { CreateQuestionMutation } from '../../queries/mutations'
+import { QuestionListQuery, TagListQuery } from '../../queries/queries'
 
 import stylesTagsInput from './styles-tagsinput'
 
@@ -66,14 +71,16 @@ class CreateQuestion extends Component {
     this.setState({ activeType: newType })
   }
 
-  handleSave = () => {
+  handleSave = (e) => {
+    e.preventDefault()
+
     this.props.createQuestion({
       description: 'hello world',
       options: this.state.options,
       tags: this.state.selectedTags,
       title: this.state.title,
       type: this.state.activeType,
-    })
+    }).then(() => Router.push('/questions'))
   }
 
   render() {
@@ -87,6 +94,30 @@ class CreateQuestion extends Component {
       }),
     }
 
+    const types = [
+      {
+        name: intl.formatMessage({
+          defaultMessage: 'Single-Choice',
+          id: 'common.questionTypes.sc',
+        }),
+        value: 'SC',
+      },
+      {
+        name: intl.formatMessage({
+          defaultMessage: 'Multiple-Choice',
+          id: 'common.questionTypes.mc',
+        }),
+        value: 'MC',
+      },
+      {
+        name: intl.formatMessage({
+          defaultMessage: 'Free-Form',
+          id: 'common.questionTypes.free',
+        }),
+        value: 'FREE',
+      },
+    ]
+
     return (
       <TeacherLayout
         intl={intl}
@@ -97,43 +128,67 @@ class CreateQuestion extends Component {
         })}
         sidebar={{ activeItem: 'createQuestion' }}
       >
-        <div className="grid">
-          <div className="questionName">
-            <h2>Question name</h2>
-            <input type="text" value={this.state.title} onChange={this.handleTitleChange} />
+        <Helmet defer={false}>{createLinks(['button', 'form'])}</Helmet>
+
+        <form className="ui form" onSubmit={this.handleSave}>
+          <div className="field questionName">
+            <label htmlFor="questionName">
+              <FormattedMessage
+                defaultMessage="Question name"
+                id="teacher.createQuestion.questionName"
+              />
+            </label>
+            <input
+              name="questionName"
+              type="text"
+              value={this.state.title}
+              onChange={this.handleTitleChange}
+            />
           </div>
 
-          <div className="questionType">
-            <h2>Question type</h2>
+          <div className="field questionType">
+            <label htmlFor="types">
+              <FormattedMessage
+                defaultMessage="Question type"
+                id="teacher.createQuestion.questionType"
+              />
+            </label>
             <TypeChooser
+              name="types"
               activeType={this.state.activeType}
-              types={[
-                { name: 'Single Choice', value: 'SC' },
-                { name: 'Multiple Choice', value: 'MC' },
-                { name: 'Free-Form', value: 'FREE' },
-              ]}
+              types={types}
               handleChange={this.handleTypeChange}
             />
           </div>
 
-          <div className="questionTags">
-            <h2>Tags</h2>
-            <TagsInput value={this.state.selectedTags} onChange={this.handleTagChange} />
+          <div className="field questionTags">
+            <label htmlFor="tags">
+              <FormattedMessage defaultMessage="Tags" id="teacher.createQuestion.tags" />
+            </label>
+            <TagsInput
+              name="tags"
+              value={this.state.selectedTags}
+              onChange={this.handleTagChange}
+            />
           </div>
 
-          <div className="preview">
-            <h2>Preview</h2>
-            <div className="previewContent">abcd</div>
+          <div className="field content">
+            <label htmlFor="content">
+              <FormattedMessage defaultMessage="Content" id="teacher.createQuestion.content" />
+            </label>
+            <textarea
+              name="content"
+              value={this.state.content}
+              onChange={this.handleContentChange}
+            />
           </div>
 
-          <div className="content">
-            <h2>Content</h2>
-            <textarea value={this.state.content} onChange={this.handleContentChange}/>
-          </div>
-
-          <div className="answerOptions">
-            <h2>Options</h2>
+          <div className="field answerOptions">
+            <label htmlFor="options">
+              <FormattedMessage defaultMessage="Options" id="teacher.createQuestion.options" />
+            </label>
             <SCCreationOptions
+              name="options"
               options={this.state.options}
               handleDeleteOption={this.handleDeleteOption}
               handleNewOption={this.handleNewOption}
@@ -141,106 +196,126 @@ class CreateQuestion extends Component {
             />
           </div>
 
-          <button className="discard">Discard</button>
-          <button className="save" onClick={this.handleSave}>
-            Save
+          <div className="preview">this is a preview</div>
+
+          <button
+            className="ui button discard"
+            type="reset"
+            onClick={() => Router.push('/questions')}
+          >
+            <FormattedMessage defaultMessage="Discard" id="teacher.createQuestion.button.discard" />
           </button>
-        </div>
+          <button className="ui primary button save" type="submit">
+            <FormattedMessage defaultMessage="Save" id="common.button.save" />
+          </button>
+        </form>
+
         <style global jsx>
           {stylesTagsInput}
         </style>
         <style jsx>{`
+          form {
+            display: flex;
+            flex-direction: column;
+
+            padding: 1rem;
+          }
+
+          .field > label {
+            font-size: 1.2rem;
+          }
+
           @supports (grid-gap: 1rem) {
-            .grid {
-              display: grid;
-              padding: 1rem 0;
-              margin: 0 20%;
+            @media all and (min-width: 768px) {
+              form {
+                display: grid;
 
-              grid-gap: 1rem;
-              grid-template-columns: repeat(6, 1fr);
-              grid-template-rows: auto;
-              grid-template-areas: 'name name name name preview preview'
-                'type type tags tags preview preview'
-                'content content content content content content'
-                'options options options options options options';
+                grid-gap: 1rem;
+                grid-template-columns: repeat(6, 1fr);
+                grid-template-rows: auto;
+                grid-template-areas: 'name name name name preview preview'
+                  'type type tags tags preview preview'
+                  'content content content content content content'
+                  'options options options options options options';
+              }
+
+              form > .field {
+                margin-bottom: 0;
+              }
+
+              input {
+                width: 100%;
+              }
+
+              .option {
+                border: 1px solid lightgrey;
+                padding: 1rem;
+              }
+
+              .option:hover {
+                border: 1px solid blue;
+                cursor: pointer;
+              }
+
+              .option:not(:last-child) {
+                margin-bottom: 0.5rem;
+              }
+
+              .questionName {
+                grid-area: name;
+              }
+
+              .questionType {
+                grid-area: type;
+              }
+
+              .questionType > .option {
+              }
+
+              .questionTags {
+                grid-area: tags;
+              }
+
+              .preview {
+                grid-area: preview;
+
+                border: 1px solid lightgrey;
+                padding: 1rem;
+              }
+
+              .content {
+                grid-area: content;
+              }
+
+              .content > div {
+                border: 1px solid lightgrey;
+                height: 20rem;
+                padding: 1rem;
+              }
+
+              .answerOptions {
+                grid-area: options;
+              }
+
+              .answerOptions > .option {
+              }
+
+              .answerOptions > .option.placeholder {
+                background-color: lightgrey;
+                border-color: grey;
+                text-align: center;
+              }
+
+              .save {
+                align-self: center;
+              }
             }
 
-            h2 {
-              grid-row: 1/2;
-              grid-column: 1/4;
-
-              margin: 0;
-              margin-bottom: 0.5rem;
-            }
-
-            input {
-              width: 100%;
-            }
-
-            .option {
-              border: 1px solid lightgrey;
-              padding: 1rem;
-            }
-
-            .option:hover {
-              border: 1px solid blue;
-              cursor: pointer;
-            }
-
-            .option:not(:last-child) {
-              margin-bottom: 0.5rem;
-            }
-
-            .questionName {
-              grid-area: name;
-            }
-
-            .questionType {
-              grid-area: type;
-            }
-
-            .questionType > .option {
-            }
-
-            .questionTags {
-              grid-area: tags;
-            }
-
-            .preview {
-              grid-area: preview;
-            }
-
-            .previewContent {
-              border: 1px solid lightgrey;
-              padding: 1rem;
-              height: 100%;
-            }
-
-            .content {
-              grid-area: content;
-            }
-
-            .content > div {
-              border: 1px solid lightgrey;
-              height: 20rem;
-              padding: 1rem;
-            }
-
-            .answerOptions {
-              grid-area: options;
-            }
-
-            .answerOptions > .option {
-            }
-
-            .answerOptions > .option.placeholder {
-              background-color: lightgrey;
-              border-color: grey;
-              text-align: center;
-            }
-
-            .save {
-              align-self: center;
+            @media all and (min-width: 991px) {
+              form {
+                margin: 0 20%;
+                padding: 1rem 0;
+              }
             }
           }
         `}</style>
@@ -254,7 +329,10 @@ export default withData(
     graphql(CreateQuestionMutation, {
       props: ({ mutate }) => ({
         createQuestion: ({ description, options, tags, title, type }) =>
-          mutate({ variables: { description, options, tags, title, type } }),
+          mutate({
+            refetchQueries: [{ query: QuestionListQuery }, { query: TagListQuery }],
+            variables: { description, options, tags, title, type },
+          }),
       }),
     })(CreateQuestion),
   ),
