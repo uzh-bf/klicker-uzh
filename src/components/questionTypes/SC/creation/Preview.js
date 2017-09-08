ff938395fc8a5b4c54128e4f7e91bc4c8122f7e4
Roland Schläfli <rolandschlaefli@gmail.com>
2017-09-08 14:08:44 +0200

    Implement simple preview component for single choice questions

---
+++ src/components/questionTypes/SC/creation/Preview.js
@@ -0,0 +1,60 @@
+// @flow
+
+import React from 'react'
+import { FormattedMessage } from 'react-intl'
+
+import AnswerOptions from '../answer/Options'
+
+type Props = {
+  description: string,
+  options: Array<{
+    label: string,
+  }>,
+  title: string,
+}
+
+const defaultProps = {
+  description: 'DESCRIPTION',
+  title: 'TITLE',
+}
+
+const Preview = ({ title, description, options }: Props) => (
+  <div className="preview">
+    <div className="title">{title || 'TITLE'}</div>
+    <div className="description">{description || 'DESCRIPTION'}</div>
+    <div className="options">
+      <AnswerOptions activeOption={-1} options={(options.length === 0 && [{ label: 'OPTION' }]) || options} />
+    </div>
+    <div className="button"><FormattedMessage defaultMessage="Submit" id="common.button.submit" /></div>
+
+    <style jsx>{`
+        .preview {
+          display: flex;
+          flex-direction: column;
+
+          border: 1px solid lightgrey;
+          height: 100%;
+          padding: 1rem;
+        }
+
+        .title, .description, .options {
+          margin-bottom: 1rem;
+        }
+
+        .title {
+          font-weight: bold;
+        }
+
+        .button {
+          align-self: flex-end;
+
+          border: 1px solid lightgrey;
+          padding: .5rem;
+        }
+      `}</style>
+  </div>
+)
+
+Preview.defaultProps = defaultProps
+
+export default Preview
