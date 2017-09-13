40cc82a35bfd72cae317af0bad2f2cd7760084a4
Roland Schläfli <rolandschlaefli@gmail.com>
2017-09-13 13:11:57 +0200

    Add validation to the question creation form, improve styling

--- src/components/forms/QuestionCreationForm.js
+++ src/components/forms/QuestionCreationForm.js
@@ -1,141 +1,185 @@
+// @flow
+
 import React from 'react'
-import Router from 'next/router'
+import _get from 'lodash/get'
+import isEmpty from 'validator/lib/isEmpty'
 import { connect } from 'react-redux'
 import { Field, reduxForm } from 'redux-form'
 import { FormattedMessage } from 'react-intl'
-import _get from 'lodash/get'
 
 import TitleInput from '../questions/creation/TitleInput'
 import TagInput from '../questions/creation/TagInput'
 import TypeChooser from '../questionTypes/TypeChooser'
+import stylesTagsInput from './styles-tagsinput'
 import {
   SCCreationOptions,
   SCCreationPreview,
   SCCreationContent,
 } from '../../components/questionTypes/SC'
-import stylesTagsInput from './styles-tagsinput'
 
-const validate = () => null
+import type { OptionType, TagType } from '../../types'
+
+// form validation
+const validate = ({ content, options, tags, title }) => {
+  const errors = {}
+
+  if (!title || isEmpty(title)) {
+    errors.title = 'form.createQuestion.title.empty'
+  }
+
+  if (!content || isEmpty(content)) {
+    errors.content = 'form.createQuestion.content.empty'
+  }
+
+  if (!options || options.length === 0) {
+    errors.options = 'form.createQuestion.options.empty'
+  }
+
+  if (!tags || tags.length === 0) {
+    errors.tags = 'form.createQuestion.tags.empty'
+  }
+
+  return errors
+}
+
+type Props = {
+  content: string,
+  intl: $IntlShape,
+  invalid: boolean,
+  options: Array<OptionType>,
+  tags: Array<TagType>,
+  title: string,
+  handleSubmit: () => void,
+  onDiscard: () => void,
+}
 
 const defaultProps = {
+  content: '',
   options: [],
+  tags: [],
+  title: '',
 }
 
-const QuestionCreationForm = ({ content, intl, options, tags, title, handleSubmit }) => {
-  return (
-    <form className="ui form" onSubmit={handleSubmit}>
-      <div className="questionInput questionTitle">
-        <Field name="title" component={TitleInput} />
-      </div>
+const QuestionCreationForm = ({
+  intl,
+  invalid,
+  content,
+  options,
+  tags,
+  title,
+  handleSubmit: onSubmit,
+  onDiscard,
+}: Props) => (
+  <form className="ui form" onSubmit={onSubmit}>
+    <div className="questionInput questionTitle">
+      <Field name="title" component={TitleInput} />
+    </div>
 
-      <div className="questionInput questionType">
-        <Field name="type" component={TypeChooser} intl={intl} />
-      </div>
+    <div className="questionInput questionType">
+      <Field name="type" component={TypeChooser} intl={intl} />
+    </div>
 
-      <div className="questionInput questionTags">
-        <Field name="tags" component={TagInput} tags={tags} />
-      </div>
+    <div className="questionInput questionTags">
+      <Field name="tags" component={TagInput} tags={tags} />
+    </div>
 
-      <div className="questionInput questionContent">
-        <Field name="content" component={SCCreationContent} />
-      </div>
+    <div className="questionInput questionContent">
+      <Field name="content" component={SCCreationContent} />
+    </div>
 
-      <div className="questionInput questionOptions">
-        <Field name="options" component={SCCreationOptions} />
-      </div>
+    <div className="questionInput questionOptions">
+      <Field name="options" component={SCCreationOptions} />
+    </div>
 
-      <div className="questionPreview">
-        <SCCreationPreview
-          title={title}
-          description={content}
-          options={options.map(option => ({ label: option.name }))}
-        />
-      </div>
+    <div className="questionPreview">
+      <SCCreationPreview title={title} description={content} options={options} />
+    </div>
 
-      <button className="ui button discard" type="reset" onClick={() => Router.push('/questions')}>
-        <FormattedMessage defaultMessage="Discard" id="teacher.createQuestion.button.discard" />
-      </button>
-      <button className="ui primary button save" type="submit">
-        <FormattedMessage defaultMessage="Save" id="common.button.save" />
-      </button>
+    <button className="ui button discard" type="reset" onClick={onDiscard}>
+      <FormattedMessage defaultMessage="Discard" id="teacher.createQuestion.button.discard" />
+    </button>
+    <button className="ui primary button save" disabled={invalid} type="submit">
+      <FormattedMessage defaultMessage="Save" id="common.button.save" />
+    </button>
 
-      <style global jsx>
-        {stylesTagsInput}
-      </style>
-      <style jsx>{`
-        form {
-          display: flex;
-          flex-direction: column;
+    <style global jsx>
+      {stylesTagsInput}
+    </style>
+    <style jsx>{`
+      form {
+        display: flex;
+        flex-direction: column;
 
-          padding: 1rem;
-        }
+        padding: 1rem;
+      }
 
-        .questionInput {
-          margin-bottom: 1rem;
-        }
+      .questionInput,
+      .questionPreview {
+        margin-bottom: 1rem;
+      }
 
-        .questionInput > :global(.field > label) {
-          font-size: 1.2rem;
-        }
+      .questionInput > :global(.field > label) {
+        font-size: 1.2rem;
+      }
 
-        @supports (grid-gap: 1rem) {
-          @media all and (min-width: 768px) {
-            form {
-              display: grid;
+      @supports (grid-gap: 1rem) {
+        @media all and (min-width: 768px) {
+          form {
+            display: grid;
 
-              grid-gap: 1rem;
-              grid-template-columns: repeat(6, 1fr);
-              grid-template-rows: auto;
-              grid-template-areas: 'title title title title preview preview'
-                'type type tags tags preview preview'
-                'content content content content content content'
-                'options options options options options options';
-            }
-
-            .questionInput {
-              margin-bottom: 0;
-            }
-
-            .questionTitle {
-              grid-area: title;
-            }
-
-            .questionType {
-              grid-area: type;
-            }
-
-            .questionTags {
-              grid-area: tags;
-            }
-
-            .questionPreview {
-              grid-area: preview;
-            }
-
-            .questionContent {
-              grid-area: content;
-            }
-
-            .questionOptions {
-              grid-area: options;
-            }
-
-            .save {
-              align-self: center;
-            }
+            grid-gap: 1rem;
+            grid-template-columns: repeat(6, 1fr);
+            grid-template-rows: auto;
+            grid-template-areas: 'title title title title preview preview'
+              'type type tags tags preview preview'
+              'content content content content content content'
+              'options options options options options options';
           }
 
-          @media all and (min-width: 991px) {
-            form {
-              margin: 0 20%;
-              padding: 1rem 0;
-            }
+          .questionInput {
+            margin-bottom: 0;
+          }
+
+          .questionTitle {
+            grid-area: title;
+          }
+
+          .questionType {
+            grid-area: type;
+          }
+
+          .questionTags {
+            grid-area: tags;
+          }
+
+          .questionPreview {
+            grid-area: preview;
+            margin-bottom: 0;
+          }
+
+          .questionContent {
+            grid-area: content;
+          }
+
+          .questionOptions {
+            grid-area: options;
+          }
+
+          .save {
+            align-self: center;
           }
         }
-      `}</style>
-    </form>
-  )
-}
+
+        @media all and (min-width: 991px) {
+          form {
+            margin: 0 20%;
+            padding: 1rem 0;
+          }
+        }
+      }
+    `}</style>
+  </form>
+)
 
 QuestionCreationForm.defaultProps = defaultProps
 
