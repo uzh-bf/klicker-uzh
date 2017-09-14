5089e35b0dc1afc6f271372e4d49f03bc048e264
Pascal Zehnder <pascal.zehnder@bf.uzh.ch>
2017-09-14 11:07:30 +0200

    Add initial Buttons for chosing option

--- src/components/questionTypes/FREE/creation/options/Options.js
+++ src/components/questionTypes/FREE/creation/options/Options.js
@@ -16,6 +16,42 @@ class Options extends React.Component {
         <label htmlFor="options">
           <FormattedMessage defaultMessage="Options" id="teacher.createQuestion.options" />
         </label>
+        <button
+          className={'noLimitations'}
+          type="button"
+        >
+          <FormattedMessage
+            defaultMessage="No Limitations"
+            id="teacher.createQuestion.options.noLimitations"
+          />
+        </button>
+        <button
+          className={'noLimitations'}
+          type="button"
+        >
+          <FormattedMessage
+            defaultMessage="Number Range"
+            id="teacher.createQuestion.options.numberRange"
+          />
+        </button>
+
+        <style jsx>{`
+          button {
+            background-color: white;
+            border: 1px solid lightgrey;
+            cursor: pointer;
+            padding: 1rem;
+            outline: none;
+          }
+
+          button.active {
+            border-color: orange;
+          }
+
+          button:not(:last-child) {
+            margin-bottom: 0.5rem;
+          }
+        `}</style>
       </div>
     )
   }
