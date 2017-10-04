f36e9ba57b5aef60701c2e341c2f36342c4389e5
Roland Schläfli <rolandschlaefli@gmail.com>
2017-10-04 17:21:23 +0200

    Optimize restrictedToNumbers calculatio

--- src/components/questionTypes/Free/creation/Preview.js
+++ src/components/questionTypes/Free/creation/Preview.js
@@ -23,13 +23,17 @@ const defaultProps = {
   title: 'TITLE',
 }
 
-const Preview = ({ title, options, description }: Props) => (
-  <div className="preview">
-    <div className="title">{title || 'TITLE'}</div>
-    <div className="description">{description || 'DESCRIPTION'}</div>
-    {options.restrictions.type === 'NUMBERS' &&
-      options.restrictions.min &&
-      options.restrictions.max && (
+const Preview = ({ title, options, description }: Props) => {
+  const restrictedToNumbers =
+    options.restrictions.type === 'NUMBERS' &&
+    options.restrictions.min !== null &&
+    options.restrictions.max !== null
+
+  return (
+    <div className="preview">
+      <div className="title">{title || 'TITLE'}</div>
+      <div className="description">{description || 'DESCRIPTION'}</div>
+      {restrictedToNumbers && (
         <div className="diagram">
           <div className="min">Min: {options.restrictions.min}</div>
           <div className="max">Max: {options.restrictions.max}</div>
@@ -37,10 +41,8 @@ const Preview = ({ title, options, description }: Props) => (
           <div className="box" />
         </div>
       )}
-    {options.restrictions.type === 'NUMBERS' &&
-    options.restrictions.min &&
-    options.restrictions.max ? (
-      <div className="selection">
+      {restrictedToNumbers ? (
+        <div className="selection">
           {/* TODO how to align title horizontally centered? */}
           <b className="title">Selection:</b>
           <div className="box">{(+options.restrictions.min + +options.restrictions.max) / 2}</div>
@@ -50,82 +52,83 @@ const Preview = ({ title, options, description }: Props) => (
           <div className="box" />
         </div>
       )}
-    <div className="button">
-      <FormattedMessage defaultMessage="Submit" id="common.button.submit" />
+      <div className="button">
+        <FormattedMessage defaultMessage="Submit" id="common.button.submit" />
+      </div>
+
+      <style jsx>{`
+        .preview {
+          display: flex;
+          flex-direction: column;
+
+          border: 1px solid lightgrey;
+          height: 100%;
+          padding: 1rem;
+        }
+
+        .title,
+        .description,
+        .selection {
+          margin-bottom: 1rem;
+        }
+
+        .title {
+          font-weight: bold;
+        }
+
+        .button {
+          align-self: flex-end;
+
+          border: 2px solid lightgrey;
+          padding: 0.5rem;
+        }
+
+        .diagram > .max {
+          float: right;
+        }
+
+        .diagram > .line {
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 0.1rem;
+          background: #000;
+        }
+
+        .diagram > .box {
+          position: relative;
+          top: -1rem;
+          left: 50%;
+          width: 1rem;
+          height: 2rem;
+          background: grey;
+        }
+
+        .selection {
+          display: flex;
+          align-self: flex-end;
+        }
+
+        .selection > .box {
+          border: 1px solid lightgrey;
+          padding: 0.5rem 2rem;
+          margin-left: 1rem;
+        }
+
+        .freeText {
+          margin-bottom: 1rem;
+        }
+
+        .freeText > .box {
+          border: 1px solid lightgrey;
+          padding: 0.5rem 2rem;
+          height: 2rem;
+          width: 100%;
+        }
+      `}</style>
     </div>
-
-    <style jsx>{`
-      .preview {
-        display: flex;
-        flex-direction: column;
-
-        border: 1px solid lightgrey;
-        height: 100%;
-        padding: 1rem;
-      }
-
-      .title,
-      .description,
-      .selection {
-        margin-bottom: 1rem;
-      }
-
-      .title {
-        font-weight: bold;
-      }
-
-      .button {
-        align-self: flex-end;
-
-        border: 2px solid lightgrey;
-        padding: 0.5rem;
-      }
-
-      .diagram > .max {
-        float: right;
-      }
-
-      .diagram > .line {
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 0.1rem;
-        background: #000;
-      }
-
-      .diagram > .box {
-        position: relative;
-        top: -1rem;
-        left: 50%;
-        width: 1rem;
-        height: 2rem;
-        background: grey;
-      }
-
-      .selection {
-        display: flex;
-        align-self: flex-end;
-      }
-
-      .selection > .box {
-        border: 1px solid lightgrey;
-        padding: 0.5rem 2rem;
-        margin-left: 1rem;
-      }
-
-      .freeText {
-        margin-bottom: 1rem;
-      }
-
-      .freeText > .box {
-        border: 1px solid lightgrey;
-        padding: 0.5rem 2rem;
-        height: 2rem;
-        width: 100%;
-      }
-    `}</style>
-  </div>
-)
+  )
+}
 
 Preview.defaultProps = defaultProps
 
