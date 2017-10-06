477ff010b91c379cda1e81b77628f07470b42827
Roland Schläfli <rolandschlaefli@gmail.com>
2017-10-06 16:14:45 +0200

    Add stories for form components

--- src/components/common/common.stories.js
+++ src/components/common/common.stories.js
@@ -4,10 +4,24 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 
+import { intlMock } from '../../../.storybook/utils'
+
 import ActionButton from './ActionButton'
 import ListWithHeader from './ListWithHeader'
-
 import Collapser from './Collapser'
+
+import Navbar from './navbar/Navbar'
+import AccountArea from './navbar/AccountArea'
+import SearchArea from './navbar/SearchArea'
+import SessionArea from './navbar/SessionArea'
+
+import Sidebar from './sidebar/Sidebar'
+import SidebarItem from './sidebar/SidebarItem'
+
+const sidebarItems = [
+  { href: 'i1', label: 'Item 1', name: 'item1' },
+  { href: 'i2', label: 'Item 2', name: 'item2' },
+]
 
 // create a stateful wrapper for the component
 class CollapserWrapper extends React.Component {
@@ -28,7 +42,7 @@ class CollapserWrapper extends React.Component {
 }
 
 // specify some example content
-const content = (
+const collapserContent = (
   <div>
     <p>
       hello this is a very short question that is getting longer and longer as we speak. it is in
@@ -56,7 +70,53 @@ storiesOf('common', module)
   .add('ListWithHeader', () => (
     <ListWithHeader items={['abcd', 'cdef']}>hello world</ListWithHeader>
   ))
-  .add('Collapser', () => <CollapserWrapper>{content}</CollapserWrapper>)
+  .add('Collapser', () => <CollapserWrapper>{collapserContent}</CollapserWrapper>)
   .add('Collapser (extended)', () => (
-    <Collapser handleCollapseToggle={() => action('collapser-clicked')}>{content}</Collapser>
+    <Collapser handleCollapseToggle={() => action('collapser-clicked')}>
+      {collapserContent}
+    </Collapser>
+  ))
+
+storiesOf('common/navbar', module)
+  .add('Navbar', () => (
+    <Navbar
+      accountShort="AW"
+      intl={intlMock}
+      search={{
+        handleSearch: query => action(`search ${query}`),
+        handleSort: () => action('sort'),
+      }}
+      title="Example page"
+    />
+  ))
+  .add('AccountArea', () => <AccountArea accountShort="AW" />)
+  .add('SearchArea', () => (
+    <SearchArea intl={intlMock} handleSearch={query => action(`search ${query}`)} />
+  ))
+  .add('SessionArea', () => <SessionArea sessionId="AW" />)
+
+storiesOf('common/sidebar', module)
+  .add('Sidebar (visible)', () => (
+    <Sidebar
+      visible
+      activeItem="item1"
+      items={sidebarItems}
+      handleSidebarItemClick={item => action(`click ${item}`)}
+    >
+      PAGE CONTENT
+    </Sidebar>
+  ))
+  .add('Sidebar (invisible)', () => (
+    <Sidebar
+      activeItem="item1"
+      items={sidebarItems}
+      handleSidebarItemClick={item => action(`click ${item}`)}
+    >
+      PAGE CONTENT
+    </Sidebar>
+  ))
+  .add('SidebarItem', () => (
+    <SidebarItem href="bla" name="hello" handleSidebarItemClick={() => action('click')}>
+      ITEM CONTENT
+    </SidebarItem>
   ))
--- src/components/forms/Forms.stories.js
+++ src/components/forms/Forms.stories.js
@@ -3,17 +3,24 @@
 import React from 'react'
 import { Provider as ReduxProvider } from 'react-redux'
 import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
 import { createStore, combineReducers } from 'redux'
 import { reducer as formReducer } from 'redux-form'
 import { DragDropContextProvider } from 'react-dnd'
 import HTML5Backend from 'react-dnd-html5-backend'
+
+import { intlMock } from '../../../.storybook/utils'
 
 import LoginForm from './LoginForm'
 import PasswordResetForm from './PasswordResetForm'
 import RegistrationForm from './RegistrationForm'
 import QuestionCreationForm from './QuestionCreationForm'
 import SessionCreationForm from './SessionCreationForm'
-import { intlMock } from '../../../.storybook/utils'
+
+import FormWithLinks from './components/FormWithLinks'
+import QuestionDropzone from './components/QuestionDropzone'
+import SemanticInput from './components/SemanticInput'
+import SessionTimeline from './components/SessionTimeline'
 
 const rootReducer = combineReducers({
   form: formReducer,
@@ -33,4 +40,52 @@ storiesOf('forms', module)
   .add('QuestionCreationForm [NoTest]', () => <QuestionCreationForm intl={intlMock} />)
   .add('SessionCreationForm', () => (
     <SessionCreationForm intl={intlMock} handleSubmit={() => null} />
+  ))
+
+storiesOf('forms - components', module)
+  .addDecorator(getStory => <ReduxProvider store={store}>{getStory()}</ReduxProvider>)
+  .addDecorator(getStory => (
+    <DragDropContextProvider backend={HTML5Backend}>{getStory()}</DragDropContextProvider>
+  ))
+  .add('FormWithLinks', () => (
+    <FormWithLinks
+      button={{
+        invalid: false,
+        label: 'button',
+        onSubmit: (e) => {
+          e.preventDefault()
+          action('submit')
+        },
+      }}
+      links={[
+        { href: 'href1', label: 'link1' },
+        { href: 'href2', label: 'link2' },
+        { href: 'href3', label: 'link3' },
+      ]}
+    >
+      form fields
+    </FormWithLinks>
+  ))
+  .add('QuestionDropzone', () => (
+    <div style={{ height: 100, width: 100 }}>
+      <QuestionDropzone />
+    </div>
+  ))
+  .add('SemanticInput', () => <SemanticInput label="label" meta={{}} input={{}} />)
+  .add('SemanticInput (with error)', () => (
+    <SemanticInput
+      label="label"
+      meta={{ error: 'fail', invalid: true, touched: true }}
+      input={{}}
+    />
+  ))
+  .add('SessionTimeline', () => (
+    <SessionTimeline
+      input={{
+        value: [
+          { id: 'id1', title: 'question1', type: 'SC' },
+          { id: 'id2', title: 'question2', type: 'FREE' },
+        ],
+      }}
+    />
   ))
