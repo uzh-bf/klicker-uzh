bd8d6d4bd2f0ff64974e853e126d4318c5784289
Roland Schläfli <rolandschlaefli@gmail.com>
2017-10-13 08:58:46 +0200

    (fix) Remove votes from a response

--- src/models/Response.js
+++ src/models/Response.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose')
 const Response = new mongoose.Schema({
   key: { type: Number, min: 0, required: true },
   value: { type: Object, required: true },
-  votes: { type: Number, min: 0, default: 0 },
 
   createdAt: { type: Date, default: Date.now() },
 })
