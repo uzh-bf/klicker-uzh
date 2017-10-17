74e9f1150b01a6483ba76d06d9b8cec4a611203f
Alex Scheitlin <alex.scheitlin@uzh.ch>
2017-10-17 15:45:02 +0200

    question length cut and last used computation and button handling for mobile

--- src/components/questions/QuestionDetails.js
+++ src/components/questions/QuestionDetails.js
@@ -17,101 +17,103 @@ const defaultProps = {
   lastUsed: [],
 }
 
-const QuestionDetails = ({ description, lastUsed }) => (
-  <div className="questionDetails">
-    <div className="column description">
-      <i>{description}</i>
-    </div>
-    <div className="column">
-      <p>
-        <strong>Antworten Total:</strong> 999
-      </p>
-      <p>
-        <strong>Korrekte Antworten:</strong> 88%
-      </p>
-    </div>
-    <div className="column">Test3</div>
+const QuestionDetails = ({ description, lastUsed }) => {
+  const maxDescLength = 30
+  let newDescription = ''
+  let newLastUsed = []
 
-    <div className="column">
-      {lastUsed.length > 0 && (
-        <ListWithHeader items={lastUsed}>
+  if (description.length > maxDescLength) {
+    newDescription = `${description.substring(0, maxDescLength - 4)} ...`
+  } else {
+    newDescription = description
+  }
+
+  if (lastUsed.length > 0) {
+    newLastUsed = lastUsed
+  } else {
+    newLastUsed = ['never used']
+  }
+
+  return (
+    <div className="questionDetails">
+      <div className="column description">{newDescription}</div>
+      <div className="column">
+        <p>
+          Antworten Total: <strong>999</strong>
+        </p>
+        <p>
+          Korrekte Antworten: <strong>88%</strong>
+        </p>
+      </div>
+
+      <div className="column">
+        <ListWithHeader items={newLastUsed}>
           <FormattedMessage id="questionPool.question.lastUsed" defaultMessage="Last used" />
         </ListWithHeader>
-      )}
+      </div>
 
-      {lastUsed.length === 0 && (
-        <ListWithHeader items={['never used']}>
-          <FormattedMessage id="questionPool.question.lastUsed" defaultMessage="Last used" />
-        </ListWithHeader>
-      )}
-    </div>
-
-    <div className="column buttons">
-      <div>
+      <div className="column buttons">
         <Button className="button">
           <FaEye />
         </Button>
-      </div>
-      <div>
         <Button className="button">
           <FaPencil />
         </Button>
-      </div>
-      <div>
         <Button className="button">
           <FaTrash />
         </Button>
       </div>
-    </div>
 
-    <style jsx>{`
-      .questionDetails {
-        display: flex;
-        flex-direction: column;
+      <style jsx>{`
+        .questionDetails {
+          display: flex;
+          flex-direction: column;
 
-        background-color: lightgrey;
-        border: 1px solid grey;
-
-        .column {
-          text-align: center;
-        }
-
-        @media all and (min-width: 768px) {
-          flex-direction: row;
-          min-height: 7rem;
+          background-color: lightgrey;
+          border: 1px solid grey;
 
           .column {
-            flex: 1;
-            padding: 1rem;
-            text-align: left;
+            text-align: center;
+          }
 
-            &.description {
-              word-wrap: break-word;
-            }
+          @media all and (min-width: 768px) {
+            flex-direction: row;
+            min-height: 7rem;
 
-            &.buttons {
-              padding: 0;
-              flex: none;
+            .column {
+              flex: 1;
+              padding: 1rem;
+              text-align: left;
 
-              :global(.button) {
-                margin: 0;
-                margin-bottom: 5px;
+              &.description {
+                word-wrap: break-word;
               }
 
-              :global(.button:last) {
-                margin-bottom: 0;
-              }
-            }
+              &.buttons {
+                padding: 0;
+                flex: none;
 
-            &:not(:last-child) {
-              border-right: 1px solid grey;
+                :global(.button) {
+                  margin: 0;
+                  margin-bottom: 5px;
+                  display: block;
+                }
+
+                :global(.button:last) {
+                  margin-bottom: 0;
+                }
+              }
+
+              &:not(:last-child) {
+                border-right: 1px solid grey;
+              }
             }
           }
         }
-      }
-    `}</style>
-  </div>
-)
+      `}</style>
+    </div>
+  )
+}
 
 QuestionDetails.propTypes = propTypes
 QuestionDetails.defaultProps = defaultProps
