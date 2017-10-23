60bf63a0c7eae6d2615bd4bd3ab4914f321cd46f
Roland Schläfli <rolandschlaefli@gmail.com>
2017-10-23 21:12:37 +0200

    Add stub components for word cloud and tabular representation

---
+++ src/components/evaluation/charts/CloudChart.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const propTypes = {}
+
+function CloudChart({}) {}
+
+CloudChart.propTypes = propTypes
+
+export default CloudChart
---
+++ src/components/evaluation/charts/TableChart.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const propTypes = {}
+
+function TableChart({}) {}
+
+TableChart.propTypes = propTypes
+
+export default TableChart
