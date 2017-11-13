836d372554ae6b070a2bd3f138a8a75efa264ab4
Roland Schläfli <rolandschlaefli@gmail.com>
2017-11-13 16:02:07 +0100

    (fix) Less strict validation for first and last name

--- src/components/forms/RegistrationForm.js
+++ src/components/forms/RegistrationForm.js
@@ -22,15 +22,15 @@ const validate = ({
 }) => {
   const errors = {}
 
-  if (!firstName || !isAlpha(firstName) || isEmpty(firstName)) {
+  if (!firstName || isEmpty(firstName)) {
     errors.firstName = 'form.firstName.invalid'
   }
 
-  if (!lastName || !isAlpha(lastName) || isEmpty(lastName)) {
+  if (!lastName || isEmpty(lastName)) {
     errors.lastName = 'form.lastName.invalid'
   }
 
-  if (!institution || !isAlpha(institution) || isEmpty(institution)) {
+  if (!institution || isEmpty(institution)) {
     errors.institution = 'form.institution.invalid'
   }
 
