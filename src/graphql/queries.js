f777dd330602fd185493c265f839e00519a05b6c
Roland Schläfli <rolandschlaefli@gmail.com>
2017-11-17 13:17:26 +0100

    Add evaluation button to session list, remove active instances query

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -108,56 +108,6 @@ export const AccountSummaryQuery = gql`
   }
 `
 
-export const ActiveInstancesQuery = gql`
-  query ActiveInstances {
-    activeInstances {
-      id
-      isOpen
-      version
-      responses {
-        id
-        value
-        createdAt
-      }
-      results {
-        ... on SCQuestionResults {
-          choices
-        }
-        ... on FREEQuestionResults {
-          free {
-            count
-            key
-            value
-          }
-        }
-      }
-      question {
-        id
-        title
-        type
-        versions {
-          description
-          options {
-            ... on SCQuestionOptions {
-              choices {
-                correct
-                name
-              }
-            }
-            ... on FREEQuestionOptions {
-              restrictions {
-                min
-                max
-                type
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-`
-
 export const JoinSessionQuery = gql`
   query JoinSession($shortname: String!) {
     joinSession(shortname: $shortname) {
