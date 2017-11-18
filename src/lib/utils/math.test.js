2668fcce6d2f264e3f8a2f3331090082bc6c5abd
Pascal Zehnder <pascal.zehnder@bf.uzh.ch>
2017-11-18 21:54:48 +0100

    Histogram (#203)

---
+++ src/lib/utils/math.test.js
@@ -0,0 +1,28 @@
+import _round from 'lodash/round'
+import { calculateMax, calculateMin, calculateMean, calculateMedian } from './math'
+
+describe('math', () => {
+  const results = {
+    data: [
+      { count: 4, value: -3 },
+      { count: 5, value: 3 },
+      { count: 2, value: 4 },
+      { count: 1, value: 7 },
+      { count: 2, value: 10 },
+    ],
+    totalResponses: 14,
+  }
+
+  it('calculates max and min correctly', () => {
+    expect(calculateMax(results)).toEqual(10)
+    expect(calculateMin(results)).toEqual(-3)
+  })
+
+  it('calculates mean correctly', () => {
+    expect(_round(calculateMean(results), 3)).toEqual(2.714)
+  })
+
+  it('calculates median correctly', () => {
+    expect(calculateMedian(results)).toEqual(3)
+  })
+})
