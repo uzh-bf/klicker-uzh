aa3425a0baec77dcf07008dc70b84cd58260ebfa
Roland Schläfli <rolandschlaefli@gmail.com>
2017-11-18 23:32:05 +0100

    (fix) Final lint errors

--- src/components/common/Autosuggest.js
+++ src/components/common/Autosuggest.js
@@ -5,6 +5,8 @@ import Autosuggest from 'react-autosuggest'
 import _sortBy from 'lodash/sortBy'
 
 const autocompleteRenderInput = (tags, currentValue) => ({ addTag, ...props }) => {
+  const { ref, value } = props
+
   const handleOnChange = (e, { newValue, method }) => {
     if (method === 'enter') {
       e.preventDefault()
@@ -13,35 +15,34 @@ const autocompleteRenderInput = (tags, currentValue) => ({ addTag, ...props }) =
     }
   }
 
-  const inputValue = (props.value && props.value.trim().toLowerCase()) || ''
+  const inputValue = (value && value.trim().toLowerCase()) || ''
   const inputLength = inputValue.length
 
   // calculate suggestions for possible tags
   const suggestions = tags.filter(
-    tag =>
-      tag.name.toLowerCase().slice(0, inputLength) === inputValue &&
-      !currentValue.includes(tag.name),
+    ({ name }) =>
+      name.toLowerCase().slice(0, inputLength) === inputValue && !currentValue.includes(name),
   )
 
   // sort the suggestions
   const sortedSuggestions = _sortBy(suggestions, ['name'])
 
-  const getSuggestionValue = suggestion => suggestion.name
-  const renderSuggestion = suggestion => <span>{suggestion.name}</span>
+  const getSuggestionValue = ({ name }) => name
+  const renderSuggestion = ({ name }) => <span>{name}</span>
 
   return (
     <Autosuggest
       getSuggestionValue={getSuggestionValue}
       inputProps={{ ...props, onChange: handleOnChange }}
-      onSuggestionsClearRequested={() => {}}
+      ref={ref}
+      renderSuggestion={renderSuggestion}
+      shouldRenderSuggestions={val => val && val.trim().length > 0}
+      suggestions={sortedSuggestions}
       onSuggestionSelected={(e, { suggestion }) => {
         addTag(suggestion.name)
       }}
+      onSuggestionsClearRequested={() => {}}
       onSuggestionsFetchRequested={() => {}}
-      ref={props.ref}
-      renderSuggestion={renderSuggestion}
-      shouldRenderSuggestions={value => value && value.trim().length > 0}
-      suggestions={sortedSuggestions}
     />
   )
 }
