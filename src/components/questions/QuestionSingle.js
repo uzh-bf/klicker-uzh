fd1e64401cdc2080a0ccead393ff802f643d91a1
Alex Scheitlin <alex.scheitlin@uzh.ch>
2017-11-20 07:09:18 +0100

    remove description from sessions (only display title)

--- src/components/questions/QuestionSingle.js
+++ src/components/questions/QuestionSingle.js
@@ -2,18 +2,14 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const propTypes = {
-  /* id: PropTypes.string.isRequired, */
-  description: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   type: PropTypes.string.isRequired,
 }
 
-const QuestionSingle = ({ /* id, */ description, type, title }) => (
+const QuestionSingle = ({ type, title }) => (
   <div className="questionSingle">
-    {/* <div className="id">{`#${id.substring(0, 7)}`}</div> */}
-    <div className="id">{title}</div>
+    <div className="title">{title}</div>
     <div className="type">{type}</div>
-    <div className="content">{description}</div>
 
     <style jsx>{`
       .questionSingle {
@@ -22,18 +18,12 @@ const QuestionSingle = ({ /* id, */ description, type, title }) => (
         flex-flow: row wrap;
         padding: 0.3rem;
 
-        .id,
+        .title,
         .type {
           flex: 1;
-          padding-bottom: 0.3rem;
         }
         .type {
           text-align: right;
-        }
-        .content {
-          border-top: 1px solid lightgrey;
-          flex: 0 0 100%;
-          padding-top: 0.3rem;
         }
       }
     `}</style>
