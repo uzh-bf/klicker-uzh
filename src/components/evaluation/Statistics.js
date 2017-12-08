b73f7e3aa22b59096fce81bd28d096a0c92ddb3f
AlexScheitlin <alex.scheitlin@bluewin.ch>
2017-12-08 11:33:01 +0100

    Statistics (#258)

--- src/components/evaluation/Statistics.js
+++ src/components/evaluation/Statistics.js
@@ -10,6 +10,9 @@ const propTypes = {
   mean: PropTypes.number,
   median: PropTypes.number,
   min: PropTypes.number,
+  q1: PropTypes.number,
+  q3: PropTypes.number,
+  sd: PropTypes.number,
 }
 
 const defaultProps = {
@@ -17,10 +20,13 @@ const defaultProps = {
   mean: undefined,
   median: undefined,
   min: undefined,
+  q1: undefined,
+  q3: undefined,
+  sd: undefined,
 }
 
 const Statistics = ({
-  max, mean, median, min,
+  max, mean, median, min, q1, q3, sd,
 }) => (
   <div className="statistics">
     <h2>
@@ -31,14 +37,23 @@ const Statistics = ({
       <EvaluationListItem color="white" marker="MIN">
         {min ? _round(min, 2) : '-'}
       </EvaluationListItem>
-      <EvaluationListItem color="white" marker="MAX">
-        {max ? _round(max, 2) : '-'}
+      <EvaluationListItem color="black" marker="Q1">
+        {q1 ? _round(q1, 2) : '-'}
+      </EvaluationListItem>
+      <EvaluationListItem color="red" marker="MEDIAN">
+        {median ? _round(median, 2) : '-'}
       </EvaluationListItem>
       <EvaluationListItem color="Blue" marker="MEAN">
         {mean ? _round(mean, 2) : '-'}
       </EvaluationListItem>
-      <EvaluationListItem color="red" marker="MEDIAN">
-        {median ? _round(median, 2) : '-'}
+      <EvaluationListItem color="black" marker="Q3">
+        {q3 ? _round(q3, 2) : '-'}
+      </EvaluationListItem>
+      <EvaluationListItem color="white" marker="MAX">
+        {max ? _round(max, 2) : '-'}
+      </EvaluationListItem>
+      <EvaluationListItem color="white" marker="SD">
+        {sd ? _round(sd, 2) : '-'}
       </EvaluationListItem>
     </div>
 
