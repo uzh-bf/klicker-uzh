b1c75f417594020c76caf05439f73b49b4f8fe26
Roland Schläfli <rolandschlaefli@gmail.com>
2017-12-14 18:09:44 +0100

    Add some reasonable first tests

--- cypress/integration/testing.js
+++ cypress/integration/testing.js
@@ -1,5 +1,8 @@
 /* eslint-disable no-undef */
 
+const email = ''
+const password = ''
+
 describe('/', () => {
   it('successfully loads', () => {
     cy.visit('/')
@@ -10,16 +13,63 @@ describe('/user/login', () => {
   it('successfully loads', () => {
     cy.visit('/user/login')
   })
+
+  it('allows logging in', () => {
+    cy.get('input[name=email]').type(email)
+    cy.get('input[name=password]').type(`${password}{enter}`)
+
+    cy.url().should('include', '/questions')
+
+    // our auth cookie should be present
+    cy.getCookie('jwt').should('exist')
+  })
 })
 
 describe('/user/registration', () => {
   it('successfully loads', () => {
-    cy.visit('/user/login')
+    cy.visit('/user/registration')
   })
+
+  it.skip('allows creating a new account', () => {})
 })
 
 describe('/user/resetPassword', () => {
   it('successfully loads', () => {
-    cy.visit('/user/login')
+    cy.visit('/user/resetPassword')
+  })
+
+  it.skip('allows submitting a reset request', () => {})
+})
+
+describe('/questions', () => {
+  beforeEach(() => {
+    cy.request('POST', 'http://localhost:4000/graphql', {
+      query: `
+        mutation Login($email: String!, $password: String!) {
+          login(email: $email, password: $password) {
+            id
+            email
+            shortname
+            runningSession {
+              id
+            }
+          }
+        }
+      `,
+      variables: {
+        email,
+        password,
+      },
+    })
+  })
+
+  it('successfully loads', () => {
+    cy.visit('/questions')
+  })
+})
+
+describe('/sessions', () => {
+  it('successfully loads', () => {
+    cy.visit('/sessions')
   })
 })
