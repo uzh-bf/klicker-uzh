224c1ac066bf9f169ebd355cc3eca26ee9ebafaa
Roland Schläfli <rolandschlaefli@gmail.com>
2017-12-15 18:53:54 +0100

    (fix) process.send not available on normal start

--- src/server.js
+++ src/server.js
@@ -26,10 +26,13 @@ const redis = getRedis()
 
 server.listen(process.env.PORT, (err) => {
   if (err) throw err
-  console.log(`[klicker-api] ready on http://${process.env.APP_DOMAIN}:${process.env.PORT}${process.env.APP_PATH}!`)
 
   // send a ready message to PM2
-  process.send('ready')
+  if (process.send) {
+    process.send('ready')
+  }
+
+  console.log(`[klicker-api] ready on http://${process.env.APP_DOMAIN}:${process.env.PORT}${process.env.APP_PATH}!`)
 })
 
 process.on('SIGINT', async () => {
