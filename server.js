7fea8a7db5134cf17f178c73524814dfe4205ecf
Roland Schläfli <rolandschlaefli@gmail.com>
2017-12-15 12:02:13 +0100

    Add APM_NAME to env

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ if (process.env.APM_SERVER_URL) {
 
   apm = require('elastic-apm-node').start({
     active: !dev,
-    appName: 'klicker-next',
+    appName: process.env.APM_NAME,
     secretToken: process.env.APM_SECRET_TOKEN,
     serverUrl: process.env.APM_SERVER_URL,
   })
