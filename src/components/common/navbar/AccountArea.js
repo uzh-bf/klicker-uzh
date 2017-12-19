fe726f4e6de2cd2c5fce505130d4977db1a90c3c
Pascal Zehnder <pascal.zehnder@bf.uzh.ch>
2017-12-19 13:50:34 +0100

    Link to user settings

--- src/components/common/navbar/AccountArea.js
+++ src/components/common/navbar/AccountArea.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Dropdown } from 'semantic-ui-react'
 import { FormattedMessage } from 'react-intl'
+import Link from 'next/link'
 
 const propTypes = {
   accountShort: PropTypes.string.isRequired,
@@ -10,9 +11,11 @@ const propTypes = {
 const AccountArea = ({ accountShort }) => (
   <Dropdown item simple text={accountShort.toUpperCase()}>
     <Dropdown.Menu>
-      <Dropdown.Item disabled>
-        <FormattedMessage defaultMessage="Settings" id="common.string.settings" />
-      </Dropdown.Item>
+      <Link href="/user/settings">
+        <Dropdown.Item>
+          <FormattedMessage defaultMessage="Settings" id="common.string.settings" />
+        </Dropdown.Item>
+      </Link>
       <Dropdown.Item disabled>
         <FormattedMessage defaultMessage="Logout" id="common.string.logout" />
       </Dropdown.Item>
