64423343cb62f16723bbefd8d84799fdb607ce3b
Roland Schläfli <rolandschlaefli@gmail.com>
2018-02-26 15:13:37 +0100

    Updated persisted query logic

--- src/lib/initApollo.js
+++ src/lib/initApollo.js
@@ -3,6 +3,7 @@
 
 import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
+import { createPersistedQueryLink } from 'apollo-link-persisted-queries'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import fetch from 'isomorphic-unfetch'
 
@@ -13,20 +14,53 @@ if (!process.browser) {
   global.fetch = fetch
 }
 
+// HACK: this is a copy of the default persisted-queries disable function
+// it needs to be provided as the config passed in is not yet merged with defaults
+// otherwise it would lead to ".disable not defined" style errors...
+const defaultDisable = ({ graphQLErrors, operation }) => {
+  // if the server doesn't support persisted queries, don't try anymore
+  if (
+    graphQLErrors &&
+    graphQLErrors.some(({ message }) => message === 'PersistedQueryNotSupported')
+  ) {
+    return true
+  }
+
+  const { response } = operation.getContext()
+  // if the server responds with bad request
+  // apollo-server responds with 400 for GET and 500 for POST when no query is found
+  if (response && response.status && (response.status === 400 || response.status === 500)) {
+    return true
+  }
+
+  return false
+}
+
 function create(initialState) {
-  console.log('creating')
-  return new ApolloClient({
-    cache: new InMemoryCache({
-      addTypename: true,
-      dataIdFromObject: o => o.id,
-    }).restore(initialState || {}),
+  const cache = new InMemoryCache({
+    addTypename: true,
+    dataIdFromObject: o => o.id,
+  }).restore(initialState || {})
+
+  const link = new HttpLink({
+    credentials: 'include', // Additional fetch() options like `credentials` or `headers`
+    uri: process.env.API_URL || 'http://localhost:4000/graphql',
+  })
+
+  const persistQueriesLink = createPersistedQueryLink({
+    // we need to pass both disable and generateHash (or it will bug)
+    disable: defaultDisable,
+    generateHash: ({ documentId }) => documentId,
+  })
+
+  const client = new ApolloClient({
+    cache,
     connectToDevTools: process.browser,
-    link: new HttpLink({
-      credentials: 'include', // Additional fetch() options like `credentials` or `headers`
-      uri: process.env.API_URL || 'http://localhost:4000/graphql',
-    }),
+    link: persistQueriesLink.concat(link),
     ssrMode: !process.browser, // Disables forceFetch on the server (so queries are only run once)
   })
+
+  return client
 }
 
 export default function initApollo(initialState) {
--- src/next.config.js
+++ src/next.config.js
@@ -6,8 +6,24 @@ const { ANALYZE } = process.env
 
 module.exports = {
   webpack: (config) => {
+    // add the webpack context replacement plugin to remove moment locales
     config.plugins.push(new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /de|en/))
 
+    // push graphql loaders into the webpack config
+    config.module.rules.push({
+      test: /\.graphql$/,
+      use: [
+        {
+          loader: 'graphql-persisted-document-loader',
+          options: {
+            addTypename: true,
+          },
+        },
+        { loader: 'graphql-tag/loader' },
+      ],
+    })
+
+    // add the bundle analyzer plugin if enabled
     if (ANALYZE) {
       const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 
