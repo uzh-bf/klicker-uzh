74cd527c9ffaf60cdcd982ed836b76a77da892e5
Roland Schläfli <rolandschlaefli@gmail.com>
2018-03-05 09:27:48 +0100

    Hardcoded question types in tag list

--- src/components/questions/TagList.js
+++ src/components/questions/TagList.js
@@ -5,16 +5,12 @@ import { compose, withProps, branch, renderComponent } from 'recompose'
 import { FormattedMessage } from 'react-intl'
 
 import { LoadingDiv } from '../common'
+import { QUESTION_TYPES } from '../../lib'
 
 const propTypes = {
+  activeType: PropTypes.string.isRequired,
   handleTagClick: PropTypes.func.isRequired,
   tags: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-    }),
-  ),
-  types: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string.isRequired,
       name: PropTypes.string.isRequired,
@@ -24,10 +20,9 @@ const propTypes = {
 
 const defaultProps = {
   tags: [],
-  types: [],
 }
 
-export const TagListPres = ({ tags, types, handleTagClick }) => (
+export const TagListPres = ({ activeType, tags, handleTagClick }) => (
   <div className="tagList">
     {tags.length === 0 ? (
       <FormattedMessage defaultMessage="No tags available." id="tagList.string.noTags" />
@@ -38,19 +33,50 @@ export const TagListPres = ({ tags, types, handleTagClick }) => (
       <List.Header className="listHeader types">
         <FormattedMessage defaultMessage="Types" id="tagList.header.types" />
       </List.Header>
-      {types.map(({ isActive, name }) => (
-        <List.Item
-          active={isActive}
-          className="listItem"
-          key={name}
-          onClick={() => handleTagClick(name, true)}
-        >
-          <List.Icon name={isActive ? 'folder' : 'folder outline'} />
-          <List.Content>
-            <FormattedMessage defaultMessage="type" id={`common.${name}.label`} />
-          </List.Content>
-        </List.Item>
-      ))}
+      <List.Item
+        active={activeType === QUESTION_TYPES.SC}
+        className="listItem"
+        key="SC"
+        onClick={() => handleTagClick('SC', true)}
+      >
+        <List.Icon name={activeType === QUESTION_TYPES.SC ? 'folder' : 'folder outline'} />
+        <List.Content>
+          <FormattedMessage defaultMessage="SC" id="common.SC.label" />
+        </List.Content>
+      </List.Item>
+      <List.Item
+        active={activeType === QUESTION_TYPES.MC}
+        className="listItem"
+        key="MC"
+        onClick={() => handleTagClick('MC', true)}
+      >
+        <List.Icon name={activeType === QUESTION_TYPES.MC ? 'folder' : 'folder outline'} />
+        <List.Content>
+          <FormattedMessage defaultMessage="MC" id="common.MC.label" />
+        </List.Content>
+      </List.Item>
+      <List.Item
+        active={activeType === QUESTION_TYPES.FREE}
+        className="listItem"
+        key="FREE"
+        onClick={() => handleTagClick('FREE', true)}
+      >
+        <List.Icon name={activeType === QUESTION_TYPES.FREE ? 'folder' : 'folder outline'} />
+        <List.Content>
+          <FormattedMessage defaultMessage="FREE" id="common.FREE.label" />
+        </List.Content>
+      </List.Item>
+      <List.Item
+        active={activeType === QUESTION_TYPES.FREE_RANGE}
+        className="listItem"
+        key="FREE_RANGE"
+        onClick={() => handleTagClick('FREE_RANGE', true)}
+      >
+        <List.Icon name={activeType === QUESTION_TYPES.FREE_RANGE ? 'folder' : 'folder outline'} />
+        <List.Content>
+          <FormattedMessage defaultMessage="FREE_RANGE" id="common.FREE_RANGE.label" />
+        </List.Content>
+      </List.Item>
       <List.Header className="listHeader tags">
         <FormattedMessage defaultMessage="Tags" id="tagList.header.tags" />
       </List.Header>
@@ -101,12 +127,12 @@ TagListPres.defaultProps = defaultProps
 export default compose(
   branch(({ data }) => data.loading, renderComponent(LoadingDiv)),
   branch(({ data }) => data.error, renderComponent(({ data }) => <div>{data.error}</div>)),
-  withProps(({ activeTags, activeType, data: { loading, tags } }) => ({
+  withProps(({ activeTags, data: { loading, tags } }) => ({
     loading,
     tags: tags && tags.map(tag => ({ ...tag, isActive: activeTags.includes(tag.name) })),
-    types: ['SC', 'MC', 'FREE', 'FREE_RANGE'].map(type => ({
+    /* types: ['SC', 'MC', 'FREE', 'FREE_RANGE'].map(type => ({
       isActive: type === activeType,
       name: type,
-    })),
+    })), */
   })),
 )(TagListPres)
