3e7e9af8ca4661fe938a7e3a00606bbc41b7b9fd
Roland Schläfli <rolandschlaefli@gmail.com>
2018-03-22 16:55:36 +0100

    Start with form modifications

--- src/components/sessions/creation/SessionTimelineInput.js
+++ src/components/sessions/creation/SessionTimelineInput.js
@@ -4,12 +4,14 @@ import ReactTooltip from 'react-tooltip'
 import { adjust } from 'ramda'
 import { FormattedMessage } from 'react-intl'
 import { Icon, Message } from 'semantic-ui-react'
-
+import { compose, withHandlers } from 'recompose'
+import { Droppable, Draggable, DragDropContext } from 'react-beautiful-dnd'
 import QuestionDropzone from './QuestionDropzone'
 import QuestionSingle from '../../questions/QuestionSingle'
 
 const propTypes = {
-  onChange: PropTypes.func.isRequired,
+  handleExtendBlock: PropTypes.func.isRequired,
+  handleNewBlock: PropTypes.func.isRequired,
   value: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string.isRequired,
@@ -23,38 +25,55 @@ const propTypes = {
   ).isRequired,
 }
 
-const SessionTimelineInput = ({ value, onChange }) => {
-  // handle creation of an entirely new block
-  const handleNewBlock = (newQuestion) => {
-    onChange([...value, { questions: [newQuestion] }])
-  }
-
-  // handle extending a block with a further question
-  const handleExtendBlock = blockIndex => (newQuestion) => {
-    onChange(
-      adjust(prev => ({ ...prev, questions: [...prev.questions, newQuestion] }), blockIndex, value),
-    )
-  }
-
-  return (
-    <div className="sessionTimeline">
-      {value.map((block, index) => (
+const SessionTimelineInput = ({
+  value,
+  handleNewBlock,
+  handleExtendBlock,
+  onDragStart,
+  onDragEnd,
+}) => (
+  <div className="sessionTimeline">
+    <DragDropContext onDragEnd={onDragEnd} onDragStart={onDragStart}>
+      {value.map((block, blockIndex) => (
         <div className="timelineItem" key={block.id}>
           <div className="title">
-            <span>Block {index + 1}</span>
-            <Icon name="settings" />
+            <span>Block {blockIndex + 1}</span>
+            {/* <Icon name="settings" /> */}
           </div>
 
           <div className="questions">
-            {block.questions.map(({
- id, title, type, version,
-}) => (
-  <QuestionSingle id={id} title={title} type={type} version={version} />
-            ))}
+            <Droppable droppableId={`block-${blockIndex}`} type="QUESTION">
+              {(provided, snapshot) => (
+                <div ref={provided.innerRef}>
+                  {block.questions.map((q, questionIndex) => (
+                    <Draggable draggableId={`question-${q.id}`} index={questionIndex} key={q.id} type="QUESTION">
+                      {(provided2, snapshot2) => (
+                        <div>
+                          <div
+                            ref={provided2.innerRef}
+                            {...provided2.draggableProps}
+                            {...provided2.dragHandleProps}
+                          >
+                            <QuestionSingle
+                              id={q.id}
+                              title={q.title}
+                              type={q.type}
+                              version={q.version}
+                            />
+                          </div>
+                          {provided2.placeholder}
+                        </div>
+                      )}
+                    </Draggable>
+                  ))}
+                  {provided.placeholder}
+                </div>
+              )}
+            </Droppable>
           </div>
 
           <div className="questionDropzone">
-            <QuestionDropzone onDrop={handleExtendBlock(index)} />
+            <QuestionDropzone onDrop={handleExtendBlock(blockIndex)} />
           </div>
         </div>
       ))}
@@ -89,81 +108,114 @@ const SessionTimelineInput = ({ value, onChange }) => {
           </div>
         )}
       </div>
-      <style jsx>{`
-        @import 'src/theme';
+    </DragDropContext>
+    <style jsx>{`
+      @import 'src/theme';
 
-        .sessionTimeline {
+      .sessionTimeline {
+        display: flex;
+        flex-direction: column;
+
+        height: 100%;
+        padding: 0.5rem;
+
+        > .timelineItem {
           display: flex;
           flex-direction: column;
+          max-height: 25rem;
+          overflow: hidden;
 
-          height: 100%;
+          .questions {
+            overflow-y: auto;
+          }
+
           padding: 0.5rem;
 
-          > .timelineItem {
+          > :global(*):not(:first-child) {
+            margin-top: -2px;
+          }
+
+          .title {
             display: flex;
-            flex-direction: column;
-            max-height: 25rem;
-            overflow: hidden;
+            flex-direction: row;
+            justify-content: space-between;
 
-            .questions {
-              overflow-y: auto;
+            font-weight: bold;
+            margin-bottom: 0.5rem;
+
+            a {
+              color: $color-primary;
+              font-size: 1.25rem;
             }
+          }
 
-            padding: 0.5rem;
+          .message,
+          .questionDropzone {
+            margin-top: 0.5rem;
+          }
+
+          .questions {
+            font-size: 0.9rem;
 
             > :global(*):not(:first-child) {
               margin-top: -2px;
             }
-
-            .title {
-              display: flex;
-              flex-direction: row;
-              justify-content: space-between;
-
-              font-weight: bold;
-              margin-bottom: 0.5rem;
-
-              a {
-                color: $color-primary;
-                font-size: 1.25rem;
-              }
-            }
-
-            .message,
-            .questionDropzone {
-              margin-top: 0.5rem;
-            }
-
-            .questions {
-              font-size: 0.9rem;
-
-              > :global(*):not(:first-child) {
-                margin-top: -2px;
-              }
-            }
           }
+        }
 
-          @include desktop-tablet-only {
-            flex-flow: row wrap;
+        @include desktop-tablet-only {
+          flex-flow: row wrap;
 
-            border-left: 1px solid lightgrey;
-            border-bottom: 1px solid lightgrey;
+          border-left: 1px solid lightgrey;
+          border-bottom: 1px solid lightgrey;
 
-            > .timelineItem {
-              width: 15rem;
+          > .timelineItem {
+            width: 15rem;
 
-              &:not(:last-child) {
-                border-right: 1px solid lightgrey;
-                margin-bottom: 0;
-              }
+            &:not(:last-child) {
+              border-right: 1px solid lightgrey;
+              margin-bottom: 0;
             }
           }
         }
-      `}</style>
-    </div>
-  )
-}
+      }
+    `}</style>
+  </div>
+)
 
 SessionTimelineInput.propTypes = propTypes
 
-export default SessionTimelineInput
+export default compose(
+  withHandlers({
+    // handle extending a block with a further question
+    handleExtendBlock: ({ onChange, value }) => blockIndex => (newQuestion) => {
+      onChange(
+        adjust(
+          prev => ({ ...prev, questions: [...prev.questions, newQuestion] }),
+          blockIndex,
+          value,
+        ),
+      )
+    },
+
+    // handle creation of an entirely new block
+    handleNewBlock: ({ onChange, value }) => (newQuestion) => {
+      onChange([...value, { questions: [newQuestion] }])
+    },
+
+    onDragEnd: ({ value, onChange }) => ({ source, destination }) => {
+      if (!destination) {
+        return
+      }
+
+      console.log(source, destination, value, onChange)
+
+      // TODO:
+
+      // onChange(tempValue)
+    },
+    onDragStart: () => (a) => {
+      console.log(a)
+    },
+  }),
+)(SessionTimelineInput)
