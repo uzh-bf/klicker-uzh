e6ca9531856ba831a20ce2da8ab915909cbd1065
Roland Schläfli <rolandschlaefli@gmail.com>
2018-05-07 14:44:25 +0200

    Replace dynamic import with require

--- src/lib/withFingerprint.js
+++ src/lib/withFingerprint.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import dynamic from 'next/dynamic'
 
 export default (ComposedComponent) => {
   let fingerprint
@@ -13,7 +12,7 @@ export default (ComposedComponent) => {
 
       // otherwise generate a new fingerprint and store it in a cookie
       try {
-        const Fingerprint2 = dynamic(import('fingerprintjs2'))
+        const Fingerprint2 = require('fingerprintjs2')
         new Fingerprint2().get((result) => {
           sessionStorage.setItem('fp', result)
           resolve(result)
