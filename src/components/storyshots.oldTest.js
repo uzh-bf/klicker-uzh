e6dffcac5192778ba3c33b0dfafb27f8807d410a
Roland Schläfli <rolandschlaefli@gmail.com>
2018-05-07 15:29:40 +0200

    Temporarily disable storyshots

--- src/components/storyshots.oldTest.js
+++ src/components/storyshots.oldTest.js
@@ -2,6 +2,7 @@
  * @jest-environment jsdom
  */
 
+// eslint-disable-next-line
 import initStoryshots from '@storybook/addon-storyshots'
 
 // HACK: workaround for https://github.com/Semantic-Org/Semantic-UI-React/issues/1702
