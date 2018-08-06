5a5fb92c04810f9c1bd29cb61227ab225abf6b93
Roland Schläfli <rolandschlaefli@gmail.com>
2018-08-06 16:00:23 +0200

    Release Preparations (#682)

--- src/components/forms/userSettings/AccountDataForm.js
+++ src/components/forms/userSettings/AccountDataForm.js
@@ -69,7 +69,7 @@ const AccountDataForm = ({ intl }) => (
                     handleBlur={handleBlur}
                     handleChange={handleChange}
                     icon="hashtag"
-                    inlineLabel="beta.klicker.uzh.ch/join/"
+                    inlineLabel={`${process.env.APP_BASE_URL}/join/`}
                     intl={intl}
                     label={intl.formatMessage(messages.shortnameLabel)}
                     name="shortname"
