45e0f4e40bbaca4099e011ed5993f5b8f3491bc4
Roland Schlaefli <rolandschlaefli@gmail.com>
2018-08-09 19:19:07 +0200

    Add ellipsis for lengthy question options

--- src/components/evaluation/EvaluationListItem.js
+++ src/components/evaluation/EvaluationListItem.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
 import _isFinite from 'lodash/isFinite'
-import { Icon } from 'semantic-ui-react'
+import { Icon, Popup } from 'semantic-ui-react'
 import { QUESTION_GROUPS } from '../../constants'
 
 const propTypes = {
@@ -34,67 +34,75 @@ const EvaluationListItem = ({ color, correct, children, marker, reverse, percent
     )}
 
     {marker && reverse && <div className="marker">{marker}</div>}
-    <div className="content">{children}</div>
+    <div className="content">
+      {children.length > 70 ? (
+        <>
+          {children.slice(0, 70)}{' '}
+          <Popup content={children} trigger={<Icon name="ellipsis horizontal" size="small" />} wide="very" />
+        </>
+      ) : (
+        children
+      )}
+    </div>
     {QUESTION_GROUPS.WITH_PERCENTAGES.includes(questionType) &&
       showGraph &&
       _isFinite(percentage) && <div className="percentage">{percentage}%</div>}
     {marker && !reverse && <div className="marker">{marker}</div>}
 
-    <style jsx>
-      {`
-        @import 'src/theme';
+    <style jsx>{`
+      @import 'src/theme';
 
-        .evaluationListItem {
-          display: flex;
-          flex-flow: row wrap;
-          align-items: center;
+      .evaluationListItem {
+        display: flex;
+        flex-flow: row wrap;
+        align-items: center;
 
-          padding: 0.1rem 0;
+        padding: 0.1rem 0;
 
-          border-bottom: 1px solid lightgrey;
+        border-bottom: 1px solid lightgrey;
 
-          &:first-child {
-            border-top: 1px solid lightgrey;
-          }
+        &:first-child {
+          border-top: 1px solid lightgrey;
+        }
 
-          &.correct {
-            background-color: $color-correct;
-          }
+        &.correct {
+          background-color: $color-correct;
+        }
 
-          .colorSquare :global(i) {
-            flex: 0 0 auto;
+        .colorSquare :global(i) {
+          flex: 0 0 auto;
 
-            color: ${color};
-          }
+          color: ${color};
+        }
 
-          .marker {
-            flex: 0 0 1.5rem;
-            text-align: center;
+        .marker {
+          flex: 0 0 1.5rem;
+          text-align: center;
 
-            font-weight: bold;
-          }
+          font-weight: bold;
+        }
 
+        .content {
+          flex: 1;
+          font-size: 0.9rem;
+        }
+
+        .percentage {
+          flex: 0 0 2rem;
+          font-size: 0.8rem;
+          text-align: right;
+        }
+
+        &.reverse {
+          justify-content: space-between;
+
+          .marker,
           .content {
-            flex: 1;
-          }
-
-          .percentage {
-            flex: 0 0 2rem;
-            font-size: 0.8rem;
-            text-align: right;
-          }
-
-          &.reverse {
-            justify-content: space-between;
-
-            .marker,
-            .content {
-              flex: 0 0 auto;
-            }
+            flex: 0 0 auto;
           }
         }
-      `}
-    </style>
+      }
+    `}</style>
   </div>
 )
 
