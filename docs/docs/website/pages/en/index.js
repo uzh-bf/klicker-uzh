672bbf4049998533e118e3a6526f426b7c1360bb
Roland Schlaefli <rolandschlaefli@gmail.com>
2018-09-13 13:27:33 +0200

    Update docusaurus for new repository

--- docs/docs/website/pages/en/index.js
+++ docs/docs/website/pages/en/index.js
@@ -5,25 +5,25 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-const React = require('react');
+const React = require('react')
 
-const CompLibrary = require('../../core/CompLibrary.js');
-const MarkdownBlock = CompLibrary.MarkdownBlock; /* Used to read markdown */
-const Container = CompLibrary.Container;
-const GridBlock = CompLibrary.GridBlock;
+const CompLibrary = require('../../core/CompLibrary.js')
+const MarkdownBlock = CompLibrary.MarkdownBlock /* Used to read markdown */
+const Container = CompLibrary.Container
+const GridBlock = CompLibrary.GridBlock
 
-const siteConfig = require(process.cwd() + '/siteConfig.js');
+const siteConfig = require(process.cwd() + '/siteConfig.js')
 
 function imgUrl(img) {
-  return siteConfig.baseUrl + 'img/' + img;
+  return siteConfig.baseUrl + 'img/' + img
 }
 
 function docUrl(doc, language) {
-  return siteConfig.baseUrl + 'docs/' + (language ? language + '/' : '') + doc;
+  return siteConfig.baseUrl + 'docs/' + (language ? language + '/' : '') + doc
 }
 
 function pageUrl(page, language) {
-  return siteConfig.baseUrl + (language ? language + '/' : '') + page;
+  return siteConfig.baseUrl + (language ? language + '/' : '') + page
 }
 
 class Button extends React.Component {
@@ -34,13 +34,13 @@ class Button extends React.Component {
           {this.props.children}
         </a>
       </div>
-    );
+    )
   }
 }
 
 Button.defaultProps = {
   target: '_self',
-};
+}
 
 const SplashContainer = props => (
   <div className="homeContainer">
@@ -48,7 +48,7 @@ const SplashContainer = props => (
       <div className="wrapper homeWrapper">{props.children}</div>
     </div>
   </div>
-);
+)
 
 const ProjectTitle = props => (
   <h2 className="projectTitle">
@@ -56,7 +56,7 @@ const ProjectTitle = props => (
     <span>{siteConfig.subtitle_index}</span>
     <small>{siteConfig.tagline}</small>
   </h2>
-);
+)
 
 const PromoSection = props => (
   <div className="section promoSection">
@@ -64,85 +64,92 @@ const PromoSection = props => (
       <div className="pluginRowBlock">{props.children}</div>
     </div>
   </div>
-);
+)
 
 class HomeSplash extends React.Component {
   render() {
-    let language = this.props.language || '';
+    let language = this.props.language || ''
     return (
       <SplashContainer>
         <div className="inner">
           <ProjectTitle />
           <PromoSection>
-            <Button href={'https://beta.klicker.uzh.ch'} target={'blank'}>Klicker Application</Button>
-            <Button href={docUrl('introduction.html', language)}>How to use Klicker</Button>
+            <Button href={'https://app.klicker.uzh.ch'} target={'blank'}>
+              Klicker Application
+            </Button>
+            <Button href={docUrl('introduction.html', language)}>
+              How to use Klicker
+            </Button>
             <Button href={docUrl('faq.html', language)}>FAQ</Button>
           </PromoSection>
         </div>
       </SplashContainer>
-    );
+    )
   }
 }
 
 const Block = props => (
-  <Container
-    padding={['bottom']}
-    id={props.id}
-    background={props.background}>
+  <Container padding={['bottom']} id={props.id} background={props.background}>
     <GridBlock align="center" contents={props.children} layout={props.layout} />
   </Container>
-);
+)
 
 const NewFeatures = props => (
   <div
     className="productShowcaseSection"
-    style={{textAlign: 'center', marginBottom: '20px'}}>
+    style={{ textAlign: 'center', marginBottom: '20px' }}
+  >
     <h2>New Features</h2>
-    <MarkdownBlock>The new Klicker provides a lot of new features!</MarkdownBlock>
+    <MarkdownBlock>
+      The new Klicker provides a lot of new features!
+    </MarkdownBlock>
   </div>
-);
+)
 
 const Features1 = props => (
   <div>
-    <Container padding={["bottom", "top"]} background="light">
+    <Container padding={['bottom', 'top']} background="light">
       <GridBlock
         contents={[
           {
-            content: 'Preparing sessions for speeches or lectures in advance and grouping questions make the application of Klicker simpler and more structured.',
+            content:
+              'Preparing sessions for speeches or lectures in advance and grouping questions make the application of Klicker simpler and more structured.',
             image: imgUrl('question_pool.png'),
             imageAlign: 'right',
             title: 'Revised Question Management',
-          }
+          },
         ]}
         layout="twoColumn"
       />
-  </Container>
-  <Container padding={["bottom", "top"]}>
+    </Container>
+    <Container padding={['bottom', 'top']}>
       <GridBlock
         contents={[
           {
-            content: 'New question types allow more accurate and versatile responses and enable additional evaluation techniques.',
+            content:
+              'New question types allow more accurate and versatile responses and enable additional evaluation techniques.',
             image: imgUrl('question_types.png'),
             imageAlign: 'left',
             title: 'Additional Question Types',
-          }
+          },
         ]}
         layout="twoColumn"
       />
     </Container>
   </div>
-);
+)
 
 const ImprovedCharts = props => (
   <div
     className="productShowcaseSection lightBackground"
-    style={{textAlign: 'center', marginBottom: '10px', paddingTop: '10px'}}>
+    style={{ textAlign: 'center', marginBottom: '10px', paddingTop: '10px' }}
+  >
     <h2>Improved and Additional Charts</h2>
   </div>
-);
+)
 
 const Charts = props => (
-  <Container padding={["bottom"]} background="light">
+  <Container padding={['bottom']} background="light">
     <GridBlock
       align="center"
       contents={[
@@ -163,7 +170,7 @@ const Charts = props => (
           image: imgUrl('stacked_chart.png'),
           imageAlign: 'bottom',
           title: 'Stacked Chart (Multiple Choice)',
-        }
+        },
       ]}
       layout="threeColumn"
     />
@@ -183,52 +190,54 @@ const Charts = props => (
           image: imgUrl('histogram.png'),
           imageAlign: 'bottom',
           title: 'Histogram (Number Range)',
-        }
+        },
       ]}
       layout="twoColumn"
     />
   </Container>
-);
+)
 
 const Features2 = props => (
   <div>
-    <Container padding={["bottom", "top"]}>
+    <Container padding={['bottom', 'top']}>
       <GridBlock
         contents={[
           {
-            content: 'With feedback about speed and difficulty speakers get the chance to respond to their audience. ',
+            content:
+              'With feedback about speed and difficulty speakers get the chance to respond to their audience. ',
             image: imgUrl('feedback.png'),
             imageAlign: 'right',
             title: 'Instant Audience Feedback',
-          }
+          },
         ]}
         layout="twoColumn"
       />
     </Container>
-    <Container padding={["bottom", "top"]} background="light">
+    <Container padding={['bottom', 'top']} background="light">
       <GridBlock
         contents={[
           {
-            content: 'The question blocks containing all the questions for the audience are placed on a timeline that leads through the speech.',
+            content:
+              'The question blocks containing all the questions for the audience are placed on a timeline that leads through the speech.',
             image: imgUrl('running_session.gif'),
             imageAlign: 'left',
             title: 'Advanced Presentation Mode',
-          }
+          },
         ]}
         layout="twoColumn"
       />
     </Container>
   </div>
-);
+)
 
 class Index extends React.Component {
   render() {
-    let language = this.props.language || '';
+    let language = this.props.language || ''
 
     return (
       <div>
         <HomeSplash language={language} />
-        
+
         <div className="mainContainer">
           <NewFeatures />
           <Features1 />
@@ -236,10 +245,9 @@ class Index extends React.Component {
           <Charts />
           <Features2 />
         </div>
-        
       </div>
-    );
+    )
   }
 }
 
-module.exports = Index;
+module.exports = Index
--- docs/docs/website/siteConfig.js
+++ docs/docs/website/siteConfig.js
@@ -8,17 +8,18 @@
 const users = []
 
 const siteConfig = {
-  title: '' /* empty, as we have the logo in the header title for your website */,
+  title:
+    '' /* empty, as we have the logo in the header title for your website */,
   title_index: 'Klicker',
   subtitle_index: 'UZH',
   tagline: 'An open source instant audience response system',
   url: 'https://uzh-bf.github.io' /* your website url */,
-  baseUrl: '/klicker-docs/' /* base url for your project */,
-  projectName: 'klicker-docs',
+  baseUrl: '/klicker-uzh/' /* base url for your project */,
+  projectName: 'klicker-uzh',
   organizationName: 'uzh-bf',
   headerLinks: [
-    {doc: 'introduction', label: 'Docs'},
-    {doc: 'faq', label: 'FAQ'},
+    { doc: 'introduction', label: 'Docs' },
+    { doc: 'faq', label: 'FAQ' },
     /*{doc: 'doc4', label: 'API'},*/
     /*{page: 'help', label: 'Help'},*/
     /*{blog: true, label: 'Blog'},*/
@@ -29,26 +30,22 @@ const siteConfig = {
   favicon: 'img/favicon.png',
   /* colors for website */
   colors: {
-    primaryColor: '#1e70bf',  /* header, navigation bar and sidebar */
-    secondaryColor: '#1e70bf',  /* second row of header for narrow and mobile view */
+    primaryColor: '#1e70bf' /* header, navigation bar and sidebar */,
+    secondaryColor:
+      '#1e70bf' /* second row of header for narrow and mobile view */,
     customHeaderColor: '#375164',
   },
   users,
   /* custom fonts for website */
   fonts: {
-    myFont: [
-      "Open Sans",
-      "Serif"
-    ]
+    myFont: ['Open Sans', 'Serif'],
   },
   // This copyright info is used in /core/Footer.js and blog rss/atom feeds.
   copyright:
-    'Copyright © ' +
+    'Copyright &copy; ' +
     new Date().getFullYear() +
     ' IBF Teaching Center, Department of Banking and Finance, University of Zurich. All rights reserved.\n' +
     'Products and Services displayed herein are trademarks or registered trademarks of their respective owners.',
-  // organizationName: 'deltice', // or set an env variable ORGANIZATION_NAME
-  // projectName: 'test-site', // or set an env variable PROJECT_NAME
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks
     theme: 'default',
@@ -56,7 +53,7 @@ const siteConfig = {
   scripts: ['https://buttons.github.io/buttons.js'],
   stylesheets: ['https://fonts.googleapis.com/css?family=Open+Sans'],
   // You may provide arbitrary config keys to be used as needed by your template.
-  repoUrl: 'https://github.com/uzh-bf/klicker-docs',
-};
+  repoUrl: 'https://github.com/uzh-bf/klicker-uzh',
+}
 
-module.exports = siteConfig;
+module.exports = siteConfig
