d5c53aab980e2ba93ee4c9317cff280726cae639
Roland Schläfli <rolandschlaefli@gmail.com>
2019-03-02 16:12:21 +0100

    Remove old navbar component

--- src/components/common/navbar/index.js
+++ src/components/common/navbar/index.js
@@ -1,4 +1,4 @@
 export { default as AccountArea } from './AccountArea'
-export { NavbarPres, default as Navbar } from './Navbar'
+export { NavbarPres, default as Navbar } from './Navbar.tsx'
 export { default as SearchArea } from './SearchArea'
 export { default as SessionArea } from './SessionArea'
