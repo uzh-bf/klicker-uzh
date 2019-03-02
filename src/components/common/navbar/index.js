0fa5bc7beb5d192699314ce9cee56a3d05145c7b
Roland Schläfli <rolandschlaefli@gmail.com>
2019-03-02 17:48:29 +0100

    Convert index for navbar components to typescript file

--- src/components/common/navbar/index.js
+++
@@ -1,4 +0,0 @@
-export { default as AccountArea } from './AccountArea.tsx'
-export { NavbarPres, default as Navbar } from './Navbar.tsx'
-export { default as SearchArea } from './SearchArea.tsx'
-export { default as SessionArea } from './SessionArea.tsx'
---
+++ src/components/common/navbar/index.ts
@@ -0,0 +1,4 @@
+export { default as AccountArea } from './AccountArea'
+export { NavbarPres, default as Navbar } from './Navbar'
+export { default as SearchArea } from './SearchArea'
+export { default as SessionArea } from './SessionArea'
