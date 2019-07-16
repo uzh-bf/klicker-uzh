a7823aa187e4bc82aaf9e6fe9c7773639a0fce50
Roland Schlaefli <rolandschlaefli@gmail.com>
2019-07-16 13:48:26 +0200

    Remove stories

--- src/components/forms/sessionCreation/QuestionDropzone.js
+++ src/components/forms/sessionCreation/QuestionDropzone.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
-import { DropTarget } from 'react-dnd'
+import { DropTarget } from 'react-dnd-cjs'
 import { Icon } from 'semantic-ui-react'
 
 const propTypes = {
