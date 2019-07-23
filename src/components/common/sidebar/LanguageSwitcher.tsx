0ca695ae1ca7159d2551b529fe4b4662ed6daec8
Roland Schlaefli <rolandschlaefli@gmail.com>
2019-07-23 11:29:30 +0200

    Refactor language switcher to use custom cookie hook

---
+++ src/components/common/sidebar/LanguageSwitcher.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Dropdown } from 'semantic-ui-react'
+
+import { useCookie } from '../../../lib/hooks'
+
+const languageOptions = [
+  {
+    flag: 'gb',
+    key: 'en',
+    text: 'English',
+    value: 'en',
+  },
+  {
+    flag: 'de',
+    key: 'de',
+    text: 'Deutsch',
+    value: 'de',
+  },
+]
+
+function LanguageSwitcher(): React.ReactElement {
+  const [locale, setLocale] = useCookie({ cookieName: 'locale', initialValue: 'en', reloadLocation: true })
+  return (
+    <div className="languageSwitcher">
+      <Dropdown
+        fluid
+        search
+        selection
+        defaultValue={locale}
+        options={languageOptions}
+        placeholder="Select Language"
+        onChange={(_, data): void => setLocale(data.value)}
+      />
+    </div>
+  )
+}
+
+export default LanguageSwitcher
