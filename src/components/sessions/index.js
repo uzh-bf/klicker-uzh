a6580406f95bf3e7e3d4f2f4ee7ab939677ddf87
Roland Schlaefli <rolandschlaefli@gmail.com>
2019-07-29 15:21:24 +0200

    TS and modal styling for cancel modal

--- src/components/sessions/index.js
+++ src/components/sessions/index.js
@@ -2,4 +2,4 @@ export { default as Session } from './Session'
 export { default as SessionList } from './SessionList'
 // eslint-disable-next-line import/no-cycle
 export { default as SessionTimeline } from './SessionTimeline'
-export { default as CancelModal } from './CancelModal'
+export { default as CancelModal } from './CancelModal.tsx'
