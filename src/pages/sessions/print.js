a2cc79f69b6955bc8c9acf0f0c13bc26bce280b8
Roland Schlaefli <rolandschlaefli@gmail.com>
2019-08-05 15:38:16 +0200

    Improve pdf print layout

--- src/pages/sessions/print.js
+++ src/pages/sessions/print.js
@@ -1,9 +1,9 @@
 /* eslint-disable */
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import _round from 'lodash/round'
 import _get from 'lodash/get'
-import { defineMessages, intlShape } from 'react-intl'
+import { intlShape, FormattedMessage } from 'react-intl'
 import { compose, withProps, withStateHandlers, branch, renderComponent, renderNothing } from 'recompose'
 import { withRouter } from 'next/router'
 import { graphql } from 'react-apollo'
@@ -11,18 +11,11 @@ import { max, min, mean, median, quantileSeq, std } from 'mathjs'
 
 import { CHART_DEFAULTS, QUESTION_GROUPS, QUESTION_TYPES, SESSION_STATUS } from '../../constants'
 import { toValueArray, pageWithIntl, withLogging } from '../../lib'
-import { Chart, Possibilities } from '../../components/evaluation'
+import { Chart, Possibilities, VisualizationType } from '../../components/evaluation'
 import { SessionEvaluationQuery, SessionEvaluationPublicQuery } from '../../graphql'
 import { sessionStatusShape, statisticsShape } from '../../propTypes'
 import { CommonLayout } from '../../components/layouts'
 import { extractInstancesFromSession } from './evaluation'
-
-const messages = defineMessages({
-  pageTitle: {
-    defaultMessage: 'PdfPrint',
-    id: 'evaluation.pageTitle',
-  },
-})
 
 const propTypes = {
   activeInstance: PropTypes.object.isRequired,
@@ -54,129 +47,155 @@ const divStyle = {
 }
 
 function Print({ activeInstances, instanceSummary, intl, isPublic, sessionId, sessionStatus }) {
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     window.print()
-  //   }, 1000)
-  // })
-
-  const activeVisualizations = CHART_DEFAULTS
-
-  return activeInstances.map(activeInstance => {
-    if (activeInstance.question.type === QUESTION_TYPES.FREE_RANGE) {
-      // convert the result data into an array with primitive numbers
-      const valueArray = toValueArray(activeInstance.results.data)
-      const hasResults = valueArray.length > 0
-
-      activeInstance.statistics = {
-        bins,
-        max: hasResults && max(valueArray),
-        mean: hasResults && mean(valueArray),
-        median: hasResults && median(valueArray),
-        min: hasResults && min(valueArray),
-        onChangeBins: e => setBins(+e.target.value),
-        q1: hasResults && quantileSeq(valueArray, 0.25),
-        q3: hasResults && quantileSeq(valueArray, 0.75),
-        sd: hasResults && std(valueArray),
-      }
-    } else {
-      activeInstance.results = {
-        ...activeInstance.results,
-        data:
-          _get(activeInstance, 'results.data') &&
-          activeInstance.results.data.map(({ correct, count, value }) => ({
-            correct,
-            count,
-            percentage: _round(100 * (count / _get(activeInstance, 'results.totalResponses')), 1),
-            value,
-          })),
-        totalResponses: _get(activeInstance, 'results.totalResponses'),
-      }
-    }
-
-    const { results, question, version, statistics } = activeInstance
-    const { description, options } = question.versions[version]
-
-    console.log(results)
-
-    return (
-      <>
-        <div className="description">{description}</div>
-        <div className="flexContainer">
-          <div className="chart">
-            <Chart
-              activeVisualization={activeVisualizations[question.type]}
-              data={results.data}
-              instanceId={activeInstance.id}
-              intl={intl}
-              isPublic={false}
-              numBins={null}
-              questionType={question.type}
-              restrictions={options.FREE_RANGE && options.FREE_RANGE.restrictions}
-              sessionId={sessionId}
-              sessionStatus={sessionStatus}
-              showGraph={true}
-              showSolution={false}
-              statistics={statistics}
-              totalResponses={results.totalResponses}
-            />
-          </div>
-          <div className="possibilities">
-            <Possibilities
-              data={results.data}
-              questionOptions={options}
-              questionType={question.type}
-              showGraph={true}
-              showSolution={true}
-            />
-            Total responses: {results.totalResponses}
-          </div>
-        </div>
-
-        <style jsx global>{`
-          html {
-            font-size: 1em;
-          }
-        `}</style>
-
-        <style jsx>{`
-          @import 'src/theme';
-
-          @page {
-            size: landscape;
-          }
-
-          .description {
-            background-color: $color-primary-background;
-            border-top: 2px solid $color-primary;
-            border-bottom: 2px solid $color-primary;
-            font-weight: bold;
-            font-size: 1.2em;
-            padding: 1em;
-            text-align: left;
-
-            page-break-after: always;
-          }
-
-          .flexContainer {
-            display: flex;
-            flex-flow: row nowrap;
-          }
-
-          .possibilities {
-            flex: 1;
-            padding: 1em;
-            border-bottom: 1px solid lightgrey;
-          }
-
-          .chart {
-            flex: 1;
-            height: 30em;
-          }
-        `}</style>
-      </>
-    )
+  useEffect(() => {
+    setTimeout(() => {
+      window.print()
+    }, 1000)
   })
+
+  const [activeVisualizations, setActiveVisualizations] = useState(CHART_DEFAULTS)
+
+  return (
+    <CommonLayout baseFontSize={20}>
+      {activeInstances.map(activeInstance => {
+        if (activeInstance.question.type === QUESTION_TYPES.FREE_RANGE) {
+          // convert the result data into an array with primitive numbers
+          const valueArray = toValueArray(activeInstance.results.data)
+          const hasResults = valueArray.length > 0
+
+          activeInstance.statistics = {
+            bins,
+            max: hasResults && max(valueArray),
+            mean: hasResults && mean(valueArray),
+            median: hasResults && median(valueArray),
+            min: hasResults && min(valueArray),
+            onChangeBins: e => setBins(+e.target.value),
+            q1: hasResults && quantileSeq(valueArray, 0.25),
+            q3: hasResults && quantileSeq(valueArray, 0.75),
+            sd: hasResults && std(valueArray),
+          }
+        } else {
+          activeInstance.results = {
+            ...activeInstance.results,
+            data:
+              _get(activeInstance, 'results.data') &&
+              activeInstance.results.data.map(({ correct, count, value }) => ({
+                correct,
+                count,
+                percentage: _round(100 * (count / _get(activeInstance, 'results.totalResponses')), 1),
+                value,
+              })),
+            totalResponses: _get(activeInstance, 'results.totalResponses'),
+          }
+        }
+
+        const { results, question, version, statistics } = activeInstance
+        const { description, options } = question.versions[version]
+        const activeVisualization = activeVisualizations[question.type]
+
+        return (
+          <>
+            <div className="description">{description}</div>
+            <div className="flexContainer">
+              <div className="chart">
+                <Chart
+                  activeVisualization={activeVisualization}
+                  data={results.data}
+                  instanceId={activeInstance.id}
+                  intl={intl}
+                  isPublic={false}
+                  numBins={null}
+                  questionType={question.type}
+                  restrictions={options.FREE_RANGE && options.FREE_RANGE.restrictions}
+                  sessionId={sessionId}
+                  sessionStatus={sessionStatus}
+                  showGraph={true}
+                  showSolution={false}
+                  statistics={statistics}
+                  totalResponses={results.totalResponses}
+                />
+              </div>
+              <div className="possibilities">
+                <Possibilities
+                  data={results.data}
+                  questionOptions={options}
+                  questionType={question.type}
+                  showGraph={true}
+                  showSolution={true}
+                />
+                <div className="totalResponses">
+                  <FormattedMessage id="evaluation.totalParticipants.label" defaultMessage="Total participants:" />
+                  {results.totalResponses}
+                </div>
+                <div className="visualizationType">
+                  <VisualizationType
+                    activeVisualization={activeVisualization}
+                    intl={intl}
+                    questionType={question.type}
+                    onChangeType={(type, newVisualization) =>
+                      setActiveVisualizations(currentState => ({ ...currentState, [type]: newVisualization }))
+                    }
+                  />
+                </div>
+              </div>
+            </div>
+          </>
+        )
+      })}
+      <style jsx global>{`
+        html {
+          font-size: 1.5em;
+        }
+      `}</style>
+
+      <style jsx>{`
+        @import 'src/theme';
+
+        @page {
+          size: landscape;
+        }
+
+        .description {
+          background-color: $color-primary-background;
+          border-top: 2px solid $color-primary;
+          border-bottom: 2px solid $color-primary;
+          font-weight: bold;
+          font-size: 1.2em;
+          padding: 1em;
+          text-align: left;
+        }
+
+        .flexContainer {
+          display: flex;
+          flex-flow: row nowrap;
+          page-break-after: always;
+          height: 100%;
+        }
+
+        .possibilities {
+          flex: 0 0 30%;
+          padding: 1em;
+        }
+
+        .chart {
+          padding: 1rem;
+          flex: 1;
+          height: 30em;
+        }
+
+        .totalResponses {
+          padding: 1rem;
+          padding-left: 0;
+        }
+
+        .visualizationType {
+          @media print {
+            display: none;
+          }
+        }
+      `}</style>
+    </CommonLayout>
+  )
 }
 
 Print.propTypes = propTypes
