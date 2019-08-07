cc727e10fb294732eb6e593e6df997497507697d
Roland Schlaefli <rolandschlaefli@gmail.com>
2019-08-07 09:42:16 +0200

    Remove Updates popup from navbar

--- frontend/src/components/common/navbar/SessionArea.tsx
+++ frontend/src/components/common/navbar/SessionArea.tsx
@@ -1,9 +1,8 @@
 /* eslint-disable react/prop-types, react/no-array-index-key */
 
 import * as React from 'react'
-import { Button, Icon, Menu, Popup, List, Modal, Embed } from 'semantic-ui-react'
-import { defineMessages, FormattedMessage, InjectedIntlProps } from 'react-intl'
-import { CHANGELOG } from '../../../constants'
+import { Button, Icon, Menu, Popup, Modal, Embed } from 'semantic-ui-react'
+import { defineMessages, FormattedMessage, IntlShape } from 'react-intl'
 
 const messages = defineMessages({
   support: {
@@ -16,7 +15,8 @@ const messages = defineMessages({
   },
 })
 
-interface Props extends InjectedIntlProps {
+interface Props {
+  intl: IntlShape
   sessionId?: string
 }
 
@@ -24,143 +24,103 @@ const defaultProps = {
   sessionId: undefined,
 }
 
-const SessionArea: React.FunctionComponent<Props> = ({ intl, sessionId }): React.ReactElement<any> => (
-  <>
-    <Menu.Item button>
-      <Button
-        icon
-        as="a"
-        color={sessionId ? 'green' : undefined}
-        disabled={!sessionId}
-        href="/sessions/running"
-        labelPosition="left"
+function SessionArea({ intl, sessionId }: Props): React.ReactElement<any> {
+  return (
+    <>
+      <Menu.Item button>
+        <Button
+          icon
+          as="a"
+          color={sessionId ? 'green' : undefined}
+          disabled={!sessionId}
+          href="/sessions/running"
+          labelPosition="left"
+        >
+          <Icon name="play" />
+          <FormattedMessage defaultMessage="Running Session" id="sessionArea.toRunningSession" />
+        </Button>
+      </Menu.Item>
+
+      <Popup
+        basic
+        hoverable
+        closeOnDocumentClick={false}
+        closeOnPortalMouseLeave={false}
+        closeOnTriggerMouseLeave={false}
+        hideOnScroll={false}
+        on="click"
+        position="bottom right"
+        trigger={<Menu.Item content={intl.formatMessage(messages.support)} icon="help" />}
       >
-        <Icon name="play" />
-        <FormattedMessage defaultMessage="Running Session" id="sessionArea.toRunningSession" />
-      </Button>
-    </Menu.Item>
-
-    <Popup
-      basic
-      hoverable
-      closeOnDocumentClick={false}
-      closeOnPortalMouseLeave={false}
-      closeOnTriggerMouseLeave={false}
-      hideOnScroll={false}
-      on="click"
-      position="bottom right"
-      trigger={<Menu.Item content={intl.formatMessage(messages.whatsNew)} icon="info" />}
-    >
-      <Popup.Content>
-        <div className="popupChanges popupContent">
-          <h3>Planned features (major)</h3>
-          <p>
-            <strong>
-              A public roadmap is available on{' '}
-              <a href="https://github.com/uzh-bf/klicker-uzh/projects/1" rel="noopener noreferrer" target="_blank">
-                GitHub
-              </a>
-              .
-            </strong>
-          </p>
-          <h3>New features (major)</h3>
-          <List bulleted>
-            {CHANGELOG.new.map(({ text, items }, index) => (
-              <List.Item key={index}>
-                {items ? <h4>{text}</h4> : text}
-                {items && (
-                  <List.List>
-                    {items.map(item => (
-                      <List.Item>{item}</List.Item>
-                    ))}
-                  </List.List>
-                )}
-              </List.Item>
-            ))}
-          </List>
-        </div>
-      </Popup.Content>
-    </Popup>
-
-    <Popup
-      basic
-      hoverable
-      closeOnDocumentClick={false}
-      closeOnPortalMouseLeave={false}
-      closeOnTriggerMouseLeave={false}
-      hideOnScroll={false}
-      on="click"
-      position="bottom right"
-      trigger={<Menu.Item content={intl.formatMessage(messages.support)} icon="help" />}
-    >
-      <Popup.Content>
-        <div className="popupHelp popupContent">
-          <h3>Support</h3>
-          <a href="mailto:klicker.support@uzh.ch">
-            <Button fluid icon labelPosition="left">
-              <Icon name="mail" />
-              klicker.support@uzh.ch
-            </Button>
-          </a>
-
-          <a href="https://uzh-bf.github.io/klicker-uzh/" rel="noopener noreferrer" target="_blank">
-            <Button fluid icon labelPosition="left">
-              <Icon name="external" />
-              Documentation
-            </Button>
-          </a>
-
-          <Modal
-            closeIcon
-            trigger={
+        <Popup.Content>
+          <div className="popupHelp popupContent">
+            <h3>Support</h3>
+            <a href="mailto:klicker.support@uzh.ch">
               <Button fluid icon labelPosition="left">
-                <Icon name="video" />
-                Introductory Video
+                <Icon name="mail" />
+                klicker.support@uzh.ch
               </Button>
-            }
-          >
-            <Modal.Header>Introductory Video</Modal.Header>
-            <Modal.Content>
-              <Embed id="Dpx7BWKeqlo" source="youtube" />
-            </Modal.Content>
-          </Modal>
-        </div>
-      </Popup.Content>
-    </Popup>
+            </a>
 
-    <style jsx>
-      {`
-        h3 {
-          font-size: 1.2rem;
-        }
+            <a href="https://uzh-bf.github.io/klicker-uzh/" rel="noopener noreferrer" target="_blank">
+              <Button fluid icon labelPosition="left">
+                <Icon name="external" />
+                Documentation
+              </Button>
+            </a>
 
-        h4 {
-          font-size: 1rem;
-          margin: 0;
-        }
+            <Modal
+              closeIcon
+              trigger={
+                <Button fluid icon labelPosition="left">
+                  <Icon name="video" />
+                  Introductory Video
+                </Button>
+              }
+            >
+              <Modal.Header>Introductory Video</Modal.Header>
+              <Modal.Content>
+                <Embed id="Dpx7BWKeqlo" source="youtube" />
+              </Modal.Content>
+            </Modal>
+          </div>
+        </Popup.Content>
+      </Popup>
 
-        .popupContent {
-          padding: 0;
-
-          :global(.button) {
-            width: 100%;
-            margin-bottom: 0.5rem;
+      <style jsx>
+        {`
+          h3 {
+            font-size: 1.2rem;
           }
-        }
 
-        .popupHelp {
-          width: 20rem;
-        }
+          h4 {
+            font-size: 1rem;
+            margin: 0;
+          }
 
-        .popupChanges {
-          overflow: auto;
-          max-height: 40rem;
-          width: 35rem;
-        }
-      `}
-    </style>
-  </>
-)
+          .popupContent {
+            padding: 0;
+
+            :global(.button) {
+              width: 100%;
+              margin-bottom: 0.5rem;
+            }
+          }
+
+          .popupHelp {
+            width: 20rem;
+          }
+
+          .popupChanges {
+            overflow: auto;
+            max-height: 40rem;
+            width: 35rem;
+          }
+        `}
+      </style>
+    </>
+  )
+}
 
 SessionArea.defaultProps = defaultProps
 
