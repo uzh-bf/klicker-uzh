21e6e4f95c58d170812e0c5c974ec62bc7fcf750
Roland Schlaefli <rolandschlaefli@gmail.com>
2019-08-28 21:53:06 +0200

    Reduce entrypoint to most necessary logic

--- src/pages/entrypoint.tsx
+++ src/pages/entrypoint.tsx
@@ -1,25 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import { useQuery } from '@apollo/react-hooks'
-import { Loader, Message } from 'semantic-ui-react'
+import { Message } from 'semantic-ui-react'
 
 import useLogging from '../lib/hooks/useLogging'
-import StaticLayout from '../components/layouts/StaticLayout'
 import CheckAccountStatusQuery from '../graphql/queries/CheckAccountStatusQuery.graphql'
 
 function Entrypoint(): React.ReactElement {
   useLogging()
 
-  const { data, loading, error } = useQuery(CheckAccountStatusQuery)
+  const router = useRouter()
 
-  if (loading) {
-    return <Loader active />
-  }
+  const { data, error } = useQuery(CheckAccountStatusQuery)
+
+  useEffect((): void => {
+    if (data && data.checkAccountStatus) {
+      router.push('/questions')
+    }
+  }, [data])
 
   if (error) {
     return <Message error>{error.message}</Message>
   }
 
-  return <StaticLayout>{data.checkAccountStatus}</StaticLayout>
+  return null
 }
 
 export default Entrypoint
