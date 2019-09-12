bc500f2f02caaa86f2fa139353bed50106d1ab5d
Roland Schläfli <rolandschlaefli@gmail.com>
2019-09-12 18:52:44 +0200

    AAI (#1039)

---
+++ src/pages/entrypoint.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import { useQuery } from '@apollo/react-hooks'
+import { Message } from 'semantic-ui-react'
+
+import useLogging from '../lib/hooks/useLogging'
+import CheckAccountStatusQuery from '../graphql/queries/CheckAccountStatusQuery.graphql'
+
+function Entrypoint(): React.ReactElement {
+  useLogging()
+
+  const router = useRouter()
+
+  const { data, error } = useQuery(CheckAccountStatusQuery)
+
+  useEffect((): void => {
+    if (data && data.checkAccountStatus) {
+      router.push('/questions')
+    }
+  }, [data])
+
+  if (error) {
+    return <Message error>{error.message}</Message>
+  }
+
+  return null
+}
+
+export default Entrypoint
