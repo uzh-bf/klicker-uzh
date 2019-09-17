b3bb94b11ff0d949929da61107c504ca3afe5738
Roland Schlaefli <rolandschlaefli@gmail.com>
2019-09-17 10:13:17 +0200

    Add user export script

---
+++ backend/src/scripts/export_users.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+
+const mongoose = require('mongoose')
+const { UserModel } = require('../models')
+
+mongoose.Promise = Promise
+
+mongoose.connect(`mongodb://${process.env.MONGO_URL}`, {
+  keepAlive: true,
+  reconnectTries: Number.MAX_VALUE,
+  reconnectInterval: 1000,
+})
+
+mongoose.connection
+  .once('open', async () => {
+    const users = await UserModel.find({})
+    users.map(user => console.log(`${user.email};${user.institution};${user.lastLoginAt};${user.createdAt}`))
+    mongoose.connection.close()
+  })
+
+  .on('error', error => {
+    console.warn('> Warning: ', error)
+  })
