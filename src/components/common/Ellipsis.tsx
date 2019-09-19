c23de019d5ca771ab505785eda55f8fb7ddec0b9
Roland Schlaefli <rolandschlaefli@gmail.com>
2019-09-19 16:32:26 +0200

    Improve line-breaking of long items in evaluation list

--- src/components/common/Ellipsis.tsx
+++ src/components/common/Ellipsis.tsx
@@ -1,19 +1,31 @@
 import React from 'react'
+import { Popup, Icon } from 'semantic-ui-react'
 
 interface Props {
   children: string
   maxLength?: number
+  withPopup?: boolean
 }
 
-function Ellipsis({ children, maxLength }: Props): React.ReactElement {
+function Ellipsis({ children, maxLength, withPopup }: Props): React.ReactElement {
   if (children.length <= maxLength) {
     return <span title={children}>{children}</span>
   }
-  return <span title={children}>{children.substr(0, maxLength)} ...</span>
+  return (
+    <span title={children}>
+      {children.substr(0, maxLength)}{' '}
+      {withPopup ? (
+        <Popup content={children} trigger={<Icon name="ellipsis horizontal" size="small" />} wide="very" />
+      ) : (
+        <Icon name="ellipsis horizontal" size="small" />
+      )}
+    </span>
+  )
 }
 
 Ellipsis.defaultProps = {
   maxLength: 60,
+  withPopup: false,
 }
 
 export default Ellipsis
