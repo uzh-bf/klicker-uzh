811e9608e3e06d34b151cc4f45ea812c8b6b8ee6
Roland Schlaefli <rolandschlaefli@gmail.com>
2020-05-02 21:38:58 +0200

    Implement focus lock on question creation and edit form

---
+++ src/components/common/ForwardRefAs.tsx
@@ -0,0 +1,9 @@
+import React, { Ref } from 'react'
+
+interface Props {
+  forwardedRef: Ref<any>
+}
+
+export default (As: any) => ({ forwardedRef, ...props }: Props): React.ReactElement => (
+  <As ref={forwardedRef} {...props} />
+)
