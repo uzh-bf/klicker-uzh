921d15e49a60c693d08edb7edbdb038c58c3b7dd
Roland Schläfli <rolandschlaefli@gmail.com>
2020-09-08 14:18:46 +0200

    Fix: Issue with Focus on Question Creation, Initial Dependency Upgrades (#1634)

--- src/components/questions/ActionBar.tsx
+++ src/components/questions/ActionBar.tsx
@@ -71,9 +71,12 @@ function ActionBar({
   questions,
 }: Props): React.ReactElement {
   const intl = useIntl()
+
   const { addToast } = useToasts()
+
   const [csvData, setCsvData] = useState([])
   const [allItemsChecked, setAllItemsChecked] = useState(false)
+  const [isAnyModalOpen, setIsAnyModalOpen] = useState(false)
 
   const [getQuestionStatistics, { data, error }] = useMutation(QuestionStatisticsMutation)
   const [exportQuestions, { data: exportData, error: exportError }] = useMutation(ExportQuestionsMutation)
@@ -191,6 +194,7 @@ function ActionBar({
           labeled
           className="primary icon"
           direction="left"
+          disabled={isAnyModalOpen}
           icon="plus square"
           text={intl.formatMessage(messages.create)}
         >
@@ -199,7 +203,7 @@ function ActionBar({
               <Icon name="play" />
               <FormattedMessage defaultMessage="New Session" id="questionPool.button.createSession" />
             </Dropdown.Item>
-            <QuestionCreationModal>
+            <QuestionCreationModal handleModalOpenChange={setIsAnyModalOpen}>
               {({ setIsModalOpen }): any => (
                 <Dropdown.Item onClick={(): void => setIsModalOpen(true)}>
                   <Icon name="question circle" />
