1070d046db1b1b3606e441d1b18c06a977bd76e5
lulocher <luca.locher@bf.uzh.ch>
2020-09-21 15:43:10 +0200

    finished new table for the user management in the admin area

--- src/components/common/CustomizableTable.tsx
+++ src/components/common/CustomizableTable.tsx
@@ -4,116 +4,139 @@ import { Button, Table, Confirm } from 'semantic-ui-react'
 import EditTableRowForm from '../forms/EditTableRowForm'
 
 interface Props {
-  columns: { 
-    title: string, // labels column
-    attributeName: string, // used to identify the correct attribute of the data object
-    width?: any, // describes the width of a column (optional)
+  columns: {
+    title: string // labels column
+    attributeName: string // used to identify the correct attribute of the data object
+    width?: any // describes the width of a column (optional)
     isEditable?: boolean // true if attribute is editable, false otherwise (must only be given if hasModification is true)
-  } [],
-  data: any [], 
-  deletionConfirmation?: boolean,
-  editConfirmation?: boolean,
-  hasDeletion?: boolean,
-  hasModification?: boolean,
-  handleDeletion?: (id: string, confirm: boolean) => Promise<void>,
-  handleModification?: (id: string, values : any, confirm: boolean) => Promise<void>,
+  }[]
+  data: any[]
+  deletionConfirmation?: boolean
+  editConfirmation?: boolean
+  hasDeletion?: boolean
+  hasModification?: boolean
+  handleDeletion?: (id: string, confirm: boolean) => Promise<void>
+  handleModification?: (id: string, values: any, confirm: boolean) => Promise<void>
 }
 
 const defaultProps = {
-
   deletionConfirmation: false,
   hasDeletion: false,
   hasModification: false,
 }
 
 function CustomizableTable({
-  columns, 
-  data, 
-  deletionConfirmation, 
-  editConfirmation, 
-  hasDeletion, 
-  hasModification, 
-  handleDeletion, 
-  handleModification
-} : Props ) : React.ReactElement {
+  columns,
+  data,
+  deletionConfirmation,
+  editConfirmation,
+  hasDeletion,
+  hasModification,
+  handleDeletion,
+  handleModification,
+}: Props): React.ReactElement {
+  const [sortBy, setSortBy] = useState(columns[0].attributeName)
+  const [sortDirection, setSortDirection]: any = useState('descending')
+  const [activeId, setActiveId] = useState(undefined)
+  const [editableRow, setEditableRow] = useState(undefined)
 
-    const [sortBy, setSortBy] = useState(columns[0].attributeName)
-    const [sortDirection, setSortDirection] : any = useState('descending')
-    const [activeId, setActiveId] = useState(undefined)
-    const [editableRow, setEditableRow] = useState(undefined)
+  const sortedData = sortDirection === 'ascending' ? _sortBy(data, sortBy) : _sortBy(data, sortBy).reverse()
 
-    const sortedData = sortDirection === 'ascending' ? _sortBy(data, sortBy) : _sortBy(data, sortBy).reverse()
+  const onSort = (clickedColumn: string): Function => (): void => {
+    // if the same column as previously active is clicked, reverse the sort direction
+    if (sortBy === clickedColumn) {
+      setSortDirection(sortDirection === 'ascending' ? 'descending' : 'ascending')
+    }
 
-    const onSort = (clickedColumn: string): Function => (): void => {
-        // if the same column as previously active is clicked, reverse the sort direction
-        if (sortBy === clickedColumn) {
-          setSortDirection(sortDirection === 'ascending' ? 'descending' : 'ascending')
-        }
-    
-        // otherwise update the property we sort by
-        setSortBy(clickedColumn)
-      }
+    // otherwise update the property we sort by
+    setSortBy(clickedColumn)
+  }
 
-      return (
-        <div className="tableChart">
-            <Table sortable striped>
-              <Table.Header>
-                {
-                  columns.map((column) : React.ReactElement => (
-                      <Table.HeaderCell sorted={sortBy === column.attributeName ? sortDirection : null} width={column.width} onClick={onSort(column.attributeName)}  >
-                        {column.title}
-                      </Table.HeaderCell>                 
-                  ))
-                  }
-              </Table.Header>
-              <Table.Body>
-                {
-                  sortedData.map(( object, index ) : React.ReactElement => (
-                    editableRow !== index && (
-                      <Table.Row className="displayRow">
-                          {columns.map(( column ): React.ReactElement => (
-                            <Table.Cell>
-                              {object[column.attributeName]}
-                            </Table.Cell>
-                          ))}
-                          {(hasModification || hasDeletion) && (
-                            <Table.Cell textAlign="right">
-                              {hasModification && <Button icon="edit" onClick={() : void => {
-                                setEditableRow(index)
-                              }}/>}
-                              {hasDeletion && <Button icon="trash" onClick={() : void => {
-                                handleDeletion(object.id, false)
-                                setActiveId(object.id)
-                                }}/>}
-                            </Table.Cell>)}
-                        </Table.Row>) || (
-                        <Table.Row className="editRow">
-                          <EditTableRowForm
-                            columns={columns}
-                            data={object}
-                            editConfirmation={editConfirmation}
-                            handleModification={handleModification}
-                            onDiscard={() : void => setEditableRow(undefined)}
-                            onSuccessfulModification={() : void => {
-                              setEditableRow(undefined)
-                            }} 
+  return (
+    <div className="tableChart">
+      <Table sortable striped>
+        <Table.Header>
+          {columns.map(
+            (column): React.ReactElement => (
+              <Table.HeaderCell
+                sorted={sortBy === column.attributeName ? sortDirection : null}
+                width={column.width}
+                onClick={onSort(column.attributeName)}
+              >
+                {column.title}
+              </Table.HeaderCell>
+            )
+          )}
+        </Table.Header>
+        <Table.Body>
+          {sortedData.map(
+            (object, index): React.ReactElement =>
+              (editableRow !== index && (
+                <Table.Row className="displayRow">
+                  {columns.map(
+                    (column): React.ReactElement => (
+                      <Table.Cell>{object[column.attributeName]}</Table.Cell>
+                    )
+                  )}
+                  {(hasModification || hasDeletion) && (
+                    <Table.Cell textAlign="right">
+                      <div className="buttonArea">
+                        {hasModification && (
+                          <Button
+                            icon="edit"
+                            onClick={(): void => {
+                              setEditableRow(index)
+                            }}
                           />
-                        </Table.Row>
-                      )
-                  ))
-                }
-              </Table.Body>
-            </Table>
-            <Confirm 
-              cancelButton={'Go Back'}
-              confirmButton={'Delete User'}
-              content={`Are you sure that you want to delete the user ${activeId}?`}
-              open={deletionConfirmation}
-              onCancel={() : Promise<void> => handleDeletion(activeId, false)}
-              onConfirm={() : Promise<void>  => handleDeletion(activeId, true)}
-            />
-        </div>
-      )
+                        )}
+                        {hasDeletion && (
+                          <Button
+                            icon="trash"
+                            onClick={(): void => {
+                              handleDeletion(object.id, false)
+                              setActiveId(object.id)
+                            }}
+                          />
+                        )}
+                      </div>
+                    </Table.Cell>
+                  )}
+                </Table.Row>
+              )) || (
+                <Table.Row className="editRow">
+                  <EditTableRowForm
+                    columns={columns}
+                    data={object}
+                    editConfirmation={editConfirmation}
+                    handleModification={handleModification}
+                    onDiscard={(): void => setEditableRow(undefined)}
+                    onSuccessfulModification={(): void => {
+                      setEditableRow(undefined)
+                    }}
+                  />
+                </Table.Row>
+              )
+          )}
+        </Table.Body>
+      </Table>
+      <Confirm
+        cancelButton={'Go Back'}
+        confirmButton={'Delete User'}
+        content={`Are you sure that you want to delete the user ${activeId}?`}
+        open={deletionConfirmation}
+        onCancel={(): Promise<void> => handleDeletion(activeId, false)}
+        onConfirm={(): Promise<void> => handleDeletion(activeId, true)}
+      />
+      <style jsx>
+        {`
+          .buttonArea {
+            display: flex;
+            flex-direction: row;
+          }
+        `}
+      </style>
+    </div>
+  )
 }
 
 CustomizableTable.defaultProps = defaultProps
--- src/components/forms/EditTableRowForm.tsx
+++ src/components/forms/EditTableRowForm.tsx
@@ -1,153 +1,175 @@
-import React from 'react'
-import { FormattedMessage } from 'react-intl'
+import React, { useState } from 'react'
 import { Formik } from 'formik'
 import { object } from 'yup'
-import {  Button, Confirm, Table } from 'semantic-ui-react'
-
+import { Button, Confirm, Table, Dropdown } from 'semantic-ui-react'
+import { ROLES } from '../../constants'
 import FormikInput from './components/FormikInput'
 import validationSchema from './common/validationSchema'
 
-function ConfirmationContent({ columns, initialValues , values } : {columns : any, initialValues : any, values : any}) : React.ReactElement {
-    return (
-      <p className="content">
-        <h3>The following changes will be made: {'\n'}</h3>
-        {columns.map((column) => (
-            initialValues[column.attributeName] !== values[column.attributeName] ? <p>{'\n'}{column.title}: {initialValues[column.attributeName]} -&gt; {values[column.attributeName]}</p> : ''
-        ))}
-    <style jsx>
+function ConfirmationContent({
+  columns,
+  initialValues,
+  values,
+}: {
+  columns: any
+  initialValues: any
+  values: any
+}): React.ReactElement {
+  return (
+    <p className="content">
+      <h3>The following changes will be made: {'\n'}</h3>
+      {columns.map((column) =>
+        initialValues[column.attributeName] !== values[column.attributeName] ? (
+          <p>
+            {'\n'}
+            {column.title}: {initialValues[column.attributeName]} -&gt; {values[column.attributeName]}
+          </p>
+        ) : (
+          ''
+        )
+      )}
+      <style jsx>
         {`
-            @import 'src/theme';
-            .content{ padding: 1rem;}
+          @import 'src/theme';
+          .content {
+            padding: 1rem;
+          }
         `}
-    </style>
+      </style>
     </p>
-    )}
+  )
+}
 
 interface Props {
-    data: any,
-    columns: any [],
-    editConfirmation: boolean,
-    handleModification: (id : string, values : any, confirm : boolean) => Promise<void>
-    onDiscard: () => void
-    onSuccessfulModification: () => void
+  data: any
+  columns: any[]
+  editConfirmation: boolean
+  handleModification: (id: string, values: any, confirm: boolean) => Promise<void>
+  onDiscard: () => void
+  onSuccessfulModification: () => void
 }
-    
+
 function EditTableRowForm({
+  data,
+  columns,
+  editConfirmation,
+  handleModification,
+  onDiscard,
+  onSuccessfulModification,
+}: Props): React.ReactElement {
+  const [activeRole, setActiveRole] = useState(data.role)
 
-    data,
-    columns,
-    editConfirmation,
-    handleModification,
-    onDiscard,
-    onSuccessfulModification,
-} : Props) : React.ReactElement {
+  const initialValues = {
+    role: undefined,
+  }
+  columns.forEach((column) => {
+    initialValues[column.attributeName] = data[column.attributeName]
+  })
 
-    const initialValues = {}
-    columns.forEach((column) => {
-        initialValues[column.attributeName] = data[column.attributeName]
-    })
+  return (
+    <Formik
+      initialValues={initialValues}
+      render={({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        isValid,
+        dirty,
+        setFieldValue,
+      }): React.ReactElement => (
+        <>
+          {columns.map(
+            (column): React.ReactElement =>
+              (column.isEditable && column.attributeName !== 'role' && (
+                <Table.Cell verticalAlign={'top'} width={column.width}>
+                  <FormikInput
+                    required
+                    error={errors[column.attributeName]}
+                    errorMessage={`Please provide a valid ${column.title}`}
+                    handleBlur={handleBlur}
+                    handleChange={handleChange}
+                    name={column.attributeName}
+                    touched={touched[column.attributeName]}
+                    type={column.attributeName}
+                    value={values[column.attributeName]}
+                  />
+                </Table.Cell>
+              )) ||
+              (column.isEditable &&
+              column.attributeName === 'role' && ( // Only needed when there is the option to modify via a dropdown, could be generalized
+                  <Table.Cell verticalAlign={'top'} width={column.width}>
+                    <Dropdown
+                      compact
+                      selection
+                      options={[
+                        { text: 'User', value: ROLES.USER },
+                        { text: 'Admin', value: ROLES.ADMIN },
+                      ]}
+                      placeholder={activeRole}
+                      value={activeRole}
+                      onChange={(_, { value }): void => {
+                        setActiveRole(value)
+                        setFieldValue('role', value)
+                      }}
+                    />
+                  </Table.Cell>
+                )) || (
+                <Table.Cell verticalAlign={'middle'} width={column.width}>
+                  {values[column.attributeName]}
+                </Table.Cell>
+              )
+          )}
+          <Table.Cell textAlign="right">
+            <div className="buttonCell">
+              <Button
+                primary
+                className="save"
+                disabled={!isValid || (!dirty && (!initialValues.role || initialValues.role === activeRole))}
+                icon="save"
+                onClick={(): void => {
+                  handleModification(data.id, values, false)
+                }}
+              />
+              <Button className="discard" icon="undo" type="button" onClick={onDiscard} />
+            </div>
+          </Table.Cell>
 
-    return (
-       <Formik
-            initialValues={initialValues}
-            render={({
-                values,
-                errors,
-                touched,
-                handleChange,
-                handleBlur,
-                isValid,
-                dirty,
-            }) : React.ReactElement => (
-                <>
-                    {columns.map((column) : React.ReactElement => (
-                        (column.isEditable && (
-                        <Table.Cell verticalAlign={'top'} width={column.width}>
-                            <FormikInput 
-                                required
-                                error={errors[column.attributeName]}
-                                errorMessage={`Please provide a valid ${column.title}`}
-                                handleBlur={handleBlur}
-                                handleChange={handleChange}
-                                name={column.attributeName}
-                                touched={touched[column.attributeName]}
-                                type={column.attributeName}
-                                value={values[column.attributeName]}
-                            /> 
-                        </Table.Cell> )) || (
-                            <Table.Cell verticalAlign={'middle'} width={column.width}>
-                                {values[column.attributeName]}
-                            </Table.Cell>
-                        ) 
-                    ))}
-                        <Table.Cell textAlign="right">
-                            <div className="buttonCell">
-                                <div className="saveButton">
-                                    <Button
-                                        primary
-                                        className="save"
-                                        disabled={ !isValid || !dirty}
-                                        onClick={() : void => {
-                                            handleModification(data.id, values, false)
-                                        }}
-                                    >
-                                        <FormattedMessage defaultMessage="Save" id="form.button.save" />
-                                    </Button>
-                                </div>
-                                <div className="saveButton">
-                                    <Button 
-                                        className="discard"  
-                                        type="button"
-                                        onClick={onDiscard} 
-                                    ><FormattedMessage defaultMessage="Discard" id="form.button.discard" />
-                                    </Button>
-                                </div>
-                            </div>
-                        </Table.Cell>
-                        
-                        <Confirm 
-                            cancelButton={'Go Back'}
-                            confirmButton={'Modify User'}
-                            content={() : React.ReactElement => {
-                            return <ConfirmationContent columns={columns} initialValues={initialValues} values={values}/>}
-                        }
-                            open={editConfirmation}
-                            onCancel={() : Promise<void> => handleModification(data.id, values, false)}
-                            onConfirm={() : void  => {
-                                handleModification(data.id, values, true)
-                                onSuccessfulModification()
-                            }}
-                        />
-                        <style jsx>
-                            {`
-                                @import 'src/theme';
+          <Confirm
+            cancelButton={'Go Back'}
+            confirmButton={'Modify User'}
+            content={(): React.ReactElement => {
+              return <ConfirmationContent columns={columns} initialValues={initialValues} values={values} />
+            }}
+            open={editConfirmation}
+            onCancel={(): Promise<void> => handleModification(data.id, values, false)}
+            onConfirm={(): void => {
+              handleModification(data.id, values, true)
+              onSuccessfulModification()
+            }}
+          />
+          <style jsx>
+            {`
+              @import 'src/theme';
 
-                                :global(.ui.input > input ){
-                                  width: 100%;
-                                }
-                                .buttonCell{
-                                    display: flex; 
-                                    flex-direction: row;   
-                                    .saveButton {
-                                        width: 50%;
-                                    } 
-                                }
-                            `}
-                        </style>
-                    </>
-            )
-        }
-        validationSchema={object()
-            .shape({
-                ...validationSchema
-            })}
-        onSubmit={() : Promise<void> => handleModification(data.id, undefined, false)}
-
-       />
-    )
+              :global(.ui.input > input) {
+                width: 100%;
+              }
+              .buttonCell {
+                display: flex;
+                flex-direction: row;
+              }
+            `}
+          </style>
+        </>
+      )}
+      validationSchema={object().shape({
+        ...validationSchema,
+      })}
+      onSubmit={(): Promise<void> => handleModification(data.id, undefined, false)}
+    />
+  )
 }
 
 export default EditTableRowForm
-
-
-
