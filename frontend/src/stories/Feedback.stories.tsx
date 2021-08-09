7a2e17bcd7ec076027b1bbf389efc3318cb86311
Roland Schläfli <rolandschlaefli@gmail.com>
2021-08-09 16:21:19 +0200

    deps: install storybook (#2519)
    Co-authored-by: maximilian.weber <maximilian.weber@bf.uzh.ch>

---
+++ frontend/src/stories/Feedback.stories.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import Feedback from '../components/feedbacks/Feedback'
+
+export default {
+  title: 'Example/Feedback',
+  component: Feedback,
+} as ComponentMeta<typeof Feedback>
+
+const Template: ComponentStory<typeof Feedback> = (args) => <Feedback {...args} />
+
+export const Primary = Template.bind({})
+
+Primary.args = {
+  votes: 2,
+  content: 'Feedback',
+}
