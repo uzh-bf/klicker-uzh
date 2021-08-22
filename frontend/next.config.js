e5b837c670c0f3ce25069ea6396f7251c79b4456
Roland Schlaefli <rolandschlaefli@gmail.com>
2021-08-22 16:55:35 +0200

    chore: simplify next and babel configs

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,10 @@
 /* eslint-disable import/no-extraneous-dependencies, no-param-reassign */
 
-const webpack = require('webpack')
-const withCSS = require('@zeit/next-css')
-const withSourceMaps = require('@zeit/next-source-maps')()
-const { DEVELOPMENT_SERVER, PHASE_PRODUCTION_BUILD } = require('next/constants')
-const SentryWebpackPlugin = require('@sentry/webpack-plugin')
+// const webpack = require('webpack')
+// const withCSS = require('@zeit/next-css')
+// const withSourceMaps = require('@zeit/next-source-maps')()
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
+// const SentryWebpackPlugin = require('@sentry/webpack-plugin')
 
 const CFG = require('./src/klicker.conf.js')
 
@@ -50,7 +50,7 @@ module.exports = (phase) => {
       }
 
       // ignore test files when bundling
-      webpackConfig.plugins.push(new webpack.IgnorePlugin(/src\/pages.*\/test.*/))
+      // webpackConfig.plugins.push(new webpack.IgnorePlugin(/src\/pages.*\/test.*/))
 
       // push graphql loaders into the webpack config
       webpackConfig.module.rules.push({
@@ -67,27 +67,27 @@ module.exports = (phase) => {
       })
 
       // push url-loader to enable loading fonts
-      webpackConfig.module.rules.push({
-        test: /\.(jpe?g|png|svg|gif|eot|ttf|woff|woff2)$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              fallback: 'file-loader',
-              esModule: false,
-              limit: 100000,
-              name: '[name]-[hash].[ext]',
-              outputPath: `${isServer ? '../' : ''}static/images/`,
-              publicPath: '/_next/static/images/',
-            },
-          },
-        ],
-      })
+      // webpackConfig.module.rules.push({
+      //   test: /\.(jpe?g|png|svg|gif|eot|ttf|woff|woff2)$/,
+      //   use: [
+      //     {
+      //       loader: 'url-loader',
+      //       options: {
+      //         fallback: 'file-loader',
+      //         esModule: false,
+      //         limit: 100000,
+      //         name: '[name]-[hash].[ext]',
+      //         outputPath: `${isServer ? '../' : ''}static/images/`,
+      //         publicPath: '/_next/static/images/',
+      //       },
+      //     },
+      //   ],
+      // })
 
-      webpackConfig.module.rules.push({
-        test: /\.mjs$/,
-        type: 'javascript/auto',
-      })
+      // webpackConfig.module.rules.push({
+      //   test: /\.mjs$/,
+      //   type: 'javascript/auto',
+      // })
 
       if (
         SERVICES_CFG.sentry.dsn &&
@@ -106,50 +106,50 @@ module.exports = (phase) => {
           })
         )
 
-      webpackConfig.resolve.extensions = ['.ts', '.tsx', '.mjs', '.js']
+      // webpackConfig.resolve.extensions = ['.ts', '.tsx', '.mjs', '.js']
 
       return webpackConfig
     },
   }
 
   // development only configuration
-  if (phase === DEVELOPMENT_SERVER) {
-    // activate polling to make watching work inside Docker on Windows
-    config.webpackDevMiddleware = (middlewareConfig) => ({
-      ...middlewareConfig,
-      watchOptions: {
-        poll: 1000,
-      },
-    })
-  }
+  // if (phase === DEVELOPMENT_SERVER) {
+  //   // activate polling to make watching work inside Docker on Windows
+  //   config.webpackDevMiddleware = (middlewareConfig) => ({
+  //     ...middlewareConfig,
+  //     watchOptions: {
+  //       poll: 1000,
+  //     },
+  //   })
+  // }
 
   // enable next-css plugin
   // allows importing css files
-  config = withCSS(config)
+  // config = withCSS(config)
 
   // enable sourcemaps
-  config = withSourceMaps(config)
+  // config = withSourceMaps(config)
 
   // build only configuration
-  if (phase === PHASE_PRODUCTION_BUILD) {
-    // setup the bundle analyzer plugin
-    const withBundleAnalyzer = require('@zeit/next-bundle-analyzer')
-    config = withBundleAnalyzer({
-      analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
-      analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
-      bundleAnalyzerConfig: {
-        browser: {
-          analyzerMode: 'static',
-          reportFilename: 'bundles/client.html',
-        },
-        server: {
-          analyzerMode: 'static',
-          reportFilename: '../bundles/server.html',
-        },
-      },
-      ...config,
-    })
-  }
+  // if (phase === PHASE_PRODUCTION_BUILD) {
+  // setup the bundle analyzer plugin
+  // const withBundleAnalyzer = require('@zeit/next-bundle-analyzer')
+  // config = withBundleAnalyzer({
+  //   analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
+  //   analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
+  //   bundleAnalyzerConfig: {
+  //     browser: {
+  //       analyzerMode: 'static',
+  //       reportFilename: 'bundles/client.html',
+  //     },
+  //     server: {
+  //       analyzerMode: 'static',
+  //       reportFilename: '../bundles/server.html',
+  //     },
+  //   },
+  //   ...config,
+  // })
+  // }
 
   return config
 }
