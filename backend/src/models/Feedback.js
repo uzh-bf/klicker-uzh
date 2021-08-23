f4c9e30bb2c114c68e41487196e0a2caca5f1c6a
Roland Schlaefli <rolandschlaefli@gmail.com>
2021-08-23 21:30:09 +0200

    db: add and extend models for pinned, resolved, and responses

--- backend/src/models/Feedback.js
+++ backend/src/models/Feedback.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose')
 
+const FeedbackResponse = require('./FeedbackResponse')
+
 module.exports = new mongoose.Schema(
   {
+    pinned: { type: Boolean, default: false },
+    resolved: { type: Boolean, default: false },
     content: { type: String, required: true },
     votes: { type: Number, default: 0, min: 0 },
+    responses: [{ type: FeedbackResponse }],
   },
   { timestamps: true }
 )
