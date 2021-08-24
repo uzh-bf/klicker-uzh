9914900b61c0a5991b16cd1b5dbe9640e3f4ebd9
Roland Schlaefli <rolandschlaefli@gmail.com>
2021-08-24 22:28:59 +0200

    wip: initial approaches to sorting and filtering in q&a

--- frontend/src/components/interaction/feedbacks/FeedbackChannel.tsx
+++ frontend/src/components/interaction/feedbacks/FeedbackChannel.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react'
-import { Checkbox, Dropdown } from 'semantic-ui-react'
+import { Checkbox, Dropdown, Input } from 'semantic-ui-react'
 import { defineMessages, FormattedMessage, useIntl } from 'react-intl'
+import _sortBy from 'lodash/sortBy'
+import * as JsSearch from 'js-search'
 
 import Feedback from './Feedback'
 
@@ -46,26 +48,43 @@ function FeedbackChannel({
   handleRespondToFeedback,
   subscribeToMore,
 }: Props) {
-  const [showResolved, setShowResolved] = useState(false)
-  const [showOpen, setShowOpen] = useState(true)
-  const [sortBy, setSortBy] = useState('upvotes')
-
   useEffect((): void => {
     if (subscribeToMore) {
       subscribeToMore()
     }
   }, [])
 
+  const [searchString, setSearchString] = useState('')
+  const [showResolved, setShowResolved] = useState(false)
+  const [showOpen, setShowOpen] = useState(true)
+  const [showUnpinned, setShowUnpinned] = useState(true)
+  const [sortBy, setSortBy] = useState('upvotes')
+
+  const search = new JsSearch.Search('content')
+  search.searchIndex = new JsSearch.TfIdfSearchIndex()
+  search.indexStrategy = new JsSearch.AllSubstringsIndexStrategy()
+  search.addDocuments(feedbacks)
+
+  const sortedFeedbacks = _sortBy(
+    feedbacks.filter((item) => {
+      if (!showResolved && item.resolved) return false
+      if (!showOpen && !item.resolved) return false
+      if (!showUnpinned && !item.pinned) return false
+      return true
+    }),
+    [sortBy]
+  )
+
   const intl = useIntl()
 
   return (
     <div>
-      <div>
+      {/* <div>
         <FormattedMessage
           defaultMessage="The Feedback-Channel allows you to get open feedback from your participants. If you publish the feedbacks, your participants will be able to see what other participants have already posted."
           id="runningSession.feedbackChannel.info"
         />
-      </div>
+      </div> */}
 
       {/* <div className="flex flex-col mt-4 md:flex-row">
         <div>
@@ -91,6 +110,7 @@ function FeedbackChannel({
 
       <div className="flex flex-row items-end justify-between mt-4">
         <div>
+          <Input placeholder="Search..." value={searchString} onChange={(_, { data }) => setSearchString(data)} />
           <Checkbox checked={showResolved} label="Resolved" onChange={() => setShowResolved((current) => !current)} />
           <Checkbox
             checked={showOpen}
@@ -98,11 +118,17 @@ function FeedbackChannel({
             label="Open"
             onChange={() => setShowOpen((current) => !current)}
           />
+          <Checkbox
+            checked={showUnpinned}
+            className="ml-4"
+            label="Unpinned"
+            onChange={() => setShowUnpinned((current) => !current)}
+          />
         </div>
 
         <Dropdown
           selection
-          disabled={feedbacks?.length === 0}
+          disabled={sortedFeedbacks?.length === 0}
           options={[
             { text: 'Sort by Recency', value: 'recency' },
             { text: 'Sort by Upvotes', value: 'upvotes' },
