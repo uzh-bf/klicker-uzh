78648aa326e6f9256c9fe481dad335592d33b88f
Roland Schlaefli <rolandschlaefli@gmail.com>
2021-08-24 22:27:43 +0200

    enhance: integrate the resolved status on the join page

--- frontend/src/components/sessions/join/PublicFeedback.tsx
+++ frontend/src/components/sessions/join/PublicFeedback.tsx
@@ -11,23 +11,24 @@ interface Props {
   createdAt: string
   content: string
   votes: number
+  resolved: boolean
   pinned: boolean
   responses: IFeedbackResponse[]
 }
 
-function PublicFeedback({ content, createdAt, votes, pinned, responses }: Props) {
+function PublicFeedback({ content, createdAt, votes, pinned, resolved, responses }: Props) {
   return (
-    <div className="border border-solid bg-primary-10% border-primary">
+    <div className="border border-solid  border-primary bg-primary-10%">
       <div className="flex items-center ">
         <div className="flex-1 p-2">
-          <p className="mb-0">{content}</p>
+          <p className="mb-0 prose">{content}</p>
           <div className="flex flex-row items-center mt-2 text-gray-500">
             <div className="text-sm text-gray-500">{dayjs(createdAt).format('DD.MM.YYYY HH:mm')}</div>
             <div className="ml-4">{pinned && <Icon name="pin" size="small" />}</div>
           </div>
         </div>
         <div className="flex flex-col items-end justify-between flex-initial p-2">
-          <Button basic className="text-xl text-gray-500 !p-4 !mr-0">
+          <Button basic className="text-xl text-gray-500 !p-4 !mr-0" disabled={true || resolved}>
             {votes}
             <span className="ml-2">
               <Icon name="thumbs up outline" />
@@ -39,7 +40,7 @@ function PublicFeedback({ content, createdAt, votes, pinned, responses }: Props)
         <div>
           {responses.map((response) => (
             <div className="p-2 bg-white" key={response.id}>
-              <div>{response.content}</div>
+              <div className="prose">{response.content}</div>
               <div className="mt-2 text-sm text-gray-500">{dayjs(response.createdAt).format('DD.MM.YYYY HH:mm')}</div>
             </div>
           ))}
