16e125a1b690d0abd54e5c345ddbf3ffb5521e53
Roland Schlaefli <rolandschlaefli@gmail.com>
2021-08-29 18:11:02 +0200

    fix: include semantic-ui library in purgecss config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  purge: ['./src/**/*.{js,ts,jsx,tsx}'],
+  purge: [
+    './src/**/*.{js,ts,jsx,tsx}',
+    './src/lib/semantic/dist/**/*.css',
+    './node_modules/semantic-ui-react/dist/**/*.js',
+  ],
   mode: 'jit',
   darkMode: false, // or 'media' or 'class'
   theme: {
