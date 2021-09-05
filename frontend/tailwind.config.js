b039f6ff1b014ca608fda294c9c15bbe7da13f5f
Roland Schlaefli <rolandschlaefli@gmail.com>
2021-09-05 16:23:05 +0200

    chore: small upgrades and improvements

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,5 @@
 module.exports = {
-  purge: [
-    './src/**/*.{js,ts,jsx,tsx}',
-    './src/lib/semantic/dist/**/*.css',
-    './node_modules/semantic-ui-react/dist/**/*.js',
-  ],
+  purge: ['./src/**/*.{js,ts,jsx,tsx}', './node_modules/semantic-ui-react/dist/**/*.js'],
   mode: 'jit',
   darkMode: false, // or 'media' or 'class'
   theme: {
