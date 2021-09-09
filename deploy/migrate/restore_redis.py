338a6eb2f5b8ae8e3986c3a2512d315536d0a716
Roland Schlaefli <rolandschlaefli@gmail.com>
2021-09-09 17:37:24 +0200

    deploy: more work on script

--- deploy/migrate/restore_redis.py
+++ deploy/migrate/restore_redis.py
@@ -1,12 +1,15 @@
 import os
 import redisdl
+import json
 from dotenv import load_dotenv
 
 load_dotenv()
 
-with open("dump.json") as f:
-    redisdl.load(
-        f,
+with open("dump.json", "r") as f:
+    data = f.read().replace("\n", "")
+
+    redisdl.loads(
+        data,
         host=os.getenv("REDIS_TARGET_HOSTNAME", "localhost"),
         port=os.getenv("REDIS_TARGET_PORT", 6380),
         password=os.getenv("REDIS_TARGET_PASSWORD"),
