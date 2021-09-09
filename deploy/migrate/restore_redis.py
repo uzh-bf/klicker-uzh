493e7ab5fa206690ea4f1d1a64d52978143bfa2f
Roland Schlaefli <rolandschlaefli@gmail.com>
2021-09-09 15:03:52 +0200

    deploy: update migration scripts

---
+++ deploy/migrate/restore_redis.py
@@ -0,0 +1,14 @@
+import os
+import redisdl
+from dotenv import load_dotenv
+
+load_dotenv()
+
+with open("dump.json") as f:
+    redisdl.load(
+        f,
+        host=os.getenv("REDIS_TARGET_HOSTNAME", "localhost"),
+        port=os.getenv("REDIS_TARGET_PORT", 6380),
+        password=os.getenv("REDIS_TARGET_PASSWORD"),
+        db=os.getenv("REDIS_TARGET_DB", 0),
+    )
---
+++ deploy/migrate/sync_s3.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rclone sync aws-klicker-prod:tc-klicker-prod exo-klicker-qa:klicker-qa
