e9ba8d2c0902a96a72fdfe18439744156040e829
Roland Schlaefli <rolandschlaefli@gmail.com>
2021-09-10 10:13:44 +0200

    deploy: fixed redis migration commands

--- deploy/migrate/restore_redis.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# export $(grep -v '^#' .env | xargs)
-
-# python redisdl.py --host $REDIS_TARGET_HOSTNAME --port $REDIS_TARGET_PORT --password "$REDIS_TARGET_PASSWORD" --db 0 --ssl -l dump.json
-exo storage setacl sos://klicker-qa/ --recursive public-read
