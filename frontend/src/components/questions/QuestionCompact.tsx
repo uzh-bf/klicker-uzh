58da30571dfcc17ad74dcde10acc9da00fe69585
Julius Schlapbach <julius.schlapbach@gmx.net>
2021-09-27 15:28:48 +0200

    wip: added modifier buttons to tabular question view

--- frontend/src/components/questions/QuestionCompact.tsx
+++ frontend/src/components/questions/QuestionCompact.tsx
@@ -1,14 +1,13 @@
 import React, { useState } from 'react'
 import clsx from 'clsx'
-import dayjs from 'dayjs'
+//import dayjs from 'dayjs'
 import { FormattedMessage } from 'react-intl'
 import { useDrag } from 'react-dnd'
-import { Checkbox, Dropdown, Label } from 'semantic-ui-react'
+import { Button } from 'semantic-ui-react'
 import { generateTypesShort } from '../../lib/utils/lang'
 import { useIntl } from 'react-intl'
-
-import QuestionDetails from './QuestionDetails'
-import QuestionTags from './QuestionTags'
+import QuestionDetailsModal from './QuestionDetailsModal'
+import QuestionDuplicationModal from './QuestionDuplicationModal'
 
 interface Props {
   checked?: boolean
@@ -48,6 +47,8 @@ function QuestionCompact({
   const [activeVersion, setActiveVersion]: any = useState(versions.length - 1)
   const { description } = versions[activeVersion]
   const intl = useIntl()
+  const [isDuplicationModalOpen, setIsDuplicationModalOpen] = useState(false)
+  const [isModificationModalOpen, setIsModificationModalOpen] = useState(false)
   const [collectedProps, drag] = useDrag({
     item: {
       id,
@@ -72,7 +73,13 @@ function QuestionCompact({
       })}
       ref={drag}
     >
-      <div className="p-4 border border-gray-400 border-2 border-solid p-6 bg-gray-100 rounded-lg h-full">
+      <div
+        className={clsx(
+          checked && '!bg-green-100',
+          'p-4 border border-gray-400 border-2 border-solid p-6 bg-gray-100 rounded-lg h-full cursor-pointer'
+        )}
+        onClick={(): void => onCheck({ version: activeVersion })}
+      >
         <div className="mb-14">
           <div className="text-right">
             {tags.map((tag: any) => (
@@ -83,21 +90,33 @@ function QuestionCompact({
             {generateTypesShort(intl)[type]}
           </div>
         </div>
-        {/*<div className="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500">
-          <svg
-            fill="none"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            className="w-6 h-6"
-            viewBox="0 0 24 24"
-          >
-            <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
-          </svg>
-          </div>*/}
+
         <h2 className="title">{title}</h2>
         <p className="leading-relaxed text-base clampedDescription">{description}</p>
+        <div className="border-0 border-b-2 border-solid border-gray-300 mb-4" />
+        <div className="float-right mb-4">
+          <Button onClick={(): void => setIsModificationModalOpen(true)}>
+            <FormattedMessage defaultMessage="View / Edit" id="questionDetails.button.edit" />
+          </Button>
+          {isModificationModalOpen && (
+            <QuestionDetailsModal
+              handleSetIsOpen={setIsModificationModalOpen}
+              isOpen={isModificationModalOpen}
+              questionId={id}
+            />
+          )}
+
+          <Button onClick={(): void => setIsDuplicationModalOpen(true)}>
+            <FormattedMessage defaultMessage="Duplicate" id="questionDetails.button.duplicate" />
+          </Button>
+          {isDuplicationModalOpen && (
+            <QuestionDuplicationModal
+              handleSetIsOpen={setIsDuplicationModalOpen}
+              isOpen={isDuplicationModalOpen}
+              questionId={id}
+            />
+          )}
+        </div>
       </div>
       <style jsx>{`
         .clampedDescription {
@@ -107,43 +126,6 @@ function QuestionCompact({
           -webkit-line-clamp: 3;
         }
       `}</style>
-      {/*<div className={clsx('checker', { active: !draggable })}>
-        <Checkbox
-          checked={checked}
-          id={`check-${id}`}
-          type="checkbox"
-          onClick={(): void => onCheck({ version: activeVersion })}
-        />
-      </div>
-      <div className="wrapper items-center">
-        <h2 className="title">
-          {isArchived && (
-            <Label color="red" size="tiny">
-              <FormattedMessage defaultMessage="ARCHIVED" id="questionPool.question.titleArchive" />
-            </Label>
-          )}{' '}
-          {title}
-        </h2>
-
-        <div className="versionChooser">
-          <Dropdown
-            disabled={versions.length === 1}
-            options={versions.map((version, index): any => ({
-              key: index,
-              text: `v${index + 1} - ${dayjs(version.createdAt).format('DD.MM.YYYY HH:mm')}`,
-              value: index,
-            }))}
-            value={activeVersion}
-            onChange={(_, data): void => setActiveVersion(data.value)}
-          />
-        </div>
-
-        <div className="tags">
-          <QuestionTags tags={tags} type={type} />
-        </div>
-
-
-          </div>*/}
     </div>
   )
 }
