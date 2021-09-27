83c377d56ab5038be9897b18ef1e6b40564aa311
Julius Schlapbach <julius.schlapbach@gmx.net>
2021-09-27 15:52:02 +0200

    wip: styling of tabular question layout

--- frontend/src/components/questions/QuestionCompact.tsx
+++ frontend/src/components/questions/QuestionCompact.tsx
@@ -44,7 +44,7 @@ function QuestionCompact({
   creationMode,
   isArchived,
 }: Props): React.ReactElement {
-  const [activeVersion, setActiveVersion]: any = useState(versions.length - 1)
+  const activeVersion = versions.length - 1
   const { description } = versions[activeVersion]
   const intl = useIntl()
   const [isDuplicationModalOpen, setIsDuplicationModalOpen] = useState(false)
@@ -65,17 +65,23 @@ function QuestionCompact({
 
   return (
     <div
-      className={clsx('question', {
-        creationMode,
-        draggable: creationMode,
-        isArchived,
-        isDragging: collectedProps.isDragging,
-      })}
+      className={clsx(
+        'question',
+        {
+          creationMode,
+          draggable: creationMode,
+          isArchived,
+          isDragging: collectedProps.isDragging,
+        },
+        'relative'
+      )}
       ref={drag}
     >
       <div
         className={clsx(
           checked && '!bg-green-100',
+          creationMode && '!cursor-[grab]',
+          collectedProps.isDragging && '!cursor-[grabbing]',
           'p-4 border border-gray-400 border-2 border-solid p-6 bg-gray-100 rounded-lg h-full cursor-pointer'
         )}
         onClick={(): void => onCheck({ version: activeVersion })}
@@ -92,30 +98,32 @@ function QuestionCompact({
         </div>
 
         <h2 className="title">{title}</h2>
-        <p className="leading-relaxed text-base clampedDescription">{description}</p>
-        <div className="border-0 border-b-2 border-solid border-gray-300 mb-4" />
-        <div className="float-right mb-4">
-          <Button onClick={(): void => setIsModificationModalOpen(true)}>
-            <FormattedMessage defaultMessage="View / Edit" id="questionDetails.button.edit" />
-          </Button>
-          {isModificationModalOpen && (
-            <QuestionDetailsModal
-              handleSetIsOpen={setIsModificationModalOpen}
-              isOpen={isModificationModalOpen}
-              questionId={id}
-            />
-          )}
+        <p className="leading-relaxed text-base clampedDescription mb-16">{description}</p>
+        <div className="absolute bottom-0 w-full pl-6 pr-8 -ml-6 ">
+          <div className="border-0 border-b-2 border-solid border-gray-300 mb-4" />
+          <div className="float-right mb-4">
+            <Button onClick={(): void => setIsModificationModalOpen(true)}>
+              <FormattedMessage defaultMessage="View / Edit" id="questionDetails.button.edit" />
+            </Button>
+            {isModificationModalOpen && (
+              <QuestionDetailsModal
+                handleSetIsOpen={setIsModificationModalOpen}
+                isOpen={isModificationModalOpen}
+                questionId={id}
+              />
+            )}
 
-          <Button onClick={(): void => setIsDuplicationModalOpen(true)}>
-            <FormattedMessage defaultMessage="Duplicate" id="questionDetails.button.duplicate" />
-          </Button>
-          {isDuplicationModalOpen && (
-            <QuestionDuplicationModal
-              handleSetIsOpen={setIsDuplicationModalOpen}
-              isOpen={isDuplicationModalOpen}
-              questionId={id}
-            />
-          )}
+            <Button onClick={(): void => setIsDuplicationModalOpen(true)}>
+              <FormattedMessage defaultMessage="Duplicate" id="questionDetails.button.duplicate" />
+            </Button>
+            {isDuplicationModalOpen && (
+              <QuestionDuplicationModal
+                handleSetIsOpen={setIsDuplicationModalOpen}
+                isOpen={isDuplicationModalOpen}
+                questionId={id}
+              />
+            )}
+          </div>
         </div>
       </div>
       <style jsx>{`
