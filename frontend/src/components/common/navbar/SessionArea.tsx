099edd02f3a8026d6c9768b54620869380bde64d
Roland Schlaefli <rolandschlaefli@gmail.com>
2021-10-31 14:52:21 +0100

    enhance(frontend): rework the support modal with more information and links to the feedbear page

--- frontend/src/components/common/navbar/SessionArea.tsx
+++ frontend/src/components/common/navbar/SessionArea.tsx
@@ -1,8 +1,11 @@
 /* eslint-disable react/prop-types, react/no-array-index-key */
 
 import * as React from 'react'
-import { Button, Icon, Menu, Popup, Modal, Header } from 'semantic-ui-react'
+import { Button, Icon, Menu, Modal } from 'semantic-ui-react'
+import Image from 'next/image'
 import { defineMessages, FormattedMessage, useIntl } from 'react-intl'
+import KlickerLogoSrc from '../../../../public/KlickerUZH_Gray_Transparent.svg'
+import SupportEntry from './SupportEntry'
 
 const messages = defineMessages({
   support: {
@@ -52,68 +55,94 @@ function SessionArea({ sessionId }: Props): React.ReactElement {
         onOpen={() => setOpen(true)}
       >
         <Modal.Content>
-          <div className="flex flex-row flex-wrap">
-            <div className="flex flex-col flex-1 md:pr-4">
-              <div className="p-4 border border-solid rounded-lg hover:bg-gray-200 hover:cursor-pointer">
-                <a
-                  href="https://www.klicker.uzh.ch/docs/introduction/getting_started"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  <Header size="small">
-                    <Icon name="book" />
-                    <Header.Content>
-                      Documentation
-                      <Header.Subheader>User Manual, Feature Overview, etc.</Header.Subheader>
-                    </Header.Content>
-                  </Header>
-                </a>
+          <div className="flex flex-col flex-wrap gap-8 md:gap-16 md:flex-row">
+            <div className="flex flex-col justify-between flex-1">
+              <div className="order-2 md:order-1">
+                <h2>Your Feedback</h2>
+                <p className="prose prose-lg">
+                  Do you have any feedback for us? Are you experiencing issues when using the KlickerUZH? Please provide
+                  us with your feedback on our public roadmap so we can work to improve the KlickerUZH for you.
+                </p>
+                <div className="flex flex-col gap-2">
+                  <SupportEntry
+                    href="https://klicker-uzh.feedbear.com/boards/feature-requests"
+                    subtitle="I would like to request a new feature."
+                    title="Feature Request"
+                  />
+                  <SupportEntry
+                    href="https://klicker-uzh.feedbear.com/boards/bug-reports"
+                    subtitle="I would like to report a bug or issue."
+                    title="Bug Report"
+                  />
+                  <SupportEntry
+                    href="https://klicker-uzh.feedbear.com/boards/self-hosting"
+                    subtitle="I have problems when self-hosting the KlickerUZH."
+                    title="Self-Hosting"
+                  />
+                </div>
               </div>
-              <div className="p-4 mt-2 border border-solid rounded-lg hover:bg-gray-200 hover:cursor-pointer">
-                <a href="https://www.klicker.uzh.ch/docs/faq/faq" rel="noopener noreferrer" target="_blank">
-                  <Header size="small">
-                    <Icon name="question" />
 
-                    <Header.Content>
-                      FAQ
-                      <Header.Subheader>Frequently Asked Questions</Header.Subheader>
-                    </Header.Content>
-                  </Header>
-                </a>
-              </div>
-              <div className="p-4 mt-2 border border-solid rounded-lg hover:bg-gray-200 hover:cursor-pointer">
-                <a href="https://github.com/uzh-bf/klicker-uzh" rel="noopener noreferrer" target="_blank">
-                  <Header size="small">
-                    <Icon name="code" />
-                    <Header.Content>
-                      Github Repository
-                      <Header.Subheader>Source Code and Discussions</Header.Subheader>
-                    </Header.Content>
-                  </Header>
-                </a>
-              </div>
-              <div className="p-4 mt-2 border border-solid rounded-lg hover:bg-gray-200 hover:cursor-pointer">
-                <a href="mailto:klicker.support@uzh.ch">
-                  <Header size="small">
-                    <Icon name="mail outline" />
-                    <Header.Content>
-                      Contact
-                      <Header.Subheader>Email to klicker.support@uzh.ch</Header.Subheader>
-                    </Header.Content>
-                  </Header>
-                </a>
+              <div className="order-1 md:order-2 w-[200px] py-8 md:p-0 m-auto md:m-0">
+                <Image alt="KlickerUZH Logo" src={KlickerLogoSrc} />
               </div>
             </div>
-            <div className="mt-4 md:mt-0 flex-1 min-w-[500px] min-h-[800px]">
-              <iframe
-                className="clickup-embed clickup-dynamic-height"
-                height="100%"
-                src="https://forms.clickup.com/f/4dv27-1781/Z5ZVQBCR5R5FXXPB0Q"
-                // style="background: transparent; border: 1px solid #ccc;"
-                title="ClickUp Feedback Form"
-                width="100%"
-                // onWheel=""
-              />
+
+            <div className="flex flex-col justify-between flex-1">
+              <div className="flex flex-col gap-2">
+                <h2>Further Resources</h2>
+                <div className="font-bold text-gray-600">Documentation</div>
+                <SupportEntry
+                  href="https://www.klicker.uzh.ch/docs/introduction/getting_started"
+                  icon="book"
+                  subtitle="Tutorials, feature documentation, and release notes"
+                  title="Documentation"
+                />
+                <SupportEntry
+                  href="https://www.klicker.uzh.ch/docs/faq/faq"
+                  icon="question"
+                  subtitle="Frequently asked questions"
+                  title="FAQ"
+                />
+                <div className="mt-4 font-bold text-gray-600">Connect with Us</div>
+                <SupportEntry
+                  href="mailto:klicker.support@uzh.ch"
+                  icon="mail outline"
+                  subtitle="Contact us at klicker.support@uzh.ch"
+                  title="Email"
+                />
+                <SupportEntry
+                  href="https://github.com/uzh-bf/klicker-uzh/discussions"
+                  icon="talk outline"
+                  subtitle="A place for discussions and questions regarding the KlickerUZH"
+                  title="Community"
+                />
+                <div className="mt-4 font-bold text-gray-600">About the Project</div>
+                <SupportEntry
+                  href="https://www.klicker.uzh.ch/docs/blog"
+                  icon="info"
+                  subtitle="Regular updates regarding the progress of our project"
+                  title="Project Updates"
+                />
+                <SupportEntry
+                  href="https://www.klicker.uzh.ch/roadmap"
+                  icon="target"
+                  subtitle="Our current priorities and plans for the future"
+                  title="Roadmap"
+                />
+                <SupportEntry
+                  href="https://klicker-uzh.feedbear.com/updates"
+                  icon="list"
+                  subtitle="Overview of changes in our latest releases"
+                  title="Release Notes"
+                />
+                <div className="mt-4 font-bold text-gray-600">Open-Source</div>
+                <SupportEntry
+                  href="https://github.com/uzh-bf/klicker-uzh"
+                  icon="github"
+                  subtitle="Source code of the open-source project"
+                  title="Github Repository"
+                />
+              </div>
             </div>
           </div>
         </Modal.Content>
