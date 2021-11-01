2e9b7ad2a02ce2dd01b8a076f79e759e03dcf1a3
Roland Schlaefli <rolandschlaefli@gmail.com>
2021-11-01 16:01:06 +0100

    fix(backend): ensure feedbacks have a resolvedAt property

--- backend/src/models/FeedbackResponse.js
+++ backend/src/models/FeedbackResponse.js
@@ -5,7 +5,6 @@ module.exports = new mongoose.Schema(
     content: { type: String, required: true },
     positiveReactions: { type: Number, default: 0 },
     negativeReactions: { type: Number, default: 0 },
-    resolvedAt: { type: Date },
   },
   { timestamps: true }
 )
