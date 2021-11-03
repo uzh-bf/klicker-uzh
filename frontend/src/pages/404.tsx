f313a2eb762796bc7d7978f4a06c847687665bc8
Roland Schlaefli <rolandschlaefli@gmail.com>
2021-11-03 21:54:57 +0100

    real 404 event for matomo

--- frontend/src/pages/404.tsx
+++ frontend/src/pages/404.tsx
@@ -1,10 +1,12 @@
 import Error from 'next/error'
 import { useEffect } from 'react'
 import { push } from '@socialgouv/matomo-next'
+import { useRouter } from 'next/router'
 
 export default function NotFound() {
+  const router = useRouter()
   useEffect(() => {
-    push(['trackEvent', 'AAI Entrypoint', 'User Logged In'])
+    push(['trackEvent', 'Error', '404', router.pathname])
   }, [])
   // Opinionated: do not record an exception in Sentry for 404
   return <Error statusCode={404} />
