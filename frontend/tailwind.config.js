d8db91683617d94aa845d52d5ca4a2781eb2120d
Julius Schlapbach <julius.schlapbach@gmx.net>
2021-11-26 14:22:12 +0100

    fix: fixed tailwind config mistake

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,6 +6,8 @@ module.exports = {
     extend: {
       screens: {
         print: { raw: 'print' },
+        xl: '1280px',
+        '2xl': '1536px',
       },
       colors: {
         'blue-100': '#0028a5',
@@ -40,12 +42,6 @@ module.exports = {
           'linear-gradient(to bottom, transparent 0%, transparent calc(50% - 0.81px), lightgrey calc(50% - 0.8px), lightgrey calc(50% + 0.8px), transparent calc(50% + 0.81px), transparent 100%)',
         'timeline-mobile':
           'linear-gradient(to right, transparent 0%, transparent calc(50% - 1.01px), lightgrey calc(50% - 1px), lightgrey calc(50% + 1px), transparent calc(50% + 1.01px), transparent 100%)',
-      },
-      screens: {
-        xl: '1280px',
-        // => @media (min-width: 1280px) { ... }
-        '2xl': '1536px',
-        // => @media (min-width: 1536px) { ... }
       },
     },
   },
