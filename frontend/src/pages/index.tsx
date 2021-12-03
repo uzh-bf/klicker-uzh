415cf5e43f85b13350aa18d5dc24fa7a54da4930
Julius Schlapbach <julius.schlapbach@gmx.net>
2021-12-03 14:15:25 +0100

    enhance(frontend): add new content to welcome page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -4,8 +4,11 @@ import { Button, List, Input } from 'semantic-ui-react'
 import { push } from '@socialgouv/matomo-next'
 import KlickerLogoSrc from '../../public/KlickerUZH_Gray_Transparent.png'
 import Image from 'next/image'
+import getConfig from 'next/config'
 
 import StaticLayout from '../components/layouts/StaticLayout'
+
+const { publicRuntimeConfig } = getConfig()
 
 const links = [
   {
@@ -38,7 +41,7 @@ function Index(): React.ReactElement {
 
         <p className="italic text-center">Welcome to the open source instant audience response system.</p>
 
-        <div className="flex flex-col w-full p-4 mb-4 border border-solid lg:items-center lg:justify-between lg:flex-row lg:flex-wrap border-primary bg-primary-10">
+        <div className="flex flex-col w-full p-4 mb-4 border border-solid rounded-md lg:items-center lg:justify-between lg:flex-row lg:flex-wrap border-primary bg-primary-10">
           <p className="mb-2 lg:flex-00full">Want to participate in a poll?</p>
           <Input
             fluid
@@ -59,60 +62,107 @@ function Index(): React.ReactElement {
         </div>
 
         <div className="flex flex-col lg:flex-row">
-          <a
-            className="block p-4 mb-2 text-black border border-solid border-primary bg-primary-10 lg:p-4 lg:h-72 lg:w-72 lg:mr-2"
-            href="https://www.klicker.uzh.ch/docs/introduction/getting_started"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            <h2 className="text-2xl leading-6">Project</h2>
-            <p>Learn more about Klicker.</p>
-          </a>
-          <a
-            className="block p-4 mb-2 text-black border border-solid border-primary bg-primary-10 lg:p-4 lg:h-72 lg:w-72 lg:mr-2"
-            href="https://www.klicker.uzh.ch/docs/introduction/getting_started"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            <h2 className="block text-2xl leading-6">Lecturer</h2>
-            <p>How to use Klicker as a lecturer!</p>
-          </a>
-          <div className="p-4 mb-0 text-black border border-solid last:mr-0 border-primary bg-primary-10 lg:p-4 lg:h-72 lg:w-72 lg:mr-2">
-            <h2 className="text-2xl leading-6">Development</h2>
+          <div className="block p-4 mb-2 text-black border border-solid rounded-md border-primary bg-primary-10 lg:p-4 lg:h-72 lg:w-72 lg:mr-2">
+            <h2 className="text-2xl leading-6">User Guide</h2>
             <List divided relaxed>
-              {/* <List.Item>
-              <List.Icon name="slack" size="large" verticalAlign="middle" />
-              <List.Content>
-                <List.Header as="a">Slack-Channel</List.Header>
-                <List.Description>Support & Discussions</List.Description>
-              </List.Content>
-            </List.Item>    */}
-
               <List.Item>
-                <List.Icon name="mail" size="large" verticalAlign="middle" />
+                <List.Icon name="book" size="large" verticalAlign="middle" />
                 <List.Content>
-                  <List.Header as="a" href="mailto:klicker.support@uzh.ch">
-                    klicker.support@uzh.ch
+                  <List.Header
+                    as="a"
+                    target="_blank"
+                    href="https://www.klicker.uzh.ch/docs/introduction/getting_started"
+                  >
+                    Documentation
                   </List.Header>
-                  <List.Description>Support</List.Description>
+                  <List.Description>Get started with KlickerUZH</List.Description>
                 </List.Content>
               </List.Item>
               <List.Item>
+                <List.Icon name="question circle" size="large" verticalAlign="middle" />
+                <List.Content>
+                  <List.Header as="a" target="_blank" href="https://www.klicker.uzh.ch/docs/faq/faq">
+                    FAQ
+                  </List.Header>
+                  <List.Description>Frequently Asked Questions</List.Description>
+                </List.Content>
+              </List.Item>
+              <List.Item>
+                <List.Icon name="users" size="large" verticalAlign="middle" />
+                <List.Content>
+                  {/* //TODO: update usecase link to Docusaurus once separate page exists */}
+                  <List.Header
+                    as="a"
+                    target="_blank"
+                    href="https://www.klicker.uzh.ch/docs/introduction/getting_started"
+                  >
+                    Use Cases
+                  </List.Header>
+                  <List.Description>KlickerUZH in Action</List.Description>
+                </List.Content>
+              </List.Item>
+            </List>
+          </div>
+          <div className="block p-4 mb-2 text-black border border-solid rounded-md border-primary bg-primary-10 lg:p-4 lg:h-72 lg:w-72 lg:mr-2">
+            <h2 className="block text-2xl leading-6">Development</h2>
+            <List divided relaxed>
+              <List.Item>
                 <List.Icon name="github" size="large" verticalAlign="middle" />
                 <List.Content>
-                  <List.Header as="a" href="https://www.klicker.uzh.ch/roadmap">
+                  <List.Header as="a" target="_blank" href="https://github.com/uzh-bf/klicker-uzh">
+                    uzh-bf/klicker-uzh
+                  </List.Header>
+                  <List.Description>GitHub Repository</List.Description>
+                </List.Content>
+              </List.Item>
+              <List.Item>
+                <List.Icon name="sitemap" size="large" verticalAlign="middle" />
+                <List.Content>
+                  <List.Header as="a" target="_blank" href="https://www.klicker.uzh.ch/roadmap">
                     Public Roadmap
                   </List.Header>
                   <List.Description>Public Roadmap</List.Description>
                 </List.Content>
               </List.Item>
               <List.Item>
+                <List.Icon name="bullhorn" size="large" verticalAlign="middle" />
+                <List.Content>
+                  <List.Header as="a" target="_blank" href="https://www.klicker.uzh.ch/docs/blog">
+                    Project Updates
+                  </List.Header>
+                  <List.Description>Updates on developments</List.Description>
+                </List.Content>
+              </List.Item>
+            </List>
+          </div>
+          <div className="p-4 mb-0 text-black border border-solid rounded-md last:mr-0 border-primary bg-primary-10 lg:p-4 lg:h-72 lg:w-72 lg:mr-2">
+            <h2 className="text-2xl leading-6">Support</h2>
+            <List divided relaxed>
+              <List.Item>
+                <List.Icon name="mail" size="large" verticalAlign="middle" />
+                <List.Content>
+                  <List.Header as="a" href={`mailto:${publicRuntimeConfig.supportEmail}`}>
+                    {publicRuntimeConfig.supportEmail}
+                  </List.Header>
+                  <List.Description>Contact us!</List.Description>
+                </List.Content>
+              </List.Item>
+              <List.Item>
+                <List.Icon name="lightbulb" size="large" verticalAlign="middle" />
+                <List.Content>
+                  <List.Header as="a" target="_blank" href="https://klicker-uzh.feedbear.com/boards/feature-requests">
+                    Feature Requests
+                  </List.Header>
+                  <List.Description>Request a new feature</List.Description>
+                </List.Content>
+              </List.Item>
+              <List.Item>
                 <List.Icon name="github" size="large" verticalAlign="middle" />
                 <List.Content>
-                  <List.Header as="a" href="https://github.com/uzh-bf/klicker-uzh">
-                    uzh-bf/klicker-uzh
+                  <List.Header as="a" target="_blank" href="https://github.com/uzh-bf/klicker-uzh/discussions">
+                    Discussions
                   </List.Header>
-                  <List.Description>Github Repository</List.Description>
+                  <List.Description>GitHub discussions</List.Description>
                 </List.Content>
               </List.Item>
             </List>
