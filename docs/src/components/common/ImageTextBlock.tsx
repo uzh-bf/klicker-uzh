3af83a3bed86f3563f48a6a8543be2fbb031bc71
Julius Schlapbach <julius.schlapbach@gmx.net>
2021-12-17 13:43:54 +0100

    wip(docs): add possiblity to zoom in on usecase image tiles

--- docs/src/components/common/ImageTextBlock.tsx
+++ docs/src/components/common/ImageTextBlock.tsx
@@ -1,6 +1,8 @@
 import clsx from 'clsx'
 import React from 'react'
 import { ArrowRightIcon } from '@heroicons/react/solid'
+import Zoom from 'react-medium-image-zoom'
+import 'react-medium-image-zoom/dist/styles.css'
 
 interface ImageTextBlockProps {
   title: string
@@ -25,8 +27,10 @@ const ImageTextBlock = ({
         'grid grid-cols-1 md:grid-cols-2 p-3 border-2 border-solid border-gray-300 rounded-lg w-full'
       )}
     >
-      <div className="order-1 w-full px-8 mx-auto mt-2 mb-4 text-center md:my-auto md:order-0">
-        <img src={imgSrc} alt={'Image for ' + title} className="max-h-80" />
+      <div className="order-1 w-full px-2 mx-auto mt-2 mb-4 text-center md:px-6 md:my-auto md:order-0">
+        <Zoom overlayBgColorEnd="gray">
+          <img src={imgSrc} className={'max-h-80'} />
+        </Zoom>
       </div>
       <div className="relative w-full order-0 md:order-1">
         <div className="mb-2 text-lg font-bold sm:text-xl">{title}</div>
