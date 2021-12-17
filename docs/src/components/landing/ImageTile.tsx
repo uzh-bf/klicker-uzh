1d12d5572e36bcb46392e61690183a9dd5256100
Julius Schlapbach <julius.schlapbach@gmx.net>
2021-12-17 15:37:31 +0100

    style(docs): improved landing page styling to avoid image distortion

--- docs/src/components/landing/ImageTile.tsx
+++ docs/src/components/landing/ImageTile.tsx
@@ -9,8 +9,13 @@ interface ImageTileProps {
 
 const ImageTile = ({ imgSrc, content, className }: ImageTileProps) => {
   return (
-    <div className={clsx(className, 'text-center lg:w-1/3')}>
-      <img src={imgSrc} className="h-80 md:h-60 lg:h-80" />
+    <div className={clsx(className, 'text-center md:w-1/3')}>
+      <div className="relative h-80 md:h-60 lg:h-80">
+        <img
+          src={imgSrc}
+          className="absolute top-0 bottom-0 left-0 right-0 max-w-full max-h-full m-auto"
+        />
+      </div>
       <div className="mt-2 text-xl font-bold">{content}</div>
     </div>
   )
