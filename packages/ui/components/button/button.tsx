5d763b8f8ebddba6672b7843feceb74ab1d23713
Roland Schlaefli <rolandschlaefli@gmail.com>
2021-12-24 15:33:40 +0100

    basic bit setup

---
+++ packages/ui/components/button/button.tsx
@@ -0,0 +1,16 @@
+import React, { ReactNode } from 'react';
+
+export type ButtonProps = {
+  /**
+   * a node to be rendered in the special component.
+   */
+  children?: ReactNode;
+};
+
+export function Button({ children }: ButtonProps) {
+  return (
+    <div>
+      {children}
+    </div>
+  );
+}
