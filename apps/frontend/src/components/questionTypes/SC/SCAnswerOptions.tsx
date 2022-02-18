ab4121feda4e71522641c81dc387bfce01202cec
Roland Schlaefli <rolandschlaefli@gmail.com>
2022-02-18 21:19:00 +0100

    enhance(frontend): render formulas in answer options

--- apps/frontend/src/components/questionTypes/SC/SCAnswerOptions.tsx
+++ apps/frontend/src/components/questionTypes/SC/SCAnswerOptions.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import clsx from 'clsx'
 import { Button } from 'semantic-ui-react'
+import useMarkdown from '../../../lib/hooks/useMarkdown'
 
 interface Props {
   disabled?: boolean
@@ -19,32 +20,25 @@ const defaultProps = {
   value: [],
 }
 
-function SCAnswerOptions({ value, disabled, options, onChange }: Props): React.ReactElement {
+function Option({ active, children, disabled, id, onClick }) {
+  const content = useMarkdown({ content: children })
   return (
-    <div className="flex flex-col gap-2 options">
-      {options.map(
-        (option, index): React.ReactElement => (
-          <div className={clsx('option', { active: value.includes(index) })} key={option.id}>
-            <Button fluid className="mr-0" disabled={disabled} onClick={onChange(index)}>
-              {option.name}
-            </Button>
-          </div>
-        )
-      )}
+    <div className={clsx('option', { active })} key={id}>
+      <Button fluid className="mr-0" disabled={disabled} onClick={onClick}>
+        {content}
+      </Button>
 
       <style jsx>{`
         @import 'src/theme';
 
-        .options > .option {
-          &.active {
-            :global(button),
-            :global(button.disabled) {
-              border: 2px solid rgb(0, 97, 0) !important;
-              background-color: rgb(198, 239, 206) !important;
-              color: rgb(0, 97, 0) !important;
+        .option.active {
+          :global(button),
+          :global(button.disabled) {
+            border: 2px solid rgb(0, 97, 0) !important;
+            background-color: rgb(198, 239, 206) !important;
+            color: rgb(0, 97, 0) !important;
 
-              animation: bounce 0.3s;
-            }
+            animation: bounce 0.3s;
           }
         }
 
@@ -59,6 +53,26 @@ function SCAnswerOptions({ value, disabled, options, onChange }: Props): React.R
           }
         }
       `}</style>
+    </div>
+  )
+}
+
+function SCAnswerOptions({ value, disabled, options, onChange }: Props): React.ReactElement {
+  return (
+    <div className="flex flex-col gap-2">
+      {options.map(
+        (option, index): React.ReactElement => (
+          <Option
+            active={value.includes(index)}
+            disabled={disabled}
+            id={option.id}
+            key={option.id}
+            onClick={onChange(index)}
+          >
+            {option.name}
+          </Option>
+        )
+      )}
     </div>
   )
 }
