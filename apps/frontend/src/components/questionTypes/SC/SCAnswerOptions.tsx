9e1b4c387853ec82aa01b01daafae8b2eceeb983
Julius Schlapbach <julius.schlapbach@gmx.net>
2022-03-14 20:17:46 +0100

    fix(frontend): match all markdown list styles in answering options and reduce font weight

--- apps/frontend/src/components/questionTypes/SC/SCAnswerOptions.tsx
+++ apps/frontend/src/components/questionTypes/SC/SCAnswerOptions.tsx
@@ -21,10 +21,10 @@ const defaultProps = {
 }
 
 function Option({ active, children, disabled, id, onClick }) {
-  const content = useMarkdown({ content: children.replace(/^(- |[0-9]+. )/g, '') })
+  const content = useMarkdown({ content: children.replace(/^(- |[0-9]+. |\* |\+ )/g, '') })
   return (
     <div className={clsx('option', { active })} key={id}>
-      <Button fluid className="mr-0" disabled={disabled} onClick={onClick}>
+      <Button fluid className="mr-0 !font-medium" disabled={disabled} onClick={onClick}>
         {content}
       </Button>
 
