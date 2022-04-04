6237069055e620f2d8cca2a6086fe68d06156c08
Julius Schlapbach <80708107+sjschlapbach@users.noreply.github.com>
2022-04-04 16:32:59 +0200

    enhance(backend): populate new user accounts with demo data (#2650)
    * enhance(backend): populate new user accounts with demo data - transfer from branch PopulateNewUserAccounts see closed PR 2634
    
    * fix(backend): populate confusionValues using proper timesteps
    
    * wip: try to extract into separate function
    
    * enhance(backend): refactored demo data to use models now
    
    * chore(backend): improved demo questions to show off formula and formatting highlights
    
    * refactor(backend): hydration of demo data in separate function
    
    * chore(backend): changed demo session names
    
    * fix(backend): added required links for demo sessions
    
    * bugfix for created demo session, refactoring for clarity
    
    * remove console.warn
    
    * chore(backend): moved component arguments behind ...rest
    
    Co-authored-by: Roland Schlaefli <rolandschlaefli@gmail.com>

--- apps/backend/src/services/accounts.js
+++ apps/backend/src/services/accounts.js
@@ -1,9 +1,15 @@
+/* eslint-disable no-await-in-loop */
+const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
 const JWT = require('jsonwebtoken')
 const _get = require('lodash/get')
+const dayjs = require('dayjs')
 const passwordGenerator = require('generate-password')
 const { isLength, isEmail, normalizeEmail } = require('validator')
 const { AuthenticationError, UserInputError } = require('apollo-server-express')
+const { v4: uuidv4 } = require('uuid')
+
+const { ObjectId } = mongoose.Types
 
 const CFG = require('../klicker.conf.js')
 const validators = require('../lib/validators')
@@ -130,6 +136,317 @@ const checkAvailability = async ({ email, shortname }) => {
   return result
 }
 
+const DEMO_QUESTIONS = [
+  {
+    title: 'Demoquestion SC',
+    type: 'SC',
+    content: 'Which of the following statements is applicable to _KlickerUZH_?',
+    options: {
+      SC: {
+        randomized: false,
+        choices: [
+          { correct: true, name: 'KlickerUZH is an open-source audience response system' },
+          { correct: false, name: 'KlickerUZH is owned by Google' },
+          { correct: false, name: 'KlickerUZH cannot be used by everyone' },
+          { correct: false, name: 'KlickerUZH is not a project of the University of Zurich' },
+        ],
+      },
+    },
+  },
+  {
+    title: 'Demoquestion MC',
+    type: 'MC',
+    content:
+      'Which of the following formulas have the form of a Taylor polynomial of some degree $$n$$: $$T_n f(x;a)$$? (multiple answers are possible)',
+    options: {
+      MC: {
+        randomized: false,
+        choices: [
+          { correct: false, name: '$$T_n f(x;a) = \\sum_{|\\alpha| = 0}^{n} (x - a)^\\alpha D^\\alpha f(a-x)$$' },
+          {
+            correct: true,
+            name: "$$T_n f(x;a) = f(a) + \\frac{f'(a)}{1!}(x - a) + \\frac{f''(a)}{2!}(x - a)^2 + ... + \\frac{f^{(n)}(a)}{n!}(x - a)^n$$",
+          },
+          { correct: true, name: '$$T_4 sin(x;0) = x - \\frac{x^3}{6}$$' },
+          { correct: false, name: '$$T_4 cos(x;0) = x + \\frac{x^3}{6}$$' },
+        ],
+      },
+    },
+  },
+  {
+    title: 'Demoquestion FT',
+    type: 'FREE',
+    content: 'Describe a main principle of a social market economy.',
+    options: false,
+  },
+  {
+    title: 'Demoquestion NR',
+    type: 'FREE_RANGE',
+    content: 'Estimate the length of the **longest** river in the world (answer in kilometres).',
+    options: {
+      FREE_RANGE: {
+        restrictions: { min: 0, max: 8000 },
+      },
+    },
+  },
+]
+
+function prepareDemoSessionData({ id, name, user, blockInstances, blockStatus = 'PLANNED', ...rest }) {
+  return new SessionModel({
+    status: 'CREATED',
+    execution: 0,
+    activeBlock: -1,
+    activeStep: 0,
+    participants: [],
+    confusionTS: [],
+    feedbacks: [],
+    ...rest,
+    _id: id,
+    settings: {
+      isParticipantAuthenticationEnabled: false,
+      isConfusionBarometerActive: true,
+      isEvaluationPublic: false,
+      isFeedbackChannelActive: true,
+      isFeedbackChannelPublic: true,
+      authenticationMode: 'NONE',
+      storageMode: 'SECRET',
+    },
+    namespace: uuidv4(),
+    name,
+    blocks: blockInstances.map((instances) => ({
+      execution: 1,
+      status: blockStatus,
+      timeLimit: -1,
+      randomSelection: -1,
+      showSolutions: false,
+      instances,
+    })),
+    activeInstances: [],
+    user,
+  })
+}
+
+async function hydrateDemoData({ userId }) {
+  const user = await UserModel.findById(userId)
+
+  // create demo tag
+  const demoTag = await new TagModel({
+    name: 'DEMO',
+    questions: [],
+    user: userId,
+  }).save()
+  user.tags.push(demoTag.id)
+
+  // create demo questions with demo tag from demo data above
+  const demoQuestions = await Promise.all(
+    DEMO_QUESTIONS.map(async ({ title, type, content, options }) => {
+      const newQuestion = await new QuestionModel({
+        tags: [demoTag.id],
+        title,
+        type,
+        user: userId,
+        versions: [
+          {
+            content,
+            description: content
+              .replace(/(\${2})[^]*?[^\\]\1/gm, '$FORMULA$')
+              .replaceAll('<br>', '')
+              .match(/[\p{L}\p{N}\p{P}\p{M}\s]|[$Formula$]|[(0-9)+. ]|[- ]/gu)
+              .join(''),
+            options,
+            files: [],
+            solution: undefined,
+          },
+        ],
+      }).save()
+
+      demoTag.questions.push(newQuestion.id)
+      user.questions.push(newQuestion.id)
+
+      return newQuestion
+    })
+  )
+
+  await Promise.all([user.save(), demoTag.save()])
+
+  // create instances for session population
+  const sessionId1 = ObjectId()
+
+  // prepare 8 question instances for the session without results (in pairs of two)
+  const questionInstances = await demoQuestions.reduce(async (acc, question) => {
+    const instances = await Promise.all(
+      [question, question].map(async (questionData) => {
+        const newInstance = new QuestionInstanceModel({
+          question: questionData.id,
+          session: sessionId1,
+          user: userId,
+          version: 0,
+          results: null,
+        })
+        return newInstance.save()
+      })
+    )
+    const instanceIds = instances.map((instance) => instance.id)
+    question.instances.push(...instanceIds)
+    await question.save()
+    return [...(await acc), ...instanceIds]
+  }, Promise.resolve([]))
+
+  // create demo session without results
+  const demoSession = prepareDemoSessionData({
+    id: sessionId1,
+    user: userId,
+    name: 'Demosession',
+    blockInstances: [
+      [questionInstances[0]],
+      [questionInstances[4]],
+      [questionInstances[2], questionInstances[6]],
+      [questionInstances[1], questionInstances[3], questionInstances[5], questionInstances[7]],
+    ],
+  })
+  await demoSession.save()
+
+  const sessionId2 = ObjectId()
+
+  const aggrValuesNR = Array.from({ length: 40 }, () => Math.floor(Math.random() * 1500) + 6500)
+    .concat(Array.from({ length: 185 }, () => Math.floor(Math.random() * 250) + 6693))
+    .reduce((obj, b) => {
+      // eslint-disable-next-line no-param-reassign
+      obj[b] = obj[b] + 1 || 1
+      return obj
+    }, {})
+  const NRObject = Object.keys(aggrValuesNR).reduce((a, v) => ({ ...a, [v]: { value: v, count: aggrValuesNR[v] } }), {})
+
+  const instancesWithResults = await Promise.all([
+    new QuestionInstanceModel({
+      question: demoQuestions[0],
+      session: sessionId2,
+      user: userId,
+      version: 0,
+      results: {
+        CHOICES: [107, 16, 19, 20],
+        totalParticipants: 162,
+      },
+    }).save(),
+    new QuestionInstanceModel({
+      question: demoQuestions[1],
+      session: sessionId2,
+      user: userId,
+      version: 0,
+      results: {
+        CHOICES: [17, 56, 61, 18],
+        totalParticipants: 152,
+      },
+    }).save(),
+    new QuestionInstanceModel({
+      question: demoQuestions[2],
+      session: sessionId2,
+      user: userId,
+      version: 0,
+      results: {
+        totalParticipants: 3,
+        FREE: {
+          hashValue1: {
+            count: 1,
+            value:
+              'This system produces the highest level of economic benefit and social justice for all participants.',
+          },
+          hashValue2: {
+            count: 1,
+            value:
+              'The system is based on free-market capitalism, but also includes special provisions for social programs, etc.',
+          },
+          hashValue3: {
+            count: 1,
+            value: 'The free social market economy is the most common system in the modern world.',
+          },
+        },
+      },
+    }).save(),
+    new QuestionInstanceModel({
+      question: demoQuestions[3],
+      session: sessionId2,
+      user: userId,
+      version: 0,
+      results: {
+        totalParticipants: 225,
+        FREE: NRObject,
+      },
+    }).save(),
+  ])
+
+  const confusionValues = Array(120)
+    .fill(0)
+    .map((t, index) => {
+      return {
+        speed: Math.floor(Math.random() * 5) - 2,
+        difficulty: Math.floor(Math.random() * 5) - 2,
+        createdAt: dayjs()
+          .add(index * 40, 'seconds')
+          .toDate(),
+      }
+    })
+
+  const feedbacks = [
+    {
+      published: true,
+      pinned: false,
+      resolved: true,
+      votes: 13,
+      content: 'Which answer was correct for the question about the longest river in the world?',
+      responses: [
+        {
+          positiveReactions: 10,
+          negativeReactions: 1,
+          content: "The Nile river is the longest in the world with a length of 6'693 kilometres.",
+        },
+      ],
+      resolvedAt: Date.now(),
+    },
+    {
+      published: true,
+      pinned: false,
+      resolved: false,
+      votes: 7,
+      content: 'Could you please explain again what the main principles of a social market economy are?',
+      responses: [],
+    },
+    {
+      published: true,
+      pinned: false,
+      resolved: false,
+      votes: 4,
+      content: 'This example about the fundamental principle of algebra was very helpful, thank you.',
+      responses: [],
+    },
+  ]
+
+  // create evaluation session with question responses, feedbacks and confusion feedback
+  const evaluationSession = prepareDemoSessionData({
+    id: sessionId2,
+    name: 'Demosession with results',
+    status: 'COMPLETED',
+    user: userId,
+    blockInstances: [
+      [instancesWithResults[0].id],
+      [instancesWithResults[1].id],
+      [instancesWithResults[2].id, instancesWithResults[3].id],
+    ],
+    blockStatus: 'EXECUTED',
+    feedbacks,
+    confusionTS: confusionValues,
+    startedAt: Date.now(),
+    finishedAt: Date.now(),
+    activeBlock: 2,
+    activeStep: 6,
+  })
+  await evaluationSession.save()
+
+  await UserModel.findByIdAndUpdate(userId, {
+    $push: { sessions: [evaluationSession.id, demoSession.id] },
+  })
+}
+
 /**
  * Register an account for a new user
  * @param {String} email The email of the new user
@@ -213,10 +530,16 @@ const signup = async (
       } catch (e) {
         sendSlackNotification('accounts', `Activation email could not be sent to ${normalizedEmail}`)
       }
-    }
 
-    // return the data of the newly created user
-    return newUser
+      try {
+        await hydrateDemoData({ userId: newUser._id })
+      } catch (e) {
+        sendSlackNotification('accounts', `Demo data hydration failed for ${normalizedEmail} ${e.message}`)
+      }
+
+      // return the data of the newly created user
+      return newUser
+    }
   }
 
   throw new UserInputError('SIGNUP_FAILED')
