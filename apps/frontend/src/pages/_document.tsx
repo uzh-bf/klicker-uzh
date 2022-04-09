a04ce1a7a389aead2a837072ea5895d42b5e5449
Roland Schlaefli <rolandschlaefli@gmail.com>
2022-04-09 10:20:21 +0200

    enhance(frontend): add klicker favicon

--- apps/frontend/src/pages/_document.tsx
+++ apps/frontend/src/pages/_document.tsx
@@ -47,6 +47,10 @@ class IntlDocument extends Document<Props> {
           />
           <meta content="text/html; charset=utf-8" httpEquiv="Content-type" />
           <meta content="IE=Edge" httpEquiv="X-UA-Compatible" />
+          <link href="/apple-touch-icon.png" rel="apple-touch-icon" sizes="180x180" />
+          <link href="/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png" />
+          <link href="/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
+          <link href="/site.webmanifest" rel="manifest" />
           <link
             crossOrigin="anonymous"
             href="https://cdn.jsdelivr.net/npm/katex@0.15.2/dist/katex.min.css"
