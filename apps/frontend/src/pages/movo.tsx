562c3ee6edcf163fda2244407605dad04cec9659
Julius Schlapbach <julius.schlapbach@gmx.net>
2022-04-11 14:37:54 +0200

    chore(frontend): remove modal structure for import

--- apps/frontend/src/pages/movo.tsx
+++ apps/frontend/src/pages/movo.tsx
@@ -2,7 +2,7 @@ import React, { useState, useCallback } from 'react'
 import { useMutation } from '@apollo/client'
 import { useRouter } from 'next/router'
 // import Button from '../components/common/Button'
-import { Modal, Button, Icon } from 'semantic-ui-react'
+import { Button, Icon } from 'semantic-ui-react'
 import { useDropzone } from 'react-dropzone'
 import { FormattedMessage } from 'react-intl'
 import { useToasts } from 'react-toast-notifications'
@@ -43,48 +43,38 @@ function MovoImport(): React.ReactElement {
     onDrop,
   })
 
-  const onResetImportState = (): void => {
-    setMovoJSON('')
-  }
-
   return (
     <>
       <h1>Movo Import</h1>
-      <Modal closeIcon size="fullscreen" trigger={<Button>Upload</Button>} onClose={onResetImportState}>
-        <Modal.Header>Movo Question &amp; Session Import</Modal.Header>
-        <Modal.Content>
-          <div className="flex flex-row flex-nowrap">
-            <div className="flex-[0_0_20%]" {...getRootProps()}>
-              <input {...getInputProps()} />
-              <Button fluid icon primary disabled={false} type="button">
-                <Icon className="!mr-10" name="plus" />
-                Upload Movo Export
-              </Button>
-            </div>
-            <div className="flex-1 pl-4">Movo JSON Content: {movoJSON}</div>
-          </div>
-        </Modal.Content>
-        <Modal.Actions>
-          <Button
-            icon
-            primary
-            onClick={() => {
-              try {
-                movoImport({
-                  variables: { dataset: movoJSON },
-                })
-              } catch (error) {
-                console.log(error)
-              }
-              router.push('./questions')
-            }}
-          >
-            {/* disabled={questions.length === 0} */}
-            <Icon name="upload" />
-            <FormattedMessage defaultMessage="Start Upload" id="questionImport.button.upload" />
+      <div className="flex flex-row flex-nowrap">
+        <div className="flex-[0_0_20%]" {...getRootProps()}>
+          <input {...getInputProps()} />
+          <Button fluid icon primary disabled={false} type="button">
+            <Icon className="!mr-10" name="plus" />
+            Upload Movo Export
           </Button>
-        </Modal.Actions>
-      </Modal>
+        </div>
+        <div className="flex-1 pl-4">Movo JSON Content: {movoJSON}</div>
+      </div>
+      <Button
+        icon
+        primary
+        disabled={movoJSON === ''}
+        onClick={() => {
+          try {
+            movoImport({
+              variables: { dataset: movoJSON },
+            })
+          } catch (error) {
+            console.log(error)
+          }
+          router.push('./questions')
+        }}
+      >
+        {/* disabled={questions.length === 0} */}
+        <Icon name="upload" />
+        <FormattedMessage defaultMessage="Start Upload" id="questionImport.button.upload" />
+      </Button>
     </>
   )
 }
