84695858ff7fdfafb81022669ba2328653d64fb7
Roland Schlaefli <rolandschlaefli@gmail.com>
2022-05-12 11:08:25 +0200

    chore(frontend): replace websocket link with official implementation

--- apps/frontend/src/lib/apolloClient.ts
+++ apps/frontend/src/lib/apolloClient.ts
@@ -11,42 +11,7 @@ import { print, GraphQLError } from 'graphql'
 import { createClient, ClientOptions, Client } from 'graphql-ws'
 import { createPersistedQueryLink } from '@apollo/client/link/persisted-queries'
 import { sha256 } from 'crypto-hash'
-// custom websocket link to make apollo work with graphql-ws
-// ref: https://www.npmjs.com/package/graphql-ws#apollo-client
-class WebSocketLink extends ApolloLink {
-  private client: Client
-
-  constructor(options: ClientOptions) {
-    super()
-    this.client = createClient(options)
-  }
-
-  public request(operation: Operation): Observable<FetchResult> {
-    return new Observable((sink) => {
-      return this.client.subscribe<FetchResult>(
-        { ...operation, query: print(operation.query) },
-        {
-          next: sink.next.bind(sink),
-          complete: sink.complete.bind(sink),
-          error: (err) => {
-            if (err instanceof Error) {
-              return sink.error(err)
-            }
-
-            if (err instanceof CloseEvent) {
-              return sink.error(
-                // reason will be available on clean closes
-                new Error(`Socket closed with event ${err.code} ${err.reason || ''}`)
-              )
-            }
-
-            return sink.error(new Error((err as GraphQLError[]).map(({ message }) => message).join(', ')))
-          },
-        }
-      )
-    })
-  }
-}
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions'
 
 export default function createApolloClient() {
   const { publicRuntimeConfig, serverRuntimeConfig } = getConfig()
@@ -75,18 +40,20 @@ export default function createApolloClient() {
   // on the client, differentiate between websockets and http requests
   if (isBrowser) {
     // create a websocket link from the client
-    const wsLink = new WebSocketLink({
-      url: publicRuntimeConfig.apiUrlWS || 'ws://localhost:4000/graphql',
-      // connectionParams: () => {
-      // const session = getSession();
-      // if (!session) {
-      //   return {};
-      // }
-      // return {
-      //   Authorization: `Bearer ${session.token}`,
-      // };
-      // },
-    })
+    const wsLink = new GraphQLWsLink(
+      createClient({
+        url: publicRuntimeConfig.apiUrlWS || 'ws://localhost:4000/graphql',
+        // connectionParams: () => {
+        // const session = getSession();
+        // if (!session) {
+        //   return {};
+        // }
+        // return {
+        //   Authorization: `Bearer ${session.token}`,
+        // };
+        // },
+      })
+    )
 
     // swap out the http link with a split based on operation type
     // use websocket link for subscriptions, http link for remainder
