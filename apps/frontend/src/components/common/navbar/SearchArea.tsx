e50cc615e1eb36a9e014dbe390e0d707bde5ade9
Julius Schlapbach <julius.schlapbach@gmx.net>
2022-06-03 00:21:04 +0200

    wip(frontend): create custom dropdown based on radix component

--- apps/frontend/src/components/common/navbar/SearchArea.tsx
+++ apps/frontend/src/components/common/navbar/SearchArea.tsx
@@ -1,6 +1,9 @@
+import React from 'react'
 import { defineMessages, useIntl } from 'react-intl'
-import { Dropdown, Input } from 'semantic-ui-react'
-import { SortAscendingIcon, SortDescendingIcon } from '@heroicons/react/solid'
+import { Input } from 'semantic-ui-react'
+import { SortAscendingIcon, SortDescendingIcon, ChevronDownIcon } from '@heroicons/react/solid'
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
+import clsx from 'clsx'
 import CustomButton from '../CustomButton'
 
 const messages = defineMessages({
@@ -54,23 +57,41 @@ function SearchArea({
       {withSorting && (
         <>
           <CustomButton
-            className={'!p-[0.65rem] bg-white  w-11 h-11 mr-1'}
+            className={'!p-[0.65rem] bg-white w-11 h-11 mr-1'}
             disabled={false}
             onClick={handleSortOrderToggle}
           >
             {sortOrder ? <SortDescendingIcon /> : <SortAscendingIcon />}
           </CustomButton>
 
-          <Dropdown
-            selection
-            className="!leading-4 text-base"
-            options={sortingTypes.map(({ content, id }): any => ({
-              text: content,
-              value: id,
-            }))}
-            value={sortBy}
-            onChange={(_, data): void => handleSortByChange(data.value)}
-          />
+          <DropdownMenu.Root>
+            <DropdownMenu.Trigger className="flex flex-row pl-5 text-left bg-white border border-solid rounded-md w-44 h-11 border-grey-80">
+              <div className="flex-1 h-full py-2.5">{sortingTypes.find((type) => type.id === sortBy).content}</div>
+              <ChevronDownIcon className="w-5 mr-2" />
+            </DropdownMenu.Trigger>
+
+            <DropdownMenu.Content className="flex flex-col px-2 bg-white border border-solid rounded-md border-grey-80">
+              {sortingTypes.map(({ content, id }, index): any => (
+                <>
+                  <DropdownMenu.Item
+                    className="[all:_unset] w-50 hover:bg-blue-20 bg-white align-middle !px-6 !py-1 !rounded-md !my-1"
+                    key={content}
+                    onClick={() => handleSortByChange(id)}
+                  >
+                    <span className={clsx(sortBy === id && 'font-bold', 'text-lg')}>{content}</span>
+                  </DropdownMenu.Item>
+                  <div
+                    className={clsx(
+                      'h-[0.075rem] bg-grey-80 opacity-40',
+                      sortingTypes.length - 1 === index && 'hidden'
+                    )}
+                  />
+                </>
+              ))}
+
+              <DropdownMenu.Arrow className="bg-white" />
+            </DropdownMenu.Content>
+          </DropdownMenu.Root>
         </>
       )}
     </div>
