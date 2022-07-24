16dbf7648857f742754737c00a9e7f2bbf45bf9f
Roland Schlaefli <rolandschlaefli@gmail.com>
2022-07-24 13:13:46 +0200

    add stubs for backend-api

---
+++ apps/backend-api/src/app.ts
@@ -0,0 +1,44 @@
+import express from 'express'
+import { createServer } from '@graphql-yoga/node'
+
+const app = express()
+
+const graphQLServer = createServer({
+  schema: {
+    typeDefs: /* GraphQL */ `
+      scalar File
+      type Query {
+        hello: String
+      }
+      type Mutation {
+        getFileName(file: File!): String
+      }
+      type Subscription {
+        countdown(from: Int!): Int!
+      }
+    `,
+    resolvers: {
+      Query: {
+        hello: () => 'world',
+      },
+      Mutation: {
+        getFileName: (root, { file }: { file: File }) => file.name,
+      },
+      Subscription: {
+        countdown: {
+          async *subscribe(_, { from }) {
+            for (let i = from; i >= 0; i--) {
+              await new Promise((resolve) => setTimeout(resolve, 1000))
+              yield { countdown: i }
+            }
+          },
+        },
+      },
+    },
+  },
+  logging: false,
+})
+
+app.use('/graphql', graphQLServer)
+
+export default app
---
+++ apps/backend-api/src/index.ts
@@ -0,0 +1,5 @@
+import app from './app'
+
+app.listen(4000, () => {
+  console.log('GraphQL API located at http://localhost:4000/graphql')
+})
