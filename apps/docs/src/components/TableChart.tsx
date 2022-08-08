6d7d1e84367a84b47ac1b44bdadd11ecd5099c3a
Roland Schlaefli <rolandschlaefli@gmail.com>
2022-08-08 15:36:24 +0200

    fix(docs): add react import to all files with jsx

--- apps/docs/src/components/TableChart.tsx
+++ apps/docs/src/components/TableChart.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { twMerge } from 'tailwind-merge'
 
 export default function TableChart({ className, title, data }) {
--- apps/docs/src/components/UserForm.tsx
+++ apps/docs/src/components/UserForm.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import React, { useEffect } from 'react'
 
 const UserForm = () => {
   useEffect(() => {
