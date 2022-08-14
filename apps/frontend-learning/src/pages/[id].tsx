87bca2f3a742473105df48ea59874142ec451de9
Roland Schlaefli <rolandschlaefli@gmail.com>
2022-08-14 11:50:01 +0200

    enhance: add codegen in shared graphql package

--- apps/frontend-learning/src/pages/[id].tsx
+++ apps/frontend-learning/src/pages/[id].tsx
@@ -1,36 +1,22 @@
-import { gql, useQuery } from '@apollo/client'
+import { useQuery } from '@apollo/client'
+import { GetLearningElementDocument } from '@klicker-uzh/graphql/dist/ops'
 import { useRouter } from 'next/router'
-
-const GET_LEARNING_ELEMENT = gql`
-  query GetLearningElement($id: ID!) {
-    learningElement(id: $id) {
-      id
-      course {
-        id
-      }
-      instance {
-        id
-        questionData
-      }
-    }
-  }
-`
 
 function Index() {
   const router = useRouter()
 
-  const { loading, error, data } = useQuery(GET_LEARNING_ELEMENT, {
+  const { loading, error, data } = useQuery(GetLearningElementDocument, {
     variables: {
-      id: router.query.id,
+      id: router.query.id as string,
     },
   })
 
-  if (loading) return <p>Loading...</p>
+  if (loading || !data) return <p>Loading...</p>
   if (error) return <p>Oh no... {error.message}</p>
 
   return (
     <div className="p-4">
-      {data.learningElement.instance.questionData.content}
+      {data.learningElement?.instance?.questionData.content}
     </div>
   )
 }
