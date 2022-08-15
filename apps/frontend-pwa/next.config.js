7faabfeebfcde76951a4f1547485c6e83806b686
florinavogel <58864918+florinavogel@users.noreply.github.com>
2022-08-15 15:06:15 +0200

    enhance(apps/frontend-pwa): setup foundations for next-pwa (#2740)

--- apps/frontend-pwa/next.config.js
+++ apps/frontend-pwa/next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const withPWA = require("next-pwa")
+
+const nextConfig = withPWA({
   reactStrictMode: true,
   swcMinify: true,
-}
+  pwa: {
+    dest: "public",
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development',
+  },
+  publicRuntimeConfig: { apiURL: "http://localhost:7071/api/graphql" }
+})
 
 module.exports = nextConfig
