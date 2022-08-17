07e863ea8fb75714b12fbc8284d238b9ff8d6511
Roland Schlaefli <rolandschlaefli@gmail.com>
2022-08-17 11:22:14 +0200

    chore(apps/frontend-learning): add basic lti integration

--- apps/frontend-learning/src/pages/course/[id].tsx
+++ apps/frontend-learning/src/pages/course/[id].tsx
@@ -1,5 +1,31 @@
-function CourseOverview() {
-  return <div className="flex flex-row p-4"></div>
+import getLtiContext from '@klicker-uzh/lti'
+
+function CourseOverview({ context }: any) {
+  return (
+    <div className="flex flex-row p-4">
+      <pre>
+        <code>{JSON.stringify(context, null, 2)}</code>
+      </pre>
+    </div>
+  )
+}
+
+export const getServerSideProps = async (ctx: any) => {
+  const ltiContext = await getLtiContext({
+    ctx,
+    key: 'key',
+    secret: 'secret',
+    persist: false,
+    cookieOptions: {
+      path: '/',
+    },
+  })
+  console.warn(ltiContext)
+  return {
+    props: {
+      context: ltiContext,
+    },
+  }
 }
 
 export default CourseOverview
---
+++ apps/frontend-learning/src/types/app.d.ts
@@ -0,0 +1,1 @@
+declare module '@klicker-uzh/lti'
