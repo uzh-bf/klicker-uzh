5b58ee88252b7a2017e1dc179c7861ccec3f65cf
Roland Schlaefli <rolandschlaefli@gmail.com>
2022-08-19 13:39:35 +0200

    chore(apps/frontend-pwa): add .map files to gitignore

--- apps/frontend-pwa/next.config.js
+++ apps/frontend-pwa/next.config.js
@@ -1,16 +1,16 @@
 /** @type {import('next').NextConfig} */
 
-const withPWA = require("next-pwa")
+const withPWA = require('next-pwa')
 
 const nextConfig = withPWA({
   reactStrictMode: true,
   swcMinify: true,
   pwa: {
-    dest: "public",
+    dest: 'public',
     skipWaiting: true,
     disable: process.env.NODE_ENV === 'development',
   },
-  publicRuntimeConfig: { apiURL: "http://localhost:7071/api/graphql" }
+  publicRuntimeConfig: { apiURL: 'http://localhost:7071/api/graphql' },
 })
 
 module.exports = nextConfig
