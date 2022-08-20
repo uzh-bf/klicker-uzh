cf1965686672331740c1d5be2339dff0921e786d
Roland Schlaefli <rolandschlaefli@gmail.com>
2022-08-20 15:20:35 +0200

    set a participant auth cookie using LTI information when opening the course page

--- apps/frontend-learning/src/pages/course/[id].tsx
+++ apps/frontend-learning/src/pages/course/[id].tsx
@@ -1,11 +1,16 @@
+import { RegisterParticipantFromLtiDocument } from '@klicker-uzh/graphql/dist/ops'
 import bodyParser from 'body-parser'
 import { GetServerSideProps } from 'next'
+import nookies from 'nookies'
+import { addApolloState, initializeApollo } from '../../lib/apollo'
 
 function CourseOverview({ context }: any) {
   return <div className="flex flex-row p-4"></div>
 }
 
-export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { req, res } = ctx
+
   const { request, response } = await new Promise((resolve) => {
     bodyParser.urlencoded({ extended: true })(req, res, () => {
       bodyParser.json()(req, res, () => {
@@ -14,13 +19,35 @@ export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
     })
   })
 
-  // TODO: ...
+  const apolloClient = initializeApollo()
 
-  return {
-    props: {
-      context: {},
+  const result = await apolloClient.mutate({
+    mutation: RegisterParticipantFromLtiDocument,
+    variables: {
+      participantId: request.body.lis_person_sourcedid,
+      participantEmail: request.body.lis_person_contact_email_primary,
     },
+  })
+
+  // if a JWT was received from the API, set a cookie in the participant browser
+  if (result.data?.registerParticipantFromLTI) {
+    nookies.set(
+      ctx,
+      'participant_token',
+      result.data?.registerParticipantFromLTI,
+      {
+        domain: process.env.API_DOMAIN ?? 'localhost',
+        path: '/api/graphql',
+        httpOnly: true,
+        maxAge: 60 * 60 * 24 * 7,
+        secure: process.env.NODE_ENV === 'production',
+      }
+    )
   }
+
+  return addApolloState(apolloClient, {
+    props: {},
+  })
 }
 
 export default CourseOverview
