147fab078f445e2ad15883d7a70484ae3e44cdd6
Roland Schlaefli <rolandschlaefli@gmail.com>
2022-08-20 15:19:57 +0200

    integrate backend with LTI registration

--- apps/backend-sls/GraphQL/graphql/authorization.ts
+++ apps/backend-sls/GraphQL/graphql/authorization.ts
@@ -1,7 +1,20 @@
 import { preExecRule } from '@graphql-authz/core'
+import { UserRole } from '@klicker-uzh/prisma'
 
 const IsAuthenticated = preExecRule()((ctx: any) => {
   return !!ctx.user
+})
+
+const IsParticipant = preExecRule()((ctx: any) => {
+  return ctx.user && ctx.user.role === UserRole.PARTICIPANT
+})
+
+const IsUser = preExecRule()((ctx: any) => {
+  return ctx.user && ctx.user.role === UserRole.USER
+})
+
+const IsAdmin = preExecRule()((ctx: any) => {
+  return ctx.user && ctx.user.role === UserRole.ADMIN
 })
 
 export const Rules = {
@@ -12,9 +25,10 @@ export const AuthSchema = {
   Mutation: {
     '*': { __authz: { rules: ['Reject'] } },
     login: { __authz: { rules: ['Allow'] } },
+    registerParticipantFromLTI: { __authz: { rules: ['Allow'] } },
   },
   Query: {
     '*': { __authz: { rules: ['Reject'] } },
-    // learningElement: { __authz: { rules: ['IsAuthenticated'] } },
+    learningElement: { __authz: { rules: ['Allow'] } },
   },
 }
