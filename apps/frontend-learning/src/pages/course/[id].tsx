d359fa288b8f3f6ee0d45a664c4c087bae1bff17
Roland Schlaefli <rolandschlaefli@gmail.com>
2022-08-21 21:34:19 +0200

    add join and leave logic to the leaderboard

--- apps/frontend-learning/src/pages/course/[id].tsx
+++ apps/frontend-learning/src/pages/course/[id].tsx
@@ -1,11 +1,19 @@
+import { useMutation } from '@apollo/client'
+import {
+  JoinCourseDocument,
+  LeaveCourseDocument,
+} from '@klicker-uzh/graphql/dist/ops'
+import { Button } from '@uzh-bf/design-system'
 import { GetServerSideProps } from 'next'
 import Image from 'next/image'
 import { PropsWithChildren } from 'react'
 import { twMerge } from 'tailwind-merge'
 
-import PlaceholderIMG from '../../../public/avatars/placeholder.png'
 import { addApolloState } from '../../lib/apollo'
 import { getParticipantToken } from '../../lib/token'
+
+const PLACEHOLDER_IMG =
+  'https://sos-ch-dk-2.exo.io/klicker-uzh-dev/avatars/placeholder.png'
 
 interface ParticipantProps {
   avatar?: string
@@ -30,13 +38,13 @@ function Participant({
       <div className="relative w-auto h-6 border rounded-full aspect-square">
         <Image
           className="rounded-full"
-          src={avatar || PlaceholderIMG}
+          src={avatar || PLACEHOLDER_IMG}
           alt="Participant Avatar"
           layout="fill"
         />
       </div>
       <div>{pseudonym}</div>
-      <div>{children}</div>
+      <div className="flex-1">{children}</div>
     </div>
   )
 }
@@ -44,20 +52,46 @@ function Participant({
 function ParticipantOther(props: ParticipantProps) {
   return (
     <Participant {...props}>
-      <div>{props.points}</div>
+      <div>Points: {props.points}</div>
     </Participant>
   )
 }
 
-function ParticipantSelf(props: ParticipantProps) {
+interface ParticipantSelfProps extends ParticipantProps {
+  isActive: boolean
+  onJoinCourse: () => void
+  onLeaveCourse: () => void
+}
+
+function ParticipantSelf(props: ParticipantSelfProps) {
   return (
     <Participant isHighlighted {...props}>
-      <div>{props.points ? props.points : 'Not participating. Join?'}</div>
+      <div className="flex flex-row items-center justify-between">
+        <div>Points: {props.points}</div>
+        <div>
+          {props.isActive ? (
+            <Button className="text-sm" onClick={() => props.onLeaveCourse()}>
+              Leave course
+            </Button>
+          ) : (
+            <Button className="text-sm" onClick={() => props.onJoinCourse()}>
+              Join course
+            </Button>
+          )}
+        </div>
+      </div>
     </Participant>
   )
 }
 
 function CourseOverview({ course, participation, participant }: any) {
+  const [joinCourse] = useMutation(JoinCourseDocument, {
+    variables: { courseId: course.id },
+  })
+  const [leaveCourse] = useMutation(LeaveCourseDocument, {
+    variables: { courseId: course.id },
+  })
+
   return (
     <div className="">
       <div className="flex flex-row items-center justify-between px-4 py-2 text-white bg-uzh-blue-100">
@@ -65,7 +99,7 @@ function CourseOverview({ course, participation, participant }: any) {
         <div className="relative w-auto h-8 aspect-square">
           <Image
             className="rounded-full"
-            src={participant.avatar || PlaceholderIMG}
+            src={participant?.avatar || PLACEHOLDER_IMG}
             alt="Participant Avatar"
             layout="fill"
           />
@@ -74,18 +108,21 @@ function CourseOverview({ course, participation, participant }: any) {
 
       <div className="p-4 space-y-1">
         <ParticipantOther
-          pseudonym={participant.pseudonym}
-          avatar={participant.avatar}
+          pseudonym={participant?.pseudonym}
+          avatar={participant?.avatar}
           points={participation?.points}
         />
         <ParticipantSelf
-          pseudonym={participant.pseudonym}
-          avatar={participant.avatar}
+          isActive={participation?.isActive}
+          pseudonym={participant?.pseudonym}
+          avatar={participant?.avatar}
           points={participation?.points}
+          onJoinCourse={joinCourse}
+          onLeaveCourse={leaveCourse}
         />
         <ParticipantOther
-          pseudonym={participant.pseudonym}
-          avatar={participant.avatar}
+          pseudonym={participant?.pseudonym}
+          avatar={participant?.avatar}
           points={participation?.points}
         />
       </div>
