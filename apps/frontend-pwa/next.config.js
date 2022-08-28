83390ea1b7f30c1810924fd44423c3a5e19a0c78
Roland Schlaefli <rolandschlaefli@gmail.com>
2022-08-28 22:45:14 +0200

    miscellaneous architectural improvements

--- apps/frontend-pwa/next.config.js
+++ apps/frontend-pwa/next.config.js
@@ -3,7 +3,7 @@
 const withPWA = require('next-pwa')({
   dest: 'public',
   skipWaiting: true,
-  // disable: process.env.NODE_ENV === 'development',
+  disable: process.env.NODE_ENV === 'development',
 })
 
 const nextConfig = withPWA({
