42d3d2dffe3844dbca41c84c3dcb769e6b359e1b
Julius Schlapbach <julius.schlapbach@gmx.net>
2022-09-01 13:24:03 +0200

    wip: start migrating components from legacy version to new klicker pwa

--- apps/frontend-pwa/src/components/Layout.tsx
+++ apps/frontend-pwa/src/components/Layout.tsx
@@ -1,7 +1,165 @@
-import { PropsWithChildren } from "react";
+import { faArrowsRotate, faBars } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Button } from '@uzh-bf/design-system'
+import React, { useEffect, useState } from 'react'
+import { FormattedMessage } from 'react-intl'
+import { Icon } from 'semantic-ui-react'
 
-function Layout({ children}: PropsWithChildren) {
-  return <div>{children}</div>
+// TODO: move NotificationBadge to design system?
+// import NotificationBadge from '../common/NotificationBadge'
+
+interface LayoutProps {
+  children: React.ReactNode
+  isAuthenticationEnabled?: boolean
+  isInteractionEnabled?: boolean
+  sidebar: any
+  subscribeToMore: () => void
+  title: string
+  questionIds: any
+  feedbackIds?: any
+  responseIds?: any
 }
+
+const defaultProps = {
+  isAuthenticationEnabled: false,
+  isInteractionEnabled: false,
+}
+
+function Layout({
+  children,
+  isAuthenticationEnabled,
+  isInteractionEnabled,
+  sidebar,
+  title,
+  questionIds,
+  feedbackIds,
+  responseIds,
+  subscribeToMore,
+}: LayoutProps): React.ReactElement {
+  useEffect((): void => {
+    subscribeToMore()
+  }, [])
+
+  const [totalUnseenCount, setTotalUnseenCount] = useState(0)
+  const [unseenQuestions, setUnseenQuestions] = useState(0)
+  const [unseenFeedbacks, setUnseenFeedbacks] = useState(0)
+
+  useEffect(() => {
+    if (sidebar.activeItem === 'activeQuestion' && questionIds) {
+      questionIds.forEach((questionId: string) => {
+        sessionStorage?.setItem(questionId, 'true')
+      })
+    } else if (feedbackIds) {
+      feedbackIds.forEach((feedbackId: string) => {
+        sessionStorage?.setItem(feedbackId, 'true')
+      })
+
+      if (responseIds) {
+        responseIds.forEach((responseId: string) => {
+          sessionStorage?.setItem(responseId, 'true')
+        })
+      }
+    }
+  }, [questionIds, feedbackIds, responseIds])
+
+  useEffect(() => {
+    const unseenQuestionCount = questionIds.filter(
+      (questionId: string) => !sessionStorage?.getItem(questionId)
+    ).length
+    const unseenFeedbackCount = feedbackIds.filter(
+      (feedbackId: string) => !sessionStorage?.getItem(feedbackId)
+    ).length
+    const unseenResponseCount = responseIds.filter(
+      (responseId: string) => !sessionStorage?.getItem(responseId)
+    ).length
+    setUnseenQuestions(unseenQuestionCount)
+    setUnseenFeedbacks(unseenFeedbackCount + unseenResponseCount)
+    setTotalUnseenCount(
+      unseenQuestionCount + unseenFeedbackCount + unseenResponseCount
+    )
+  }, [questionIds, feedbackIds, responseIds])
+
+  return (
+    <div>
+      <div className="flex flex-col min-h-full">
+        <div className="border-0 !border-b border-solid border-gray-300  flex justify-between flex-initial p-[0.3rem] items-center md:!hidden">
+          <div className="relative h-10 mt-0 w-11">
+            <Button
+              active={sidebar.sidebarVisible}
+              className="justify-center w-10 h-10"
+              disabled={!isInteractionEnabled}
+              onClick={sidebar.handleToggleSidebarVisible}
+            >
+              <Button.Icon>
+                <FontAwesomeIcon icon={faBars} />
+              </Button.Icon>
+            </Button>
+            {isInteractionEnabled && (
+              <NotificationBadge count={totalUnseenCount} />
+            )}
+          </div>
+
+          <h1 className="m-0 !text-lg">
+            {isAuthenticationEnabled && <Icon color="green" name="lock" />}{' '}
+            {title}
+          </h1>
+          <Button
+            className="justify-center w-10 h-10"
+            onClick={(): void => window.location.reload()}
+          >
+            <Button.Icon>
+              <FontAwesomeIcon icon={faArrowsRotate} />
+            </Button.Icon>
+          </Button>
+        </div>
+
+        <div className="flex flex-1">
+          {/* // TODO: replace sidebar with menubar at the bottom for mobile devices
+           <Sidebar
+            items={[
+              <SidebarItem
+                active={sidebar.activeItem === 'activeQuestion'}
+                handleSidebarItemClick={sidebar.handleSidebarActiveItemChange(
+                  'activeQuestion'
+                )}
+                icon="question"
+                key="activeQuestion"
+                name="activeQuestion"
+                unseenItems={unseenQuestions}
+              >
+                <FormattedMessage
+                  defaultMessage="Active Question"
+                  id="joinSession.sidebar.activeQuestion"
+                />
+              </SidebarItem>,
+              isInteractionEnabled && (
+                <SidebarItem
+                  active={sidebar.activeItem === 'feedbackChannel'}
+                  handleSidebarItemClick={sidebar.handleSidebarActiveItemChange(
+                    'feedbackChannel'
+                  )}
+                  icon="talk"
+                  key="feedbackChannel"
+                  name="feedbackChannel"
+                  unseenItems={unseenFeedbacks}
+                >
+                  <FormattedMessage
+                    defaultMessage="Feedback-Channel"
+                    id="joinSession.sidebar.feedbackChannel"
+                  />
+                </SidebarItem>
+              ),
+            ].filter(Boolean)}
+            visible={sidebar.sidebarVisible}
+          >
+            {children}
+          </Sidebar> */}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+Layout.defaultProps = defaultProps
 
 export default Layout
---
+++ apps/frontend-pwa/src/components/liveSession/QuestionFiles.tsx
@@ -0,0 +1,55 @@
+import getConfig from 'next/config'
+import Image from 'next/image'
+import React from 'react'
+import { Card, Image as SemanticImage, Modal } from 'semantic-ui-react'
+
+const { publicRuntimeConfig } = getConfig()
+
+interface Props {
+  files: {
+    id: string
+    name: string
+    description?: string
+  }[]
+  isCompact?: boolean
+}
+
+const defaultProps = {
+  files: [],
+  isCompact: false,
+}
+
+function QuestionFiles({ files, isCompact }: Props): React.ReactElement {
+  return (
+    <div className="flex flex-row flex-wrap gap-2">
+      {files.map(({ id, name, description }, ix): React.ReactElement => {
+        const fileSrc = `${publicRuntimeConfig.s3root}/${name}`
+        const previewImage = (
+          <Card>
+            <Image alt={description} crossOrigin="anonymous" height={50} objectFit="cover" src={fileSrc} width={75} />
+            {!isCompact && (
+              <Card.Content extra className="!p-1 !text-xs">
+                #{ix + 1}
+              </Card.Content>
+            )}
+          </Card>
+        )
+
+        return (
+          <div className="w-[75px]" key={id}>
+            <Modal closeIcon trigger={previewImage}>
+              {description && <Modal.Header>{description}</Modal.Header>}
+              <Modal.Content image>
+                <SemanticImage alt={description} crossOrigin="anonymous" src={fileSrc} />
+              </Modal.Content>
+            </Modal>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+QuestionFiles.defaultProps = defaultProps
+
+export default QuestionFiles
