e2d8b99e534e49ee925dddf86b086a3da319a979
Julius Schlapbach <julius.schlapbach@gmx.net>
2022-09-01 18:49:31 +0200

    wip: fetch data in serverSideProps using apollo client

--- apps/frontend-pwa/src/pages/session/[id].tsx
+++ apps/frontend-pwa/src/pages/session/[id].tsx
@@ -1,24 +1,42 @@
 // TODO: remove solution data from getSession query (in service as for learning element)
 
-import { useQuery } from '@apollo/client'
 import { GetSessionDocument } from '@klicker-uzh/graphql/dist/ops'
-import { useRouter } from 'next/router'
+import { GetServerSideProps } from 'next'
+// import { useRouter } from 'next/router'
+// import { useQuery } from '@apollo/client'
 
-function Index() {
-  const router = useRouter()
+import { initializeApollo } from '@lib/apollo'
 
-  const { loading, error, data } = useQuery(GetSessionDocument, {
+function Index({ session }: any) {
+  // const router = useRouter()
+
+  // const { loading, error, data } = useQuery(GetSessionDocument, {
+  //   variables: {
+  //     id: router.query.id as string,
+  //   },
+  // })
+
+  // if (loading || !data) return <p>Loading...</p>
+  // if (error) return <p>Oh no... {error.message}</p>
+
+  // console.log(data)
+
+  console.log(session)
+
+  return <div className="p-4">{session?.id}</div>
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const apolloClient = initializeApollo()
+
+  const result = await apolloClient.query({
+    query: GetSessionDocument,
     variables: {
-      id: router.query.id as string,
+      id: 'cfa0ca6e-b22a-4974-99fa-245a33a939ce' as string,
     },
   })
 
-  if (loading || !data) return <p>Loading...</p>
-  if (error) return <p>Oh no... {error.message}</p>
-
-  console.log(data)
-
-  return <div className="p-4">{data.session?.id}</div>
+  return { props: { session: result.data?.session } }
 }
 
 export default Index
