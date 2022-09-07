746ebc147e86550f22fcc5bfb8dd40e0218e99ec
Julius Schlapbach <julius.schlapbach@gmx.net>
2022-09-07 15:52:50 +0200

    refactor: reorganize menu bar on mobile devices in separate layout component

--- apps/frontend-pwa/src/components/Layout.tsx
+++ apps/frontend-pwa/src/components/Layout.tsx
@@ -1,7 +1,83 @@
-import { PropsWithChildren } from 'react'
+import { faCommentDots } from '@fortawesome/free-regular-svg-icons'
+import { faQuestion, faRankingStar } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Head from 'next/head'
+import React from 'react'
+import { twMerge } from 'tailwind-merge'
 
-function Layout({ children }: PropsWithChildren) {
-  return <div>{children}</div>
+import MobileMenuBar from './common/MobileMenuBar'
+
+interface LayoutProps {
+  children: React.ReactNode
+  displayName: string
+  isFeedbackAreaVisible: boolean
+  isGamificationEnabled: boolean
+  setActiveMobilePage: (value: string) => void
+  className?: string
+}
+
+function Layout({
+  children,
+  displayName,
+  isFeedbackAreaVisible,
+  isGamificationEnabled,
+  setActiveMobilePage,
+  className,
+}: LayoutProps) {
+  const mobileMenuItems = [
+    {
+      value: 'questions',
+      label: 'Questions',
+      icon: <FontAwesomeIcon icon={faQuestion} size="lg" />,
+    },
+  ]
+
+  if (isFeedbackAreaVisible) {
+    mobileMenuItems.push({
+      value: 'feedbacks',
+      label: 'Feedback',
+      icon: <FontAwesomeIcon icon={faCommentDots} size="lg" />,
+    })
+  }
+  if (isGamificationEnabled) {
+    mobileMenuItems.push({
+      value: 'leaderboard',
+      label: 'Leaderboard',
+      icon: <FontAwesomeIcon icon={faRankingStar} size="lg" />,
+    })
+  }
+
+  return (
+    <div className="w-full h-full md:p-1.5 bg-uzh-grey-60">
+      <Head>
+        <title>Live Session</title>
+        <meta
+          name="description"
+          content={'Live Session ' + displayName}
+          charSet="utf-8"
+        ></meta>
+      </Head>
+
+      <div
+        className={twMerge(
+          'absolute flex flex-row top-0 left-0 right-0 md:p-1.5 overflow-auto gap-1.5',
+          'bg-white md:bg-uzh-grey-60 md:overflow-scroll min-h-screen h-max',
+          (isFeedbackAreaVisible || isGamificationEnabled) &&
+            'bottom-16 md:bottom-0'
+        )}
+      >
+        {children}
+        {mobileMenuItems.length > 1 && (
+          <div className="absolute bottom-0 w-full h-16 md:hidden">
+            <MobileMenuBar
+              menuItems={mobileMenuItems}
+              onClick={setActiveMobilePage}
+            />
+          </div>
+        )}
+      </div>
+    </div>
+  )
 }
 
 export default Layout
--- apps/frontend-pwa/src/pages/session/[id].tsx
+++ apps/frontend-pwa/src/pages/session/[id].tsx
@@ -1,38 +1,17 @@
 // TODO: remove solution data in a more specialized query than getSession (only the active instances are required)
 
-import { faCommentDots } from '@fortawesome/free-regular-svg-icons'
-import { faQuestion, faRankingStar } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Session } from '@klicker-uzh/graphql/dist/ops'
 import { GetServerSideProps } from 'next'
-import Head from 'next/head'
 import { useRouter } from 'next/router'
 import { useState } from 'react'
 import { twMerge } from 'tailwind-merge'
 
+import Leaderboard from '@components/common/Leaderboard'
+import Layout from '@components/Layout'
 import { addApolloState } from '@lib/apollo'
 import { getSessionData } from '@lib/joinData'
 import FeedbackArea from '../../components/liveSession/FeedbackArea'
-import MobileMenuBar from '../../components/liveSession/MobileMenuBar'
 import QuestionArea from '../../components/liveSession/QuestionArea'
-
-const mobileMenuItems = [
-  {
-    value: 'questions',
-    label: 'Questions',
-    icon: <FontAwesomeIcon icon={faQuestion} size="lg" />,
-  },
-  {
-    value: 'feedbacks',
-    label: 'Feedback',
-    icon: <FontAwesomeIcon icon={faCommentDots} size="lg" />,
-  },
-  {
-    value: 'leaderboard',
-    label: 'Leaderboard',
-    icon: <FontAwesomeIcon icon={faRankingStar} size="lg" />,
-  },
-]
 
 function Index({
   activeBlock,
@@ -54,87 +33,90 @@ function Index({
   const handleNewResponse = () => {}
 
   return (
-    <div className="md:p-1.5 w-full h-full bg-uzh-grey-60">
-      <Head>
-        <title>Live Session</title>
-        <meta
-          name="description"
-          content={'Live Session ' + displayName}
-          charSet="utf-8"
-        ></meta>
-      </Head>
+    <Layout
+      displayName={displayName}
+      setActiveMobilePage={setActiveMobilePage}
+      isFeedbackAreaVisible={
+        isFeedbackChannelPublic || isAudienceInteractionActive
+      }
+      isGamificationEnabled={isGamificationEnabled}
+    >
+      <div
+        className={twMerge(
+          'p-4 md:rounded-lg md:border-2 md:border-solid md:border-uzh-blue-40 w-full bg-white hidden md:block min-h-full',
+          (isFeedbackChannelPublic || isAudienceInteractionActive) &&
+            'md:w-1/2',
+          activeMobilePage === 'questions' && 'block'
+        )}
+      >
+        {activeBlock === -1 ||
+        activeBlock === blocks?.length ||
+        blocks?.length === 0 ||
+        !blocks ? (
+          isGamificationEnabled ? (
+            <div
+              className={twMerge('w-full bg-white hidden md:block min-h-full')}
+            >
+              <Leaderboard />
+            </div>
+          ) : (
+            'Keine Frage aktiv.'
+          )
+        ) : (
+          <QuestionArea
+            expiresAt={blocks[activeBlock]?.expiresAt}
+            questions={
+              blocks[activeBlock]?.instances.map((question: any) => {
+                return { ...question.questionData, instanceId: question.id }
+              }) || []
+            }
+            handleNewResponse={handleNewResponse}
+            sessionId={sessionId}
+            timeLimit={blocks[activeBlock]?.timeLimit as number}
+            execution={blocks[activeBlock]?.execution || 0}
+          />
+        )}
+      </div>
 
-      <div className="absolute flex flex-row bottom-16 md:bottom-0 top-0 left-0 right-0 md:p-1.5 overflow-auto gap-1.5 bg-white md:bg-uzh-grey-60 md:overflow-scroll min-h-screen h-max">
+      {isGamificationEnabled && (
         <div
           className={twMerge(
-            'p-4 md:rounded-lg md:border-2 md:border-solid md:border-uzh-blue-40 w-full bg-white hidden md:block min-h-full',
-            (isFeedbackChannelPublic || isAudienceInteractionActive) &&
-              'md:w-1/2',
-            activeMobilePage === 'questions' && 'block'
+            'w-full p-4 bg-white hidden min-h-full',
+            activeMobilePage === 'leaderboard' && 'block md:hidden'
           )}
         >
-          {activeBlock === -1 ||
-          activeBlock === blocks?.length ||
-          blocks?.length === 0 ||
-          !blocks ? (
-            'Keine Frage aktiv.'
-          ) : (
-            <QuestionArea
-              expiresAt={blocks[activeBlock]?.expiresAt}
-              questions={
-                blocks[activeBlock]?.instances.map((question: any) => {
-                  return { ...question.questionData, instanceId: question.id }
-                }) || []
-              }
-              handleNewResponse={handleNewResponse}
-              sessionId={sessionId}
-              timeLimit={blocks[activeBlock]?.timeLimit as number}
-              execution={blocks[activeBlock]?.execution || 0}
-            />
-          )}
+          <Leaderboard />
         </div>
+      )}
 
-        {isGamificationEnabled && (
-          <div
-            className={twMerge(
-              'w-full md:w-1/2 p-4 bg-white md:border-2 md:border-solid md:rounded-lg md:border-uzh-blue-40 hidden md:block min-h-full',
-              activeMobilePage === 'leaderboard' && 'block'
-            )}
-          >
-            LEADERBOARD
-          </div>
-        )}
-
-        {(isFeedbackChannelPublic || isAudienceInteractionActive) && (
-          <div
-            className={twMerge(
-              'w-full md:w-1/2 p-4 bg-white md:border-2 md:border-solid md:rounded-lg md:border-uzh-blue-40 hidden md:block min-h-full',
-              activeMobilePage === 'feedbacks' && 'block'
-            )}
-          >
-            <FeedbackArea feedbacks={[]} />
-          </div>
-        )}
-      </div>
-
-      <div className="absolute bottom-0 w-full h-16 md:hidden">
-        <MobileMenuBar
-          menuItems={mobileMenuItems}
-          onClick={setActiveMobilePage}
-        />
-      </div>
-    </div>
+      {(isFeedbackChannelPublic || isAudienceInteractionActive) && (
+        <div
+          className={twMerge(
+            'w-full md:w-1/2 p-4 bg-white md:border-2 md:border-solid md:rounded-lg md:border-uzh-blue-40 hidden md:block min-h-full',
+            activeMobilePage === 'feedbacks' && 'block'
+          )}
+        >
+          <FeedbackArea feedbacks={[]} />
+        </div>
+      )}
+    </Layout>
   )
 }
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const withNewSessionData = await getSessionData(ctx)
 
-  return addApolloState(withNewSessionData.apolloClient, {
-    props: {
-      ...withNewSessionData.result,
-    },
-  })
+  console.log(withNewSessionData)
+
+  if (withNewSessionData) {
+    return addApolloState(withNewSessionData.apolloClient, {
+      props: {
+        ...withNewSessionData.result,
+      },
+    })
+  } else {
+    return {}
+  }
 }
 
 export default Index
