04e7874290b594f100bf0ed0adf549aa969ad1ce
mxjweb <maximilian.weber@bluewin.ch>
2022-09-08 11:53:07 +0200

    feat: avater view without tailwind added

--- apps/frontend-pwa/src/pages/profile.tsx
+++ apps/frontend-pwa/src/pages/profile.tsx
@@ -2,14 +2,14 @@ import { useMutation, useQuery } from '@apollo/client'
 import Layout from '@components/Layout'
 import { SelfDocument } from '@klicker-uzh/graphql/dist/ops'
 import { NextPageWithLayout } from '@pages/_app'
-import { H1 } from '@uzh-bf/design-system'
+import { H1, Button } from '@uzh-bf/design-system'
 import { ThreeDots } from 'react-loader-spinner'
-import Avatar, { genConfig } from 'react-nice-avatar'
+import { BigHead } from '@bigheads/core'
+import Router from 'next/router'
 
 const Profile = () => {
   const [pageInIframe, setPageInIframe] = useState(false)
   const { data, error, loading } = useQuery(SelfDocument)
-  const config = genConfig({ sex: 'man', mouthStyle: 'laugh' })
 
   // detect if the page is currently shown as an iframe (i.e. in OLAT) -> hide the logout button in this case
   useEffect(() => {
@@ -36,11 +36,33 @@ const Profile = () => {
   }
 
   return (
-    <div className="flex h-screen">
-      <div className="m-auto">
-        <Avatar className="w-32 h-32" {...config} />
-        <H1>Test</H1>
-      </div>
+    <div>
+      <H1>Profil</H1>
+      <BigHead
+        style={{ width: '400px', height: '400px'}}
+        accessory="roundGlasses"
+        body="breasts"
+        circleColor="blue"
+        clothing="vneck"
+        clothingColor="blue"
+        eyebrows="serious"
+        eyes="leftTwitch"
+        faceMask={false}
+        faceMaskColor="green"
+        facialHair="stubble"
+        graphic="none"
+        hair="balding"
+        hairColor="blonde"
+        hat="none"
+        hatColor="white"
+        lashes={false}
+        lipColor="red"
+        mask
+        mouth="lips"
+        skinTone="yellow"
+      />
+      <p className=''>{data?.self?.username}</p>
+      <Button onClick={() => Router.push('/edit_profile')}>Bearbeiten</Button>
     </div>
   )
 }
