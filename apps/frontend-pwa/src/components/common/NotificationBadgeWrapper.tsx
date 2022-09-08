1b72ad6ebc98f124f957baa2fb067a54ac0b23ea
Julius Schlapbach <julius.schlapbach@gmx.net>
2022-09-08 17:01:05 +0200

    enhance(frontend): finalize styling of notification badge with more customization options

--- apps/frontend-pwa/src/components/common/NotificationBadgeWrapper.tsx
+++ apps/frontend-pwa/src/components/common/NotificationBadgeWrapper.tsx
@@ -5,49 +5,50 @@ interface NotificationBadgeWrapperProps {
   count?: number
   withoutCount?: boolean
   size?: 'sm' | 'md' | 'lg' | 'xl'
+  badgePosition?: string
+  badgeStyle?: string
   className?: string
   children: React.ReactNode
 }
 
-const defaultProps = {} // TODO
+const defaultProps = {
+  count: undefined,
+  withoutCount: undefined,
+  size: 'md',
+  badgePosition: '',
+  badgeStyle: '',
+  className: '',
+}
 
 function NotificationBadgeWrapper({
   count,
   withoutCount,
   size,
+  badgePosition,
+  badgeStyle,
   className,
   children,
 }: NotificationBadgeWrapperProps) {
   const sizeStyles = {
     sm: 'h-4 w-4 text-xs',
-    md: 'h-[1.2rem] w-[1.2rem] text-sm',
-    lg: 'h-[1.6rem] w-[1.6rem] text-lg',
-    xl: 'h-7 w-7 text-xl pt-[0.1rem]',
+    md: 'h-5 w-5 text-sm leading-[1.1rem]',
+    lg: 'h-[1.6rem] w-[1.6rem] text-lg leading-6',
+    xl: 'h-7 w-7 text-xl leading-7',
   }
-  console.log(count)
-  if (!count && withoutCount) {
+
+  if (count || withoutCount) {
     return (
       <div className={twMerge('relative', className)}>
         <div className={twMerge('flex flex-1')}>{children}</div>
         <div
           className={twMerge(
             'absolute right-1 z-10 text-center text-white bg-red-600 rounded-full top-1',
-            sizeStyles[size || 'md']
-          )}
-        />
-      </div>
-    )
-  } else if (count) {
-    return (
-      <div className={twMerge('relative', className)}>
-        <div className={twMerge('flex flex-1')}>{children}</div>
-        <div
-          className={twMerge(
-            'absolute right-1 z-10 text-center text-white bg-red-600 rounded-full top-1',
-            sizeStyles[size || 'md']
+            sizeStyles[size || 'md'],
+            badgePosition,
+            badgeStyle
           )}
         >
-          {count < 10 && count > 0 ? count : '9+'}
+          {!count ? '' : count < 10 && count > 0 ? count : '9+'}
         </div>
       </div>
     )
@@ -56,4 +57,5 @@ function NotificationBadgeWrapper({
   }
 }
 
+NotificationBadgeWrapper.defaultProps = defaultProps
 export default NotificationBadgeWrapper
