e36b3f0865f6703f127dd5bd24b7786773a5afc9
mxjweb <maximilian.weber@bluewin.ch>
2022-09-08 17:44:25 +0200

    feat: dropdown working
    Co-authored-by: Roland Schläfli <roland.schlaefli@bf.uzh.ch>

--- apps/frontend-pwa/src/pages/edit_profile.tsx
+++ apps/frontend-pwa/src/pages/edit_profile.tsx
@@ -4,59 +4,84 @@ import { SelfDocument } from '@klicker-uzh/graphql/dist/ops'
 import { NextPageWithLayout } from '@pages/_app'
 import { H1, Button } from '@uzh-bf/design-system'
 import { ThreeDots } from 'react-loader-spinner'
-import { BigHead } from '@bigheads/core'
-import Router from 'next/router'
+import { BigHead, AvatarProps, Avatar } from '@bigheads/core'
+import React, { useState } from 'react'
+import { ErrorMessage, Field, Form, Formik } from 'formik'
+import { getNamedRouteRegex } from 'next/dist/shared/lib/router/utils/route-regex'
+import * as yup from 'yup'
 
-const Edit_profile: NextPageWithLayout = () => {
+const EditProfile: NextPageWithLayout = () => {
+  // TODO: add avatar specification as JSON field in DB and retrieve here
   const { data, error, loading } = useQuery(SelfDocument)
 
-  if (loading) {
-    return (
-      <div className="grid items-center justify-center">
-        <ThreeDots
-          height="80"
-          width="80"
-          radius="9"
-          color="#4fa94d"
-          ariaLabel="three-dots-loading"
-          visible={true}
-        />
-      </div>
-    )
-  }
+// TODO: the initial values are set based on the user profile (current state of db)
+const initialValues: {
+  name: string
+  body: AvatarProps['body']
+} = { name: data.username, body: 'chest' }
+
 
   return (
-    <div>
-      <H1>Profil Bearbeiten</H1>
-      <BigHead
-        style={{ width: '400px', height: '400px'}}
-        accessory="roundGlasses"
-        body="breasts"
-        circleColor="blue"
-        clothing="vneck"
-        clothingColor="blue"
-        eyebrows="serious"
-        eyes="leftTwitch"
-        faceMask={false}
-        faceMaskColor="green"
-        facialHair="stubble"
-        graphic="none"
-        hair="balding"
-        hairColor="blonde"
-        hat="none"
-        hatColor="white"
-        lashes={false}
-        lipColor="red"
-        mask
-        mouth="lips"
-        skinTone="yellow"
-      />
-    </div>
-  )
+      <Formik 
+      validationSchema={yup.object({
+        body: yup.string(),
+        // TODO: min and max length of username
+        name: yup.string().min(5),
+      })}
+      initialValues={initialValues} onSubmit={(values, { setSubmitting }) => {
+        setSubmitting(true)
+        console.log('submit', values)
+        setSubmitting(false)
+      }}>
+        {({ values, errors, isSubmitting, isValid }) => {
+          return (
+            <div>
+            <H1>Profil Bearbeiten</H1>
+        <BigHead
+          style={{ width: '300px', height: '300px'}}
+          accessory="roundGlasses"
+          body={values.body}
+          circleColor="blue"
+          clothing='shirt'
+          clothingColor="blue"
+          eyebrows="serious"
+          eyes="leftTwitch"
+          faceMask={false}
+          faceMaskColor="green"
+          facialHair="stubble"
+          graphic="none"
+          hair="balding"
+          hairColor="blonde"
+          hat="none"
+          hatColor="white"
+          lashes={false}
+          lipColor="red"
+          mask
+          mouth="lips"
+          skinTone="yellow"
+        />
+        <Form>
+        
+              <Field as="select" name="body">
+                <option value='chest'>Male</option>
+                <option value='breasts'>Female</option>
+              </Field>
+
+              <Field type="text" name="username" />
+
+              <Button type="submit" disabled={isSubmitting || !isValid}>Save</Button>
+            </Form>
+            <div>{values.body}</div>
+        </div>
+
+          )
+        }}
+      </Formik>
+      )
 }
 
-Edit_profile.getLayout = function getLayout(page) {
+EditProfile.getLayout = function getLayout(page) {
   return <Layout>{page}</Layout>
-}
+} 
 
-export default Edit_profile
+export default EditProfile
