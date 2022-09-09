df5ce722d26ed13cc88dd956fd5f15ff27f019c3
Julius Schlapbach <julius.schlapbach@gmx.net>
2022-09-09 10:59:43 +0200

    chore(backend-*): fix text suite and config for the cache to work as expected

--- apps/frontend-pwa/next.config.js
+++ apps/frontend-pwa/next.config.js
@@ -12,6 +12,8 @@ const nextConfig = withPWA({
   publicRuntimeConfig: {
     API_URL:
       process.env.NEXT_PUBLIC_API_URL ?? 'http://127.0.0.1:7071/api/graphql',
+    ADDRESPONSE_URL:
+      process.env.NEXT_PUBLIC_ADDRESPONSE_URL ?? 'http://127.0.0.1:7072/api/AddResponse',
   },
   serverRuntimeConfig: {
     APP_DOMAIN: process.env.APP_DOMAIN ?? '127.0.0.1',
