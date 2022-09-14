360336a59d1cc8aad9d46a27a9183c05c15d0a2e
florinavogel <florina.vogel@gmail.com>
2022-09-14 14:22:58 +0200

    Apply styling changes to participant login

--- apps/frontend-pwa/src/pages/login.tsx
+++ apps/frontend-pwa/src/pages/login.tsx
@@ -1,10 +1,9 @@
 import { FetchResult, useMutation } from '@apollo/client'
-import { faArrowRightToBracket } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { LoginParticipantDocument } from '@klicker-uzh/graphql/dist/ops'
 import * as RadixLabel from '@radix-ui/react-label'
 import { Button, H1 } from '@uzh-bf/design-system'
 import { ErrorMessage, Field, Form, Formik } from 'formik'
+import Image from 'next/image'
 import Router from 'next/router'
 import { useState } from 'react'
 import { twMerge } from 'tailwind-merge'
@@ -45,77 +44,84 @@ function LoginForm() {
     }
   }
   return (
-    <Formik
-      initialValues={{ username: '', password: '' }}
-      validationSchema={loginSchema}
-      onSubmit={onSubmit}
-    >
-      {({ errors, touched, isSubmitting }) => {
-        return (
-          <div className="flex flex-col items-center justify-center w-full h-full">
-            <H1>Sign in</H1>
-            <Form className="w-40 sm:w-64">
-              <RadixLabel.Root
-                htmlFor="username"
-                className="text-sm leading-7 text-gray-600"
-              >
-                Username
-              </RadixLabel.Root>
-              <Field
-                name="username"
-                type="text"
-                className={twMerge(
-                  'w-full px-3 py-1 text-base leading-8 text-gray-700 transition-colors',
-                  'duration-200 ease-in-out bg-gray-100 bg-opacity-50 border border-gray-300',
-                  'rounded outline-none focus:ring-2 focus:bg-transparent focus:ring-indigo-200 focus:border-indigo-500',
-                  errors.username &&
-                    touched.username &&
-                    'border-red-400 bg-red-50'
-                )}
-              />
-              <ErrorMessage
-                name="username"
-                component="div"
-                className="text-sm text-red-400"
-              />
-
-              <RadixLabel.Root
-                className="text-sm leading-7 text-gray-600"
-                htmlFor="password"
-              >
-                Password
-              </RadixLabel.Root>
-              <Field
-                name="password"
-                type="password"
-                className={twMerge(
-                  'w-full px-3 py-1 text-base leading-8 text-gray-700 transition-colors',
-                  'duration-200 ease-in-out bg-gray-100 bg-opacity-50 border border-gray-300',
-                  'rounded outline-none focus:ring-2 focus:bg-transparent focus:ring-indigo-200 focus:border-indigo-500',
-                  errors.password &&
-                    touched.password &&
-                    'border-red-400 bg-red-50'
-                )}
-              />
-              <ErrorMessage
-                name="password"
-                component="div"
-                className="text-sm text-red-400"
-              />
-              {error && <ErrorNotification message={error} />}
-              <div className="flex justify-center mt-7">
-                <Button active type="submit" disabled={isSubmitting}>
-                  <Button.Icon>
-                    <FontAwesomeIcon icon={faArrowRightToBracket} />
-                  </Button.Icon>
-                  <Button.Label>Submit</Button.Label>
-                </Button>
+    <div className="relative flex flex-col items-center justify-center w-screen h-screen pb-20">
+      <Formik
+        initialValues={{ username: '', password: '' }}
+        validationSchema={loginSchema}
+        onSubmit={onSubmit}
+      >
+        {({ errors, touched, isSubmitting }) => {
+          return (
+            <div className="">
+              <div className="w-full mb-8 text-center sm:mb-12">
+                <Image
+                  src="/KlickerLogo.png"
+                  width={300}
+                  height={90}
+                  alt="KlickerUZH Logo"
+                />
               </div>
-            </Form>
-          </div>
-        )
-      }}
-    </Formik>
+              <H1>Login</H1>
+              <div className="mb-10">
+                <Form className="w-72 sm:w-96">
+                  <RadixLabel.Root
+                    htmlFor="username"
+                    className="text-sm leading-7 text-gray-600"
+                  >
+                    Username
+                  </RadixLabel.Root>
+                  <Field
+                    name="username"
+                    type="text"
+                    className={twMerge(
+                      'w-full rounded bg-uzh-grey-20 bg-opacity-50 border border-uzh-grey-60 focus:border-uzh-blue-50 mb-2',
+                      errors.username &&
+                        touched.username &&
+                        'border-red-400 bg-red-50'
+                    )}
+                  />
+                  <ErrorMessage
+                    name="username"
+                    component="div"
+                    className="text-sm text-red-400"
+                  />
+
+                  <RadixLabel.Root
+                    className="text-sm leading-7 text-gray-600"
+                    htmlFor="password"
+                  >
+                    Passwort
+                  </RadixLabel.Root>
+                  <Field
+                    name="password"
+                    type="password"
+                    className={twMerge(
+                      'w-full rounded bg-uzh-grey-20 bg-opacity-50 border border-uzh-grey-60 focus:border-uzh-blue-50 mb-2',
+                      touched.password && 'border-red-400 bg-red-50'
+                    )}
+                  />
+                  <ErrorMessage
+                    name="password"
+                    component="div"
+                    className="text-sm text-red-400"
+                  />
+                  {error && <ErrorNotification message={error} />}
+                  <div className="flex justify-center mt-7">
+                    <Button
+                      type="submit"
+                      disabled={isSubmitting}
+                      className="mt-2 border-uzh-grey-80"
+                    >
+                      <Button.Label>Anmelden</Button.Label>
+                    </Button>
+                  </div>
+                </Form>
+              </div>
+            </div>
+          )
+        }}
+      </Formik>
+    </div>
   )
 }
 
