27c7740feb746734cd6c9fd9b17a77c66fc1c5a4
florinavogel <58864918+florinavogel@users.noreply.github.com>
2022-09-14 16:06:14 +0200

    feat(apps/frontend-pwa): add logic to login page and connect index to backend (#2878)

---
+++ apps/frontend-pwa/src/components/CourseElement.tsx
@@ -0,0 +1,79 @@
+import { faBellSlash } from '@fortawesome/free-regular-svg-icons'
+import { faBell } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import * as TogglePrimitive from '@radix-ui/react-toggle'
+import Link from 'next/link'
+import { Dispatch, SetStateAction, useState } from 'react'
+import { twMerge } from 'tailwind-merge'
+
+type courseElementProps = {
+  courseName: string
+  courseId: string
+  disabled: boolean
+  onSubscribeClick: (
+    subscribed: boolean,
+    setSubscribed: Dispatch<SetStateAction<boolean>>,
+    courseId: string
+  ) => void
+}
+
+const CourseElement = (props: courseElementProps) => {
+  // TODO: Fetch initial subscription state from DB
+  const [subscribed, setSubscribed] = useState(false)
+
+  const computedClassNameSubscribed = twMerge(
+    props.disabled
+      ? 'text-uzh-grey-20'
+      : 'text-uzh-yellow-100 hover:text-uzh-grey-20'
+  )
+
+  const computedClassNameNotSubscribed = twMerge(
+    props.disabled
+      ? 'text-uzh-grey-20'
+      : 'text-uzh-grey-20 hover:text-uzh-yellow-100'
+  )
+
+  const computedClassNameButton = twMerge(
+    'h-full p-3 text-2xl rounded-r-md',
+    props.disabled ? 'bg-slate-400' : 'bg-slate-600'
+  )
+
+  return (
+    <div className="flex justify-between w-full mt-4 mb-4 mr-4align-center">
+      <span className="w-full p-4 text-l text-start rounded-l-md bg-uzh-grey-20 hover:bg-uzh-grey-40">
+        <Link href={`/course/${props.courseId}`}>{props.courseName}</Link>
+      </span>
+      <span className={computedClassNameButton}>
+        <TogglePrimitive.Root
+          defaultPressed={subscribed}
+          onPressedChange={setSubscribed}
+          asChild
+        >
+          <button
+            disabled={props.disabled}
+            onClick={() =>
+              props.onSubscribeClick(subscribed, setSubscribed, props.courseId)
+            }
+          >
+            {subscribed ? (
+              <FontAwesomeIcon
+                icon={faBell}
+                fixedWidth
+                className={computedClassNameSubscribed}
+              />
+            ) : (
+              <FontAwesomeIcon
+                icon={faBellSlash}
+                fixedWidth
+                flip="horizontal"
+                className={computedClassNameNotSubscribed}
+              />
+            )}
+          </button>
+        </TogglePrimitive.Root>
+      </span>
+    </div>
+  )
+}
+
+export default CourseElement
---
+++ apps/frontend-pwa/worker/index.js
@@ -0,0 +1,29 @@
+'use strict'
+
+self.addEventListener('push', function (event) {
+  const data = JSON.parse(event.data.text())
+  event.waitUntil(
+    registration.showNotification(data.title, {
+      body: data.message,
+      icon: '/manifest-icon-192.maskable.png'
+    })
+  )
+})
+
+self.addEventListener('notificationclick', function (event) {
+  event.notification.close()
+  event.waitUntil(
+    clients.matchAll({ type: 'window', includeUncontrolled: true }).then(function (clientList) {
+      if (clientList.length > 0) {
+        let client = clientList[0]
+        for (let i = 0; i < clientList.length; i++) {
+          if (clientList[i].focused) {
+            client = clientList[i]
+          }
+        }
+        return client.focus()
+      }
+      return clients.openWindow('/')
+    })
+  )
+})
