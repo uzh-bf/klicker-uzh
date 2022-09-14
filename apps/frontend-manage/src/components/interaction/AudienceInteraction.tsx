d93357eb9c509615c37263842c04afb20f3e0972
Julius Schlapbach <julius.schlapbach@gmx.net>
2022-09-14 18:51:10 +0200

    wip: implement toggles to turn on and off audience interaction and moderation

---
+++ apps/frontend-manage/src/components/interaction/AudienceInteraction.tsx
@@ -0,0 +1,235 @@
+import { faUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { push } from '@socialgouv/matomo-next'
+import { Button, Switch } from '@uzh-bf/design-system'
+import { useState } from 'react'
+import { twMerge } from 'tailwind-merge'
+
+// import DeleteFeedbackMutation from '../../graphql/mutations/DeleteFeedbackMutation.graphql'
+// import DeleteFeedbackResponseMutation from '../../graphql/mutations/DeleteFeedbackResponseMutation.graphql'
+// import PinFeedbackMutation from '../../graphql/mutations/PinFeedbackMutation.graphql'
+// import PublishFeedbackMutation from '../../graphql/mutations/PublishFeedbackMutation.graphql'
+// import ResolveFeedbackMutation from '../../graphql/mutations/ResolveFeedbackMutation.graphql'
+// import RespondToFeedbackMutation from '../../graphql/mutations/RespondToFeedbackMutation.graphql'
+// import UpdateSessionSettingsMutation from '../../graphql/mutations/UpdateSessionSettingsMutation.graphql'
+// import RunningSessionQuery from '../../graphql/queries/RunningSessionQuery.graphql'
+// import ConfusionAddedSubscription from '../../graphql/subscriptions/ConfusionAddedSubscription.graphql'
+// import FeedbackAddedSubscription from '../../graphql/subscriptions/FeedbackAddedSubscription.graphql'
+import { GetCockpitSessionDocument, ChangeSessionSettingsDocument } from '@klicker-uzh/graphql/dist/ops'
+import { useMutation } from '@apollo/client'
+
+// import ConfusionCharts from './confusion/ConfusionCharts'
+// import FeedbackChannel from './feedbacks/FeedbackChannel'
+
+interface Props {
+  sessionId: string
+  sessionName: string
+  confusionValues: any
+  feedbacks: any[]
+  isAudienceInteractionActive: boolean
+  isModerationEnabled: boolean
+  isGamificationEnabled: boolean
+}
+
+function AudienceInteraction({
+  sessionId,
+  sessionName,
+  confusionValues,
+  feedbacks,
+  isAudienceInteractionActive,
+  isModerationEnabled,
+  isGamificationEnabled
+}: Props) {
+  const deleteFeedback = (props: any) => null
+  const pinFeedback = (props: any) => null
+  const publishFeedback = (props: any) => null
+  const resolveFeedback = (props: any) => null
+  const respondToFeedback = (props: any) => null
+  const deleteFeedbackResponse = (props: any) => null
+
+  const [isChecked, setIsChecked] = useState(false)
+
+  const [changeSessionSettings] = useMutation(ChangeSessionSettingsDocument)
+
+  //   const [deleteFeedback] = useMutation(DeleteFeedbackMutation)
+  //   const [pinFeedback] = useMutation(PinFeedbackMutation)
+  //   const [publishFeedback] = useMutation(PublishFeedbackMutation)
+  //   const [resolveFeedback] = useMutation(ResolveFeedbackMutation)
+  //   const [respondToFeedback] = useMutation(RespondToFeedbackMutation)
+  //   const [deleteFeedbackResponse] = useMutation(DeleteFeedbackResponseMutation)
+
+  return (
+    <div>
+      <div className="flex flex-col flex-wrap justify-between gap-4 md:flex-row">
+        <div className="text-2xl font-bold print:hidden">
+          Publikumsinteraktion
+        </div>
+
+        <div className="hidden print:block">
+          <h1>Session &quot;{sessionName}&quot; - Feedback-Channel</h1>
+        </div>
+
+        <div className="flex flex-col flex-wrap self-start gap-4 md:flex-row print:hidden">
+          {isAudienceInteractionActive && (
+            <div className="order-3 md:order-1">
+              <a
+                href={`/sessions/feedbacks`}
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <Button className="h-10 px-4">
+                  <Button.Icon>
+                    <FontAwesomeIcon icon={faUpRightFromSquare} />
+                  </Button.Icon>
+                  <Button.Label>Dozierendenansicht</Button.Label>
+                </Button>
+              </a>
+            </div>
+          )}
+
+          <div className="flex items-center order-1 md:order-2">
+            <Switch
+              id="audienceInteraction-switch"
+              checked={isAudienceInteractionActive}
+              label=""
+              onCheckedChange={(): void => {
+                changeSessionSettings({
+                  refetchQueries: [{ query: GetCockpitSessionDocument }],
+                  variables: {
+                    id: sessionId,
+                    isAudienceInteractionActive: !isAudienceInteractionActive,
+                  },
+                })
+                push([
+                  'trackEvent',
+                  'Running Session',
+                  !isAudienceInteractionActive
+                    ? 'Feedback Channel Activated'
+                    : 'Feedback Channel Deactivated',
+                ])
+              }}
+            />
+            Publikumsinteraktion aktivieren
+          </div>
+
+          <div className="flex items-center order-2 md:order-3">
+            <Switch
+              id="moderation-switch"
+              checked={isModerationEnabled}
+              label=""
+              disabled={!isAudienceInteractionActive}
+              onCheckedChange={(): void => {
+                changeSessionSettings({
+                  refetchQueries: [{ query: GetCockpitSessionDocument }],
+                  variables: {
+                    id: sessionId,
+                    isModerationEnabled: !isModerationEnabled,
+                  },
+                })
+                push([
+                  'trackEvent',
+                  'Running Session',
+                  'Feedback Moderation Toggled',
+                  String(!isModerationEnabled),
+                ])
+              }}
+            />
+            <span className={twMerge(!isAudienceInteractionActive && 'text-gray-400')}>
+              Moderation aktivieren
+            </span>
+          </div>
+        </div>
+      </div>
+
+      {isAudienceInteractionActive && (
+        <div className="flex flex-col gap-4 md:flex-row md:flex-wrap">
+          <div className="flex flex-col flex-1 md:flex-row">
+            <div className="flex-1">
+              {/* <FeedbackChannel
+                feedbacks={feedbacks}
+                handleDeleteFeedback={(feedbackId: string): void => {
+                  deleteFeedback({ variables: { feedbackId, sessionId } })
+                  push(['trackEvent', 'Running Session', 'Feedback Deleted'])
+                }}
+                handleDeleteFeedbackResponse={(
+                  feedbackId: string,
+                  responseId: string
+                ) => {
+                  deleteFeedbackResponse({
+                    variables: { sessionId, feedbackId, responseId },
+                  })
+                  push([
+                    'trackEvent',
+                    'Running Session',
+                    'Feedback Response Deleted',
+                  ])
+                }}
+                handlePinFeedback={(feedbackId: string, pinState: boolean) => {
+                  pinFeedback({
+                    variables: { sessionId, feedbackId, pinState },
+                  })
+                  push([
+                    'trackEvent',
+                    'Running Session',
+                    'Feedback Pinned',
+                    String(pinState),
+                  ])
+                }}
+                handlePublishFeedback={(
+                  feedbackId: string,
+                  publishState: boolean
+                ) => {
+                  publishFeedback({
+                    variables: { sessionId, feedbackId, publishState },
+                  })
+                  push([
+                    'trackEvent',
+                    'Running Session',
+                    'Feedback Published',
+                    String(publishState),
+                  ])
+                }}
+                handleResolveFeedback={(
+                  feedbackId: string,
+                  resolvedState: boolean
+                ) => {
+                  resolveFeedback({
+                    variables: { sessionId, feedbackId, resolvedState },
+                  })
+                  push([
+                    'trackEvent',
+                    'Running Session',
+                    'Feedback Resolved',
+                    String(resolvedState),
+                  ])
+                }}
+                handleRespondToFeedback={(
+                  feedbackId: string,
+                  response: string
+                ) => {
+                  respondToFeedback({
+                    variables: { sessionId, feedbackId, response },
+                  })
+                  push([
+                    'trackEvent',
+                    'Running Session',
+                    'Feedback Response Added',
+                    response.length,
+                  ])
+                }}
+                isActive={isAudienceInteractionActive}
+                isPublic={isModerationEnabled}
+              /> */}
+            </div>
+          </div>
+
+          {/* <div className="flex-initial mx-auto md:mt-4 p-4 w-[300px] sm:w-[600px] lg:w-[300px] bg-primary-bg rounded shadow print:hidden border-primary border-solid border">
+            <ConfusionCharts confusionValues={confusionValues} />
+          </div> */}
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default AudienceInteraction
--- apps/frontend-manage/src/pages/cockpit/[id].tsx
+++ apps/frontend-manage/src/pages/cockpit/[id].tsx
@@ -1,10 +1,51 @@
 import { useQuery } from '@apollo/client'
-import Layout from '../../components/Layout'
-import { GetCockpitSessionDocument, LecturerSession } from '@klicker-uzh/graphql/dist/ops'
+import {
+  GetCockpitSessionDocument,
+  LecturerSession,
+} from '@klicker-uzh/graphql/dist/ops'
 import { useRouter } from 'next/router'
+
+import Layout from '../../components/Layout'
+import AudienceInteraction from '../../components/interaction/AudienceInteraction'
 
 function Cockpit() {
   const router = useRouter()
+
+  // TODO: add toast notifications - this react library react-toast-notifications is outdated and will not work with react 18
+  // const { addToast } = useToasts()
+
+  // useEffect((): void => {
+  //   router.prefetch('/sessions/evaluation')
+  //   router.prefetch('/sessions/feedbacks')
+  //   router.prefetch('/join')
+  //   router.prefetch('/qr')
+  // }, [router])
+
+  // const accountSummary = useQuery(AccountSummaryQuery)
+  // const { data, loading, error, subscribeToMore } = useQuery(RunningSessionQuery, {
+  //   pollInterval: 10000,
+  // })
+  // const [updateSettings, { loading: isUpdateSettingsLoading }] = useMutation(UpdateSessionSettingsMutation)
+  // const [endSession, { loading: isEndSessionLoading }] = useMutation(EndSessionMutation)
+  // const [pauseSession, { loading: isPauseSessionLoading }] = useMutation(PauseSessionMutation)
+  // const [resetQuestionBlock, { loading: isResetQuestionBlockLoading }] = useMutation(ResetQuestionBlockMutation)
+  // const [cancelSession, { loading: isCancelSessionLoading }] = useMutation(CancelSessionMutation)
+  // const [activateNextBlock, { loading: isActivateNextBlockLoading }] = useMutation(ActivateNextBlockMutation)
+  // const [activateBlockById, { loading: isActivateBlockByIdLoading }] = useMutation(ActivateBlockByIdMutation)
+
+  // const [isParticipantListVisible, setIsParticipantListVisible] = useState(false)
+
+  // const shortname = _get(accountSummary, 'data.user.shortname')
+
+  // const isAnythingLoading = _some([
+  //   isUpdateSettingsLoading,
+  //   isEndSessionLoading,
+  //   isPauseSessionLoading,
+  //   isResetQuestionBlockLoading,
+  //   isCancelSessionLoading,
+  //   isActivateNextBlockLoading,
+  //   isActivateBlockByIdLoading,
+  // ])
 
   const {
     loading: cockpitLoading,
@@ -28,9 +69,183 @@ function Cockpit() {
     return null
   }
 
-  const { id, isAudienceInteractionActive, isModerationEnabled, isGamificationEnabled, namespace, name, displayName, status, startedAt, course, activeBlock, blocks, confusionFeedbacks } = cockpitData.cockpitSession as LecturerSession
+  console.log(cockpitData.cockpitSession)
 
-  return <Layout>TODO</Layout>
+  const {
+    id,
+    isAudienceInteractionActive,
+    isModerationEnabled,
+    isGamificationEnabled,
+    namespace,
+    name,
+    displayName,
+    status,
+    startedAt,
+    course,
+    activeBlock,
+    blocks,
+    confusionFeedbacks,
+    feedbacks
+  } = cockpitData.cockpitSession as LecturerSession
+
+  return (
+    <Layout>
+      <div className="mb-8 print:hidden">
+        {/* // TODO: add hint for authenticated session, if this is enabled */}
+        SESSION TIMELINE
+        {/* <SessionTimeline
+          // activeBlock={activeBlock}
+          activeStep={activeStep}
+          authenticationMode={settings.authenticationMode}
+          blocks={blocks}
+          handleActivateBlockById={(blockId): void => {
+            if (
+              !_some([isActivateBlockByIdLoading, isActivateNextBlockLoading])
+            ) {
+              activateBlockById({
+                variables: { blockId, sessionId: id },
+              })
+              push(['trackEvent', 'Running Session', 'Block Activated By Id'])
+            }
+          }}
+          handleActiveBlock={(): void => {
+            // startPolling(10000)
+          }}
+          handleCancelSession={async (): Promise<void> => {
+            if (!isAnythingLoading) {
+              await cancelSession({
+                refetchQueries: [
+                  { query: SessionListQuery },
+                  { query: RunningSessionQuery },
+                  { query: AccountSummaryQuery },
+                ],
+                variables: { id },
+              })
+              push(['trackEvent', 'Running Session', 'Session Canceled'])
+
+              // redirect to the question pool
+              router.push('/sessions')
+            }
+          }}
+          handleEndSession={async (): Promise<void> => {
+            if (!isAnythingLoading) {
+              // run the mutation
+              await endSession({
+                refetchQueries: [
+                  { query: SessionListQuery },
+                  { query: RunningSessionQuery },
+                  { query: AccountSummaryQuery },
+                ],
+                variables: { id },
+              })
+              push(['trackEvent', 'Running Session', 'Session Finished'])
+
+              // redirect to the question pool
+              router.push('/questions')
+            }
+          }}
+          handleNextBlock={(): void => {
+            if (
+              !_some([isActivateBlockByIdLoading, isActivateNextBlockLoading])
+            ) {
+              activateNextBlock({
+                refetchQueries: [{ query: RunningSessionQuery }],
+              })
+              push(['trackEvent', 'Running Session', 'Next Block Activated'])
+            }
+          }}
+          handleNoActiveBlock={(): void => {
+            // stopPolling()
+          }}
+          handlePauseSession={async (): Promise<void> => {
+            if (!isAnythingLoading) {
+              await pauseSession({
+                refetchQueries: [
+                  { query: SessionListQuery },
+                  { query: RunningSessionQuery },
+                  { query: AccountSummaryQuery },
+                ],
+                variables: { id },
+              })
+              push(['trackEvent', 'Running Session', 'Session Paused'])
+
+              router.push('/sessions')
+            }
+          }}
+          handleResetQuestionBlock={async (blockId): Promise<void> => {
+            await resetQuestionBlock({ variables: { sessionId: id, blockId } })
+            push(['trackEvent', 'Running Session', 'Question Block Reset'])
+
+            addToast(
+              <FormattedMessage
+                defaultMessage="Question block successfully reset."
+                id="sessions.running.resetSessionblock.success"
+              />,
+              {
+                appearance: 'success',
+              }
+            )
+          }}
+          handleToggleParticipantList={(): void =>
+            setIsParticipantListVisible((isVisible) => !isVisible)
+          }
+          handleTogglePublicEvaluation={(): void => {
+            updateSettings({
+              variables: {
+                sessionId: id,
+                settings: {
+                  isEvaluationPublic: !settings.isEvaluationPublic,
+                },
+              },
+            })
+            push(['trackEvent', 'Running Session', 'Evaluation Published'])
+          }}
+          intl={intl}
+          isEvaluationPublic={settings.isEvaluationPublic}
+          isParticipantAuthenticationEnabled={
+            settings.isParticipantAuthenticationEnabled
+          }
+          isParticipantListVisible={isParticipantListVisible}
+          participants={participants}
+          sessionId={id}
+          shortname={shortname}
+          startedAt={startedAt}
+          storageMode={settings.storageMode}
+          subscribeToMore={subscribeToMore({
+            document: RunningSessionUpdatedSubscription,
+            updateQuery: (prev, { subscriptionData }): any => {
+              if (!subscriptionData.data) return prev
+              return {
+                ...prev,
+                runningSession: {
+                  ...prev.runningSession,
+                  ..._pick(subscriptionData.data.runningSessionUpdated, [
+                    'activeBlock',
+                    'activeStep',
+                  ]),
+                },
+              }
+            },
+            variables: { sessionId: id },
+          })}
+          withQuestionBlockExperiments={
+            featureFlags?.flags?.questionBlockExperiments
+          }
+        /> */}
+      </div>
+
+      AUDIENCE INTERACTION
+      <AudienceInteraction
+        confusionValues={confusionFeedbacks}
+        feedbacks={feedbacks}
+        isAudienceInteractionActive={isAudienceInteractionActive}
+        isModerationEnabled={isModerationEnabled}
+        isGamificationEnabled={isGamificationEnabled}
+        sessionId={id}
+        sessionName={name}
+      />
+    </Layout>
+  )
 }
 
 export default Cockpit
