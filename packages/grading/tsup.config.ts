03a7933c6c9ee09616445c2e88a80f4fb651eefa
Roland Schlaefli <rolandschlaefli@gmail.com>
2022-09-15 14:01:20 +0200

    fix: install prettier in grading package with design system config

--- packages/grading/tsup.config.ts
+++ packages/grading/tsup.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from "tsup";
+import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ["src/index.ts"],
+  entry: ['src/index.ts'],
   clean: true,
   // format: 'esm',
   dts: true,
-});
+})
