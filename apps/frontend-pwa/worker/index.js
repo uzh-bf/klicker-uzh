f58b8f6ee416211c8d8fdbcbd8897d31aae71033
Roland Schläfli <rolandschlaefli@gmail.com>
2022-09-15 14:03:35 +0200

    enhance: rework seed, improve layout of PWA, error boundary and error pages (#2887)

--- apps/frontend-pwa/worker/index.js
+++ apps/frontend-pwa/worker/index.js
@@ -5,7 +5,7 @@ self.addEventListener('push', function (event) {
   event.waitUntil(
     registration.showNotification(data.title, {
       body: data.message,
-      icon: '/manifest-icon-192.maskable.png'
+      icon: '/manifest-icon-192.maskable.png',
     })
   )
 })
@@ -13,17 +13,19 @@ self.addEventListener('push', function (event) {
 self.addEventListener('notificationclick', function (event) {
   event.notification.close()
   event.waitUntil(
-    clients.matchAll({ type: 'window', includeUncontrolled: true }).then(function (clientList) {
-      if (clientList.length > 0) {
-        let client = clientList[0]
-        for (let i = 0; i < clientList.length; i++) {
-          if (clientList[i].focused) {
-            client = clientList[i]
+    clients
+      .matchAll({ type: 'window', includeUncontrolled: true })
+      .then(function (clientList) {
+        if (clientList.length > 0) {
+          let client = clientList[0]
+          for (let i = 0; i < clientList.length; i++) {
+            if (clientList[i].focused) {
+              client = clientList[i]
+            }
           }
+          return client.focus()
         }
-        return client.focus()
-      }
-      return clients.openWindow('/')
-    })
+        return clients.openWindow('/')
+      })
   )
 })
