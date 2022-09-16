02d8472b2ea1c3adb2b154c40258bc3ed51001fc
Roland Schlaefli <rolandschlaefli@gmail.com>
2022-09-16 22:24:38 +0200

    fix(apps/frontend-pwa): use .next() to return from middleware

--- apps/frontend-pwa/src/middleware.ts
+++ apps/frontend-pwa/src/middleware.ts
@@ -4,13 +4,13 @@ import type { NextRequest } from 'next/server'
 export async function middleware(request: NextRequest) {
     // if the requester has a login cookie -> skip everything else and redirect to the requested element
   if (request.cookies.get('participant_token')) {
-    return NextResponse.redirect(new URL(request.url))
+    return NextResponse.next()
   }
 
   // TODO: if the requester has no cookie yet -> call the backend and request an account
   // TODO: if a new account has been created, a cookie has been set for the backend -> redirect to the welcome page
   // TODO: if the account existed, a cookie has been set for the backend -> redirect to the requested element
-  return NextResponse.redirect(new URL(request.url))
+  return NextResponse.next()
 }
 
 export const config = {
