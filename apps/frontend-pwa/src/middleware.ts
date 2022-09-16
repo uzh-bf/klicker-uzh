c1bbc0c2e180a74426211649f00c83e0a896d687
Roland Schlaefli <rolandschlaefli@gmail.com>
2022-09-16 22:19:35 +0200

    enhance(apps/frontend-pwa): ensure middleware is skipped if a participant token is available

--- apps/frontend-pwa/src/middleware.ts
+++ apps/frontend-pwa/src/middleware.ts
@@ -2,14 +2,17 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export async function middleware(request: NextRequest) {
-  // TODO: if the requester has an LTI cookie -> skip everything else and redirect to the requested element
+    // if the requester has a login cookie -> skip everything else and redirect to the requested element
+  if (request.cookies.get('participant_token')) {
+    return NextResponse.redirect(new URL(request.url))
+  }
+
   // TODO: if the requester has no cookie yet -> call the backend and request an account
   // TODO: if a new account has been created, a cookie has been set for the backend -> redirect to the welcome page
   // TODO: if the account existed, a cookie has been set for the backend -> redirect to the requested element
   return NextResponse.redirect(new URL(request.url))
 }
 
-// See "Matching Paths" below to learn more
 export const config = {
   matcher: '/course/:path*',
 }
