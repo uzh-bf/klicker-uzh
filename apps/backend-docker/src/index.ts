811e5dce2243afac7deee8fef16274aef28b7b9b
Roland Schlaefli <rolandschlaefli@gmail.com>
2022-09-18 02:54:50 +0200

    chore(apps/backend-docker): add initial alternative backend with docker-based express

---
+++ apps/backend-docker/src/index.ts
@@ -0,0 +1,27 @@
+import { PrismaClient } from '@klicker-uzh/prisma'
+import Redis from 'ioredis'
+import prepareApp from './app'
+
+const prisma = new PrismaClient()
+
+const redisExec = new Redis({
+  family: 4,
+  host: process.env.REDIS_HOST ?? 'localhost',
+  password: process.env.REDIS_PASS ?? '',
+  port: Number(process.env.REDIS_PORT) ?? 6379,
+  tls: process.env.REDIS_TLS ? {} : undefined,
+})
+
+const redisCache = new Redis({
+  family: 4,
+  host: process.env.REDIS_CACHE_HOST ?? 'localhost',
+  password: process.env.REDIS_CACHE_PASS ?? '',
+  port: Number(process.env.REDIS_CACHE_PORT) ?? 6380,
+  tls: process.env.REDIS_CACHE_TLS ? {} : undefined,
+})
+
+const app = prepareApp({ prisma, redisCache, redisExec })
+
+app.listen(3000, () => {
+  console.log('GraphQL API located at http://0.0.0.0:3000/graphql')
+})
