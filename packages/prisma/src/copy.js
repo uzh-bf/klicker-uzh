4fccddb53360dc7897a534d5fdf41cc51d12fa64
Roland Schlaefli <rolandschlaefli@gmail.com>
2022-09-18 11:51:36 +0200

    chore: reformat

--- packages/prisma/src/copy.js
+++ packages/prisma/src/copy.js
@@ -1,34 +1,34 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require("fs");
+const path = require("path");
 
-fs.mkdirSync(path.resolve(__dirname, '../dist/runtime'), { recursive: true })
+fs.mkdirSync(path.resolve(__dirname, "../dist/runtime"), { recursive: true });
 fs.copyFileSync(
-  path.resolve(__dirname, './client/index.d.ts'),
-  path.resolve(__dirname, '../dist/index.d.ts')
-)
+  path.resolve(__dirname, "./client/index.d.ts"),
+  path.resolve(__dirname, "../dist/index.d.ts")
+);
 fs.copyFileSync(
-  path.resolve(__dirname, './prisma/schema.prisma'),
-  path.resolve(__dirname, '../dist/schema.prisma')
-)
+  path.resolve(__dirname, "./prisma/schema.prisma"),
+  path.resolve(__dirname, "../dist/schema.prisma")
+);
 fs.copyFileSync(
-  path.resolve(__dirname, './client/runtime/index-browser.d.ts'),
-  path.resolve(__dirname, '../dist/runtime/index-browser.d.ts')
-)
+  path.resolve(__dirname, "./client/runtime/index-browser.d.ts"),
+  path.resolve(__dirname, "../dist/runtime/index-browser.d.ts")
+);
 fs.copyFileSync(
-  path.resolve(__dirname, './client/runtime/index.d.ts'),
-  path.resolve(__dirname, '../dist/runtime/index.d.ts')
-)
+  path.resolve(__dirname, "./client/runtime/index.d.ts"),
+  path.resolve(__dirname, "../dist/runtime/index.d.ts")
+);
 fs.copyFileSync(
   path.resolve(
     __dirname,
     // './client/libquery_engine-debian-openssl-1.1.x.so.node'
     // "./client/query_engine-windows.dll.node"
-    './client/libquery_engine-linux-musl.so.node'
+    "./client/libquery_engine-linux-musl.so.node"
   ),
   path.resolve(
     __dirname,
     // '../dist/libquery_engine-debian-openssl-1.1.x.so.node'
     // '../dist/query_engine-windows.dll.node'
-    '../dist/libquery_engine-linux-musl.so.node'
+    "../dist/libquery_engine-linux-musl.so.node"
   )
-)
+);
