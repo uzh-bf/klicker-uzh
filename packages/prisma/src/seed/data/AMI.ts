144898ffe3dad0b256c1d3715bb85a12464cfe11
Roland Schlaefli <rolandschlaefli@gmail.com>
2022-09-19 16:55:54 +0200

    chore: add templates for SC and KPRIM

--- packages/prisma/src/seed/data/AMI.ts
+++ packages/prisma/src/seed/data/AMI.ts
@@ -5,7 +5,79 @@ export const PARTICIPANT_IDS = []
 
 export const ATTACHMENTS = []
 
-export const QUESTIONS = []
+export const QUESTIONS = [
+  {
+    name: 'Modul 1 Zieldreieck',
+    content: 'Welche der folgenden Aussagen ist **falsch**?',
+    contentPlain: 'Welche der folgenden Aussagen ist falsch?',
+    type: QuestionType.SC,
+    choices: [
+      {
+        feedback:
+          'Falsch! Zwischen den Zielsetzungen des klassischen finanziellen Zieldreiecks gibt es sowohl Zielkonflikte als auch Zielharmonien.',
+        value:
+          'Zwischen den Zielsetzungen des klassischen Zieldreiecks gibt es sowohl Zielkonflikte als auch Zielharmonien.',
+      },
+      {
+        feedback:
+          'Korrekt! Je höher die angestrebte Sicherheit, desto weniger Risiko wird eingegangen, was wiederum die Rentabilität senkt.',
+        correct: true,
+        value:
+          'Das Ziel einer hohen Rentabilität erhöht auch die Sicherheit eines Unternehmens.',
+      },
+      {
+        feedback:
+          'Falsch! Die Unabhängigkeit ist kein Ziel des klassischen Zieldreiecks.',
+        value: 'Unabhängigkeit ist *kein* Ziel des klassischen Zieldreiecks.',
+      },
+      {
+        feedback:
+          'Falsch! Eine hohe Liquidität steht im Zielkonflikt mit der Rentabilität, da Liquidität meist teuer ist.',
+        value:
+          'Eine hohe Liquidität steht im Zielkonflikt mit der Rentabilität, da Liquidität meist teuer ist.',
+      },
+      {
+        feedback:
+          'Falsch! Der Shareholder Value ist kein Ziel des klassischen Zieldreiecks.',
+        value:
+          'Der Shareholder Value ist *kein* Ziel des klassischen Zieldreiecks.',
+      },
+    ],
+  },
+  {
+    name: 'Modul 1 Bilanz',
+    content:
+      'Beurteile die folgenden Aussagen zur Bilanz auf ihre Richtigkeit:',
+    contentPlain:
+      'Beurteile die folgenden Aussagen zur Bilanz auf ihre Richtigkeit:',
+    type: QuestionType.KPRIM,
+    choices: [
+      {
+        feedback:
+          'Diese Aussage ist nicht korrekt! Die Aktivseite zeigt die Mittelverwendung auf.',
+        value: 'Die Aktivseite zeigt die Mittelherkunft auf.',
+      },
+      {
+        feedback:
+          'Diese Aussage ist nicht korrekt! Die Passivseite zeigt die Mittelherkunft auf.',
+        value: 'Die Passivseite der Bilanz zeigt die Mittelverwendung auf.',
+      },
+      {
+        feedback:
+          'Diese Aussage ist nicht korrekt! Das EK zeigt zwar die Mittelherkunft auf, diese wird aber auf der Passivseite der Bilanz abgebildet.',
+        value:
+          'Das EK zeigt die Mittelherkunft auf und steht somit auf der Aktivseite der Bilanz.',
+      },
+      {
+        feedback: 'Diese Aussage ist korrekt!',
+        correct: true,
+        value:
+          'Das Konto Flüssige Mittel zeigt die Mittelverwendung auf und steht somit auf der Aktivseite der Bilanz.',
+      },
+    ],
+  },
+  {},
+]
 
 export const LEARNING_ELEMENTS = []
 
