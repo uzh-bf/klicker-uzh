d939592d8f9483cb79d168850799ec295e73e1d4
saskse <113920910+saskse@users.noreply.github.com>
2022-09-19 17:29:03 +0200

    update

--- packages/prisma/src/seed/data/AMI.ts
+++ packages/prisma/src/seed/data/AMI.ts
@@ -21,13 +21,13 @@ export const QUESTIONS = [
       {
         feedback:
           'Falsch! Das Patent ist ein Beispiel für eine Realanlage.',
-        correct: true,
         value:
           'Patente, Aktien und Obligationen sind Beispiele für Finanzanlagen.',
       },
       {
         feedback:
           'Korrekt!',
+          correct: true,
         value: 'Wissen, Maschinen und Gebäude sind Beispiele für Realanlagen.',
       },
       {
@@ -41,6 +41,44 @@ export const QUESTIONS = [
           'Falsch! Bei der Berechnung von Indizes wird zwischen Preisgewichteter und Marktwertgewichteter Index unterschieden. Kurs- und Performanceindex stellen lediglich Arten von Indizes dar.',
         value:
           'Die Berechnung der Aktienindizes wird anhand von Kurs- und Performanceindex getätigt.',
+      },
+    ],
+  },
+  {
+    name: 'Modul 1 Investitionsprozess',
+    content: 'Welche der folgenden Aussagen zum *Investment-Prozess* ist *falsch*?',
+    contentPlain: 'Welche der folgenden Aussagen zum Investment-Prozess ist falsch?',
+    type: QuestionType.SC,sss
+    choices: [
+      {
+        feedback:
+          'Falsch! Diese Aussage ist korrekt!',
+        value:
+          'Mit der Strategischen Asset Allocation wird entschieden, in welche Anlageklassen (Aktien, Bonds, Rohstoffe, etc.) welcher Anteil des Vermögens investiert wird.',
+      },
+      {
+        feedback:
+          'Falsch! Diese Aussage ist korrekt!',
+        value:
+          'Im Security Selection Prozess werden innerhalb der Anlageklassen die einzelnen Anlageinstrumente ausgewählt und das Portfolio erstellt.',
+      },
+      {
+        feedback:
+          'Korrekt! Diese Aussage ist nicht korrekt!  Die "Top-down" Portfoliokonstruktion beginnt mit der Asset Allocation und die "Bottom-up" mit der Titelselektion.',
+          correct: true,
+        value: 'Die "Bottom-up" Portfoliobildung startet mit der Strategischen Asset Allocation.',
+      },
+      {
+        feedback:
+          'Falsch! Diese Aussage ist korrekt!',
+        value:
+          'Die "Top-down" Portfoliobildung beinhaltet die Strategische und Taktische Asset Allocation gefolgt vom Security Selection Prozess.',
+      },
+      {
+        feedback:
+        'Falsch! Diese Aussage ist korrekt!',
+        value:
+          'Ein Nachteil der "Bottom-up" Strategie kann die fehlende Diversifikation eines Portfolios und das daraus erwachsende unsystematische Risiko sein.',
       },
     ],
   },
