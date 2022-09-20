387d23777626c4f9d938f8eaaead25501815b647
Roland Schlaefli <rolandschlaefli@gmail.com>
2022-09-20 22:06:18 +0200

    chore(packages/prisma): update seed

--- packages/prisma/src/seed/seedBF1.ts
+++ packages/prisma/src/seed/seedBF1.ts
@@ -5,12 +5,24 @@ import * as DATA_BF1 from './data/BF1.js'
 
 import {
   prepareCourse,
-  prepareLearningElement,
   prepareMicroSession,
   prepareQuestion,
   prepareSession,
   prepareUser,
 } from './helpers.js'
+
+// const PARTICIPANT_IDS = [
+//   '6f45065c-667f-4259-818c-c6f6b477eb48',
+//   '0b7c946c-cfc9-4b82-ac97-b058bf48924b',
+//   '52c20f0f-f5d4-4354-a5d6-a0c103f2b9ea',
+//   '16c39a69-03b4-4ce4-a695-e7b93d535598',
+//   'c48f624e-7de9-4e1b-a16d-82d22e64828f',
+//   '7cf9a94a-31a6-4c53-85d7-608dfa904e30',
+//   'f53e6a95-689b-48c0-bfab-6625c04f39ed',
+//   '46407010-0e7c-4903-9a66-2c8d9d6909b0',
+//   '84b0ba5d-34bc-45cd-8253-f3e8c340e5ff',
+//   '05a933a0-b2bc-4551-b7e1-6975140d996d',
+// ]
 
 async function seedBF1(prisma: Prisma.PrismaClient) {
   const userBF1 = await prisma.user.upsert(
@@ -32,6 +44,19 @@ async function seedBF1(prisma: Prisma.PrismaClient) {
     })
   )
 
+  // const participantsTesting = await Promise.all(
+  //   PARTICIPANT_IDS.map(async (id, ix) => {
+  //     return prisma.participant.upsert(
+  //       await prepareParticipant({
+  //         id: PARTICIPANT_IDS[ix],
+  //         password: 'testing',
+  //         username: `testuser${ix + 1}`,
+  //         courseIds: ['2b302436-4fc3-4d5d-bbfb-1e13b4ee11b2'],
+  //       })
+  //     )
+  //   })
+  // )
+
   const questionsBF1 = await Promise.all(
     DATA_BF1.QUESTIONS.map((data) =>
       prisma.question.upsert(prepareQuestion({ ownerId: userBF1.id, ...data }))
@@ -43,18 +68,18 @@ async function seedBF1(prisma: Prisma.PrismaClient) {
     `ALTER SEQUENCE "Question_id_seq" RESTART WITH ${questionCount + 1}`
   )
 
-  const learningElementsBF1 = await Promise.all(
-    DATA_BF1.LEARNING_ELEMENTS.map((data) =>
-      prisma.learningElement.upsert(
-        prepareLearningElement({
-          ...data,
-          ownerId: userBF1.id,
-          courseId: courseBF1.id,
-          questions: questionsBF1.filter((q) => data.questions.includes(q.id)),
-        })
-      )
-    )
-  )
+  // const learningElementsBF1 = await Promise.all(
+  //   DATA_BF1.LEARNING_ELEMENTS.map((data) =>
+  //     prisma.learningElement.upsert(
+  //       prepareLearningElement({
+  //         ...data,
+  //         ownerId: userBF1.id,
+  //         courseId: courseBF1.id,
+  //         questions: questionsBF1.filter((q) => data.questions.includes(q.id)),
+  //       })
+  //     )
+  //   )
+  // )
 
   const sessionsBF1 = await Promise.all(
     DATA_BF1.SESSIONS.map((data) =>
