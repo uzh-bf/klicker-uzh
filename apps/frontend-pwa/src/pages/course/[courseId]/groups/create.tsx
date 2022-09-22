c25c522899ebcc9d35186ebb87f1640408c1eb7a
Roland Schlaefli <rolandschlaefli@gmail.com>
2022-09-22 08:48:59 +0200

    chore(apps/frontend-pwa): stubs for group join and group create

---
+++ apps/frontend-pwa/src/pages/course/[courseId]/groups/create.tsx
@@ -0,0 +1,15 @@
+import Layout from '@components/Layout'
+
+function GroupCreation({ courseId }: any) {
+  return (
+    <Layout
+      displayName="Create Group"
+      courseName="Banking and Finance I"
+      courseColor="green"
+    >
+      hello world
+    </Layout>
+  )
+}
+
+export default GroupCreation
---
+++ apps/frontend-pwa/src/pages/course/[courseId]/groups/join.tsx
@@ -0,0 +1,15 @@
+import Layout from '@components/Layout'
+
+function GroupJoin({ courseId }: any) {
+  return (
+    <Layout
+      displayName="Join Group"
+      courseName="Banking and Finance I"
+      courseColor="green"
+    >
+      hello world
+    </Layout>
+  )
+}
+
+export default GroupJoin
