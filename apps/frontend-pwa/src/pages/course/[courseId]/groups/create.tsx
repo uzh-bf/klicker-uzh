faafd2fc4fad909d692c9ba88756d387cef23b91
mxjweb <maximilian.weber@bluewin.ch>
2022-09-22 09:16:12 +0200

    raw code added for creat group

--- apps/frontend-pwa/src/pages/course/[courseId]/groups/create.tsx
+++ apps/frontend-pwa/src/pages/course/[courseId]/groups/create.tsx
@@ -1,13 +1,16 @@
 import Layout from '@components/Layout'
+import { Button, H1 } from '@uzh-bf/design-system'
 
 function GroupCreation({ courseId }: any) {
   return (
     <Layout
-      displayName="Create Group"
+      displayName="Gruppe Erstellen"
       courseName="Banking and Finance I"
       courseColor="green"
     >
-      hello world
+      <H1>Name</H1>
+      <p>[empty box for gorupname]</p>
+      <Button>Erstellen</Button>
     </Layout>
   )
 }
