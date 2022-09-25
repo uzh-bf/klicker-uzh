0261606251d04c9c605f05555b2a589629b9c9be
Roland Schlaefli <rolandschlaefli@gmail.com>
2022-09-25 15:21:06 +0200

    remove seed directory

--- packages/prisma/src/seed/constants.ts
+++
@@ -1,5 +0,0 @@
-export const USER_ID_BF1 = 'b7e21ad0-dcf6-4277-9b06-a3eb1d03147c'
-export const USER_ID_AMI = '163e4f34-52a4-4e47-866f-79a8aec2feac'
-
-export const COURSE_ID_BF1 = '2b302436-4fc3-4d5d-bbfb-1e13b4ee11b2'
-export const COURSE_ID_AMI = '2f208c63-cb02-4b46-9462-7ce735a42235'
