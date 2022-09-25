98c6a08d9d0edacfce0b83b21e822b37c768152a
Roland Schlaefli <rolandschlaefli@gmail.com>
2022-09-25 17:37:56 +0200

    chore: remove migrations

--- packages/prisma/src/prisma/migrations/20220922095003_/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "LeaderboardEntry" ADD COLUMN     "sessionParticipationId" INTEGER;
-
--- AddForeignKey
-ALTER TABLE "LeaderboardEntry" ADD CONSTRAINT "LeaderboardEntry_sessionParticipationId_fkey" FOREIGN KEY ("sessionParticipationId") REFERENCES "Participation"("id") ON DELETE SET NULL ON UPDATE CASCADE;
