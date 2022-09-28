6d9eb3b7382ca74411febc0ba7aaea041697ec4f
Roland Schlaefli <rolandschlaefli@gmail.com>
2022-09-28 10:40:03 +0200

    chore(packages/graphql): improve push sending util to loop through subscriptions

--- packages/graphql/src/lib/push.ts
+++ packages/graphql/src/lib/push.ts
@@ -1,6 +1,26 @@
 import * as dotenv from 'dotenv'
 import webpush from 'web-push'
 
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+// BF1
+// SELECT s.endpoint, s.p256dh, s.auth, p.username
+// FROM "PushSubscription" as s
+// JOIN "Course" as c ON s."courseId" = c.id
+// JOIN "Participant" as p ON s."participantId" = p.id
+// WHERE c.name = 'Banking and Finance I'
+
+// AMI
+// SELECT s.endpoint, s.p256dh, s.auth, p.username
+// FROM "PushSubscription" as s
+// JOIN "Course" as c ON s."courseId" = c.id
+// JOIN "Participant" as p ON s."participantId" = p.id
+// WHERE c.name = 'Asset Management: Investments'
+
+const SUBSCRIPTIONS = []
+
 dotenv.config()
 
 webpush.setVapidDetails(
@@ -9,18 +29,46 @@ webpush.setVapidDetails(
   process.env.VAPID_PRIVATE_KEY as string
 )
 
-const pushSubscription = {
-  endpoint: process.env.RS_MOBILE_SUB_ENDPOINT as string,
-  keys: {
-    auth: process.env.RS_MOBILE_SUB_AUTH as string,
-    p256dh: process.env.RS_MOBILE_SUB_P256 as string,
-  },
+// const pushSubscription = {
+//   endpoint: process.env.MOBILE_SUB_ENDPOINT as string,
+//   keys: {
+//     auth: process.env.MOBILE_SUB_AUTH as string,
+//     p256dh: process.env.MOBILE_SUB_P256 as string,
+//   },
+// }
+
+// webpush.sendNotification(
+//   pushSubscription,
+//   JSON.stringify({
+//     title: '',
+//     message: '',
+//   })
+// )
+
+async function sendPushNotifications() {
+  for (let sub of SUBSCRIPTIONS) {
+    console.log(sub.username)
+    await sleep(500)
+    try {
+      const result = await webpush.sendNotification(
+        {
+          endpoint: sub.endpoint,
+          keys: {
+            auth: sub.auth,
+            p256dh: sub.p256dh,
+          },
+        },
+        JSON.stringify({
+          message:
+            'Das Microlearning für BFI Woche 2 ist bis morgen um 09:00 verfügbar.',
+          title: 'KlickerUZH - Neues Microlearning',
+        })
+      )
+      console.log(result)
+    } catch (e) {
+      console.log(e)
+    }
+  }
 }
 
-webpush.sendNotification(
-  pushSubscription,
-  JSON.stringify({
-    message: 'Neues Microlearning verfügbar',
-    title: 'KlickerUZH',
-  })
-)
+sendPushNotifications()
