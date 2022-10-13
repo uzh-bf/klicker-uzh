928a5dfed733ecab1d759546aa3b90f651554567
sjschlapbach <julius.schlapbach@gmx.net>
2022-10-13 18:18:36 +0200

    chore: add stylesheet for correct katex styling

--- apps/frontend-manage/src/pages/_app.tsx
+++ apps/frontend-manage/src/pages/_app.tsx
@@ -3,7 +3,6 @@ import type { AppProps } from 'next/app'
 import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
 
-
 import { ThemeProvider } from '@uzh-bf/design-system'
 import { useApollo } from '../lib/apollo'
 
@@ -15,23 +14,29 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={apolloClient}>
       <DndProvider backend={HTML5Backend}>
-      <ThemeProvider
-        theme={{
-          primaryBg: 'bg-uzh-blue-20',
-          primaryBgDark: 'bg-uzh-blue-60',
-          primaryBgHover: 'hover:bg-uzh-blue-20',
-          primaryBgHoverNavbar: 'hover:bg-uzh-blue-40',
-          primaryBorder: 'border-uzh-blue-40',
-          primaryBorderHover: 'hover:border-uzh-blue-40',
-          primaryText: 'text-uzh-blue-100',
-          primaryTextHover: 'hover:text-uzh-blue-100',
-          primaryFill: 'fill-uzh-blue-80',
-          primaryFillHover: 'hover:fill-uzh-blue-100',
-          primaryProseHover: 'hover:text-uzh-blue-100',
-        }}
-      >
-        <Component {...pageProps} />
-      </ThemeProvider>
+        <ThemeProvider
+          theme={{
+            primaryBg: 'bg-uzh-blue-20',
+            primaryBgDark: 'bg-uzh-blue-60',
+            primaryBgHover: 'hover:bg-uzh-blue-20',
+            primaryBgHoverNavbar: 'hover:bg-uzh-blue-40',
+            primaryBorder: 'border-uzh-blue-40',
+            primaryBorderHover: 'hover:border-uzh-blue-40',
+            primaryText: 'text-uzh-blue-100',
+            primaryTextHover: 'hover:text-uzh-blue-100',
+            primaryFill: 'fill-uzh-blue-80',
+            primaryFillHover: 'hover:fill-uzh-blue-100',
+            primaryProseHover: 'hover:text-uzh-blue-100',
+          }}
+        >
+          <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/katex@0.16.2/dist/katex.min.css"
+            integrity="sha384-bYdxxUwYipFNohQlHt0bjN/LCpueqWz13HufFEV1SUatKs1cm4L6fFgCi1jT643X"
+            crossOrigin="anonymous"
+          />
+          <Component {...pageProps} />
+        </ThemeProvider>
       </DndProvider>
     </ApolloProvider>
   )
