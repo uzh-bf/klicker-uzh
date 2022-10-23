9736c1453621b2b1d8d6c5bd491dabb8dcff6204
Roland Schlaefli <rolandschlaefli@gmail.com>
2022-10-24 00:07:31 +0200

    chore(apps/backend-response-processor): remove ping function

--- apps/backend-response-processor/tsup.config.ts
+++ apps/backend-response-processor/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['Ping/index.ts', 'ProcessResponses/index.ts'],
+  entry: ['ProcessResponses/index.ts'],
   clean: true,
   dts: true,
 })
