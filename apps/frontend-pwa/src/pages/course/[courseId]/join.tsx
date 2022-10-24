a35be50731ff00adbc5d981d963dd1c4c59e41dc
sjschlapbach <julius.schlapbach@gmx.net>
2022-10-24 15:28:31 +0200

    wip: implement form and validation schema for participant account creation and course joining

--- apps/frontend-pwa/src/pages/course/[courseId]/join.tsx
+++ apps/frontend-pwa/src/pages/course/[courseId]/join.tsx
@@ -3,12 +3,19 @@ import {
   GetBasicCourseInformationDocument,
   SelfDocument,
 } from '@klicker-uzh/graphql/dist/ops'
+import { Button, H2, Label } from '@uzh-bf/design-system'
+import { ErrorMessage, Field, Form, Formik } from 'formik'
 import { GetServerSideProps } from 'next'
+import { useRouter } from 'next/router'
+import { twMerge } from 'tailwind-merge'
+import * as yup from 'yup'
+
 import Layout from '../../../components/Layout'
 
 function JoinCourse({ courseId }: { courseId: string }) {
   const { loading: loadingParticipant, data: dataParticipant } =
     useQuery(SelfDocument)
+
   const { loading: loadingCourse, data: dataCourse } = useQuery(
     GetBasicCourseInformationDocument,
     {
@@ -19,6 +26,8 @@ function JoinCourse({ courseId }: { courseId: string }) {
       skip: !courseId,
     }
   )
+
+  const router = useRouter()
 
   console.log(dataCourse)
 
@@ -35,16 +44,157 @@ function JoinCourse({ courseId }: { courseId: string }) {
     return <div>COURSE NOT AVAILABLE</div>
   }
 
+  const joinAndRegisterSchema = yup.object({
+    username: yup
+      .string()
+      .required('Bitte geben Sie einen Benutzernamen ein')
+      .min(5, 'Der Benutzername muss mindestens 5 Zeichen lang sein.')
+      .max(10, 'Der Benutzername darf nicht länger als 10 Zeichen sein.'),
+    password: yup
+      .string()
+      .required('Bitte geben Sie ein Passwort ein')
+      .min(8, 'Das Passwort muss mindestens 8 Zeichen lang sein.'),
+    passwordRepetition: yup.string().when('password', {
+      is: (val: string) => val && val.length > 0,
+      then: (schema) =>
+        schema
+          .required('Passwörter müssen übereinstimmen.')
+          .min(8, 'Das Passwort muss mindestens 8 Zeichen lang sein.')
+          .oneOf(
+            [yup.ref('password'), null],
+            'Passwörter müssen übereinstimmen.'
+          ),
+      otherwise: (schema) =>
+        schema.oneOf([''], 'Passwörter müssen übereinstimmen.'),
+    }),
+    pin: yup
+      .number()
+      .typeError('Bitte geben Sie einen numerischen PIN ein.')
+      .required('Bitte geben Sie den Kurs-PIN ein.'),
+  })
+
   return (
     <Layout
       displayName="Kurs beitreten"
-      courseName={dataCourse?.basicCourseInformation.displayName} // course.displayName
-      courseColor={'red'} // course.color
+      courseName={dataCourse.basicCourseInformation.displayName} // course.displayName
+      courseColor={dataCourse.basicCourseInformation.color} // course.color
     >
+      <H2>
+        Kurs &quot;{dataCourse.basicCourseInformation.displayName}&quot;
+        beitreten
+      </H2>
+      <div>{dataCourse.basicCourseInformation.description}</div>
       {dataParticipant?.self ? (
         <div>Join Course with existing account</div>
       ) : (
-        <div>Join course while creating a new account</div>
+        <div>
+          <Formik
+            initialValues={{
+              email: '',
+              username: '',
+              password: '',
+              passwordRepetition: '',
+              pin: '',
+            }}
+            validationSchema={joinAndRegisterSchema}
+            onSubmit={async (values) => {
+              // TODO: join course and create participant
+              // await loginUser({
+              //   variables: { email: values.email, password: values.password },
+              // })
+              console.log(values)
+              // TODO: redirect to course page
+              // router.push('/')
+            }}
+          >
+            {({ errors, touched, isSubmitting }) => {
+              return (
+                <div className="mb-10">
+                  <Form className="w-72 sm:w-96">
+                    <Label label="Nutzername" className={'italic'} />
+                    <Field
+                      name="username"
+                      type="text"
+                      className={twMerge(
+                        'w-full rounded bg-uzh-grey-20 bg-opacity-50 border border-uzh-grey-60 focus:border-uzh-blue-50 mb-2',
+                        errors.username &&
+                          touched.username &&
+                          'border-red-400 bg-red-50 mb-0'
+                      )}
+                    />
+                    <ErrorMessage
+                      name="username"
+                      component="div"
+                      className="text-sm text-red-400"
+                    />
+
+                    <Label label="Passwort" className={'italic'} />
+                    <Field
+                      name="password"
+                      type="password"
+                      className={twMerge(
+                        'w-full rounded bg-uzh-grey-20 bg-opacity-50 border border-uzh-grey-60 focus:border-uzh-blue-50 mb-2',
+                        errors.password &&
+                          touched.password &&
+                          'border-red-400 bg-red-50 mb-0'
+                      )}
+                    />
+                    <ErrorMessage
+                      name="password"
+                      component="div"
+                      className="text-sm text-red-400"
+                    />
+
+                    <Label
+                      label="Passwort (Wiederholung)"
+                      className={'italic'}
+                    />
+                    <Field
+                      name="passwordRepetition"
+                      type="password"
+                      className={twMerge(
+                        'w-full rounded bg-uzh-grey-20 bg-opacity-50 border border-uzh-grey-60 focus:border-uzh-blue-50 mb-2',
+                        errors.passwordRepetition &&
+                          touched.passwordRepetition &&
+                          'border-red-400 bg-red-50 mb-0'
+                      )}
+                    />
+                    <ErrorMessage
+                      name="passwordRepetition"
+                      component="div"
+                      className="text-sm text-red-400"
+                    />
+
+                    <Label label="Kurs-PIN" className={'italic'} />
+                    <Field
+                      name="pin"
+                      type="text"
+                      className={twMerge(
+                        'w-full rounded bg-uzh-grey-20 bg-opacity-50 border border-uzh-grey-60 focus:border-uzh-blue-50 mb-2',
+                        errors.pin &&
+                          touched.pin &&
+                          'border-red-400 bg-red-50 mb-0'
+                      )}
+                    />
+                    <ErrorMessage
+                      name="pin"
+                      component="div"
+                      className="text-sm text-red-400"
+                    />
+
+                    <Button
+                      className="float-right mt-2 border-uzh-grey-80"
+                      type="submit"
+                      disabled={isSubmitting}
+                    >
+                      <Button.Label>Kurs beitreten</Button.Label>
+                    </Button>
+                  </Form>
+                </div>
+              )
+            }}
+          </Formik>
+        </div>
       )}
     </Layout>
   )
