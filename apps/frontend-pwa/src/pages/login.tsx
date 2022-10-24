b82b60371457fc06af290eefe84468be54d68168
sjschlapbach <julius.schlapbach@gmx.net>
2022-10-24 12:58:06 +0200

    chore: center klicker logo icons on login pages

--- apps/frontend-pwa/src/pages/login.tsx
+++ apps/frontend-pwa/src/pages/login.tsx
@@ -104,6 +104,7 @@ function LoginForm() {
                   width={300}
                   height={90}
                   alt="KlickerUZH Logo"
+                  className='mx-auto'
                 />
               </div>
               <H1>Login</H1>
