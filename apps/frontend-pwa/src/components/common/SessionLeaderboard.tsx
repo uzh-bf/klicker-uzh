a6c7dbd7076d9ea305839ad48fdb1ec1072fbeee
sjschlapbach <julius.schlapbach@gmx.net>
2022-10-25 23:59:59 +0200

    wip: add localforage logic for leaderboard entries

--- apps/frontend-pwa/src/components/common/SessionLeaderboard.tsx
+++ apps/frontend-pwa/src/components/common/SessionLeaderboard.tsx
@@ -1,7 +1,8 @@
 import { useQuery } from '@apollo/client'
 import { GetSessionLeaderboardDocument } from '@klicker-uzh/graphql/dist/ops'
 import { H2 } from '@uzh-bf/design-system'
-import React from 'react'
+import localforage from 'localforage'
+import React, { useEffect, useState } from 'react'
 import { twMerge } from 'tailwind-merge'
 
 interface LeaderboardProps {
@@ -18,7 +19,70 @@ function Leaderboard({
     fetchPolicy: 'network-only',
   })
 
-  console.log(data)
+  const [currentLeaderboard, setCurrentLeaderboard] = useState([
+    { username: '', score: 0, lastBlockOrder: 0 },
+  ])
+  const [previousLeaderboard, setPreviousLeaderboard] = useState([
+    { username: '', score: 0, lastBlockOrder: 0 },
+  ])
+
+  // save the current leaderboard to local storage
+  useEffect(() => {
+    const leaderboard: {
+      username: string
+      score: number
+      lastBlockOrder: number
+    }[] = data?.sessionLeaderboard?.map(
+      ({
+        username,
+        score,
+        lastBlockOrder,
+      }: {
+        username: string
+        score: number
+        lastBlockOrder: number
+      }) => ({
+        username,
+        score,
+        lastBlockOrder,
+      })
+    )
+
+    const prevLeaderboard: {
+      username: string
+      score: number
+      lastBlockOrder: number
+    }[] = []
+
+    leaderboard?.forEach(async (item) => {
+      localforage.setItem(
+        `${item.username}-score-block${item.lastBlockOrder}`,
+        item.score
+      )
+
+      try {
+        const prevScore = await localforage.getItem(
+          `${item.username}-score-block${item.lastBlockOrder - 1}`
+        )
+        typeof prevScore === 'number' &&
+          prevLeaderboard.push({
+            username: item.username,
+            score: prevScore,
+            lastBlockOrder: item.lastBlockOrder,
+          })
+
+        setPreviousLeaderboard(prevLeaderboard)
+      } catch (error) {
+        console.warn(error)
+      }
+    })
+
+    setCurrentLeaderboard(leaderboard)
+  }, [data])
+
+  // current and potentially previous leaderboard are given in the same format (users can also join at some point midsession with this approach)
+  console.log('currentLeaderboard', currentLeaderboard)
+  console.log('previousLeaderboard', previousLeaderboard)
 
   if (loading || !data) {
     return <div>loading</div>
