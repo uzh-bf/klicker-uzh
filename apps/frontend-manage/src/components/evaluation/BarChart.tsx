9333e1e06946acc83eb16afef4aee0f63f8c39ac
sjschlapbach <julius.schlapbach@gmx.net>
2022-11-15 15:42:04 +0100

    wip: create extensible div for overflowing question content

--- apps/frontend-manage/src/components/evaluation/BarChart.tsx
+++ apps/frontend-manage/src/components/evaluation/BarChart.tsx
@@ -25,7 +25,6 @@ function BarChart({ data, showSolution }: BarChartProps): React.ReactElement {
   // add labelIn and labelOut attributes to data, set labelIn to votes if votes/totalResponses > SMALL_BAR_THRESHOLD and set labelOut to votes otherwise
   const dataWithLabels = Object.values(data.results).map(
     (result, idx) => {
-      console.log(result)
       const labelIn =
         result.count / data.participants > SMALL_BAR_THRESHOLD
           ? result.count
