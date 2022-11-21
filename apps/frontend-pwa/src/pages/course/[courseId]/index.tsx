9f2d3fa21e5c8de2c6909365a7832f6c8823fa8e
sjschlapbach <julius.schlapbach@gmx.net>
2022-11-21 19:54:23 +0100

    fix: fix layout issues with awards on course leaderboard view

--- apps/frontend-pwa/src/pages/course/[courseId]/index.tsx
+++ apps/frontend-pwa/src/pages/course/[courseId]/index.tsx
@@ -179,7 +179,7 @@ function CourseOverview({ courseId }: any) {
             {course?.awards?.length != 0 && (
               <div className="px-4 py-3 mt-4 bg-orange-100 border border-orange-200 rounded shadow md:mt-6">
                 <H3 className="mb-2 text-base">BF-Champion Awards</H3>
-                <div className="flex flex-row gap-6 text-sm text-gray-700">
+                <div className="flex flex-col gap-6 text-sm text-gray-700 md:flex-row">
                   <div className="flex-1 space-y-1">
                     {course.awards
                       ?.filter((award) => award.type === 'PARTICIPANT')
