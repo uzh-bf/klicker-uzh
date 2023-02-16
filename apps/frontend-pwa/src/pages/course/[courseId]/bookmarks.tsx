68b1b4459c5ee1a966c6b676dfa3656c836f2055
Valentin Meyer <valentinleomeyer@gmail.com>
2023-02-16 16:47:29 +0100

    added button for courses

--- apps/frontend-pwa/src/pages/course/[courseId]/bookmarks.tsx
+++ apps/frontend-pwa/src/pages/course/[courseId]/bookmarks.tsx
@@ -1,29 +1,30 @@
 import { useQuery } from '@apollo/client'
 import { faRepeat } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { GetUserCoursesDocument } from '@klicker-uzh/graphql/dist/ops'
+import { GetBookmarkedQuestionsDocument } from '@klicker-uzh/graphql/dist/ops'
 import { Button, H1 } from '@uzh-bf/design-system'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { twMerge } from 'tailwind-merge'
-import Layout from '../components/Layout'
+import Layout from '../../../components/Layout'
 
 function Bookmarks() {
   const router = useRouter()
-  const { data } = useQuery(GetUserCoursesDocument)
-  console.log(data?.userCourses?.map((course) => course.displayName))
+  const { data } = useQuery(GetBookmarkedQuestionsDocument, {
+    variables: { courseId: router.query.courseId as string },
+    skip: !router.query.courseId,
+  })
 
   return (
     <Layout courseName="KlickerUZH" displayName="Kursübersicht">
       <div className="flex flex-col gap-2 md:w-full md:max-w-xl md:p-8 md:mx-auto md:border md:rounded">
         <H1 className={{ root: 'text-xl' }}>Kurse</H1>
-        {data?.userCourses?.map((course) => (
+        {data?.getBookmarkedQuestions?.map((question) => (
           <Link
-            key={course.id}
-            href={`/bookmarks/${course.id}/course/${course.id}`}
+            key={question.id}
+            href={`/course/${question.courseId}/question/${question.id}`}
             legacyBehavior
           >
-            <div>{course.displayName}</div>
             <Button
               className={{
                 root: twMerge(
@@ -35,7 +36,7 @@ function Bookmarks() {
                 <FontAwesomeIcon icon={faRepeat} />
               </Button.Icon>
               <Button.Label className={{ root: 'flex-1 text-left' }}>
-                <div>{course.displayName}</div>
+                <div>{question.displayName}</div>
               </Button.Label>
             </Button>
           </Link>
