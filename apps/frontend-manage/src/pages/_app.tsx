e7a963253c679aef9f4413ab54d20f22aaaa5071
Roland Schlaefli <rolandschlaefli@gmail.com>
2023-02-17 23:15:46 +0100

    fix: katex

--- apps/frontend-manage/src/pages/_app.tsx
+++ apps/frontend-manage/src/pages/_app.tsx
@@ -6,7 +6,6 @@ import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
 import { useApollo } from '../lib/apollo'
 
-import Script from 'next/script'
 import '../globals.css'
 
 const sourceSansPro = Source_Sans_Pro({
@@ -43,9 +42,10 @@ function App({ Component, pageProps }: AppProps) {
               primaryProseHover: 'hover:text-uzh-blue-100',
             }}
           >
-            <Script
-              src="https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/katex.min.js"
-              integrity="sha384-PwRUT/YqbnEjkZO0zZxNqcxACrXe+j766U2amXcgMg5457rve2Y7I6ZJSm2A0mS4"
+            <link
+              rel="stylesheet"
+              href="https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/katex.min.css"
+              integrity="sha384-vKruj+a13U8yHIkAyGgK1J3ArTLzrFGBbBc0tDp4ad/EyewESeXE/Iv67Aj8gKZ0"
               crossOrigin="anonymous"
             />
             <Component {...pageProps} />
--- apps/frontend-pwa/src/pages/_app.tsx
+++ apps/frontend-pwa/src/pages/_app.tsx
@@ -5,7 +5,6 @@ import { useApollo } from '@lib/apollo'
 import { Source_Sans_Pro } from '@next/font/google'
 import { ThemeProvider } from '@uzh-bf/design-system'
 import type { AppProps } from 'next/app'
-import Script from 'next/script'
 import ErrorBoundary from '../components/ErrorBoundary'
 config.autoAddCss = false
 
@@ -44,9 +43,10 @@ function App({ Component, pageProps }: AppProps) {
             primaryProseHover: 'hover:text-uzh-blue-100',
           }}
         >
-          <Script
-            src="https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/katex.min.js"
-            integrity="sha384-PwRUT/YqbnEjkZO0zZxNqcxACrXe+j766U2amXcgMg5457rve2Y7I6ZJSm2A0mS4"
+          <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/katex.min.css"
+            integrity="sha384-vKruj+a13U8yHIkAyGgK1J3ArTLzrFGBbBc0tDp4ad/EyewESeXE/Iv67Aj8gKZ0"
             crossOrigin="anonymous"
           />
           <Component {...pageProps} />
