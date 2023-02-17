14005dc19b30a2b5f16f9adb7becee6cf038586a
Roland Schlaefli <rolandschlaefli@gmail.com>
2023-02-17 23:24:44 +0100

    fix missing font div around app

--- apps/frontend-pwa/src/pages/_app.tsx
+++ apps/frontend-pwa/src/pages/_app.tsx
@@ -20,39 +20,41 @@ function App({ Component, pageProps }: AppProps) {
   const apolloClient = useApollo(pageProps)
 
   return (
-    <ErrorBoundary>
-      <ApolloProvider client={apolloClient}>
-        <ThemeProvider
-          theme={{
-            primaryBg: 'bg-uzh-blue-20',
-            primaryBgMedium: 'bg-uzh-blue-60',
-            primaryBgDark: 'bg-uzh-blue-80',
-            primaryBgHover: 'hover:bg-uzh-blue-20',
-            primaryBgMediumHover: 'hover:bg-uzh-blue-60',
-            primaryBgDarkHover: 'hover:bg-uzh-blue-80',
-            primaryBorder: 'border-uzh-blue-40',
-            primaryBorderDark: 'border-uzh-blue-80',
-            primaryBorderHover: 'hover:border-uzh-blue-40',
-            primaryBorderDarkHover: 'hover:border-uzh-blue-80',
-            primaryBorderFocus: 'focus:border-uzh-blue-40',
-            primaryBorderDarkFocus: 'focus:border-uzh-blue-80',
-            primaryText: 'text-uzh-blue-100',
-            primaryTextHover: 'hover:text-uzh-blue-100',
-            primaryFill: 'fill-uzh-blue-80',
-            primaryFillHover: 'hover:fill-uzh-blue-100',
-            primaryProseHover: 'hover:text-uzh-blue-100',
-          }}
-        >
-          <link
-            rel="stylesheet"
-            href="https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/katex.min.css"
-            integrity="sha384-vKruj+a13U8yHIkAyGgK1J3ArTLzrFGBbBc0tDp4ad/EyewESeXE/Iv67Aj8gKZ0"
-            crossOrigin="anonymous"
-          />
-          <Component {...pageProps} />
-        </ThemeProvider>
-      </ApolloProvider>
-    </ErrorBoundary>
+    <div className={`${sourceSansPro.variable} font-sans h-full`}>
+      <ErrorBoundary>
+        <ApolloProvider client={apolloClient}>
+          <ThemeProvider
+            theme={{
+              primaryBg: 'bg-uzh-blue-20',
+              primaryBgMedium: 'bg-uzh-blue-60',
+              primaryBgDark: 'bg-uzh-blue-80',
+              primaryBgHover: 'hover:bg-uzh-blue-20',
+              primaryBgMediumHover: 'hover:bg-uzh-blue-60',
+              primaryBgDarkHover: 'hover:bg-uzh-blue-80',
+              primaryBorder: 'border-uzh-blue-40',
+              primaryBorderDark: 'border-uzh-blue-80',
+              primaryBorderHover: 'hover:border-uzh-blue-40',
+              primaryBorderDarkHover: 'hover:border-uzh-blue-80',
+              primaryBorderFocus: 'focus:border-uzh-blue-40',
+              primaryBorderDarkFocus: 'focus:border-uzh-blue-80',
+              primaryText: 'text-uzh-blue-100',
+              primaryTextHover: 'hover:text-uzh-blue-100',
+              primaryFill: 'fill-uzh-blue-80',
+              primaryFillHover: 'hover:fill-uzh-blue-100',
+              primaryProseHover: 'hover:text-uzh-blue-100',
+            }}
+          >
+            <link
+              rel="stylesheet"
+              href="https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/katex.min.css"
+              integrity="sha384-vKruj+a13U8yHIkAyGgK1J3ArTLzrFGBbBc0tDp4ad/EyewESeXE/Iv67Aj8gKZ0"
+              crossOrigin="anonymous"
+            />
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </ApolloProvider>
+      </ErrorBoundary>
+    </div>
   )
 }
 
