ca7b48496789d5d037f059f65fa095016270f881
Roland Schlaefli <rolandschlaefli@gmail.com>
2023-02-17 23:08:58 +0100

    upgrade katex

--- apps/frontend-pwa/src/pages/_document.tsx
+++ apps/frontend-pwa/src/pages/_document.tsx
@@ -236,7 +236,7 @@ class MyDocument extends Document {
           />
           <link
             rel="stylesheet"
-            href="https://cdn.jsdelivr.net/npm/katex@0.16.2/dist/katex.min.css"
+            href="https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/katex.min.css"
             integrity="sha384-bYdxxUwYipFNohQlHt0bjN/LCpueqWz13HufFEV1SUatKs1cm4L6fFgCi1jT643X"
             crossOrigin="anonymous"
           />
