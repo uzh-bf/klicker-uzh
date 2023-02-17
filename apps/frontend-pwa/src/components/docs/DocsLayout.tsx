b05127d6fd4e8c7442ee494158436e4f16474c63
sjschlapbach <julius.schlapbach@gmx.net>
2023-02-17 11:15:10 +0100

    chore: update docs layout

--- apps/frontend-pwa/src/components/docs/DocsLayout.tsx
+++ apps/frontend-pwa/src/components/docs/DocsLayout.tsx
@@ -34,93 +34,91 @@ function DocsLayout({
   return (
     <Layout course={data.basicCourseInformation} displayName="Dokumentation">
       <div className="w-full max-w-4xl mx-auto">
-        <div>
-          <Navigation
-            style={{
-              root: {
-                backgroundColor: `${data.basicCourseInformation.color}`,
-              },
-            }}
-            className={{ root: `w-full rounded-b-none` }}
+        <Navigation
+          style={{
+            root: {
+              backgroundColor: `${data.basicCourseInformation.color}`,
+            },
+          }}
+          className={{ root: `w-full rounded-b-none` }}
+        >
+          <Navigation.ButtonItem
+            label="Kursinformationen"
+            className={{ root: `text-white` }}
+            onClick={() =>
+              router.push(`/course/${data.basicCourseInformation?.id}/docs`)
+            }
+          ></Navigation.ButtonItem>
+          <Navigation.TriggerItem
+            label="Einrichtung"
+            className={{ root: `text-white` }}
+            dropdownWidth="w-[20rem]"
           >
-            <Navigation.ButtonItem
-              label="Kursinformationen"
-              className={{ root: `text-white` }}
+            <Navigation.DropdownItem
+              title="App einrichten"
               onClick={() =>
-                router.push(`/course/${data.basicCourseInformation?.id}/docs`)
+                router.push(
+                  `/course/${data.basicCourseInformation?.id}/docs/appSetup`
+                )
               }
-            ></Navigation.ButtonItem>
-            <Navigation.TriggerItem
-              label="Einrichtung"
-              className={{ root: `text-white` }}
-              dropdownWidth="w-[20rem]"
-            >
-              <Navigation.DropdownItem
-                title="App einrichten"
-                onClick={() =>
-                  router.push(
-                    `/course/${data.basicCourseInformation?.id}/docs/appSetup`
-                  )
-                }
-              />
-              <Navigation.DropdownItem
-                title="Erstmaliges Login"
-                onClick={() =>
-                  router.push(
-                    `/course/${data.basicCourseInformation?.id}/docs/login`
-                  )
-                }
-              />
-            </Navigation.TriggerItem>
-            <Navigation.TriggerItem
-              label="Features"
-              className={{ root: `text-white` }}
-              dropdownWidth="w-[20rem]"
-            >
-              <Navigation.DropdownItem
-                title="Umfragen"
-                onClick={() =>
-                  router.push(
-                    `/course/${data.basicCourseInformation?.id}/docs/polls`
-                  )
-                }
-                className={{ root: 'text-center' }}
-              />
-              <Navigation.DropdownItem
-                title="Live Q&A"
-                onClick={() =>
-                  router.push(
-                    `/course/${data.basicCourseInformation?.id}/docs/liveQA`
-                  )
-                }
-              />
-              <Navigation.DropdownItem
-                title="Gruppenaktivitäten"
-                onClick={() =>
-                  router.push(
-                    `/course/${data.basicCourseInformation?.id}/docs/groupActivities`
-                  )
-                }
-              />
-              <Navigation.DropdownItem
-                title="Microlearning"
-                onClick={() =>
-                  router.push(
-                    `/course/${data.basicCourseInformation?.id}/docs/microSessions`
-                  )
-                }
-              />
-              <Navigation.DropdownItem
-                title="Selbsttests"
-                onClick={() =>
-                  router.push(
-                    `/course/${data.basicCourseInformation?.id}/docs/learningElements`
-                  )
-                }
-              />
-            </Navigation.TriggerItem>
-          </Navigation>
-        </div>
+            />
+            <Navigation.DropdownItem
+              title="Erstmaliges Login"
+              onClick={() =>
+                router.push(
+                  `/course/${data.basicCourseInformation?.id}/docs/login`
+                )
+              }
+            />
+          </Navigation.TriggerItem>
+          <Navigation.TriggerItem
+            label="Features"
+            className={{ root: `text-white` }}
+            dropdownWidth="w-[20rem]"
+          >
+            <Navigation.DropdownItem
+              title="Umfragen"
+              onClick={() =>
+                router.push(
+                  `/course/${data.basicCourseInformation?.id}/docs/polls`
+                )
+              }
+              className={{ root: 'text-center' }}
+            />
+            <Navigation.DropdownItem
+              title="Live Q&A"
+              onClick={() =>
+                router.push(
+                  `/course/${data.basicCourseInformation?.id}/docs/liveQA`
+                )
+              }
+            />
+            <Navigation.DropdownItem
+              title="Gruppenaktivitäten"
+              onClick={() =>
+                router.push(
+                  `/course/${data.basicCourseInformation?.id}/docs/groupActivities`
+                )
+              }
+            />
+            <Navigation.DropdownItem
+              title="Microlearning"
+              onClick={() =>
+                router.push(
+                  `/course/${data.basicCourseInformation?.id}/docs/microSessions`
+                )
+              }
+            />
+            <Navigation.DropdownItem
+              title="Selbsttests"
+              onClick={() =>
+                router.push(
+                  `/course/${data.basicCourseInformation?.id}/docs/learningElements`
+                )
+              }
+            />
+          </Navigation.TriggerItem>
+        </Navigation>
         <div className="p-4 prose border rounded-b max-w-none">
           {typeof children === 'function'
             ? children(data.basicCourseInformation)
