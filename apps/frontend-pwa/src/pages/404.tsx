f739c2b9d6046d5fea770eb16d2d27cfd3cd19eb
sjschlapbach <julius.schlapbach@gmx.net>
2023-02-21 13:27:23 +0100

    wip: translate pwa error page

--- apps/frontend-pwa/src/pages/404.tsx
+++ apps/frontend-pwa/src/pages/404.tsx
@@ -2,10 +2,12 @@ import { useQuery } from '@apollo/client'
 import { faBan } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { SelfDocument } from '@klicker-uzh/graphql/dist/ops'
+import { useTranslations } from 'next-intl'
 import Link from 'next/link'
 import Layout from '../components/Layout'
 
 function MissingPage() {
+  const t = useTranslations()
   const { data: dataParticipant } = useQuery(SelfDocument)
 
   return (
@@ -13,29 +15,34 @@ function MissingPage() {
       <div className="flex flex-col items-center gap-6 mx-auto my-auto text-center">
         <div className="flex flex-row items-center gap-4 text-2xl text-red-600 sm:gap-6 md:gap-8 sm:text-3xl md:text-4xl">
           <FontAwesomeIcon icon={faBan} className="h-14 sm:h-18 md:h-20" />
-          <div>404 Page not found</div>
+          <div>{t('shared.error.404')}</div>
         </div>
         {!dataParticipant?.self && (
           <div className="max-w-[90%] sm:max-w-[70%] md:max-w-[35rem]">
-            Die von Ihnen aufgerufene Seite existiert leider nicht. Sie können
-            sich{' '}
-            <Link
-              href="/login"
-              className="text-uzh-blue-60 hover:text-uzh-blue-100"
-            >
-              anmelden
-            </Link>
-            , um eine Übersicht aller Klicker-Elemente Ihrer Kurse zu sehen.
+            {t.rich('shared.error.pwaWithoutUser', {
+              login: (text) => (
+                <Link
+                  href="/login"
+                  className="text-uzh-blue-60 hover:text-uzh-blue-100"
+                >
+                  {text}
+                </Link>
+              ),
+            })}
           </div>
         )}
         {dataParticipant?.self && (
           <div className="max-w-[90%] sm:max-w-[70%] md:max-w-[35rem]">
-            Die von Ihnen aufgerufene Seite existiert leider nicht. Sehen Sie
-            sich eine{' '}
-            <Link href="/" className="text-uzh-blue-60 hover:text-uzh-blue-100">
-              Übersicht
-            </Link>{' '}
-            aller Klicker-Elemente Ihrer Kurse an.
+            {t.rich('shared.error.pwaWithUser', {
+              home: (text) => (
+                <Link
+                  href="/"
+                  className="text-uzh-blue-60 hover:text-uzh-blue-100"
+                >
+                  {text}
+                </Link>
+              ),
+            })}
           </div>
         )}
       </div>
