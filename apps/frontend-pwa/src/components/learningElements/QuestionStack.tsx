fa17adf35048959068154a33bd0171683f02b16e
sjschlapbach <julius.schlapbach@gmx.net>
2023-03-04 19:11:09 +0100

    wip: implement submission logic

--- apps/frontend-pwa/src/components/learningElements/QuestionStack.tsx
+++ apps/frontend-pwa/src/components/learningElements/QuestionStack.tsx
@@ -1,4 +1,13 @@
-import { QuestionStack, QuestionType } from '@klicker-uzh/graphql/dist/ops'
+import { useMutation } from '@apollo/client'
+import {
+  QuestionStack,
+  QuestionType,
+  ResponseToQuestionInstanceDocument,
+} from '@klicker-uzh/graphql/dist/ops'
+import formatResponse from '@lib/formatResponse'
+import { Button } from '@uzh-bf/design-system'
+import { useTranslations } from 'next-intl'
+import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 import DynamicMarkdown from './DynamicMarkdown'
 import SingleQuestion from './SingleQuestion'
@@ -16,13 +25,16 @@ function QuestionStack({
   totalSteps,
   handleNextQuestion,
 }: QuestionStackProps) {
+  const t = useTranslations()
+  const router = useRouter()
+
   const [responses, setResponses] = useState<
     Record<string, {} | number[] | string | null>
   >({})
 
-  // const [respondToQuestionInstance] = useMutation(
-  //   ResponseToQuestionInstanceDocument
-  // )
+  const [respondToQuestionInstance] = useMutation(
+    ResponseToQuestionInstanceDocument
+  )
 
   useEffect(() => {
     stack.elements?.map((element) => {
@@ -49,64 +61,79 @@ function QuestionStack({
         }))
       }
     })
-    return () => setResponses({})
+    return () => {
+      setResponses({})
+    }
   }, [stack])
 
   console.log(responses)
 
-  // const handleSubmitResponse = () => {
-  //   // TODO: check if use here or on single question
+  const handleSubmitResponse = async () => {
+    const responsePromises = Object.keys(responses).map((key) => {
+      const element = stack.elements?.find((el) => el.id === parseInt(key))
+      if (!element?.questionInstance) return
 
-  //   if (!response) return
-  //   scrollTo(0, 0)
+      return respondToQuestionInstance({
+        variables: {
+          courseId: router.query.courseId as string,
+          id: element.questionInstance.id,
+          response: formatResponse(
+            element.questionInstance.questionData,
+            responses[key]
+          ),
+        },
+      })
+    })
 
-  //   respondToQuestionInstance({
-  //     variables: {
-  //       courseId: router.query.courseId as string,
-  //       id: currentInstance?.id as number,
-  //       response: formatResponse(questionData, response),
-  //     },
-  //   })
-  // }
-
-  console.log(stack)
+    await Promise.all(responsePromises)
+  }
 
   return (
     <div>
-      {stack.elements?.map((element) => {
-        if (element.mdContent) {
-          return (
-            <div
-              key={element.id}
-              className="py-2 my-4 border-b-2 border-red-400 border-solid last:border-none"
-            >
-              <DynamicMarkdown content={element.mdContent} />
-            </div>
-          )
+      <div>
+        {stack.elements?.map((element) => {
+          if (element.mdContent) {
+            return (
+              <div
+                key={element.id}
+                className="py-2 my-4 border-b-2 border-red-400 border-solid last:border-none"
+              >
+                <DynamicMarkdown content={element.mdContent} />
+              </div>
+            )
+          }
+          if (element.questionInstance) {
+            return (
+              <div
+                key={element.id}
+                className="py-2 my-4 border-b-2 border-red-400 border-solid last:border-none"
+              >
+                <SingleQuestion
+                  instance={element.questionInstance}
+                  currentStep={currentStep}
+                  totalSteps={totalSteps}
+                  response={responses[element.id]}
+                  setResponse={(response) =>
+                    setResponses((prev) => ({
+                      ...prev,
+                      [element.id]: response,
+                    }))
+                  }
+                />
+              </div>
+            )
+          }
+          return <div key={element.id}>Loading...</div>
+        })}
+      </div>
+      <Button
+        className={{ root: 'float-right' }}
+        onClick={
+          false ? () => handleNextQuestion() : () => handleSubmitResponse()
         }
-        if (element.questionInstance) {
-          return (
-            <div
-              key={element.id}
-              className="py-2 my-4 border-b-2 border-red-400 border-solid last:border-none"
-            >
-              <SingleQuestion
-                instance={element.questionInstance}
-                currentStep={currentStep}
-                totalSteps={totalSteps}
-                response={responses[element.id]}
-                setResponse={(response) =>
-                  setResponses((prev) => ({
-                    ...prev,
-                    [element.id]: response,
-                  }))
-                }
-              />
-            </div>
-          )
-        }
-        return <div key={element.id}>Loading...</div>
-      })}
+      >
+        {false ? t('shared.generic.continue') : t('shared.generic.sendAnswer')}
+      </Button>
     </div>
   )
 }
