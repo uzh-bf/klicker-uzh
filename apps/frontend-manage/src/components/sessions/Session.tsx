1eeafc2a794e8a81b464d9d389e0050aa6d2ef39
Roland Schlaefli <rolandschlaefli@gmail.com>
2023-03-05 19:37:50 +0100

    enhance: use update function to instantly delete the session without refetch

--- apps/frontend-manage/src/components/sessions/Session.tsx
+++ apps/frontend-manage/src/components/sessions/Session.tsx
@@ -4,7 +4,9 @@ import {
   faArrowUpRightFromSquare,
   faCalendarDays,
   faCode,
+  faPencil,
   faPlay,
+  faTrash,
 } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
@@ -44,7 +46,18 @@ function Session({ session }: SessionProps) {
   })
   const [deleteSession] = useMutation(DeleteSessionDocument, {
     variables: { id: session.id || '' },
-    refetchQueries: [GetUserSessionsDocument],
+    update(cache) {
+      const data = cache.readQuery({
+        query: GetUserSessionsDocument,
+      })
+      cache.writeQuery({
+        query: GetUserSessionsDocument,
+        data: {
+          userSessions:
+            data?.userSessions?.filter((e) => e.id !== session.id) ?? [],
+        },
+      })
+    },
   })
 
   const [showDetails, setShowDetails] = useState<boolean>(false)
@@ -55,159 +68,155 @@ function Session({ session }: SessionProps) {
   const router = useRouter()
 
   return (
-    <div key={session.id}>
-      <Collapsible
-        key={session.id}
-        open={showDetails && session.id === selectedSession}
-        onChange={() => {
-          if (session.id === selectedSession) {
-            setShowDetails(!showDetails)
-          } else {
-            setShowDetails(true)
-            setSelectedSession(session.id)
-          }
-        }}
-        staticContent={
-          <div
-            className="flex flex-row justify-between"
-            data-cy="session-block"
-          >
-            <H3 className={{ root: 'mb-0' }}>{session.name}</H3>
-            <div className="flex flex-row gap-5">
-              {session.blocks?.length !== 0 && (
-                <>
-                  <Button
-                    basic
-                    onClick={() => setEmbedModalOpen(true)}
-                    className={{
-                      root: twMerge(
+    <>
+      <div key={session.id} className="p-2 border rounded">
+        <Collapsible
+          className={{ root: 'border-0' }}
+          key={session.id}
+          open={showDetails && session.id === selectedSession}
+          onChange={() => {
+            if (session.id === selectedSession) {
+              setShowDetails(!showDetails)
+            } else {
+              setShowDetails(true)
+              setSelectedSession(session.id)
+            }
+          }}
+          staticContent={
+            <div
+              className="flex flex-row justify-between"
+              data-cy="session-block"
+            >
+              <H3 className={{ root: 'mb-0' }}>{session.name}</H3>
+              <div className="flex flex-row gap-5">
+                {session.blocks?.length !== 0 && (
+                  <>
+                    <Button
+                      basic
+                      onClick={() => setEmbedModalOpen(true)}
+                      className={{
+                        root: twMerge(
+                          'flex flex-row items-center gap-2 text-sm cursor-pointer',
+                          theme.primaryTextHover
+                        ),
+                      }}
+                    >
+                      <Button.Icon>
+                        <FontAwesomeIcon icon={faCode} size="sm" />
+                      </Button.Icon>
+                      <Button.Label>Einbettung Evaluation</Button.Label>
+                    </Button>
+                    <EmbeddingModal
+                      key={session.id}
+                      open={embedModalOpen}
+                      onClose={() => setEmbedModalOpen(false)}
+                      sessionId={session.id}
+                      questions={session.blocks?.flatMap(
+                        (block: SessionBlock) => block.instances
+                      )}
+                    />
+                  </>
+                )}
+
+                {SESSION_STATUS.RUNNING === session.status && (
+                  <Link href={`/sessions/${session.id}/cockpit`} legacyBehavior>
+                    <div
+                      className={twMerge(
                         'flex flex-row items-center gap-2 text-sm cursor-pointer',
                         theme.primaryTextHover
-                      ),
+                      )}
+                    >
+                      <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+                      <div>Dozierenden Cockpit</div>
+                    </div>
+                  </Link>
+                )}
+                {SESSION_STATUS.COMPLETED === session.status && (
+                  <Link
+                    href={`/sessions/${session.id}/evaluation`}
+                    legacyBehavior
+                  >
+                    <div
+                      className={twMerge(
+                        'flex flex-row items-center gap-2 text-sm cursor-pointer',
+                        theme.primaryTextHover
+                      )}
+                    >
+                      <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+                      <div>Session Evaluation</div>
+                    </div>
+                  </Link>
+                )}
+                {(SESSION_STATUS.PREPARED === session.status ||
+                  SESSION_STATUS.SCHEDULED === session.status) && (
+                  <Button
+                    basic
+                    onClick={async () => {
+                      await startSession()
+                      router.push(`sessions/${session.id}/cockpit`)
                     }}
                   >
-                    <Button.Icon>
-                      <FontAwesomeIcon icon={faCode} size="sm" />
-                    </Button.Icon>
-                    <Button.Label>Einbettung Evaluation</Button.Label>
-                  </Button>
-                  <EmbeddingModal
-                    key={session.id}
-                    open={embedModalOpen}
-                    onClose={() => setEmbedModalOpen(false)}
-                    sessionId={session.id}
-                    questions={session.blocks?.flatMap(
-                      (block: SessionBlock) => block.instances
-                    )}
-                  />
-                </>
-              )}
-
-              {SESSION_STATUS.RUNNING === session.status && (
-                <Link href={`/sessions/${session.id}/cockpit`} legacyBehavior>
-                  <div
-                    className={twMerge(
-                      'flex flex-row items-center gap-2 text-sm cursor-pointer',
-                      theme.primaryTextHover
-                    )}
-                  >
-                    <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
-                    <div>Dozierenden Cockpit</div>
-                  </div>
-                </Link>
-              )}
-              {SESSION_STATUS.COMPLETED === session.status && (
-                <Link
-                  href={`/sessions/${session.id}/evaluation`}
-                  legacyBehavior
-                >
-                  <div
-                    className={twMerge(
-                      'flex flex-row items-center gap-2 text-sm cursor-pointer',
-                      theme.primaryTextHover
-                    )}
-                  >
-                    <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
-                    <div>Session Evaluation</div>
-                  </div>
-                </Link>
-              )}
-              {(SESSION_STATUS.PREPARED === session.status ||
-                SESSION_STATUS.SCHEDULED === session.status) && (
-                <Button
-                  basic
-                  onClick={async () => {
-                    await startSession()
-                    router.push(`sessions/${session.id}/cockpit`)
-                  }}
-                >
-                  <div
-                    className={twMerge(
-                      'flex flex-row items-center gap-2 text-sm cursor-pointer',
-                      theme.primaryTextHover
-                    )}
-                  >
-                    <FontAwesomeIcon icon={faPlay} size="sm" />
-                    <div>Start Session</div>
-                  </div>
-                </Button>
-              )}
-              <div className="flex flex-row items-center text-sm">
-                <FontAwesomeIcon icon={faCalendarDays} className="mr-1" />
-                {dayjs(session.createdAt).format('YYYY-MM-DD HH:mm')}
-              </div>
-            </div>
-          </div>
-        }
-        closedContent={
-          <div className="italic">
-            {session.numOfBlocks} Blöcke, {session.numOfQuestions} Fragen
-          </div>
-        }
-      >
-        <div className="flex flex-row gap-2 my-2 overflow-y-scroll">
-          {session.blocks?.map((block, index) => (
-            <div key={block.id} className="flex flex-col gap-2">
-              <div className="italic">
-                Block {index + 1} ({block.instances.length} Fragen)
-              </div>
-              {block.instances.map((instance) => (
-                <div
-                  key={instance.id}
-                  className="text-sm border border-solid rounded-md w-60 border-uzh-grey-100"
-                >
-                  <div className="flex flex-row justify-between bg-uzh-grey-40 px-1 py-0.5">
-                    <Ellipsis
-                      className={{ markdown: 'font-bold' }}
-                      maxLength={20}
+                    <div
+                      className={twMerge(
+                        'flex flex-row items-center gap-2 text-sm cursor-pointer',
+                        theme.primaryTextHover
+                      )}
                     >
-                      {instance.questionData.name}
-                    </Ellipsis>
-
-                    <div className="italic">
-                      ({QUESTION_TYPES_SHORT[instance.questionData.type]})
+                      <FontAwesomeIcon icon={faPlay} size="sm" />
+                      <div>Start Session</div>
                     </div>
-                  </div>
-                  <Ellipsis maxLength={50} className={{ markdown: 'px-1' }}>
-                    {instance.questionData.content}
-                  </Ellipsis>
+                  </Button>
+                )}
+                <div className="flex flex-row items-center text-sm">
+                  <FontAwesomeIcon icon={faCalendarDays} className="mr-1" />
+                  {dayjs(session.createdAt).format('YYYY-MM-DD HH:mm')}
                 </div>
-              ))}
+              </div>
             </div>
-          ))}
-        </div>
+          }
+          closedContent={
+            <div className="italic">
+              {session.numOfBlocks} Blöcke, {session.numOfQuestions} Fragen
+            </div>
+          }
+        >
+          <div className="flex flex-row gap-2 my-2 overflow-y-scroll">
+            {session.blocks?.map((block, index) => (
+              <div key={block.id} className="flex flex-col gap-2">
+                <div className="italic">
+                  Block {index + 1} ({block.instances.length} Fragen)
+                </div>
+                {block.instances.map((instance) => (
+                  <div
+                    key={instance.id}
+                    className="text-sm border border-solid rounded-md w-60 border-uzh-grey-100"
+                  >
+                    <div className="flex flex-row justify-between bg-uzh-grey-40 px-1 py-0.5">
+                      <Ellipsis
+                        className={{ markdown: 'font-bold' }}
+                        maxLength={20}
+                      >
+                        {instance.questionData.name}
+                      </Ellipsis>
+
+                      <div className="italic">
+                        ({QUESTION_TYPES_SHORT[instance.questionData.type]})
+                      </div>
+                    </div>
+                    <Ellipsis maxLength={50} className={{ markdown: 'px-1' }}>
+                      {instance.questionData.content}
+                    </Ellipsis>
+                  </div>
+                ))}
+              </div>
+            ))}
+          </div>
+        </Collapsible>
         {(SESSION_STATUS.PREPARED === session.status ||
           SESSION_STATUS.SCHEDULED === session.status) && (
-          <div className="flex flex-row float-right gap-3">
+          <div className="flex flex-row float-right gap-1">
             <Button
-              className={{
-                root: 'bg-red-600 font-bold text-white',
-              }}
-              onClick={() => setDeletionModal(true)}
-            >
-              Session löschen
-            </Button>
-            <Button
+              className={{ root: 'text-sm py-1 px-3' }}
               onClick={() =>
                 router.push({
                   pathname: '/',
@@ -215,18 +224,32 @@ function Session({ session }: SessionProps) {
                 })
               }
             >
-              Session bearbeiten
+              <Button.Icon className={{ root: 'text-slate-600' }}>
+                <FontAwesomeIcon icon={faPencil} />
+              </Button.Icon>
+              <Button.Label>Session bearbeiten</Button.Label>
+            </Button>
+            <Button
+              className={{
+                root: 'border-red-600 text-sm py-1 px-3',
+              }}
+              onClick={() => setDeletionModal(true)}
+            >
+              <Button.Icon className={{ root: 'text-red-400' }}>
+                <FontAwesomeIcon icon={faTrash} />
+              </Button.Icon>
+              <Button.Label>Session löschen</Button.Label>
             </Button>
           </div>
         )}
-      </Collapsible>
+      </div>
       <LiveSessionDeletionModal
         deleteSession={deleteSession}
         title={session.name}
         open={deletionModal}
         setOpen={setDeletionModal}
       />
-    </div>
+    </>
   )
 }
 
