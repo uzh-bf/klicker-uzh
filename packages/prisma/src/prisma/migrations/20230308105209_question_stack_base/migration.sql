f4f32f56d59105ef8c38c13e1b4e3742090ee2e5
Roland Schlaefli <rolandschlaefli@gmail.com>
2023-03-08 12:11:04 +0100

    create new migration

--- packages/prisma/src/prisma/migrations/20230308105209_question_stack_base/migration.sql
+++ packages/prisma/src/prisma/migrations/20230308105209_question_stack_base/migration.sql
@@ -2,6 +2,7 @@
   Warnings:
 
   - You are about to drop the column `learningElementId` on the `QuestionInstance` table. All the data in the column will be lost.
+  - You are about to drop the `_ParticipationToQuestionInstance` table. If the table is not empty, all the data it contains will be lost.
   - A unique constraint covering the columns `[stackElementId]` on the table `QuestionInstance` will be added. If there are existing duplicate values, this will fail.
   - A unique constraint covering the columns `[type,stackElementId,order]` on the table `QuestionInstance` will be added. If there are existing duplicate values, this will fail.
 
@@ -12,12 +13,21 @@ CREATE TYPE "QuestionStackType" AS ENUM ('LEARNING_ELEMENT', 'MICRO_SESSION', 'L
 -- DropForeignKey
 ALTER TABLE "QuestionInstance" DROP CONSTRAINT "QuestionInstance_learningElementId_fkey";
 
+-- DropForeignKey
+ALTER TABLE "_ParticipationToQuestionInstance" DROP CONSTRAINT "_ParticipationToQuestionInstance_A_fkey";
+
+-- DropForeignKey
+ALTER TABLE "_ParticipationToQuestionInstance" DROP CONSTRAINT "_ParticipationToQuestionInstance_B_fkey";
+
 -- DropIndex
 DROP INDEX "QuestionInstance_type_learningElementId_order_key";
 
 -- AlterTable
 ALTER TABLE "QuestionInstance" DROP COLUMN "learningElementId",
 ADD COLUMN     "stackElementId" INTEGER;
+
+-- DropTable
+DROP TABLE "_ParticipationToQuestionInstance";
 
 -- CreateTable
 CREATE TABLE "QuestionStack" (
@@ -41,8 +51,20 @@ CREATE TABLE "StackElement" (
     CONSTRAINT "StackElement_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "_ParticipationToQuestionStack" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "QuestionStack_type_learningElementId_order_key" ON "QuestionStack"("type", "learningElementId", "order");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_ParticipationToQuestionStack_AB_unique" ON "_ParticipationToQuestionStack"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_ParticipationToQuestionStack_B_index" ON "_ParticipationToQuestionStack"("B");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "QuestionInstance_stackElementId_key" ON "QuestionInstance"("stackElementId");
@@ -58,3 +80,9 @@ ALTER TABLE "QuestionStack" ADD CONSTRAINT "QuestionStack_learningElementId_fkey
 
 -- AddForeignKey
 ALTER TABLE "StackElement" ADD CONSTRAINT "StackElement_stackId_fkey" FOREIGN KEY ("stackId") REFERENCES "QuestionStack"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_ParticipationToQuestionStack" ADD CONSTRAINT "_ParticipationToQuestionStack_A_fkey" FOREIGN KEY ("A") REFERENCES "Participation"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_ParticipationToQuestionStack" ADD CONSTRAINT "_ParticipationToQuestionStack_B_fkey" FOREIGN KEY ("B") REFERENCES "QuestionStack"("id") ON DELETE CASCADE ON UPDATE CASCADE;
