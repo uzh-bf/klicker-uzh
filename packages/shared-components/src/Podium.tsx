3ec4eb8c522b0398ae6fc96ab4a3fa792b61016f
sjschlapbach <julius.schlapbach@gmx.net>
2023-03-09 13:01:02 +0100

    wip: include podium visualizations on leaderboard

--- packages/shared-components/src/Podium.tsx
+++ packages/shared-components/src/Podium.tsx
@@ -1,33 +1,54 @@
 import { LeaderboardEntry } from '@klicker-uzh/graphql/dist/ops'
+import Image from 'next/image'
 import React, { useMemo } from 'react'
 import { twMerge } from 'tailwind-merge'
-import { ParticipantOther } from './Participant'
 
 function SinglePodium({
   username,
   avatar,
   score,
-  className,
+  rank,
 }: {
   username?: string
   avatar?: string
   score?: number
-  className?: string
+  rank?: number
 }): React.ReactElement {
+  const image = useMemo(() => {
+    if (rank === 1) return '/first.svg'
+    if (rank === 2) return '/second.svg'
+    if (rank === 3) return '/third.svg'
+    return '/first.svg'
+  }, [rank])
+
   return (
-    <div
-      className={twMerge(
-        'flex-1 md:border-b-4 bg-slate-300 md:border-slate-500 rounded-t-md text-sm',
-        className
-      )}
-    >
-      <ParticipantOther
-        className="bg-white shadow border-slate-400"
-        pseudonym={username}
-        avatar={avatar}
-        points={score ?? 0}
-        withAvatar={!!avatar}
+    <div className="relative text-center">
+      <Image
+        src={image}
+        alt={`Podium position ${rank}`}
+        width={300}
+        height={300}
+        className="relative"
       />
+      <Image
+        src={`${process.env.NEXT_PUBLIC_AVATAR_BASE_PATH}/${
+          avatar || 'placeholder'
+        }.svg`}
+        alt="User avatar"
+        height={25}
+        width={30}
+        className="absolute"
+        style={{
+          top: twMerge(
+            rank === 1 && '26%',
+            rank === 2 && '23%',
+            rank === 3 && '30%'
+          ),
+          left: '43%',
+        }}
+      />
+
+      <div className="w-full mx-auto italic">{username}</div>
     </div>
   )
 }
@@ -50,23 +71,23 @@ export function Podium({ leaderboard, className }: PodiumProps) {
   }, [leaderboard])
 
   return (
-    <div className="flex flex-col gap-4 md:items-end md:flex-row">
+    <div className="flex flex-row items-end">
       <SinglePodium
-        className={twMerge('order-2 h-[70px] md:order-1', className?.single)}
+        rank={2}
         username={rank2?.username}
         avatar={rank2?.avatar as string}
         score={rank2?.score}
       />
 
       <SinglePodium
-        className={twMerge('order-1 h-[80px] md:order-2', className?.single)}
+        rank={1}
         username={rank1?.username}
         avatar={rank1?.avatar as string}
         score={rank1?.score}
       />
 
       <SinglePodium
-        className={twMerge('order-3 h-[60px]', className?.single)}
+        rank={3}
         username={rank3?.username}
         avatar={rank3?.avatar as string}
         score={rank3?.score}
