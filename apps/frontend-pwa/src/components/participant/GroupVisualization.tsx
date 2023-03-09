19a6787db8bd88c3198e129e9200685e5cd9027e
sjschlapbach <julius.schlapbach@gmx.net>
2023-03-09 13:38:14 +0100

    wip: add group illustration with avatars

--- apps/frontend-pwa/src/components/participant/GroupVisualization.tsx
+++ apps/frontend-pwa/src/components/participant/GroupVisualization.tsx
@@ -1,16 +1,11 @@
 import Image from 'next/image'
 
 export const POSITIONS = [
-  [77, 75],
-  [69, 98],
-  [101, 83],
-  [93, 106],
-  [123, 92],
-  [115, 115],
-  [146, 100],
-  [138, 123],
-  [170, 108],
-  [162, 131],
+  [55, 156],
+  [55, 132.5],
+  [55, 107.5],
+  [55, 83.5],
+  [55, 55],
 ]
 
 function GroupVisualization({ participants, scaleFactor }) {
@@ -27,8 +22,8 @@ function GroupVisualization({ participants, scaleFactor }) {
     >
       <div className="absolute top-0 left-0 right-0">
         <Image
-          className="border shadow rounded-xl"
-          src="/rakete_mond.png"
+          className="border rounded-lg shadow"
+          src="/bubbelgroup.svg"
           width={width}
           height={height}
           alt=""
@@ -36,10 +31,10 @@ function GroupVisualization({ participants, scaleFactor }) {
         {/* <Image className="" src="/rocket_base.svg" fill /> */}
       </div>
 
-      {participants.slice(0, 10).map((participant, ix) => (
+      {participants.slice(0, 5).map((participant, ix) => (
         <Image
           key={participant.avatar}
-          className="absolute rounded-full"
+          className="absolute"
           style={{
             top: `${POSITIONS[ix][0] * scaleFactor}px`,
             left: `${POSITIONS[ix][1] * scaleFactor}px`,
