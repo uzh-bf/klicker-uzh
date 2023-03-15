6632c9566443e77d9203ff5b7c294ac8986b6d37
Roland Schlaefli <rolandschlaefli@gmail.com>
2023-03-15 14:29:01 +0100

    replace achievement images with own svg

--- packages/prisma/src/data/seedTEST.ts
+++ packages/prisma/src/data/seedTEST.ts
@@ -175,11 +175,11 @@ async function seedTest(prisma: Prisma.PrismaClient) {
       name: 'Explorer',
       description:
         'Du warst Teil des KlickerUZH im ersten Semester. Dankeschön!',
-      icon: 'https://sos-ch-dk-2.exo.io/klicker-prod/achievements/pilot-penguin.svg',
+      icon: '/achievements/Erkundend.svg',
       type: 'PARTICIPANT',
     },
     update: {
-      icon: 'https://sos-ch-dk-2.exo.io/klicker-prod/achievements/pilot-penguin.svg',
+      icon: '/achievements/Erkundend.svg',
     },
   })
 
@@ -190,11 +190,11 @@ async function seedTest(prisma: Prisma.PrismaClient) {
       name: 'Fleisspreis',
       description:
         'Du hast alle verfügbaren Microlearnings und Lernelemente gelöst.',
-      icon: 'https://sos-ch-dk-2.exo.io/klicker-prod/achievements/fleisspreis.svg',
+      icon: '/achievements/Fleisspreis.svg',
       type: 'PARTICIPANT',
     },
     update: {
-      icon: 'https://sos-ch-dk-2.exo.io/klicker-prod/achievements/fleisspreis.svg',
+      icon: '/achievements/Fleisspreis.svg',
     },
   })
 
@@ -205,11 +205,11 @@ async function seedTest(prisma: Prisma.PrismaClient) {
       name: 'Dream Team',
       description:
         'Du hast im Gruppentask über die Hälfte der Punkte erreicht.',
-      icon: 'https://sos-ch-dk-2.exo.io/klicker-prod/achievements/dreamteam.svg',
+      icon: '/achievements/Dreamteam.svg',
       type: 'PARTICIPANT',
     },
     update: {
-      icon: 'https://sos-ch-dk-2.exo.io/klicker-prod/achievements/dreamteam.svg',
+      icon: '/achievements/Dreamteam.svg',
     },
   })
 
@@ -234,11 +234,11 @@ async function seedTest(prisma: Prisma.PrismaClient) {
       name: 'Unerschrocken',
       description:
         'Du hast eine Woche vor Ende der Vorlesung noch keine 6 Fragen beantwortet.',
-      icon: 'https://sos-ch-dk-2.exo.io/klicker-prod/achievements/pirate-penguin.svg',
+      icon: '/achievements/Unerschrocken.svg',
       type: 'PARTICIPANT',
     },
     update: {
-      icon: 'https://sos-ch-dk-2.exo.io/klicker-prod/achievements/pirate-penguin.svg',
+      icon: '/achievements/Unerschrocken.svg',
     },
   })
 
