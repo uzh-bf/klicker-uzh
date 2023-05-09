e4e1b6882c224718b4ce859d96940327ff41cffe
sjschlapbach <julius.schlapbach@gmx.net>
2023-05-09 19:44:29 +0200

    enhance(apps/frontend-manage): adapt tag list styling for new scrolling setup

--- apps/frontend-manage/src/components/questions/tags/TagList.tsx
+++ apps/frontend-manage/src/components/questions/tags/TagList.tsx
@@ -5,13 +5,15 @@ import {
   faRectangleList as faListRegular,
 } from '@fortawesome/free-regular-svg-icons'
 import {
+  faChevronDown,
+  faChevronUp,
   faCommentDots,
   faRectangleList as faListSolid,
 } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { GetUserTagsDocument, Tag } from '@klicker-uzh/graphql/dist/ops'
 import { Button, ThemeContext, UserNotification } from '@uzh-bf/design-system'
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { QUESTION_TYPES, TYPES_LABELS } from 'shared-components/src/constants'
 import { twMerge } from 'tailwind-merge'
 import TagListItem from './TagListItem'
@@ -44,6 +46,10 @@ function TagList({
     loading: tagsLoading,
     error: tagsError,
   } = useQuery(GetUserTagsDocument)
+
+  const [questionTypesVisible, setQuestionTypesVisible] = useState(true)
+  const [userTagsVisible, setUserTagsVisible] = useState(false)
+  const [gamificationTagsVisible, setGamificationTagsVisible] = useState(false)
 
   const tags = tagsData?.userTags?.map((tag) => {
     return { name: tag.name, id: tag.id }
@@ -92,90 +98,128 @@ function TagList({
         </Button> */}
 
       <div>
-        <div className="px-4 py-1 font-bold mb-1 border border-b border-x-0 border-solid border-t-0 border-gray-300 text-[1.05rem] text-neutral-500 mt-4">
-          Fragetypen
+        <div
+          className={twMerge(
+            'flex flex-row items-center justify-between px-4 py-0.5 mb-1 mt-3',
+            'font-bold border-b border-solid border-gray-300 text-[1.05rem] text-neutral-500'
+          )}
+        >
+          <div>Fragetypen</div>
+          <FontAwesomeIcon
+            icon={questionTypesVisible ? faChevronUp : faChevronDown}
+            onClick={() => setQuestionTypesVisible(!questionTypesVisible)}
+          />
         </div>
-        <ul className="p-0 m-0 list-none">
-          {Object.values(QUESTION_TYPES).map((type) => (
+        {questionTypesVisible && (
+          <ul className="p-0 m-0 list-none">
+            {Object.values(QUESTION_TYPES).map((type) => (
+              <li
+                key={type}
+                className={twMerge(
+                  'px-4 py-0.5 hover:cursor-pointer',
+                  theme.primaryTextHover,
+                  activeType === type && theme.primaryText
+                )}
+                onClick={(): void => handleTagClick(type, true)}
+              >
+                <FontAwesomeIcon
+                  icon={activeType === type ? faListSolid : faListRegular}
+                  className="mr-2"
+                />
+                {TYPES_LABELS[type]}
+              </li>
+            ))}
+          </ul>
+        )}
+
+        <div
+          className={twMerge(
+            'flex flex-row items-center justify-between px-4 py-0.5 mb-1 mt-3',
+            'font-bold border-b border-solid border-gray-300 text-[1.05rem] text-neutral-500'
+          )}
+        >
+          <div>Tags</div>
+          <FontAwesomeIcon
+            icon={userTagsVisible ? faChevronUp : faChevronDown}
+            onClick={() => setUserTagsVisible(!userTagsVisible)}
+          />
+        </div>
+
+        {userTagsVisible &&
+          ((): React.ReactElement => {
+            if (tagsLoading) {
+              return <div>Loading...</div>
+            }
+
+            if (tagsError) {
+              return (
+                <UserNotification type="error" message={tagsError.message} />
+              )
+            }
+
+            if (tags?.length === 0) {
+              return (
+                <UserNotification type="info">
+                  Keine Tags verfügbar
+                </UserNotification>
+              )
+            }
+
+            return (
+              <ul className="p-0 m-0 list-none">
+                {tags?.map(
+                  (tag: Tag, index: number): React.ReactElement => (
+                    <TagListItem
+                      key={index}
+                      tag={tag}
+                      handleTagClick={handleTagClick}
+                      active={activeTags.includes(tag.name)}
+                    />
+                  )
+                )}
+              </ul>
+            )
+          })()}
+
+        <div
+          className={twMerge(
+            'flex flex-row items-center justify-between px-4 py-0.5 mb-1 mt-3',
+            'font-bold border-b border-solid border-gray-300 text-[1.05rem] text-neutral-500'
+          )}
+        >
+          <div>Gamification</div>
+          <FontAwesomeIcon
+            icon={gamificationTagsVisible ? faChevronUp : faChevronDown}
+            onClick={() => setGamificationTagsVisible(!gamificationTagsVisible)}
+          />
+        </div>
+
+        {gamificationTagsVisible && (
+          <ul className="p-0 m-0 list-none">
             <li
-              key={type}
               className={twMerge(
-                'px-4 py-1 hover:cursor-pointer',
+                'px-4 py-0.5 hover:cursor-pointer',
                 theme.primaryTextHover,
-                activeType === type && theme.primaryText
+                sampleSolution && theme.primaryText
               )}
-              onClick={(): void => handleTagClick(type, true)}
+              onClick={(): void => handleSampleSolutionClick(!sampleSolution)}
             >
-              <FontAwesomeIcon
-                icon={activeType === type ? faListSolid : faListRegular}
-                className="mr-2"
-              />
-              {TYPES_LABELS[type]}
+              <FontAwesomeIcon icon={faCheckCircle} className="mr-2" />
+              Musterlösung
             </li>
-          ))}
-        </ul>
-
-        <div className="px-4 py-1 font-bold mb-1 border border-b border-x-0 border-solid border-t-0 border-gray-300 text-[1.05rem] text-neutral-500 mt-4">
-          Tags
-        </div>
-
-        {((): React.ReactElement => {
-          if (tagsLoading) {
-            return <div>Loading...</div>
-          }
-
-          if (tagsError) {
-            return <UserNotification type="error" message={tagsError.message} />
-          }
-
-          if (tags?.length === 0) {
-            return <div>Keine Tags verfügbar</div>
-          }
-
-          return (
-            <ul className="p-0 m-0 list-none">
-              {tags?.map(
-                (tag: Tag, index: number): React.ReactElement => (
-                  <TagListItem
-                    key={index}
-                    tag={tag}
-                    handleTagClick={handleTagClick}
-                    active={activeTags.includes(tag.name)}
-                  />
-                )
+            <li
+              className={twMerge(
+                'px-4 py-0.5 hover:cursor-pointer',
+                theme.primaryTextHover,
+                answerFeedbacks && theme.primaryText
               )}
-            </ul>
-          )
-        })()}
-
-        <div className="px-4 py-1 font-bold mb-1 border border-b border-x-0 border-solid border-t-0 border-gray-300 text-[1.05rem] text-neutral-500 mt-4">
-          Gamification
-        </div>
-
-        <ul className="p-0 m-0 list-none">
-          <li
-            className={twMerge(
-              'px-4 py-1 hover:cursor-pointer',
-              theme.primaryTextHover,
-              sampleSolution && theme.primaryText
-            )}
-            onClick={(): void => handleSampleSolutionClick(!sampleSolution)}
-          >
-            <FontAwesomeIcon icon={faCheckCircle} className="mr-2" />
-            Musterlösung
-          </li>
-          <li
-            className={twMerge(
-              'px-4 py-1 hover:cursor-pointer',
-              theme.primaryTextHover,
-              answerFeedbacks && theme.primaryText
-            )}
-            onClick={(): void => handleAnswerFeedbacksClick(!answerFeedbacks)}
-          >
-            <FontAwesomeIcon icon={faCommentDots} className="mr-2" />
-            Antwort-Feedbacks
-          </li>
-        </ul>
+              onClick={(): void => handleAnswerFeedbacksClick(!answerFeedbacks)}
+            >
+              <FontAwesomeIcon icon={faCommentDots} className="mr-2" />
+              Antwort-Feedbacks
+            </li>
+          </ul>
+        )}
       </div>
     </div>
   )
