ca2716f8d5dcb564de06f552e029646c0a2485a0
sjschlapbach <julius.schlapbach@gmx.net>
2023-05-09 20:03:54 +0200

    chore(apps/frontend-manage): ensure that entire tag header is clickable

--- apps/frontend-manage/src/components/questions/tags/TagHeader.tsx
+++ apps/frontend-manage/src/components/questions/tags/TagHeader.tsx
@@ -1,5 +1,6 @@
 import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Button } from '@uzh-bf/design-system'
 import { twMerge } from 'tailwind-merge'
 
 interface TagHeaderProps {
@@ -10,18 +11,19 @@ interface TagHeaderProps {
 
 function TagHeader({ text, state, setState }: TagHeaderProps) {
   return (
-    <div
-      className={twMerge(
-        'flex flex-row items-center justify-between px-4 py-0.5 mb-1 mt-3',
-        'font-bold border-b border-solid border-gray-300 text-[1.05rem] text-neutral-500'
-      )}
+    <Button
+      basic
+      onClick={() => setState(!state)}
+      className={{
+        root: twMerge(
+          'flex flex-row items-center justify-between w-full px-4 py-0.5 mb-1 mt-3',
+          'font-bold border-b border-solid border-gray-300 text-[1.05rem] text-neutral-500'
+        ),
+      }}
     >
       <div>{text}</div>
-      <FontAwesomeIcon
-        icon={state ? faChevronUp : faChevronDown}
-        onClick={() => setState(!state)}
-      />
-    </div>
+      <FontAwesomeIcon icon={state ? faChevronUp : faChevronDown} />
+    </Button>
   )
 }
 
