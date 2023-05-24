5203753f63500c766cb6afc1d00d939c3a814765
BShaq <bulinshaqiri@hotmail.com>
2023-05-24 15:22:26 +0200

    fix sonar qube error

--- apps/frontend-manage/worker/index.js
+++ apps/frontend-manage/worker/index.js
@@ -1,16 +1,15 @@
 'use strict'
 
 // Activate a new service worker immediately when available in order to update the app to the newest version
-self.addEventListener('install', async () => {
+self.addEventListener('install', () => {
   self.skipWaiting()
 })
 
 // Reload each open page to make sure the new service worker is in charge
-self.addEventListener('activate', async () => {
-  await self.clients.matchAll({ type: 'window' }).then(tabs => {
-    tabs.forEach((tab) => {
-      tab.navigate(tab.url)
-    })
+self.addEventListener('activate', () => {
+  const tabs =  self.clients.matchAll({ type: 'window' })
+  tabs.forEach((tab) => {
+    tab.navigate(tab.url)
   })
 })
 
