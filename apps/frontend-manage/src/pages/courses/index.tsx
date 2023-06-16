5a3a816b5a929917b6b38ae5514554b5da80bbeb
sjschlapbach <julius.schlapbach@gmx.net>
2023-06-16 22:47:00 +0200

    refactor: simplify structure of course list buttons

--- apps/frontend-manage/src/pages/courses/index.tsx
+++ apps/frontend-manage/src/pages/courses/index.tsx
@@ -1,12 +1,12 @@
 import { useQuery } from '@apollo/client'
 import { faPeopleGroup, faPlusCircle } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { GetUserCoursesDocument } from '@klicker-uzh/graphql/dist/ops'
-import { Button, H3 } from '@uzh-bf/design-system'
+import { H3, UserNotification } from '@uzh-bf/design-system'
 import { useRouter } from 'next/router'
 
 import { useState } from 'react'
 import Layout from '../../components/Layout'
+import CourseListButton from '../../components/courses/CourseListButton'
 import CourseCreationModal from '../../components/courses/modals/CourseCreationModal'
 
 function CourseSelectionPage() {
@@ -22,63 +22,49 @@ function CourseSelectionPage() {
     return <Layout>Loading...</Layout>
   }
 
-  // TODO: refactor buttons to use identical component with custom text / icon / onClick function
-
   return (
     <Layout>
-      <div className="flex items-center justify-center w-full">
-        {dataCourses?.userCourses ? (
-          <div className="md:w-[30rem] w-[20rem]">
-            <H3>Bitte wählen Sie einen Kurs aus:</H3>
-            <div className="flex flex-col gap-2">
-              {dataCourses.userCourses.map((course) => (
-                <Button
-                  key={course.id}
-                  className={{
-                    root: 'p-2 border border-solid rounded-md bg-uzh-grey-40 border-uzh-grey-100',
-                  }}
-                  onClick={() => router.push(`/courses/${course.id}`)}
-                >
-                  <Button.Icon className={{ root: 'ml-1 mr-3' }}>
-                    <FontAwesomeIcon icon={faPeopleGroup} />
-                  </Button.Icon>
-                  <Button.Label>{course.displayName}</Button.Label>
-                </Button>
-              ))}
-              <Button
-                className={{
-                  root: 'p-2 border border-solid rounded-md bg-uzh-grey-40 border-uzh-grey-100',
-                }}
-                onClick={() => showCreateCourseModal(true)}
-              >
-                <Button.Icon className={{ root: 'ml-1 mr-3' }}>
-                  <FontAwesomeIcon icon={faPlusCircle} />
-                </Button.Icon>
-                <Button.Label>Neuen Kurs erstellen</Button.Label>
-              </Button>
+      <div className="flex justify-center w-full">
+        <div className="flex flex-col w-max">
+          <H3>Bitte wählen Sie einen Kurs aus:</H3>
+          {dataCourses?.userCourses && dataCourses.userCourses.length > 0 ? (
+            <div className="md:w-[30rem] w-[20rem]">
+              <div className="flex flex-col gap-2">
+                {dataCourses.userCourses.map((course) => (
+                  <CourseListButton
+                    key={course.id}
+                    onClick={() => router.push(`/courses/${course.id}`)}
+                    icon={faPeopleGroup}
+                    label={course.displayName}
+                  />
+                ))}
+                <CourseListButton
+                  onClick={() => showCreateCourseModal(true)}
+                  icon={faPlusCircle}
+                  label="Neuen Kurs erstellen"
+                />
+              </div>
             </div>
-          </div>
-        ) : (
-          <div>
-            <H3>Keine Kurse gefunden...</H3>
-            {/* // TODO: Create course */}
-            <Button
-              className={{
-                root: 'p-2 border border-solid rounded-md bg-uzh-grey-40 border-uzh-grey-100',
-              }}
-              onClick={() => showCreateCourseModal(true)}
-            >
-              <Button.Icon className={{ root: 'ml-1 mr-3' }}>
-                <FontAwesomeIcon icon={faPlusCircle} />
-              </Button.Icon>
-              <Button.Label>Jetzt einen Kurs erstellen!</Button.Label>
-            </Button>
-          </div>
-        )}
-        <CourseCreationModal
-          modalOpen={createCourseModal}
-          onModalClose={() => showCreateCourseModal(false)}
-        />
+          ) : (
+            <div className="md:w-[30rem] w-[20rem]">
+              <UserNotification
+                type="warning"
+                className={{ root: 'text-normal mb-3' }}
+              >
+                Es konnten keine Kurse gefunden werden.
+              </UserNotification>
+              <CourseListButton
+                onClick={() => showCreateCourseModal(true)}
+                icon={faPlusCircle}
+                label="Jetzt einen Kurs erstellen!"
+              />
+            </div>
+          )}
+          <CourseCreationModal
+            modalOpen={createCourseModal}
+            onModalClose={() => showCreateCourseModal(false)}
+          />
+        </div>
       </div>
     </Layout>
   )
