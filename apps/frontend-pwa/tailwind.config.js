33d43346fd566326e499a5d4cdce923fb78ec543
sjschlapbach <julius.schlapbach@gmx.net>
2023-07-11 13:16:52 +0200

    chore: remove unused variable specifications in tailwind config files

--- apps/frontend-pwa/tailwind.config.js
+++ apps/frontend-pwa/tailwind.config.js
@@ -11,16 +11,6 @@ module.exports = {
       ...TailwindAnimations,
       colors: {
         ...TailwindColorsUZH,
-        primary: 'var(--theme-color-primary)',
-        'primary-80': 'var(--theme-color-primary-80)',
-        'primary-60': 'var(--theme-color-primary-60)',
-        'primary-40': 'var(--theme-color-primary-40)',
-        'primary-20': 'var(--theme-color-primary-20)',
-        secondary: 'var(--theme-color-secondary)',
-        'secondary-80': 'var(--theme-color-secondary-80)',
-        'secondary-60': 'var(--theme-color-secondary-60)',
-        'secondary-40': 'var(--theme-color-secondary-40)',
-        'secondary-20': 'var(--theme-color-secondary-20)',
       },
       fontFamily: {
         ...TailwindFonts,
