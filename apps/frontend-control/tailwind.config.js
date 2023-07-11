d3be7b305321c8b4708668f400925772c7b61d5d
sjschlapbach <julius.schlapbach@gmx.net>
2023-07-11 12:48:47 +0200

    refactor(apps/frontend-control): migrate frontend-control to new design-system theme approach

--- apps/frontend-control/tailwind.config.js
+++ apps/frontend-control/tailwind.config.js
@@ -11,23 +11,32 @@ module.exports = {
       ...TailwindAnimations,
       colors: {
         ...TailwindColorsUZH,
-      },
-      screens: {
-        xs: '480px',
+        primary: 'var(--theme-color-primary)',
+        'primary-80': 'var(--theme-color-primary-80)',
+        'primary-60': 'var(--theme-color-primary-60)',
+        'primary-40': 'var(--theme-color-primary-40)',
+        'primary-20': 'var(--theme-color-primary-20)',
+        secondary: 'var(--theme-color-secondary)',
+        'secondary-80': 'var(--theme-color-secondary-80)',
+        'secondary-60': 'var(--theme-color-secondary-60)',
+        'secondary-40': 'var(--theme-color-secondary-40)',
+        'secondary-20': 'var(--theme-color-secondary-20)',
       },
       fontFamily: {
         ...TailwindFonts,
       },
     },
   },
-  plugins: [
-    require('tailwindcss-radix')({ variantPrefix: 'rdx' }),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/forms'),
-  ],
   corePlugins: {
     preflight: false,
     aspectRatio: false,
   },
+  plugins: [
+    require('tailwindcss-radix')({
+      variantPrefix: 'rdx',
+    }),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/forms'),
+  ],
 }
