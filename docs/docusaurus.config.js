e82d60d9cef0d3c440627b4ee1c545110e0f5082
Roland Schläfli <rolandschlaefli@gmail.com>
2023-07-15 19:37:01 +0200

    migrate to docusaurus default footer and add versioning dropdown

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -4,19 +4,12 @@
 const config = {
   trailingSlash: false,
   title: 'KlickerUZH',
-  tagline: 'Open source instant audience response system',
+  tagline: 'Open Source Audience Interaction',
   url: 'https://www.klicker.uzh.ch',
   baseUrl: '/',
   organizationName: 'uzh-bf',
   projectName: 'klicker-uzh',
-  scripts: [
-    // 'https://buttons.github.io/buttons.js',
-    {
-      src: 'https://betteruptime.com/widgets/announcement.js',
-      'data-id': '133428',
-      async: true,
-    },
-  ],
+  scripts: [],
   stylesheets: [
     'https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;700&display=swap',
   ],
@@ -110,6 +103,7 @@ const config = {
       },
     ],
     navbar: {
+      hideOnScroll: true,
       logo: {
         src: '/img/KlickerLogo.png',
         srcDark: '/img/KlickerLogo.png',
@@ -146,7 +140,10 @@ const config = {
         //   label: 'Knowledge Base',
         //   position: 'left',
         // },
-
+        {
+          type: 'docsVersionDropdown',
+          position: 'right',
+        },
         {
           to: 'https://community.klicker.uzh.ch',
           label: 'Community',
@@ -161,6 +158,64 @@ const config = {
           href: 'https://klicker-uzh.feedbear.com',
           label: 'Feedback',
           position: 'right',
+        },
+      ],
+    },
+    footer: {
+      logo: {
+        alt: 'KlickerUZH Logo',
+        src: '/img/KlickerLogo.png',
+        width: 250,
+        // height: 51,
+      },
+      copyright: `Copyright ${new Date().getFullYear()} Teaching Center, Department of Banking and Finance (https://www.bf.uzh.ch), University of Zurich. <br/>All rights reserved. Products and Services displayed herein are trademarks or registered trademarks of their respective owners.`,
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Home',
+              to: '/home',
+            },
+            {
+              label: 'Privacy Policy',
+              to: '/privacy_policy',
+            },
+            {
+              label: 'Terms of Service',
+              to: '/terms_of_service',
+            },
+          ],
+        },
+        {
+          title: 'Support',
+          items: [
+            {
+              label: 'FAQ',
+              to: '/faq',
+            },
+            {
+              label: 'Feedback',
+              href: 'https://klicker-uzh.feedbear.com',
+            },
+            {
+              label: 'Community',
+              href: 'https://community.klicker.uzh.ch',
+            },
+          ],
+        },
+        {
+          title: 'Technical',
+          items: [
+            {
+              label: 'System Status',
+              href: 'https://klicker-uzh.betteruptime.com/',
+            },
+            {
+              label: 'Source Code',
+              href: 'https://github.com/uzh-bf/klicker-uzh',
+            },
+          ],
         },
       ],
     },
