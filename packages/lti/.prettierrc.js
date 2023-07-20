54418a441584388a97fc249f282965f6d954a080
Roland Schläfli <rolandschlaefli@gmail.com>
2023-07-20 22:42:12 +0200

    current work on upgrades

--- packages/lti/.prettierrc.js
+++ packages/lti/.prettierrc.js
@@ -3,8 +3,8 @@ module.exports = {
   singleQuote: true,
   trailingComma: 'es5',
   plugins: [
-    // FIXME: currently cannot use tailwindcss together with organize imports (https://github.com/tailwindlabs/prettier-plugin-tailwindcss/issues/31)
-    // require('prettier-plugin-tailwindcss'),
+
+    require('prettier-plugin-tailwindcss'),
     require('prettier-plugin-organize-imports'),
   ],
 }
