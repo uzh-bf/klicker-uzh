5d4a952a224f3218775c07b687252ded1716b260
Roland Schläfli <rolandschlaefli@gmail.com>
2023-07-26 21:20:59 +0200

    update prisma migration for verification token

--- packages/prisma/src/prisma/migrations/20230726183803_next_auth_users/migration.sql
+++ packages/prisma/src/prisma/migrations/20230726183803_next_auth_users/migration.sql
@@ -58,3 +58,16 @@ CREATE UNIQUE INDEX "Account_provider_providerAccountId_key" ON "Account"("provi
 
 -- AddForeignKey
 ALTER TABLE "Session" ADD CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- CreateTable
+CREATE TABLE "VerificationToken" (
+    "identifier" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VerificationToken_token_key" ON "VerificationToken"("token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VerificationToken_identifier_token_key" ON "VerificationToken"("identifier", "token");
