2e4ce8128b9e4a0fa56aa5dccb413b629a07d43b
Roland Schläfli <rolandschlaefli@gmail.com>
2023-07-26 20:39:09 +0200

    prepare prisma schema for next auth

---
+++ packages/prisma/src/prisma/migrations/20230726183803_next_auth_users/migration.sql
@@ -0,0 +1,71 @@
+/*
+  Warnings:
+
+  - The primary key for the `Account` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `createdAt` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `ssoId` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `ssoType` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `ssoType` on the `ParticipantAccount` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[provider,providerAccountId]` on the table `Account` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `provider` to the `Account` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `providerAccountId` to the `Account` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `type` to the `Account` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Account" DROP CONSTRAINT "Account_userId_fkey";
+
+-- DropIndex
+DROP INDEX "ParticipantAccount_ssoType_ssoId_key";
+
+-- AlterTable
+ALTER TABLE "Account" DROP CONSTRAINT "Account_pkey",
+DROP COLUMN "createdAt",
+DROP COLUMN "ssoId",
+DROP COLUMN "ssoType",
+ADD COLUMN     "access_token" TEXT,
+ADD COLUMN     "expires_at" INTEGER,
+ADD COLUMN     "id_token" TEXT,
+ADD COLUMN     "provider" TEXT NOT NULL,
+ADD COLUMN     "providerAccountId" TEXT NOT NULL,
+ADD COLUMN     "refresh_token" TEXT,
+ADD COLUMN     "scope" TEXT,
+ADD COLUMN     "session_state" TEXT,
+ADD COLUMN     "token_type" TEXT,
+ADD COLUMN     "type" TEXT NOT NULL,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ALTER COLUMN "userId" SET DATA TYPE TEXT,
+ADD CONSTRAINT "Account_pkey" PRIMARY KEY ("id");
+
+-- AlterTable
+ALTER TABLE "ParticipantAccount" DROP COLUMN "ssoType";
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "emailVerified" TIMESTAMP(3),
+ADD COLUMN     "image" TEXT,
+ADD COLUMN     "name" TEXT;
+
+-- DropEnum
+DROP TYPE "SSOType";
+
+-- CreateTable
+CREATE TABLE "Session" (
+    "id" TEXT NOT NULL,
+    "sessionToken" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Session_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Session_sessionToken_key" ON "Session"("sessionToken");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Account_provider_providerAccountId_key" ON "Account"("provider", "providerAccountId");
+
+-- AddForeignKey
+ALTER TABLE "Account" ADD CONSTRAINT "Account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Session" ADD CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
