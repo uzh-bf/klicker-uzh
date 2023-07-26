a3df14dd632a0059c9d6ff741242e2db60b9f065
Roland Schläfli <rolandschlaefli@gmail.com>
2023-07-26 20:35:45 +0200

    update migration with renamed constraints

--- packages/prisma/src/prisma/migrations/20230726182929_rename_session/migration.sql
+++ packages/prisma/src/prisma/migrations/20230726182929_rename_session/migration.sql
@@ -1,1 +1,5 @@
 ALTER TABLE "Session" RENAME TO "LiveSession";
+ALTER TABLE "LiveSession" RENAME CONSTRAINT "Session_pkey" TO "LiveSession_pkey";
+ALTER TABLE "LiveSession" RENAME CONSTRAINT "Session_activeBlockId_fkey" TO "LiveSession_activeBlockId_fkey";
+ALTER TABLE "LiveSession" RENAME CONSTRAINT "Session_courseId_fkey" TO "LiveSession_courseId_fkey";
+ALTER TABLE "LiveSession" RENAME CONSTRAINT "Session_ownerId_fkey" TO "LiveSession_ownerId_fkey";
