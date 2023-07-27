97b373e356c271dbeda5a16f9067ba583abb94d5
Roland Schläfli <rolandschlaefli@gmail.com>
2023-07-27 20:45:28 +0200

    direct eduid login without providers page

--- apps/auth/src/pages/index.tsx
+++ apps/auth/src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { signIn, signOut, useSession } from 'next-auth/react'
 
-import { Button } from '@uzh-bf/design-system'
+import { Button, H1 } from '@uzh-bf/design-system'
 import Image from 'next/image'
 import Footer from 'shared-components/src/Footer'
 
@@ -15,28 +15,26 @@ function SignInOutButton() {
       </>
     )
   }
+
   return (
-    <>
-      Not signed in <br />{' '}
-      <Button className={{ root: 'p-4' }} onClick={() => signIn()}>
-        <Image
-          src="/edu-id-logo.svg"
-          width={300}
-          height={90}
-          alt="KlickerUZH Logo"
-          className="mx-auto"
-          data-cy="login-logo"
-        />
-      </Button>
-    </>
+    <Button className={{ root: 'p-4' }} onClick={() => signIn('eduid-test')}>
+      <Image
+        src="/edu-id-logo.svg"
+        width={300}
+        height={90}
+        alt="Logo"
+        className="mx-auto"
+        data-cy="login-logo"
+      />
+    </Button>
   )
 }
 
 export function Index({}) {
   return (
-    <div className="m-auto flex max-w-xl flex-grow flex-col md:!flex-grow-0 md:rounded-lg md:border md:shadow">
-      <div className="flex flex-1 flex-col items-center justify-center md:p-8">
-        <div className="mb-8 w-full border-b pb-4 text-center sm:mb-12">
+    <div className="m-auto flex max-w-2xl flex-grow flex-col md:!flex-grow-0 md:rounded-lg md:border md:shadow">
+      <div className="flex flex-1 flex-col items-center justify-center gap-8 md:p-8">
+        <div className="w-full border-b pb-4 text-center">
           <Image
             src="/KlickerLogo.png"
             width={300}
@@ -45,6 +43,9 @@ export function Index({}) {
             className="mx-auto"
             data-cy="login-logo"
           />
+        </div>
+        <div>
+          <H1>Authentication</H1>
         </div>
         <div>
           <SignInOutButton />
