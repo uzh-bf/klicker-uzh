7ae4d2b0dbff5e189d6ed433f06256f815e31398
mxmlnwbr <maximilian.weber@bluewin.ch>
2023-08-23 14:23:55 +0200

    content temp added

--- apps/docs/src/pages/use_cases/index.tsx
+++ apps/docs/src/pages/use_cases/index.tsx
@@ -1,5 +1,6 @@
 import Layout from '@theme/Layout'
-import { H1 } from '@uzh-bf/design-system'
+import { H1, H2 } from '@uzh-bf/design-system'
+import Content from '../../components/Content'
 import TitleBackground from '../../components/common/TitleBackground'
 
 export default function index() {
@@ -8,6 +9,20 @@ export default function index() {
       <TitleBackground>
         <H1 className={{ root: 'mx-auto max-w-6xl lg:pl-4' }}>Use Cases</H1>
       </TitleBackground>
+
+      <Content>
+        <H2>Strenghten Interaction</H2>
+        <div className="">CONTENT</div>
+
+        <H2 className={{ root: 'mt-6' }}>Implement Gamification Elements</H2>
+        <div className="">Test</div>
+
+        <H2 className={{ root: 'mt-6' }}>Knowledge Feedback</H2>
+        <div className="">Test</div>
+
+        <H2 className={{ root: 'mt-6' }}>Learning Beyond Classroom</H2>
+        <div className="">Test</div>
+      </Content>
     </Layout>
   )
 }
