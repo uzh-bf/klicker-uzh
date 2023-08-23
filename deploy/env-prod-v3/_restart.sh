0ee83d545f7a00d9108214d9adf94ce990d44894
Roland Schläfli <rolandschlaefli@gmail.com>
2023-08-23 23:46:44 +0200

    enhance(deploy): add configurations for auth application and eduid

--- deploy/env-prod-v3/_restart.sh
+++ deploy/env-prod-v3/_restart.sh
@@ -2,4 +2,5 @@
 kubectl rollout restart -n klicker-v2-prod deployment klicker-v2-prod-klicker-uzh-v2-backend-graphql
 kubectl rollout restart -n klicker-v2-prod deployment klicker-v2-prod-klicker-uzh-v2-frontend-manage
 kubectl rollout restart -n klicker-v2-prod deployment klicker-v2-prod-klicker-uzh-v2-frontend-pwa
+kubectl rollout restart -n klicker-v2-prod deployment klicker-v2-prod-klicker-uzh-v2-frontend-control
 kubectl get pods -n klicker-v2-prod
