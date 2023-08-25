3ab4f22386341de1aae183ed2a5d5581ebfca73d
mxmlnwbr <maximilian.weber@bluewin.ch>
2023-08-25 16:15:00 +0200

    constants file added and href working

--- apps/docs/src/pages/use_cases/index.tsx
+++ apps/docs/src/pages/use_cases/index.tsx
@@ -12,65 +12,7 @@ import Layout from '@theme/Layout'
 import { Button, H1, H2, H3 } from '@uzh-bf/design-system'
 import { useState } from 'react'
 import { useCollapse } from 'react-collapsed'
-
-const useCases = [
-  {
-    title: '(Gamified) Live Quizzes',
-    imageSrc:
-      'https://img.freepik.com/free-vector/web-help-support-page-template-design_1017-26772.jpg?w=996&t=st=1692859143~exp=1692859743~hmac=7f1540098197c20df60c26ceb08933f99857304b4aa230c0a795cd77d910323c',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia expedita, corrupti odit animi ratione placeat est numquam distinctio cum illo! Qui animi possimus vitae architecto consequuntur sit neque. Qui, ipsa!',
-    detailsRef: 'live_quiz',
-  },
-  {
-    title: 'Flipped Classroom',
-    imageSrc:
-      'https://images.unsplash.com/photo-1516321318423-f06f85e504b3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8ZmxpcHBlZCUyMGNsYXNzcm9vbXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia expedita, corrupti odit animi ratione placeat est numquam distinctio cum illo! Qui animi possimus vitae architecto consequuntur sit neque. Qui, ipsa!',
-    detailsRef: 'flipped_classroom',
-  },
-  {
-    title: 'Microlearning',
-    imageSrc:
-      'https://images.unsplash.com/photo-1494059980473-813e73ee784b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGxlYXJuaW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia expedita, corrupti odit animi ratione placeat est numquam distinctio cum illo! Qui animi possimus vitae architecto consequuntur sit neque. Qui, ipsa!',
-    detailsRef: 'microlearning',
-  },
-  {
-    title: 'Practice Quizzes and Flashcards',
-    imageSrc:
-      'https://images.unsplash.com/photo-1517429481096-5bc77134f77c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHF1aXp6fGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia expedita, corrupti odit animi ratione placeat est numquam distinctio cum illo! Qui animi possimus vitae architecto consequuntur sit neque. Qui, ipsa!',
-    detailsRef: 'flashcards',
-  },
-  {
-    title: 'Group Activities',
-    imageSrc:
-      'https://images.unsplash.com/photo-1582213782179-e0d53f98f2ca?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Z3JvdXB8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia expedita, corrupti odit animi ratione placeat est numquam distinctio cum illo! Qui animi possimus vitae architecto consequuntur sit neque. Qui, ipsa!',
-    detailsRef: 'group_activities',
-  },
-  {
-    title: 'Live Q&A and Real-Time Feedback',
-    imageSrc:
-      'https://images.unsplash.com/photo-1657987974860-a2b837df5fc0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fFElMjZBfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia expedita, corrupti odit animi ratione placeat est numquam distinctio cum illo! Qui animi possimus vitae architecto consequuntur sit neque. Qui, ipsa!',
-    detailsRef: 'live_qa',
-  },
-  {
-    title: 'Gamification',
-    imageSrc:
-      'https://images.unsplash.com/photo-1553481187-be93c21490a9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Z2FtZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia expedita, corrupti odit animi ratione placeat est numquam distinctio cum illo! Qui animi possimus vitae architecto consequuntur sit neque. Qui, ipsa!',
-    detailsRef: 'gamification',
-  },
-]
+import { useCases } from './constants'
 
 export default function index() {
   const [filteredUseCases, setFilteredUseCases] = useState(useCases)
