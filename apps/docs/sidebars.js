934497ac74f631eeecc89545fed5daba4e036d06
Roland Schläfli <rolandschlaefli@gmail.com>
2023-08-26 00:06:36 +0200

    merge core concepts to one page

--- apps/docs/sidebars.js
+++ apps/docs/sidebars.js
@@ -1,13 +1,13 @@
 module.exports = {
   docs: {
-    Introduction: ['introduction/getting_started', 'introduction/about'],
-    'Core Concepts': [
-      'core_concepts/overview',
-      'core_concepts/questions',
-      'core_concepts/activities',
-      'core_concepts/courses',
-      'core_concepts/gamification',
-    ],
+    Introduction: ['introduction/getting_started', 'introduction/core_concepts', 'introduction/about'],
+    // 'Core Concepts': [
+    //   'core_concepts/overview',
+    //   'core_concepts/questions',
+    //   'core_concepts/activities',
+    //   'core_concepts/courses',
+    //   'core_concepts/gamification',
+    // ],
     'Tutorials': [
       'tutorials/question_management'
     ]
