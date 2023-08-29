2f283f46e2e40ca6c5807f95e3911651a6b8b8a5
BShaq <bulinshaqiri@hotmail.com>
2023-08-29 15:39:41 +0200

    wip: initialize android app with capacitor js

---
+++ apps/frontend-pwa/android/app/src/main/java/ch/uzh/klicker/pwa/MainActivity.java
@@ -0,0 +1,5 @@
+package ch.uzh.klicker.pwa;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- apps/frontend-pwa/capacitor.config.ts
+++ apps/frontend-pwa/capacitor.config.ts
@@ -3,10 +3,12 @@ import { CapacitorConfig } from '@capacitor/cli'
 const config: CapacitorConfig = {
   appId: 'ch.uzh.klicker.pwa',
   appName: 'KlickerUZH',
-  // webDir: '.next',
+  //needed, otherwise there is no capacity.settings.gradle created, and hence, the build in android studio fails
+  webDir: '.next',
   server: {
     // url: 'https://pwa.klicker.uzh.ch',
     url: 'http://pwa.klicker.local',
+    cleartext: true,
   },
 }
 
