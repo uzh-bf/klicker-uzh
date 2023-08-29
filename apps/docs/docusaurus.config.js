0c3588cb47aa985098c2e1a16bf19ebeff6c2743
mxmlnwbr <maximilian.weber@bluewin.ch>
2023-08-29 15:18:58 +0200

    fix: redirect to '/' instead of '/home'

--- apps/docs/docusaurus.config.js
+++ apps/docs/docusaurus.config.js
@@ -111,7 +111,7 @@ const config = {
       logo: {
         src: '/img/KlickerLogo.png',
         srcDark: '/img/KlickerLogo.png',
-        href: '/home',
+        href: '/',
       },
       items: [
         {
