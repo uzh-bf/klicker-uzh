0d6b4693350b8756628703cec30198bdac822d59
mxmlnwbr <maximilian.weber@bluewin.ch>
2023-08-29 15:17:00 +0200

    add: TOC inside sidebar

--- apps/docs/src/components/usecases/UseCaseLayout.tsx
+++ apps/docs/src/components/usecases/UseCaseLayout.tsx
@@ -27,39 +27,242 @@ function UseCaseLayout({ path, children }: UseCaseLayoutProps) {
             sidebar={[
               { type: 'link', href: '/use_cases', label: 'Overview' },
               {
-                type: 'link',
-                href: '/use_cases/live_quiz',
+                type: 'category',
                 label: '(Gamified) Live Quizzes',
+                collapsible: true,
+                collapsed: true,
+                href: '/use_cases/live_quiz',
+                items: [
+                  {
+                    type: 'link',
+                    label: 'Introduction',
+                    href: `/use_cases/live_quiz#Introduction`,
+                  },
+                  {
+                    type: 'link',
+                    label: 'Background',
+                    href: '/use_cases/live_quiz#Background',
+                  },
+                  {
+                    type: 'link',
+                    label: 'Scenario',
+                    href: '/use_cases/live_quiz#Scenario',
+                  },
+                  {
+                    type: 'link',
+                    label: 'Background',
+                    href: '/use_cases/live_quiz#Background',
+                  },
+                  {
+                    type: 'link',
+                    label: 'Learnings',
+                    href: '/use_cases/live_quiz#Learnings',
+                  },
+                ],
               },
               {
-                type: 'link',
-                href: '/use_cases/flipped_classroom',
+                type: 'category',
                 label: 'Flipped Classroom',
+                collapsible: true,
+                collapsed: true,
+                href: '/use_cases/flipped_classroom',
+                items: [
+                  {
+                    type: 'link',
+                    label: 'Introduction',
+                    href: `/use_cases/flipped_classroom#Introduction`,
+                  },
+                  {
+                    type: 'link',
+                    label: 'Background',
+                    href: '/use_cases/flipped_classroom#Background',
+                  },
+                  {
+                    type: 'link',
+                    label: 'Scenario',
+                    href: '/use_cases/flipped_classroom#Scenario',
+                  },
+                  {
+                    type: 'link',
+                    label: 'Background',
+                    href: '/use_cases/flipped_classroom#Background',
+                  },
+                  {
+                    type: 'link',
+                    label: 'Learnings',
+                    href: '/use_cases/flipped_classroom#Learnings',
+                  },
+                ],
               },
               {
-                type: 'link',
-                href: '/use_cases/microlearning',
+                type: 'category',
                 label: 'Microlearning',
+                collapsible: true,
+                collapsed: true,
+                href: '/use_cases/microlearning',
+                items: [
+                  {
+                    type: 'link',
+                    label: 'Introduction',
+                    href: `/use_cases/microlearning#Introduction`,
+                  },
+                  {
+                    type: 'link',
+                    label: 'Background',
+                    href: '/use_cases/microlearning#Background',
+                  },
+                  {
+                    type: 'link',
+                    label: 'Scenario',
+                    href: '/use_cases/microlearning#Scenario',
+                  },
+                  {
+                    type: 'link',
+                    label: 'Background',
+                    href: '/use_cases/microlearning#Background',
+                  },
+                  {
+                    type: 'link',
+                    label: 'Learnings',
+                    href: '/use_cases/microlearning#Learnings',
+                  },
+                ],
               },
               {
-                type: 'link',
-                href: '/use_cases/practice_quiz',
+                type: 'category',
                 label: 'Practice Quizzes and Flashcards',
+                collapsible: true,
+                collapsed: true,
+                href: '/use_cases/practice_quiz',
+                items: [
+                  {
+                    type: 'link',
+                    label: 'Introduction',
+                    href: `/use_cases/practice_quiz#Introduction`,
+                  },
+                  {
+                    type: 'link',
+                    label: 'Background',
+                    href: '/use_cases/practice_quiz#Background',
+                  },
+                  {
+                    type: 'link',
+                    label: 'Scenario',
+                    href: '/use_cases/practice_quiz#Scenario',
+                  },
+                  {
+                    type: 'link',
+                    label: 'Background',
+                    href: '/use_cases/practice_quiz#Background',
+                  },
+                  {
+                    type: 'link',
+                    label: 'Learnings',
+                    href: '/use_cases/practice_quiz#Learnings',
+                  },
+                ],
               },
               {
-                type: 'link',
-                href: '/use_cases/group_activity',
+                type: 'category',
                 label: 'Group Activities',
+                collapsible: true,
+                collapsed: true,
+                href: '/use_cases/group_activity',
+                items: [
+                  {
+                    type: 'link',
+                    label: 'Introduction',
+                    href: `/use_cases/group_activity#Introduction`,
+                  },
+                  {
+                    type: 'link',
+                    label: 'Background',
+                    href: '/use_cases/group_activity#Background',
+                  },
+                  {
+                    type: 'link',
+                    label: 'Scenario',
+                    href: '/use_cases/group_activity#Scenario',
+                  },
+                  {
+                    type: 'link',
+                    label: 'Background',
+                    href: '/use_cases/group_activity#Background',
+                  },
+                  {
+                    type: 'link',
+                    label: 'Learnings',
+                    href: '/use_cases/group_activity#Learnings',
+                  },
+                ],
               },
               {
-                type: 'link',
-                href: '/use_cases/live_qa',
+                type: 'category',
                 label: 'Live Q&A and Real-Time Feedback',
+                collapsible: true,
+                collapsed: true,
+                href: '/use_cases/live_qa',
+                items: [
+                  {
+                    type: 'link',
+                    label: 'Introduction',
+                    href: `/use_cases/live_qa#Introduction`,
+                  },
+                  {
+                    type: 'link',
+                    label: 'Background',
+                    href: '/use_cases/live_qa#Background',
+                  },
+                  {
+                    type: 'link',
+                    label: 'Scenario',
+                    href: '/use_cases/live_qa#Scenario',
+                  },
+                  {
+                    type: 'link',
+                    label: 'Background',
+                    href: '/use_cases/live_qa#Background',
+                  },
+                  {
+                    type: 'link',
+                    label: 'Learnings',
+                    href: '/use_cases/live_qa#Learnings',
+                  },
+                ],
               },
               {
-                type: 'link',
-                href: '/use_cases/gamification',
+                type: 'category',
                 label: 'Gamification',
+                collapsible: true,
+                collapsed: true,
+                href: '/use_cases/gamification',
+                items: [
+                  {
+                    type: 'link',
+                    label: 'Introduction',
+                    href: `/use_cases/gamification#Introduction`,
+                  },
+                  {
+                    type: 'link',
+                    label: 'Background',
+                    href: '/use_cases/gamification#Background',
+                  },
+                  {
+                    type: 'link',
+                    label: 'Scenario',
+                    href: '/use_cases/gamification#Scenario',
+                  },
+                  {
+                    type: 'link',
+                    label: 'Background',
+                    href: '/use_cases/gamification#Background',
+                  },
+                  {
+                    type: 'link',
+                    label: 'Learnings',
+                    href: '/use_cases/gamification#Learnings',
+                  },
+                ],
               },
             ]}
             path={path}
