c15c0a04edc5524d6a13b09cae051c29124400f4
BShaq <bulinshaqiri@hotmail.com>
2023-08-29 17:50:37 +0200

    rework question instance transactions

--- apps/func-migration-v3-import/src/importQuestionInstances.ts
+++ apps/func-migration-v3-import/src/importQuestionInstances.ts
@@ -7,6 +7,83 @@ import {
   sliceIntoChunks,
 } from './utils'
 
+const restoreLostLegacyResults = async (
+  importedQuestionInstances: any,
+  // mappedQuestionInstances: Record<string, Record<string, any>>,
+  questionInstancesDict: Record<string, any>
+) => {
+  let mappedQuestionInstances: Record<string, Record<string, any>>
+  let lostResults: any[] = []
+
+  // for-loop kept due to its asynchronous nature
+  for (const questionInstance of importedQuestionInstances) {
+    const questionInstanceExists = questionInstancesDict[questionInstance._id]
+
+    if (questionInstanceExists) {
+      mappedQuestionInstances[questionInstance._id] = questionInstanceExists.id
+      continue
+    }
+
+    if (
+      questionInstance.results &&
+      !questionInstance.results.CHOICES &&
+      !questionInstance.results.FREE
+    ) {
+      mappedQuestionInstances[questionInstance._id]['lostResults'] =
+        await getLegacyResults(questionInstance._id)
+      if (questionInstance.results == null) {
+        lostResults.push(questionInstance)
+      }
+    }
+  }
+  return [mappedQuestionInstances, lostResults]
+}
+
+const processResults = (questionData: any, questionInstance: any) => {
+  let results = {}
+
+  if (questionInstance.results) {
+    if (questionData.type === 'SC' || questionData.type === 'MC') {
+      if (questionInstance.results.CHOICES) {
+        results = questionInstance.results.CHOICES.reduce(
+          (acc, choice, idx) => {
+            acc[idx.toString()] = {
+              count: choice,
+              value: idx.toString(),
+            }
+            return acc
+          },
+          {}
+        )
+      }
+    } else if (
+      questionData.type === QuestionTypeMap['FREE'] ||
+      questionData.type === QuestionTypeMap['FREE_RANGE']
+    ) {
+      if (questionInstance.results.FREE) {
+        let newResults = Object.keys(questionInstance.results.FREE).reduce(
+          (acc, hash) => {
+            acc[hash] = questionInstance.results.FREE[hash]
+            return acc
+          },
+          {}
+        )
+        results = { ...results, ...newResults }
+      } else if (questionInstance.results.FREE_RANGE) {
+        let newResults = Object.keys(
+          questionInstance.results.FREE_RANGE
+        ).reduce((acc, hash) => {
+          acc[hash] = questionInstance.results.FREE_RANGE[hash]
+          return acc
+        }, {})
+        results = { ...results, ...newResults }
+      }
+    }
+  }
+
+  return results
+}
+
 export const importQuestionInstances = async (
   prisma: PrismaClient,
   importedQuestionInstances: any,
@@ -16,7 +93,7 @@ export const importQuestionInstances = async (
   context: InvocationContext
 ) => {
   try {
-    let mappedQuestionInstancesIds: Record<string, number> = {}
+    // let mappedQuestionInstances: Record<string, Record<string, any>> = {}
     const questions = await Promise.all(
       Object.values(mappedQuestionIds).map((questionId) =>
         prisma.question.findUnique({ where: { id: questionId } })
@@ -31,19 +108,23 @@ export const importQuestionInstances = async (
         return acc
       }, {})
 
-    const batches = sliceIntoChunks(importedQuestionInstances, batchSize)
-    let lostResults: any[] = []
+    //TODO: adjust mappedQuestionInstancesIds in other files as well where used
+    let [mappedQuestionInstances, lostResults] = await restoreLostLegacyResults(
+      importedQuestionInstances,
+      questionInstancesDict
+    )
 
+    const batches = sliceIntoChunks(importedQuestionInstances, batchSize)
     for (const batch of batches) {
-      await prisma.$transaction(async (prisma) => {
-        for (const questionInstance of batch) {
+      const createdQuestionInstances = await prisma.$transaction(
+        batch.flatMap((questionInstance) => {
           const questionInstanceExists =
             questionInstancesDict[questionInstance._id]
 
           if (questionInstanceExists) {
-            mappedQuestionInstancesIds[questionInstance._id] =
+            mappedQuestionInstances[questionInstance._id] =
               questionInstanceExists.id
-            continue
+            return []
           }
 
           let questionData = {}
@@ -73,45 +154,7 @@ export const importQuestionInstances = async (
             }
           }
 
-          let results = {}
-
-          if (questionInstance.results) {
-            if (questionData.type === 'SC' || questionData.type === 'MC') {
-              if (questionInstance.results.CHOICES) {
-                results = questionInstance.results.CHOICES.reduce(
-                  (acc, choice, idx) => {
-                    acc[idx.toString()] = {
-                      count: choice,
-                      value: idx.toString(),
-                    }
-                    return acc
-                  },
-                  {}
-                )
-              }
-            } else if (
-              questionData.type === QuestionTypeMap['FREE'] ||
-              questionData.type === QuestionTypeMap['FREE_RANGE']
-            ) {
-              if (questionInstance.results.FREE) {
-                let newResults = Object.keys(
-                  questionInstance.results.FREE
-                ).reduce((acc, hash) => {
-                  acc[hash] = questionInstance.results.FREE[hash]
-                  return acc
-                }, {})
-                results = { ...results, ...newResults }
-              } else if (questionInstance.results.FREE_RANGE) {
-                let newResults = Object.keys(
-                  questionInstance.results.FREE_RANGE
-                ).reduce((acc, hash) => {
-                  acc[hash] = questionInstance.results.FREE_RANGE[hash]
-                  return acc
-                }, {})
-                results = { ...results, ...newResults }
-              }
-            }
-          }
+          const results = processResults(questionData, questionInstance)
 
           const result = {
             data: {
@@ -137,33 +180,74 @@ export const importQuestionInstances = async (
               // dropped: not relevant for now (für die wahlen wichtig)
             },
           }
-          const newQuestionInstance = await prisma.questionInstance.create({
-            data: result.data,
-          })
+          return [
+            prisma.questionInstance.create({
+              data: result.data,
+            }),
+          ]
+        })
+      )
 
-          mappedQuestionInstancesIds[questionInstance._id] =
-            newQuestionInstance.id
+      createdQuestionInstances.forEach((newQuestionInstance) => {
+        mappedQuestionInstances[newQuestionInstance.originalId]['newId'] =
+          newQuestionInstance.id
+      })
 
+      // restore results for created question instances
+      const updatedQuestionInstances = await prisma.$transaction(
+        createdQuestionInstances.flatMap((questionInstance) => {
+          const lostResultsExist =
+            mappedQuestionInstances[questionInstance.originalId]['lostResults']
+
+          if (lostResultsExist) {
+            const results = processResults(
+              questionInstance.questionData,
+              lostResultsExist
+            )
+            return [
+              prisma.questionInstance.update({
+                where: {
+                  id: questionInstance.id,
+                },
+                data: {
+                  results: results,
+                },
+              }),
+            ]
+          } else {
+            return []
+          }
+        })
+      )
+
+      const updatedQuestions = await prisma.$transaction(
+        createdQuestionInstances.flatMap((questionInstance) => {
+          const questionId =
+            mappedQuestionIds[questionInstance.questionData._id]
           if (questionId) {
-            await prisma.question.update({
-              where: {
-                id: questionId,
-              },
-              data: {
-                instances: {
-                  connect: {
-                    id: newQuestionInstance.id,
+            return [
+              prisma.question.update({
+                where: {
+                  id: questionId,
+                },
+                data: {
+                  instances: {
+                    connect: {
+                      id: questionInstance.id,
+                    },
                   },
                 },
-              },
-            })
+              }),
+            ]
+          } else {
+            return []
           }
-        }
-      })
+        })
+      )
     }
 
     context.log('lostResults.length: ', lostResults.length)
-    return mappedQuestionInstancesIds
+    return mappedQuestionInstances
   } catch (error) {
     context.error(
       'Something went wrong while importing question instances: ',
