2de5fc5c3ca088f4643fc9441824d1ed0b400c28
mxmlnwbr <maximilian.weber@bluewin.ch>
2023-08-30 21:02:54 +0200

    improved catalyst side

--- apps/docs/src/pages/catalyst.tsx
+++ apps/docs/src/pages/catalyst.tsx
@@ -1,3 +1,11 @@
+import {
+  faArrowLeft,
+  faBolt,
+  faCheck,
+  faPiggyBank,
+  faPlus,
+} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Layout from '@theme/Layout'
 
 function Catalyst() {
@@ -12,48 +20,110 @@ function Catalyst() {
           </div>
           <div className="max-w-xl mx-auto mt-6 text-2xl leading-tight text-muted sm:text-center">
             <p className="my-3 sm:my-0">
-              We don't charge based on features or usage.
+              The core components of KlickerUZH stay free.
             </p>
-            <p>Only pay if you use KlickerUZH as a bigger Institution.</p>
+            <p>
+              If you represent a larger institution, consider endorsing
+              KlickerUZH as a catalyst and unlock access to its cutting-edge
+              features.
+            </p>
           </div>
           <div className="mt-20 mx-auto max-w-[45rem] flow-root">
             <div className="grid grid-cols-1 gap-6 -mt-8 cards isolate sm:max-w-sm sm:mx-auto lg:-mx-8 lg:mt-0 md:max-w-none md:grid-cols-2 xl:-mx-4">
               <div className="p-6 rounded-lg sm:rounded-xl sm:p-8 bg-slate-100">
                 <h3 className="text-xl font-semibold leading-7">
-                  Personal use
+                  Restricted experience
                 </h3>
                 <p className="flex items-baseline mt-6 gap-x-1">
                   <span className="text-4xl font-semibold tracking-tight md:text-5xl">
-                    Free
+                    <FontAwesomeIcon icon={faPiggyBank} /> Free
                   </span>
                 </p>
-                <p>Free forever</p>
-                <ul className="mt-8 mb-2 space-y-2">
-                  <li class="flex gap-x-3">
-                    <div>✅</div>
-                    <div>bla</div>
+                <p>Basic functionalities</p>
+                <ul className="pl-0 mt-4 mb-2 space-y-2">
+                  <li className="flex gap-x-3">
+                    <div>
+                      <FontAwesomeIcon icon={faCheck} />
+                    </div>
+                    <div>Live Quizzes</div>
                   </li>
-                  <li class="flex gap-x-3">
-                    <div>✅</div>
-                    <div>bla</div>
+                  <li className="flex gap-x-3">
+                    <div>
+                      <FontAwesomeIcon icon={faCheck} />
+                    </div>
+                    <div>Classroom Interaction</div>
                   </li>
-                  <li class="flex gap-x-3">
-                    <div>✅</div>
-                    <div>bla</div>
+                  <li className="flex gap-x-3">
+                    <div>
+                      <FontAwesomeIcon icon={faCheck} />
+                    </div>
+                    <div>Latest Improvements and Fixes</div>
                   </li>
                 </ul>
               </div>
 
               <div className="p-6 rounded-lg sm:rounded-xl sm:p-8 bg-slate-100">
                 <h3 className="text-xl font-semibold leading-7">
-                  Institutional use
+                  Full experience
                 </h3>
                 <p className="flex items-baseline mt-6 gap-x-1">
                   <span className="text-4xl font-semibold tracking-tight md:text-5xl">
-                    ⚡️Catalyst
+                    <FontAwesomeIcon icon={faBolt} /> Catalyst
                   </span>
                 </p>
-                <p>Support your Project as a catalyst</p>
+                <p>Additional functionalities</p>
+                <ul className="pl-0 mt-4 mb-2 space-y-2">
+                  <li className="flex gap-x-3">
+                    <div>
+                      <FontAwesomeIcon icon={faArrowLeft} />
+                    </div>
+                    <div>Free functionalities</div>
+                    <div>
+                      <FontAwesomeIcon icon={faPlus} />
+                    </div>
+                  </li>
+                  <li className="flex gap-x-3">
+                    <div>
+                      <FontAwesomeIcon icon={faCheck} />
+                    </div>
+                    <div>Gamified Live Quizzes</div>
+                  </li>
+                  <li className="flex gap-x-3">
+                    <div>
+                      <FontAwesomeIcon icon={faCheck} />
+                    </div>
+                    <div>Groups and Group Activities</div>
+                  </li>
+                  <li className="flex gap-x-3">
+                    <div>
+                      <FontAwesomeIcon icon={faCheck} />
+                    </div>
+                    <div>Microlearning Sessions</div>
+                  </li>
+                  <li className="flex gap-x-3">
+                    <div>
+                      <FontAwesomeIcon icon={faCheck} />
+                    </div>
+                    <div>Learning Elements</div>
+                  </li>
+                  <li className="flex gap-x-3">
+                    <div>
+                      <FontAwesomeIcon icon={faCheck} />
+                    </div>
+                    <div>Courses and Challenge</div>
+                  </li>
+                  <li className="flex text-sm gap-x-3 opacity-70">
+                    To get access and for other inquiries please fill out the
+                    following
+                    <a
+                      href="https://forms.office.com/e/4AsWW1uck2"
+                      target="_blank"
+                    >
+                      form
+                    </a>
+                    .
+                  </li>
+                </ul>
               </div>
             </div>
           </div>
