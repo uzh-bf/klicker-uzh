44b852af22e2ea86dc5bd79f81b2f96e1a1136a9
Roland Schläfli <rolandschlaefli@gmail.com>
2023-09-08 19:33:25 +0200

    klicker.com

--- apps/frontend-pwa/capacitor.config.ts
+++ apps/frontend-pwa/capacitor.config.ts
@@ -1,13 +1,13 @@
 import { CapacitorConfig } from '@capacitor/cli'
 
 const config: CapacitorConfig = {
-  appId: 'ch.uzh.klicker.pwa',
+  appId: 'ch.uzh.bf.klicker.pwa',
   appName: 'KlickerUZH',
   //needed, otherwise there is no capacity.settings.gradle created, and hence, the build in android studio fails
   webDir: '.next',
   server: {
     // url: 'https://pwa.klicker.uzh.ch',
-    url: 'http://pwa.klicker.local',
+    url: 'http://pwa.klicker.com',
     cleartext: true,
   },
 }
