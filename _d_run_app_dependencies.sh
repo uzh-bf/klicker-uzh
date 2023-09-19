d6e6d7a2f3645cc091b751f9f6367a831313091b
Roland Schläfli <rolandschlaefli@gmail.com>
2023-09-19 09:23:51 +0200

    chore: add _d scripts to allow running locally with docker

---
+++ _d_run_app_dependencies.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# load port forwarding config on macOS
+sudo pfctl -ef /etc/pf.conf
+
+# enable pnpm support in volta
+grep -qxF 'export VOLTA_FEATURE_PNPM=1' ~/.zshrc || echo 'export VOLTA_FEATURE_PNPM=1' >> ~/.zshrc
+grep -qxF 'alias pn=pnpm' ~/.zshrc || echo 'alias pn=pnpm' >> ~/.zshrc
+
+# add hosts
+# sudo grep -qxF '127.0.0.1 api.klicker.com' ~/.zshrc || sudo echo '127.0.0.1 api.klicker.com' >> ~/.zshrc
+# sudo grep -qxF '127.0.0.1 pwa.klicker.com' ~/.zshrc || sudo echo '127.0.0.1 pwa.klicker.com' >> ~/.zshrc
+# sudo grep -qxF '127.0.0.1 manage.klicker.com' ~/.zshrc || sudo echo '127.0.0.1 manage.klicker.com' >> ~/.zshrc
+# sudo grep -qxF '127.0.0.1 control.klicker.com' ~/.zshrc || sudo echo '127.0.0.1 control.klicker.com' >> ~/.zshrc
+# sudo grep -qxF '127.0.0.1 auth.klicker.com' ~/.zshrc || sudo echo '127.0.0.1 auth.klicker.com' >> ~/.zshrc
+# sudo grep -qxF '127.0.0.1 func-responses.klicker.com' ~/.zshrc || sudo echo '127.0.0.1 func-responses.klicker.com' >> ~/.zshrc
+# sudo grep -qxF '127.0.0.1 func-response-processor.klicker.com' ~/.zshrc || sudo echo '127.0.0.1 func-response-processor.klicker.com' >> ~/.zshrc
+
+# start postgres, redis, and reverse proxy
+docker compose up postgres redis_exec redis_cache reverse_proxy_docker
