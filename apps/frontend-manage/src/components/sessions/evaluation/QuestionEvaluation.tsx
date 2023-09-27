580fe37c12bbbe6b6a102875dc4649c6bc418675
sjschlapbach <julius.schlapbach@gmx.net>
2023-09-27 18:21:36 +0200

    enhance(apps/frontend-manage): re-introduce percentage values and adapt layout of choices on evaluation

--- apps/frontend-manage/src/components/sessions/evaluation/QuestionEvaluation.tsx
+++ apps/frontend-manage/src/components/sessions/evaluation/QuestionEvaluation.tsx
@@ -1,11 +1,9 @@
 import { InstanceResult } from '@klicker-uzh/graphql/dist/ops'
 import { Ellipsis } from '@klicker-uzh/markdown'
 import {
-  ACTIVE_CHART_TYPES,
   CHART_COLORS,
   STATISTICS_ORDER,
 } from '@klicker-uzh/shared-components/src/constants'
-import { Select } from '@uzh-bf/design-system'
 import { useTranslations } from 'next-intl'
 import { useState } from 'react'
 import { twMerge } from 'tailwind-merge'
@@ -20,7 +18,7 @@ interface QuestionEvaluationProps {
   textSize: TextSizeType
   showSolution: boolean
   chartType: string
-  setChartType: (value: string) => void
+  totalParticipants: number
   className?: string
 }
 
@@ -30,7 +28,7 @@ function QuestionEvaluation({
   textSize,
   showSolution,
   chartType,
-  setChartType,
+  totalParticipants,
   className,
 }: QuestionEvaluationProps) {
   const t = useTranslations()
@@ -72,68 +70,68 @@ function QuestionEvaluation({
         </div>
         <div
           className={twMerge(
-            'flex-none flex flex-col gap-2 order-1 px-4 py-2 border-l md:w-64 md:order-2',
+            'flex-none flex flex-col gap-2 order-1 px-4 py-2 border-l md:w-64 lg:w-72 xl:w-80 md:order-2',
             textSize.text
           )}
         >
-          <Select
-            contentPosition="popper"
-            className={{ root: 'w-full mt-2', trigger: 'border-slate-400' }}
-            items={ACTIVE_CHART_TYPES[currentInstance.questionData.type].map(
-              (item) => {
-                return { label: t(item.label), value: item.value }
-              }
-            )}
-            value={chartType}
-            onChange={(newValue: string) => setChartType(newValue)}
-          />
-
           {(currentInstance.questionData.type === 'SC' ||
             currentInstance.questionData.type === 'MC' ||
             currentInstance.questionData.type === 'KPRIM') && (
             <div className="flex flex-col flex-1 min-h-0 gap-2 mt-2">
-              <div className="flex-none font-bold">
-                {t('manage.questionForms.answerOptions')}
-              </div>
-              <div className="flex-1 overflow-y-auto">
+              <div
+                className={twMerge(
+                  'flex-1 overflow-y-auto flex flex-col gap-2.5',
+                  textSize.text
+                )}
+              >
                 {currentInstance.questionData.options.choices.map(
-                  (choice, innerIndex) => (
-                    <div
-                      key={`${currentInstance.blockIx}-${innerIndex}`}
-                      className="flex flex-row"
-                    >
-                      <div
-                        // TODO: possibly use single color for answer options to highlight correct one? or some other approach to distinguish better
-                        style={{
-                          backgroundColor: showSolution
-                            ? choice.correct
-                              ? '#00de0d'
-                              : '#ff0000'
-                            : CHART_COLORS[innerIndex % 12],
-                        }}
-                        className={twMerge(
-                          'mr-2 items-center flex justify-center rounded-md w-7 h-7 text-white font-bold',
-                          choice.correct && showSolution && 'text-black'
-                        )}
-                      >
-                        {String.fromCharCode(65 + innerIndex)}
-                      </div>
+                  (choice, innerIndex) => {
+                    const correctFraction =
+                      parseInt(currentInstance.results[innerIndex].count) /
+                      totalParticipants
 
-                      <div className="w-[calc(100%-3rem)] line-clamp-3">
-                        <Ellipsis
-                          // maxLines={3}
-                          maxLength={60}
-                          className={{
-                            tooltip:
-                              'z-20 float-right !text-white min-w-[25rem]',
-                            markdown: textSize.text,
-                          }}
-                        >
-                          {choice.value}
-                        </Ellipsis>
+                    return (
+                      <div key={`${currentInstance.blockIx}-${innerIndex}`}>
+                        <div className="flex flex-row justify-between items-center leading-5">
+                          <div
+                            // TODO: possibly use single color for answer options to highlight correct one? or some other approach to distinguish better
+                            style={{
+                              backgroundColor: showSolution
+                                ? choice.correct
+                                  ? '#00de0d'
+                                  : '#ff0000'
+                                : CHART_COLORS[innerIndex % 12],
+                              minWidth: '1.75rem',
+                              width: `calc(${correctFraction * 100}%)`,
+                            }}
+                            className={twMerge(
+                              'mr-2 items-center flex justify-center rounded-md text-white font-bold h-5',
+                              choice.correct && showSolution && 'text-black'
+                            )}
+                          >
+                            {String.fromCharCode(65 + innerIndex)}
+                          </div>
+                          <div className="whitespace-nowrap text-right">
+                            {Math.round(100 * correctFraction)} %
+                          </div>
+                        </div>
+
+                        <div className="w-full line-clamp-3">
+                          <Ellipsis
+                            // maxLines={3}
+                            maxLength={60}
+                            className={{
+                              tooltip:
+                                'z-20 float-right !text-white min-w-[25rem]',
+                              markdown: textSize.text,
+                            }}
+                          >
+                            {choice.value}
+                          </Ellipsis>
+                        </div>
                       </div>
-                    </div>
-                  )
+                    )
+                  }
                 )}
               </div>
             </div>
