92f2ed967fc56bb759f7cf0ba52bb1ecc7fbae52
Roland Schlaefli <rolandschlaefli@gmail.com>
2023-09-28 21:20:28 +0200

    public dir in prisma package

--- packages/prisma/src/copy.js
+++ packages/prisma/src/copy.js
@@ -5,8 +5,3 @@ fs.copySync(
   path.resolve(__dirname, './client'),
   path.resolve(__dirname, '../dist/')
 )
-
-fs.copySync(
-  path.resolve(__dirname, './types.ts'),
-  path.resolve(__dirname, '../dist/types.ts')
-)
--- packages/prisma/tsup.config.ts
+++ packages/prisma/tsup.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: ['src/util.ts'],
-  clean: false,
+  clean: true,
   target: 'node18',
+  publicDir: 'src/public',
 })
