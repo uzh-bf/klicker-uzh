89ab9175c0b4a6ff50f2f61e2986c3a33696e83a
Roland Schläfli <rolandschlaefli@gmail.com>
2023-10-03 22:27:01 +0200

    hover

--- apps/frontend-manage/src/components/questions/QuestionTags.tsx
+++ apps/frontend-manage/src/components/questions/QuestionTags.tsx
@@ -25,10 +25,9 @@ function QuestionTags({
 
         return (
           <Button
-            basic
             className={{
               root: twMerge(
-                'py-1 px-3 m-1 mt-0 bg-slate-100 border border-solid rounded-md border-blue-40 w-max',
+                'py-1 px-3 m-1 mt-0 bg-slate-100 border border-solid rounded-md border-blue-40 w-max shadow-none',
                 selected && 'bg-primary-20'
               ),
             }}
