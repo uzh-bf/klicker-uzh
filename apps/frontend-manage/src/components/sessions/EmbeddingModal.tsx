9fb42c38852c96c689be58115a2f27f0ee9f941b
Roland Schläfli <rolandschlaefli@gmail.com>
2023-10-22 10:27:20 +0200

    feat: allow public access to evaluation page using HMAC verified link (#3915)
    Co-authored-by: sjschlapbach <julius.schlapbach@gmx.net>

--- apps/frontend-manage/src/components/sessions/EmbeddingModal.tsx
+++ apps/frontend-manage/src/components/sessions/EmbeddingModal.tsx
@@ -1,8 +1,50 @@
+import { useQuery } from '@apollo/client'
 import { faClipboard } from '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { QuestionInstance } from '@klicker-uzh/graphql/dist/ops'
+import {
+  GetSessionHmacDocument,
+  QuestionInstance,
+} from '@klicker-uzh/graphql/dist/ops'
 import { Button, H2, Modal } from '@uzh-bf/design-system'
 import { useTranslations } from 'next-intl'
+import Link from 'next/link'
+
+function LazyHMACLink({
+  sessionId,
+  params,
+}: {
+  sessionId: string
+  params: string
+}) {
+  const sessionHMAC = useQuery(GetSessionHmacDocument, {
+    variables: {
+      id: sessionId,
+    },
+  })
+
+  if (sessionHMAC.loading || !sessionHMAC.data?.sessionHMAC) {
+    return ''
+  }
+
+  const link = `${
+    process.env.NEXT_PUBLIC_MANAGE_URL
+  }/sessions/${sessionId}/evaluation?hmac=${sessionHMAC.data?.sessionHMAC}${
+    params ? `&${params}` : ''
+  }`
+
+  return (
+    <div className="flex flex-row items-center gap-3 px-1.5 py-0.5 mr-2 border border-solid rounded bg-uzh-grey-40">
+      <FontAwesomeIcon
+        icon={faClipboard}
+        className="hover:cursor-pointer"
+        onClick={() => navigator?.clipboard?.writeText(link)}
+      />
+      <Link href={link} target="_blank">
+        {link}
+      </Link>
+    </div>
+  )
+}
 
 interface EmbeddingModalProps {
   open: boolean
@@ -32,47 +74,28 @@ function EmbeddingModal({
       <H2 className={{ root: 'mb-4' }}>
         {t('manage.sessions.evaluationLinksEmbedding')}
       </H2>
+      <div className="mb-4">
+        <LazyHMACLink sessionId={sessionId} params={``} />
+      </div>
       <div className="flex flex-col gap-2">
         {questions?.map((question: QuestionInstance, ix: number) => {
           return (
-            <div key={question.id} className="flex flex-row items-center gap-2">
-              <div className="w-20 font-bold">Frage {ix + 1}:</div>
-              <div className="flex flex-row items-center gap-3 px-1.5 py-0.5 mr-2 border border-solid rounded bg-uzh-grey-40">
-                <FontAwesomeIcon
-                  icon={faClipboard}
-                  className="hover:cursor-pointer"
-                  onClick={() =>
-                    navigator?.clipboard?.writeText(
-                      `${process.env.NEXT_PUBLIC_MANAGE_URL}/sessions/${sessionId}/evaluation?questionIx=${ix}`
-                    )
-                  }
-                />
-                <div className="text-sm">{`${process.env.NEXT_PUBLIC_MANAGE_URL}/sessions/${sessionId}/evaluation?questionIx=${ix}`}</div>
-              </div>
-              <p>
+            <div key={question.id}>
+              <div className="font-bold">
+                Frage {ix + 1}:{' '}
                 {question.questionData.name.length > 25
                   ? `${question.questionData.name.substring(0, 25)}...`
                   : question.questionData.name}
-              </p>
+              </div>
+              <LazyHMACLink sessionId={sessionId} params={`questionIx=${ix}`} />
             </div>
           )
         })}
-        <div className="flex flex-row items-center gap-2">
+        <div>
           <div className="font-bold w-30">
             {t('shared.generic.leaderboard')}:
           </div>
-          <div className="flex flex-row items-center gap-3 px-1.5 py-0.5 mr-2 border border-solid rounded bg-uzh-grey-40">
-            <FontAwesomeIcon
-              icon={faClipboard}
-              className="hover:cursor-pointer"
-              onClick={() =>
-                navigator?.clipboard?.writeText(
-                  `${process.env.NEXT_PUBLIC_MANAGE_URL}/sessions/${sessionId}/evaluation?leaderboard=true`
-                )
-              }
-            />
-            <div className="text-sm">{`${process.env.NEXT_PUBLIC_MANAGE_URL}/sessions/${sessionId}/evaluation?leaderboard=true`}</div>
-          </div>
+          <LazyHMACLink sessionId={sessionId} params={`leaderboard=true`} />
         </div>
       </div>
     </Modal>
