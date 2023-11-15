702ed11db905fc2ef30c06d78bf0fa4255c48294
Roland Schläfli <rolandschlaefli@gmail.com>
2023-11-15 09:37:19 +0100

    fix(apps/frontend-*): add background-color: white to all _app.tsx

--- apps/frontend-control/src/pages/_app.tsx
+++ apps/frontend-control/src/pages/_app.tsx
@@ -54,6 +54,7 @@ function App({ Component, pageProps }: AppProps) {
           height: 100%;
           display: flex;
           flex-direction: column;
+          background-color: white;
         }
       `}</style>
     </div>
