5ca75c4222cef519cbce34beee3984e93824eb9f
Julius Schlapbach <80708107+sjschlapbach@users.noreply.github.com>
2023-11-16 23:01:29 +0100

    enhance(apps/frontend-pwa): hide courses without element stacks on practice area (#3951)

--- packages/graphql/src/services/participants.ts
+++ packages/graphql/src/services/participants.ts
@@ -591,3 +591,29 @@ export async function getParticipantWithAchievements(ctx: ContextWithUser) {
     achievements,
   }
 }
+
+export async function getPracticeCourses(ctx: ContextWithUser) {
+  // fetch participations including courses
+  const participations = await ctx.prisma.participation.findMany({
+    where: {
+      participantId: ctx.user.sub,
+    },
+    include: {
+      course: {
+        include: {
+          elementStacks: true,
+        },
+      },
+    },
+  })
+
+  if (participations.length === 0) return []
+
+  // sort courses by end date descending
+  const courses = participations
+    .map((p) => p.course)
+    .filter((c) => c.elementStacks.length > 0)
+    .sort((a, b) => (a.endDate > b.endDate ? -1 : 1))
+
+  return courses
+}
