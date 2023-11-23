0afb106dfbe562357883dd4593dcfc55fcfd96ca
Roland Schlaefli <rolandschlaefli@gmail.com>
2023-11-23 20:40:39 +0100

    mismatches and running build

--- apps/backend-docker/src/app.ts
+++ apps/backend-docker/src/app.ts
@@ -11,13 +11,10 @@ import { createYoga } from 'graphql-yoga'
 import { createRequire } from 'node:module'
 import passport from 'passport'
 import { Strategy as JWTStrategy } from 'passport-jwt'
-import { createRequire } from "module";
-
-const require = createRequire(import.meta.url);
-const persistedOperations = require("@klicker-uzh/graphql/dist/server.json");
 
 const require = createRequire(import.meta.url)
 const persistedOperations = require('@klicker-uzh/graphql/dist/server.json')
+
 declare namespace global {
   let __coverage__: any
 }
