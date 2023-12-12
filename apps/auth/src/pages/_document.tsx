a500f1aff5e24800c45887d381e634c94667a1c0
Stefan Simic <stefansim2000@gmail.com>
2023-12-12 13:22:23 +0100

    Update _document.tsx

--- apps/auth/src/pages/_document.tsx
+++ apps/auth/src/pages/_document.tsx
@@ -3,10 +3,8 @@ import { Head, Html, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
       <body>
         <Main />
-        <NextScript />
       </body>
     </Html>
   )
