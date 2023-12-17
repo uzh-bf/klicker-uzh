493a32118bc987e443a2ee4842cfc86a8c4e72bf
Roland Schlaefli <rolandschlaefli@gmail.com>
2023-12-17 09:17:02 +0100

    chore: update index page

--- apps/frontend/src/pages/index.tsx
+++ apps/frontend/src/pages/index.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
 import { useRouter } from 'next/router'
-import { Button, List, Input } from 'semantic-ui-react'
+import { Button, List, Input, Message } from 'semantic-ui-react'
 import { push } from '@socialgouv/matomo-next'
 import Image from 'next/image'
 import KlickerLogoSrc from '../../public/KlickerUZH_Gray_Transparent.png'
+import { defineMessages, useIntl, FormattedMessage } from 'react-intl'
+import Link from 'next/link'
 
 import StaticLayout from '../components/layouts/StaticLayout'
 
@@ -11,11 +13,7 @@ const links = [
   {
     href: '/user/login',
     label: 'Login',
-  },
-  {
-    href: '/user/registration',
-    label: 'Sign Up',
-  },
+  }
 ]
 
 function Index(): React.ReactElement {
@@ -37,6 +35,15 @@ function Index(): React.ReactElement {
         </div>
 
         <p className="italic text-center">Welcome to the open source instant audience response system.</p>
+
+        <Message info>
+              <p>A new version of KlickerUZH (v3) has been released and is accessible through <a href="www.klicker.uzh.ch">www.klicker.uzh.ch</a>. This version (v2) of KlickerUZH will be shut down on 31.12.2023 and new registrations have been disabled. A data migration allows you to transfer all of your content from v2 to v3. More information on KlickerUZH v3 can be found in our <a href="https://community.klicker.uzh.ch/t/klickeruzh-v3-0-release-information/79">community post</a>.</p>
+              <p>Already have an account? <Link href="/user/login">
+                <a>
+                  <FormattedMessage defaultMessage="Login here." id="user.registration.loginInfoLink" />
+                </a>
+              </Link></p>
+              </Message>
 
         <div className="flex flex-col w-full p-4 mb-4 lg:items-center lg:justify-between lg:flex-row lg:flex-wrap border-primary ">
           <p className="mb-2 lg:flex-[0_0_100%]">Want to participate in a poll?</p>
