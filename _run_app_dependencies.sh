1560da6c7ee821377db3012001241f3d4fb5874e
Roland Schlaefli <rolandschlaefli@gmail.com>
2023-12-23 18:21:32 -0100

    chore: remove podman-related run scripts

--- _run_app_dependencies.sh
+++ _run_app_dependencies.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# load port forwarding config on macOS
-sudo pfctl -ef /etc/pf.conf
-
 # enable pnpm support in volta
 grep -qxF 'export VOLTA_FEATURE_PNPM=1' ~/.zshrc || echo 'export VOLTA_FEATURE_PNPM=1' >> ~/.zshrc
 grep -qxF 'alias pn=pnpm' ~/.zshrc || echo 'alias pn=pnpm' >> ~/.zshrc
@@ -17,4 +14,4 @@ grep -qxF 'alias pn=pnpm' ~/.zshrc || echo 'alias pn=pnpm' >> ~/.zshrc
 # sudo grep -qxF '127.0.0.1 func-response-processor.klicker.com' ~/.zshrc || sudo echo '127.0.0.1 func-response-processor.klicker.com' >> ~/.zshrc
 
 # start postgres, redis, and reverse proxy
-podman-compose up postgres redis_exec redis_cache reverse_proxy
+docker compose up postgres redis_exec redis_cache reverse_proxy_docker
