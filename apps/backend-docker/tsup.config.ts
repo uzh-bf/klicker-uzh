481db7b8813ab8619498c06cfbaa6f442a9867a2
Roland Schlaefli <rolandschlaefli@gmail.com>
2024-01-01 21:26:24 -0100

    full coverage of graphql

--- apps/backend-docker/tsup.config.ts
+++ apps/backend-docker/tsup.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['src/index.ts'],
+  entry:
+    process.env.NODE_ENV === 'test'
+      ? ['instrumented/index.ts']
+      : ['src/index.ts'],
   format: 'esm',
   clean: true,
   target: 'node20',
