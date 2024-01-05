627dfd661f60c22d82d6c741a5f9ec4ca4238060
Roland Schläfli <rolandschlaefli@gmail.com>
2024-01-05 20:59:58 +0100

    chore: update index page and move login

--- apps/frontend/src/pages/index.tsx
+++ apps/frontend/src/pages/index.tsx
@@ -9,22 +9,8 @@ import Link from 'next/link'
 
 import StaticLayout from '../components/layouts/StaticLayout'
 
-const links = [
-  {
-    href: '/user/login',
-    label: 'Login',
-  }
-]
-
 function Index(): React.ReactElement {
   const router = useRouter()
-
-  const [shortname, setShortname] = useState('')
-
-  const redirectToJoin = (): Promise<boolean> => {
-    push(['trackEvent', 'Landing', 'Participation Clicked'])
-    return router.replace(`/join/${shortname}`)
-  }
 
   return (
     // TODO: internationalization
@@ -37,144 +23,14 @@ function Index(): React.ReactElement {
         <p className="italic text-center">Welcome to the open source instant audience response system.</p>
 
         <Message info>
-              <p>A new version of KlickerUZH (v3) has been released and is accessible through <a href="www.klicker.uzh.ch">www.klicker.uzh.ch</a>. This version (v2) of KlickerUZH will be shut down on 31.12.2023 and new registrations have been disabled. A data migration allows you to transfer all of your content from v2 to v3. More information on KlickerUZH v3 can be found in our <a href="https://community.klicker.uzh.ch/t/klickeruzh-v3-0-release-information/79">community post</a>.</p>
-              <p>Already have an account? <Link href="/user/login">
-                <a>
-                  <FormattedMessage defaultMessage="Login here." id="user.registration.loginInfoLink" />
-                </a>
-              </Link></p>
-              </Message>
-
-        <div className="flex flex-col w-full p-4 mb-4 lg:items-center lg:justify-between lg:flex-row lg:flex-wrap border-primary ">
-          <p className="mb-2 lg:flex-[0_0_100%]">Want to participate in a poll?</p>
-          <Input
-            fluid
-            className="flex-grow lg:mr-2"
-            label="app.klicker.uzh.ch/join/"
-            placeholder="account id"
-            value={shortname}
-            onChange={(e): void => setShortname(e.target.value)}
-          />
-          <Button
-            primary
-            className="!mr-0 !mt-2 lg:!mt-0"
-            disabled={!shortname || shortname === ''}
-            onClick={redirectToJoin}
-          >
-            Participate
-          </Button>
-        </div>
-
-        <div className="flex flex-col lg:flex-row">
-          <div className="block p-4 mb-2 text-black border border-solid rounded-md border-primary lg:p-4 lg:h-72 lg:w-72 lg:mr-2">
-            <h2 className="text-2xl leading-6">User Guide</h2>
-            <List divided relaxed>
-              <List.Item>
-                <List.Icon name="book" size="large" verticalAlign="middle" />
-                <List.Content>
-                  <List.Header as="a" href="https://www.klicker.uzh.ch/introduction/getting_started" target="_blank">
-                    Documentation
-                  </List.Header>
-                  <List.Description>Get started with KlickerUZH</List.Description>
-                </List.Content>
-              </List.Item>
-              <List.Item>
-                <List.Icon name="question circle" size="large" verticalAlign="middle" />
-                <List.Content>
-                  <List.Header as="a" href="https://www.klicker.uzh.ch/faq" target="_blank">
-                    FAQ
-                  </List.Header>
-                  <List.Description>Common Questions</List.Description>
-                </List.Content>
-              </List.Item>
-              <List.Item>
-                <List.Icon name="users" size="large" verticalAlign="middle" />
-                <List.Content>
-                  <List.Header as="a" href="https://www.klicker.uzh.ch/use_cases/live_qa" target="_blank">
-                    Use Cases
-                  </List.Header>
-                  <List.Description>KlickerUZH in Action</List.Description>
-                </List.Content>
-              </List.Item>
-            </List>
-          </div>
-          <div className="block p-4 mb-2 text-black border border-solid rounded-md border-primary lg:p-4 lg:h-72 lg:w-72 lg:mr-2">
-            <h2 className="block text-2xl leading-6">Development</h2>
-            <List divided relaxed>
-              <List.Item>
-                <List.Icon name="github" size="large" verticalAlign="middle" />
-                <List.Content>
-                  <List.Header as="a" href="https://github.com/uzh-bf/klicker-uzh" target="_blank">
-                    uzh-bf/klicker-uzh
-                  </List.Header>
-                  <List.Description>GitHub Repository</List.Description>
-                </List.Content>
-              </List.Item>
-              <List.Item>
-                <List.Icon name="sitemap" size="large" verticalAlign="middle" />
-                <List.Content>
-                  <List.Header as="a" href="https://www.klicker.uzh.ch/development" target="_blank">
-                    Public Roadmap
-                  </List.Header>
-                  <List.Description>Project Outline</List.Description>
-                </List.Content>
-              </List.Item>
-              <List.Item>
-                <List.Icon name="bullhorn" size="large" verticalAlign="middle" />
-                <List.Content>
-                  <List.Header as="a" href="https://community.klicker.uzh.ch/c/announce/5" target="_blank">
-                    Project Updates
-                  </List.Header>
-                  <List.Description>Updates on developments</List.Description>
-                </List.Content>
-              </List.Item>
-            </List>
-          </div>
-          <div className="p-4 mb-0 text-black border border-solid rounded-md last:mr-0 border-primary lg:p-4 lg:h-72 lg:w-72 lg:mr-2">
-            <h2 className="text-2xl leading-6">Support</h2>
-            <List divided relaxed>
-              <List.Item>
-                <List.Icon name="talk" size="large" verticalAlign="middle" />
-                <List.Content>
-                  <List.Header as="a" href="https://www.klicker.uzh.ch/community" target="_blank">
-                    Discussions
-                  </List.Header>
-                  <List.Description>User Community</List.Description>
-                </List.Content>
-              </List.Item>
-              <List.Item>
-                <List.Icon name="lightbulb" size="large" verticalAlign="middle" />
-                <List.Content>
-                  <List.Header as="a" href="https://klicker-uzh.feedbear.com/boards/feature-requests" target="_blank">
-                    Feature Requests
-                  </List.Header>
-                  <List.Description>Request a new feature</List.Description>
-                </List.Content>
-              </List.Item>
-              <List.Item>
-                <List.Icon name="github" size="large" verticalAlign="middle" />
-                <List.Content>
-                  <List.Header as="a" href="https://github.com/uzh-bf/klicker-uzh/discussions" target="_blank">
-                    Developer Support
-                  </List.Header>
-                  <List.Description>GitHub Discussions</List.Description>
-                </List.Content>
-              </List.Item>
-            </List>
-          </div>
-        </div>
-
-        <List>
-          {links.map(
-            (link): React.ReactElement => (
-              <List.Item className="mb-4 !inline-block mr-2" key={link.label}>
-                <a href={link.href} target="_self">
-                  <Button primary>{link.label}</Button>
-                </a>
-              </List.Item>
-            )
-          )}
-        </List>
+          <p>
+            A new version of KlickerUZH (v3) has been released and is accessible through{' '}
+            <a href="www.klicker.uzh.ch">www.klicker.uzh.ch</a>. This version (v2) of KlickerUZH has been discontinued.
+            A data migration allows you to transfer all of your content from v2 to v3. More information on KlickerUZH v3
+            can be found in our{' '}
+            <a href="https://community.klicker.uzh.ch/t/klickeruzh-v3-0-release-information/79">community post</a>.
+          </p>
+        </Message>
       </div>
     </StaticLayout>
   )
--- apps/frontend/src/pages/user/registration.tsx
+++ apps/frontend/src/pages/user/registration.tsx
@@ -58,12 +58,14 @@ function Registration(): React.ReactElement {
               </Message>
 
               <Message info>
-              <p>A new version of KlickerUZH (v3) has been released and is accessible through <a href="www.klicker.uzh.ch">www.klicker.uzh.ch</a>. This version (v2) of KlickerUZH will be shut down on 31.12.2023 and new registrations have been disabled. A data migration allows you to transfer all of your content from v2 to v3. More information on KlickerUZH v3 can be found in our <a href="https://community.klicker.uzh.ch/t/klickeruzh-v3-0-release-information/79">community post</a>.</p>
-              <p>Already have an account? <Link href="/user/login">
-                <a>
-                  <FormattedMessage defaultMessage="Login here." id="user.registration.loginInfoLink" />
-                </a>
-              </Link></p>
+                <p>
+                  A new version of KlickerUZH (v3) has been released and is accessible through{' '}
+                  <a href="www.klicker.uzh.ch">www.klicker.uzh.ch</a>. This version (v2) of KlickerUZH will be shut down
+                  on 31.12.2023 and new registrations have been disabled. A data migration allows you to transfer all of
+                  your content from v2 to v3. More information on KlickerUZH v3 can be found in our{' '}
+                  <a href="https://community.klicker.uzh.ch/t/klickeruzh-v3-0-release-information/79">community post</a>
+                  .
+                </p>
               </Message>
             </>
           )
