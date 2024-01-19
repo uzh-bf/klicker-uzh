682f1aa68a2a3c72b9a092230caaafa3b555b70f
sjschlapbach <julius.schlapbach@gmx.net>
2024-01-19 18:32:34 +0100

    refactor: separate flashcard element and practice quiz creation

--- packages/prisma/src/data/seedFlashcards.ts
+++ packages/prisma/src/data/seedFlashcards.ts
@@ -11,7 +11,7 @@ import Prisma, {
   ElementType,
   PublicationStatus,
 } from '../../dist'
-import { COURSE_ID_TEST, USER_ID_TEST } from './constants.js'
+import { COURSE_ID_TEST, USER_ID_TEST } from './constants'
 
 const turndown = Turndown()
 
@@ -31,18 +31,9 @@ function extractQuizInfo(doc: typeof xmlDoc) {
   }
 }
 
-async function seedFlashcardSet(
-  prismaClient: Prisma.PrismaClient,
-  fileName: string,
-  quizId: string
-) {
+async function processQuizInfo(fileName: string) {
   const __filename = fileURLToPath(import.meta.url)
   const __dirname = path.dirname(__filename)
-
-  const USER_ID = USER_ID_TEST
-  const COURSE_ID = COURSE_ID_TEST
-  // const USER_ID = USER_ID_BF1_HS23
-  // const COURSE_ID = COURSE_ID_BF1_HS23
 
   const xmlData = fs.readFileSync(path.join(__dirname, fileName), 'utf-8')
 
@@ -51,6 +42,16 @@ async function seedFlashcardSet(
   console.log(xmlDoc, xmlDoc.box.cards[0])
 
   const quizInfo = extractQuizInfo(xmlDoc)
+
+  return quizInfo
+}
+
+export async function seedFlashcardElements(
+  prismaClient: Prisma.PrismaClient,
+  fileName: string,
+  userId: string
+) {
+  const quizInfo = await processQuizInfo(fileName)
 
   const elementsFC = await Promise.allSettled(
     quizInfo.elements.map(async (data) => {
@@ -62,7 +63,7 @@ async function seedFlashcardSet(
           ...data,
           owner: {
             connect: {
-              id: USER_ID,
+              id: userId,
             },
           },
         },
@@ -70,7 +71,7 @@ async function seedFlashcardSet(
           ...data,
           owner: {
             connect: {
-              id: USER_ID,
+              id: userId,
             },
           },
         },
@@ -88,6 +89,21 @@ async function seedFlashcardSet(
     throw new Error('Failed to seed some flashcard elements')
   }
 
+  const elements = elementsFC.map((el) => el.value)
+
+  return elements
+}
+
+async function seedFlashcardSet(
+  prismaClient: Prisma.PrismaClient,
+  fileName: string,
+  quizId: string,
+  elements: Prisma.Element[],
+  userId: string,
+  courseId: string
+) {
+  const quizInfo = await processQuizInfo(fileName)
+
   const practiceQuiz = await prismaClient.practiceQuiz.upsert({
     where: {
       id: quizId,
@@ -97,12 +113,12 @@ async function seedFlashcardSet(
       name: quizInfo.title,
       displayName: quizInfo.title,
       description: quizInfo.description,
-      ownerId: USER_ID,
-      courseId: COURSE_ID,
+      ownerId: userId,
+      courseId: courseId,
       status: PublicationStatus.PUBLISHED,
       orderType: ElementOrderType.SPACED_REPETITION,
       stacks: {
-        create: elementsFC.map((el, ix) => ({
+        create: elements.map((el, ix) => ({
           order: ix,
           type: ElementStackType.PRACTICE_QUIZ,
           options: {},
@@ -115,7 +131,7 @@ async function seedFlashcardSet(
                   type: ElementInstanceType.PRACTICE_QUIZ,
                   elementType: ElementType.FLASHCARD,
                   // TODO: pick only relevant properties of the element
-                  elementData: el.value,
+                  elementData: el,
                   options: {},
                   results: {
                     0: 0,
@@ -123,8 +139,8 @@ async function seedFlashcardSet(
                     2: 0,
                     total: 0,
                   },
-                  ownerId: el.value.ownerId,
-                  elementId: el.value.id,
+                  ownerId: el.ownerId,
+                  elementId: el.id,
                 },
               ],
             },
@@ -149,7 +165,7 @@ async function seedFlashcardSet(
   // attach all element instances from the practice quiz directly to the course for repetition interface
   const course = await prismaClient.course.update({
     where: {
-      id: COURSE_ID,
+      id: courseId,
     },
     data: {
       elementStacks: {
@@ -165,50 +181,111 @@ async function seedFlashcardSet(
     course.id
   )
 
-  return elementsFC
+  return practiceQuiz
 }
 
 export async function seedFlashcards(prismaClient: Prisma.PrismaClient) {
+  const USER_ID = USER_ID_TEST
+  const COURSE_ID = COURSE_ID_TEST
+  // const USER_ID = USER_ID_BF1_HS23
+  // const COURSE_ID = COURSE_ID_BF1_HS23
+
+  const flashcards1 = await seedFlashcardElements(
+    prismaClient,
+    'data/FC_Modul_1.xml',
+    USER_ID
+  )
   await seedFlashcardSet(
     prismaClient,
     'data/FC_Modul_1.xml',
-    'e0c331b1-b66e-4fc2-b352-ba14c22c294c'
+    'e0c331b1-b66e-4fc2-b352-ba14c22c294c',
+    flashcards1,
+    USER_ID,
+    COURSE_ID
   )
 
+  const flashcards2 = await seedFlashcardElements(
+    prismaClient,
+    'data/FC_Modul_2.xml',
+    USER_ID
+  )
   await seedFlashcardSet(
     prismaClient,
     'data/FC_Modul_2.xml',
-    '36eba4d8-fa0d-46bc-b916-b53ba56637b8'
+    '36eba4d8-fa0d-46bc-b916-b53ba56637b8',
+    flashcards2,
+    USER_ID,
+    COURSE_ID
   )
 
+  const flashcards3 = await seedFlashcardElements(
+    prismaClient,
+    'data/FC_Modul_3.xml',
+    USER_ID
+  )
   await seedFlashcardSet(
     prismaClient,
     'data/FC_Modul_3.xml',
-    '9bdb2760-e631-4e00-9604-9d807a4f47a2'
+    '9bdb2760-e631-4e00-9604-9d807a4f47a2',
+    flashcards3,
+    USER_ID,
+    COURSE_ID
   )
 
+  const flashcards4 = await seedFlashcardElements(
+    prismaClient,
+    'data/FC_Modul_4.xml',
+    USER_ID
+  )
   await seedFlashcardSet(
     prismaClient,
     'data/FC_Modul_4.xml',
-    'e0760993-18e1-467a-b84f-591c1b81e727'
+    'e0760993-18e1-467a-b84f-591c1b81e727',
+    flashcards4,
+    USER_ID,
+    COURSE_ID
   )
 
+  const flashcards5 = await seedFlashcardElements(
+    prismaClient,
+    'data/FC_Modul_5.xml',
+    USER_ID
+  )
   await seedFlashcardSet(
     prismaClient,
     'data/FC_Modul_5.xml',
-    '1c699242-3740-4c05-b853-86e8d824997e'
+    '1c699242-3740-4c05-b853-86e8d824997e',
+    flashcards5,
+    USER_ID,
+    COURSE_ID
   )
 
+  const flashcards6 = await seedFlashcardElements(
+    prismaClient,
+    'data/FC_Modul_6.xml',
+    USER_ID
+  )
   await seedFlashcardSet(
     prismaClient,
     'data/FC_Modul_6.xml',
-    '35334e99-331d-481e-bd32-c84cddaf8764'
+    '35334e99-331d-481e-bd32-c84cddaf8764',
+    flashcards6,
+    USER_ID,
+    COURSE_ID
   )
 
+  const flashcards7 = await seedFlashcardElements(
+    prismaClient,
+    'data/FC_Modul_7.xml',
+    USER_ID
+  )
   await seedFlashcardSet(
     prismaClient,
     'data/FC_Modul_7.xml',
-    'd6a1f040-a78a-43ac-8778-bfc0f5b6e86c'
+    'd6a1f040-a78a-43ac-8778-bfc0f5b6e86c',
+    flashcards7,
+    USER_ID,
+    COURSE_ID
   )
 }
 
