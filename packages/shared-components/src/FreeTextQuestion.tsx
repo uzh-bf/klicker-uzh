94dc67bd10b192b0a81413e832a0dd6a46f74578
sjschlapbach <julius.schlapbach@gmx.net>
2024-02-01 23:53:27 +0100

    wip: set up structure for free text questions in practice quiz

--- packages/shared-components/src/FreeTextQuestion.tsx
+++ packages/shared-components/src/FreeTextQuestion.tsx
@@ -1,7 +1,42 @@
+import { FreeTextQuestionOptions } from '@klicker-uzh/graphql/dist/ops'
+import { Markdown } from '@klicker-uzh/markdown'
 import React from 'react'
+import { twMerge } from 'tailwind-merge'
 
-function FreeTextQuestion() {
-  return <div>Free Text Question</div>
+interface FreeTextQuestionProps {
+  content: string
+  options: FreeTextQuestionOptions
+  response?: string
+  valid: boolean
+  setResponse: (newValue: string, valid: boolean) => void
+  existingResponse?: string
+  elementIx: number
+}
+
+function FreeTextQuestion({
+  content,
+  options,
+  response,
+  valid,
+  setResponse,
+  existingResponse,
+  elementIx,
+}: FreeTextQuestionProps) {
+  return (
+    <div>
+      {content !== '<br>' && (
+        <div
+          className={twMerge(
+            'mt-4 mb-4 border-slate-300 flex-initial min-h-[6rem] bg-primary-10 border rounded leading-6 prose max-w-none prose-p:!m-0 prose-img:!m-0 p-4'
+          )}
+        >
+          <Markdown content={content} />
+        </div>
+      )}
+
+      <div>FREE TEXT INPUT</div>
+    </div>
+  )
 }
 
 export default FreeTextQuestion
