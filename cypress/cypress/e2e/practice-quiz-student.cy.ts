e2622c90f053ce525653bb32e5e43a2c765fe9c6
sjschlapbach <julius.schlapbach@gmx.net>
2024-02-01 21:17:24 +0100

    enhance: add validity checks to test suite

--- cypress/cypress/e2e/practice-quiz-student.cy.ts
+++ cypress/cypress/e2e/practice-quiz-student.cy.ts
@@ -31,16 +31,23 @@ describe('Practice Quizzes as a Student', () => {
     cy.get('[data-cy="practice-quiz-stack-submit"]').click()
 
     // skip back and forth
+    cy.get('[data-cy="practice-quiz-stack-submit"]').should('be.disabled')
     cy.get('[data-cy="practice-quiz-progress-2"]').click()
+    cy.get('[data-cy="practice-quiz-stack-submit"]').should('not.be.disabled')
     cy.get('[data-cy="practice-quiz-progress-1"]').click()
+    cy.get('[data-cy="practice-quiz-stack-submit"]').should('not.be.disabled')
     cy.get('[data-cy="practice-quiz-progress-0"]').click()
+    cy.get('[data-cy="practice-quiz-stack-submit"]').should('not.be.disabled')
     cy.get('[data-cy="practice-quiz-stack-submit"]').click()
     cy.get('[data-cy="practice-quiz-stack-submit"]').click()
     cy.get('[data-cy="practice-quiz-stack-submit"]').click()
 
     // answer another stack with a single flashcard
+    cy.get('[data-cy="practice-quiz-stack-submit"]').should('be.disabled')
     cy.get('[data-cy="flashcard-front-1"]').click()
+    cy.get('[data-cy="practice-quiz-stack-submit"]').should('be.disabled')
     cy.get('[data-cy="flashcard-response-1-Partially"]').click()
+    cy.get('[data-cy="practice-quiz-stack-submit"]').should('not.be.disabled')
     cy.get('[data-cy="practice-quiz-stack-submit"]').click()
 
     // TODO: remove from seed or answer as well
@@ -56,8 +63,10 @@ describe('Practice Quizzes as a Student', () => {
     cy.get('[data-cy="practice-quiz-progress-right"]').click()
 
     // answer stack with all flashcards
+    cy.get('[data-cy="practice-quiz-stack-submit"]').should('be.disabled')
     cy.get('[data-cy="flashcard-front-1"]').click()
     cy.get('[data-cy="flashcard-response-1-Yes"]').click()
+    cy.get('[data-cy="practice-quiz-stack-submit"]').should('be.disabled')
     cy.get('[data-cy="flashcard-front-2"]').click()
     cy.get('[data-cy="flashcard-response-2-Partially"]').click()
     cy.get('[data-cy="flashcard-front-3"]').click()
@@ -82,15 +91,21 @@ describe('Practice Quizzes as a Student', () => {
     cy.get('[data-cy="flashcard-response-12-No"]').click()
     cy.get('[data-cy="flashcard-front-13"]').click()
     cy.get('[data-cy="flashcard-response-13-No"]').click()
+    cy.get('[data-cy="practice-quiz-stack-submit"]').should('be.disabled')
     cy.get('[data-cy="flashcard-front-14"]').click()
     cy.get('[data-cy="flashcard-response-14-Yes"]').click()
+    cy.get('[data-cy="practice-quiz-stack-submit"]').should('not.be.disabled')
     cy.get('[data-cy="practice-quiz-stack-submit"]').click()
 
-    // TODO: test for all questions, that deleting also disabled the continue button again, etc.
     // TODO: answer free text question
     cy.get('[data-cy="practice-quiz-stack-submit"]').click()
 
     // answer MC question
+    cy.get('[data-cy="practice-quiz-stack-submit"]').should('be.disabled')
+    cy.get('[data-cy="mc-1-answer-option-2"]').click()
+    cy.get('[data-cy="practice-quiz-stack-submit"]').should('not.be.disabled')
+    cy.get('[data-cy="mc-1-answer-option-2"]').click()
+    cy.get('[data-cy="practice-quiz-stack-submit"]').should('be.disabled')
     cy.get('[data-cy="mc-1-answer-option-2"]').click()
     cy.get('[data-cy="mc-1-answer-option-3"]').click()
     cy.get('[data-cy="practice-quiz-stack-submit"]').click()
@@ -99,31 +114,43 @@ describe('Practice Quizzes as a Student', () => {
     cy.get('[data-cy="practice-quiz-stack-submit"]').click()
 
     // answer KPRIM question
+    cy.get('[data-cy="practice-quiz-stack-submit"]').should('be.disabled')
     cy.get('[data-cy="toggle-kp-1-answer-1-correct"]').click()
+    cy.get('[data-cy="practice-quiz-stack-submit"]').should('be.disabled')
     cy.get('[data-cy="toggle-kp-1-answer-2-incorrect"]').click()
+    cy.get('[data-cy="practice-quiz-stack-submit"]').should('be.disabled')
     cy.get('[data-cy="toggle-kp-1-answer-3-incorrect"]').click()
+    cy.get('[data-cy="practice-quiz-stack-submit"]').should('be.disabled')
     cy.get('[data-cy="toggle-kp-1-answer-4-correct"]').click()
+    cy.get('[data-cy="practice-quiz-stack-submit"]').should('not.be.disabled')
     cy.get('[data-cy="practice-quiz-stack-submit"]').click()
 
     // answer SC question
+    cy.get('[data-cy="practice-quiz-stack-submit"]').should('be.disabled')
     cy.get('[data-cy="sc-1-answer-option-2"]').click()
+    cy.get('[data-cy="practice-quiz-stack-submit"]').should('not.be.disabled')
     cy.get('[data-cy="sc-1-answer-option-2"]').click()
     cy.get('[data-cy="sc-1-answer-option-3"]').click()
     cy.get('[data-cy="practice-quiz-stack-submit"]').click()
 
     // answer MC, PRIM and SC question
+    cy.get('[data-cy="practice-quiz-stack-submit"]').should('be.disabled')
     cy.get('[data-cy="mc-2-answer-option-2"]').click()
     cy.get('[data-cy="mc-2-answer-option-3"]').click()
+    cy.get('[data-cy="practice-quiz-stack-submit"]').should('be.disabled')
     cy.get('[data-cy="toggle-kp-4-answer-1-correct"]').click()
     cy.get('[data-cy="toggle-kp-4-answer-2-incorrect"]').click()
     cy.get('[data-cy="toggle-kp-4-answer-3-incorrect"]').click()
     cy.get('[data-cy="toggle-kp-4-answer-4-correct"]').click()
+    cy.get('[data-cy="practice-quiz-stack-submit"]').should('be.disabled')
     cy.get('[data-cy="sc-5-answer-option-2"]').click()
     cy.get('[data-cy="sc-5-answer-option-2"]').click()
     cy.get('[data-cy="sc-5-answer-option-3"]').click()
+    cy.get('[data-cy="practice-quiz-stack-submit"]').should('not.be.disabled')
     cy.get('[data-cy="practice-quiz-stack-submit"]').click()
 
     // answer content elements (single and stacked)
+    cy.get('[data-cy="practice-quiz-stack-submit"]').should('not.be.disabled')
     cy.get('[data-cy="read-content-element-1"]').click()
     cy.get('[data-cy="practice-quiz-stack-submit"]').click()
     cy.get('[data-cy="read-content-element-1"]').click()
@@ -146,6 +173,7 @@ describe('Practice Quizzes as a Student', () => {
     ) // contains continue
     cy.get('[data-cy="practice-quiz-stack-submit"]').click() // continue / submit stack
 
+    // TODO: add validity checks / disabled submission button checks for the following cases
     // answer combined stack with flashcard, content element and question
     cy.get('[data-cy="flashcard-front-1"]').click()
     cy.get('[data-cy="flashcard-response-1-Yes"]').click()
