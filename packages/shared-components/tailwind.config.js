f5a0e6182bcedba724e8a3192125038f4c931353
sjschlapbach <julius.schlapbach@gmx.net>
2024-02-07 21:08:37 +0100

    chore: add missing tailwind animation classes to shared components

--- packages/shared-components/tailwind.config.js
+++ packages/shared-components/tailwind.config.js
@@ -21,7 +21,31 @@ module.exports = {
     preflight: false,
     aspectRatio: false,
   },
+
   plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        '.transform-rotateY-0': {
+          transform: 'rotateY(0deg)',
+        },
+        '.transform-rotateY-180': {
+          transform: 'rotateY(180deg)',
+        },
+        '.transform-style-preserve-3d': {
+          transformStyle: 'preserve-3d',
+        },
+        '.transition-transform-0_6s': {
+          transition: 'transform 0.6s',
+        },
+        '.backface-hidden': {
+          backfaceVisibility: 'hidden',
+        },
+      }
+      addUtilities(newUtilities)
+    },
+    // require('tailwindcss-radix')({
+    //   variantPrefix: 'rdx',
+    // }),
     require('@tailwindcss/typography'),
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/forms'),
