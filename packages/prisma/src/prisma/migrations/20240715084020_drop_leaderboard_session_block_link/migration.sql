d9085417481c4547214da84ed68670a4ce47fb55
sjschlapbach <julius.schlapbach@gmx.net>
2024-07-20 08:57:05 +0200

    chore: remove old sessionblock type from leaderboard entry types

--- packages/prisma/src/prisma/migrations/20240715084020_drop_leaderboard_session_block_link/migration.sql
+++ packages/prisma/src/prisma/migrations/20240715084020_drop_leaderboard_session_block_link/migration.sql
@@ -2,6 +2,7 @@
   Warnings:
 
   - You are about to drop the column `sessionBlockId` on the `LeaderboardEntry` table. All the data in the column will be lost.
+  - The values [SESSION_BLOCK] on the enum `LeaderboardType` will be removed. If these variants are still used in the database, this will fail.
 
 */
 -- DropForeignKey
@@ -12,3 +13,12 @@ DROP INDEX "LeaderboardEntry_type_participantId_sessionBlockId_key";
 
 -- AlterTable
 ALTER TABLE "LeaderboardEntry" DROP COLUMN "sessionBlockId";
+
+-- AlterEnum
+BEGIN;
+CREATE TYPE "LeaderboardType_new" AS ENUM ('SESSION', 'COURSE');
+ALTER TABLE "LeaderboardEntry" ALTER COLUMN "type" TYPE "LeaderboardType_new" USING ("type"::text::"LeaderboardType_new");
+ALTER TYPE "LeaderboardType" RENAME TO "LeaderboardType_old";
+ALTER TYPE "LeaderboardType_new" RENAME TO "LeaderboardType";
+DROP TYPE "LeaderboardType_old";
+COMMIT;
