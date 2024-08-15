f6e15343a999c5006eddc1f95f184dee31fc06eb
sjschlapbach <julius.schlapbach@gmx.net>
2024-08-15 17:10:54 +0200

    fix(packages/prisma): remove duplicate migration content

--- packages/prisma/src/prisma/migrations/20240810201009_basic_daily_analytics/migration.sql
+++ packages/prisma/src/prisma/migrations/20240810201009_basic_daily_analytics/migration.sql
@@ -5,11 +5,6 @@
   - Made the column `order` on table `SessionBlock` required. This step will fail if there are existing NULL values in that column.
 
 */
--- AlterTable
-ALTER TABLE "Course" ALTER COLUMN "color" SET NOT NULL;
-
--- AlterTable
-ALTER TABLE "SessionBlock" ALTER COLUMN "order" SET NOT NULL;
 
 -- CreateTable
 CREATE TABLE "AnalyticsDaily" (
