89ee8cffd424e5ced5c495f0177dc1439766f773
Roland Schläfli <rolandschlaefli@gmail.com>
2024-08-17 16:12:11 +0200

    add redirects from sessions to quizzes

--- apps/frontend-manage/next.config.js
+++ apps/frontend-manage/next.config.js
@@ -8,6 +8,35 @@ const nextConfig = {
   ...getNextBaseConfig({
     BLOB_STORAGE_ACCOUNT_URL: process.env.BLOB_STORAGE_ACCOUNT_URL,
   }),
+  async redirects() {
+    return [
+      {
+        source: '/sessions',
+        destination: '/quizzes',
+        permanent: false,
+      },
+      {
+        source: '/sessions/:id',
+        destination: '/quizzes/:id',
+        permanent: false,
+      },
+      {
+        source: '/sessions/:id/cockpit',
+        destination: '/quizzes/:id/cockpit',
+        permanent: false,
+      },
+      {
+        source: '/sessions/:id/evaluation',
+        destination: '/quizzes/:id/evaluation',
+        permanent: false,
+      },
+      {
+        source: '/sessions/:id/lecturer',
+        destination: '/quizzes/:id/lecturer',
+        permanent: false,
+      },
+    ]
+  },
 }
 
 if (process.env.NODE_ENV !== 'test') {
