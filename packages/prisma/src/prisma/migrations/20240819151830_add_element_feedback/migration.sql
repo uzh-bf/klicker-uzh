c955cd627e14325a4a50de71238795b8aaa6d7cc
sjschlapbach <julius.schlapbach@gmx.net>
2024-08-19 17:21:01 +0200

    wip: update database schema with new student element feedback table

---
+++ packages/prisma/src/prisma/migrations/20240819151830_add_element_feedback/migration.sql
@@ -0,0 +1,29 @@
+-- CreateTable
+CREATE TABLE "ElementFeedback" (
+    "id" SERIAL NOT NULL,
+    "upvote" BOOLEAN NOT NULL,
+    "downvote" BOOLEAN NOT NULL,
+    "feedback" TEXT NOT NULL,
+    "participantId" UUID NOT NULL,
+    "elementInstanceId" INTEGER NOT NULL,
+    "elementId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "ElementFeedback_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ElementFeedback_participantId_elementInstanceId_key" ON "ElementFeedback"("participantId", "elementInstanceId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ElementFeedback_participantId_elementId_key" ON "ElementFeedback"("participantId", "elementId");
+
+-- AddForeignKey
+ALTER TABLE "ElementFeedback" ADD CONSTRAINT "ElementFeedback_participantId_fkey" FOREIGN KEY ("participantId") REFERENCES "Participant"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ElementFeedback" ADD CONSTRAINT "ElementFeedback_elementInstanceId_fkey" FOREIGN KEY ("elementInstanceId") REFERENCES "ElementInstance"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ElementFeedback" ADD CONSTRAINT "ElementFeedback_elementId_fkey" FOREIGN KEY ("elementId") REFERENCES "Element"("id") ON DELETE CASCADE ON UPDATE CASCADE;
