f1b25073e42bd7e1dc450c8a9c457616aa3b12df
sjschlapbach <julius.schlapbach@gmx.net>
2024-08-21 14:00:38 +0200

    wip: extend database scheme with analytics and instance stuctures as well as migrations

--- packages/prisma/src/prisma/migrations/20240820113527_correct_counts_element_instance/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "ElementInstance" ADD COLUMN     "correctCount" INTEGER NOT NULL DEFAULT 0,
-ADD COLUMN     "partialCorrectCount" INTEGER NOT NULL DEFAULT 0,
-ADD COLUMN     "wrongCount" INTEGER NOT NULL DEFAULT 0;
