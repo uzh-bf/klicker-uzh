f5a8018b4f2d89d47a101df34368c740baefe5ac
Roland Schläfli <rolandschlaefli@gmail.com>
2024-08-23 21:57:08 +0200

    logging

--- apps/frontend-pwa/src/pages/createAccount.tsx
+++ apps/frontend-pwa/src/pages/createAccount.tsx
@@ -109,11 +109,15 @@ export async function getServerSideProps(ctx: GetServerSidePropsContext) {
   if (cookies['lti-token'] || query.jwt) {
     const token = cookies['lti-token'] ?? query.jwt
 
+    console.log('token', token)
+
     const parsedToken = JWT.verify(token, process.env.APP_SECRET as string) as {
       sub: string
       email: string
       scope: string
     }
+
+    console.log('parsedToken', parsedToken)
 
     if (parsedToken.scope === 'LTI1.3') {
       signedLtiData.token = token
