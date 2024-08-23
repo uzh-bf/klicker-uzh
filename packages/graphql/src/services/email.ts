06a9f06cce7dd118a2b41f8825ccf5ebc05b49e9
Roland Schläfli <rolandschlaefli@gmail.com>
2024-08-23 10:52:33 +0200

    fix: add EMAIL_STARTTLS config to authenticate with STARTTLS when sending via smtp

--- packages/graphql/src/services/email.ts
+++ packages/graphql/src/services/email.ts
@@ -26,6 +26,7 @@ export async function createTransport() {
           ? parseInt(process.env.EMAIL_PORT)
           : undefined,
         secure: process.env.EMAIL_SECURE === 'true',
+        requireTLS: process.env.EMAIL_STARTTLS === 'true',
         auth: {
           user: process.env.EMAIL_USER,
           pass: process.env.EMAIL_PASS,
