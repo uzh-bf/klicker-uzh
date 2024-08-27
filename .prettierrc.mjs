303504f0c40004fb7fe5ec40b1e70a57a261df9d
Roland Schläfli <rolandschlaefli@gmail.com>
2024-08-27 21:13:15 +0200

    chore: add global prettier configuration and clean up package level dependencies and config files (#4214)

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,6 +1,8 @@
-module.exports = {
+export default {
   semi: false,
   singleQuote: true,
   trailingComma: 'es5',
+  // tabWidth: 2,
+  // printWidth: 120,
   plugins: ['prettier-plugin-organize-imports', 'prettier-plugin-tailwindcss'],
 }
