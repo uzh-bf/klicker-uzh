6ada84e7647a26cc95bf777e3f054564499ba320
TheHummel <jannisalsbach@gmail.com>
2024-10-15 16:05:12 +0200

    remove jannis specific files

--- _run_app_dependencies_wsl_jannis.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# copy the prisma schema for it to be available to python files
-./util/sync-schema.sh
-
-# start postgres, redis, and reverse proxy
-docker compose up postgres redis_exec redis_cache reverse_proxy_wsl_jannis
