16246ee9a2437a7fe0cd029059ad7096c77db028
Roland Schläfli <rolandschlaefli@gmail.com>
2024-10-22 18:29:16 +0200

    update docker compose for separate mac config

--- apps/frontend-pwa/next.config.mjs
+++ apps/frontend-pwa/next.config.mjs
@@ -1,7 +1,6 @@
 import withPWAInit from '@ducanh2912/next-pwa'
 import { getNextBaseConfig, getNextPWAConfig } from '@klicker-uzh/next-config'
 
-/** @type {import('next').NextConfig} */
 let nextConfig = {
   ...getNextBaseConfig({
     BLOB_STORAGE_ACCOUNT_URL: process.env.BLOB_STORAGE_ACCOUNT_URL,
