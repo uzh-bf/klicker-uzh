0acb4f9b9aca9a2b282e3562c13e918c1622df41
sjschlapbach <julius.schlapbach@gmx.net>
2024-10-29 13:21:54 +0100

    chore: add script to fix choices options on existing instances

---
+++ packages/graphql/src/scripts/2024_10_29_fix_choices_options.ts
@@ -0,0 +1,133 @@
+import { ElementType, PrismaClient } from '@klicker-uzh/prisma'
+
+async function run() {
+  const prisma = new PrismaClient()
+  const verbose = false // verbose logging setting
+
+  // ! VALIDATION
+  // fetch all elements
+  const es = await prisma.element.findMany()
+
+  // fetch all element instances
+  const eis = await prisma.elementInstance.findMany({
+    include: { responses: true, detailResponses: true },
+  })
+
+  // fetch all question instances
+  const qis = await prisma.questionInstance.findMany()
+
+  // loop over all elements, filter for choices questions and check if choices are in order
+  let e_errors = 0
+  for (const e of es) {
+    if (
+      (e.type === ElementType.SC ||
+        e.type === ElementType.MC ||
+        e.type === ElementType.KPRIM) &&
+      (e.type === ElementType.SC ||
+        e.type === ElementType.MC ||
+        e.type === ElementType.KPRIM)
+    ) {
+      const choices = e.options.choices
+
+      // check if choices are in order with ix attribute
+      if (choices.some((c, index) => c.ix !== index)) {
+        e_errors += 1
+
+        if (verbose) {
+          console.error(
+            `Element ${e.id} has choices out of order: ${choices
+              .map((c) => c.ix)
+              .join(', ')}`
+          )
+        }
+
+        // update the element choices
+        const newChoices = choices.map((c, index) => ({
+          ...c,
+          ix: index,
+        }))
+
+        console.log('OLD CHOICES:')
+        console.log(choices)
+        console.log('NEW CHOICES:')
+        console.log(newChoices)
+
+        // TODO: uncomment to trigger element updates
+        // await prisma.element.update({
+        //   where: { id: e.id },
+        //   data: {
+        //     options: {
+        //       ...e.options,
+        //       choices: newChoices,
+        //     },
+        //   },
+        // })
+      }
+    }
+  }
+
+  // loop over all element instances, filter for choices instances, check if choices are in order
+  let ei_errors = 0
+  for (const ei of eis) {
+    if (
+      (ei.elementType === ElementType.SC ||
+        ei.elementType === ElementType.MC ||
+        ei.elementType === ElementType.KPRIM) &&
+      (ei.elementData.type === ElementType.SC ||
+        ei.elementData.type === ElementType.MC ||
+        ei.elementData.type === ElementType.KPRIM)
+    ) {
+      const choices = ei.elementData.options.choices
+
+      // check if choices are in order with ix attribute
+      if (choices.some((c, index) => c.ix !== index)) {
+        ei_errors += 1
+
+        if (verbose) {
+          console.error(
+            `ElementInstance ${ei.id} has choices out of order: ${choices
+              .map((c) => c.ix)
+              .join(
+                ', '
+              )} with ${ei.responses.length} responses and ${ei.detailResponses.length} detail responses`
+          )
+        }
+      }
+    }
+  }
+
+  let qi_errors = 0
+  for (const qi of qis) {
+    if (
+      qi.questionData.type === ElementType.SC ||
+      qi.questionData.type === ElementType.MC ||
+      qi.questionData.type === ElementType.KPRIM
+    ) {
+      const choices = qi.questionData.options.choices
+
+      // check if choices are in order with ix attribute
+      if (choices.some((c, index) => c.ix !== index)) {
+        qi_errors += 1
+
+        if (verbose) {
+          console.error(
+            `QuestionInstance ${qi.id} has choices out of order: ${choices
+              .map((c) => c.ix)
+              .join(', ')}`
+          )
+        }
+      }
+    }
+  }
+
+  console.log('Done checking choices')
+  console.log('Found', e_errors, 'elements with out of order choices')
+  console.log('Found', ei_errors, 'element instances with out of order choices')
+  console.log(
+    'Found',
+    qi_errors,
+    'question instances with out of order choices'
+  )
+}
+
+await run()
