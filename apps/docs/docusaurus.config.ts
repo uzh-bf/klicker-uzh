93795c63c88311b64b22dbcb9c5f15e66ef1878a
Roland Schläfli <rolandschlaefli@gmail.com>
2024-11-05 22:13:08 +0100

    change intro course

--- apps/docs/docusaurus.config.ts
+++ apps/docs/docusaurus.config.ts
@@ -113,7 +113,7 @@ const config: Config = {
     announcementBar: {
       id: 'release_v3',
       content:
-        'We are offering introductory courses on Zoom. For more details see <a target="_blank" href="https://community.klicker.uzh.ch/t/2024-01-10-2024-02-08-klickeruzh-v3-0-introduction-and-didactic-use-cases/257">the following page</a>.',
+        'We are offering introductory courses at UZH. For more details see <a target="_blank" href="https://community.klicker.uzh.ch/t/2024-01-10-2024-02-08-klickeruzh-v3-0-introduction-and-didactic-use-cases/257">the following page</a>.',
       backgroundColor: '#fafbfc',
       textColor: '#091E42',
       isCloseable: false,
