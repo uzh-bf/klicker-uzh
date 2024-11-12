4aded7c74d5050876c0a71b6f6fd22a066087764
sjschlapbach <julius.schlapbach@gmx.net>
2024-11-12 16:41:23 +0100

    chore: update data types and migration

---
+++ packages/prisma/src/prisma/migrations/20241112154002_average_times_float_precision/migration.sql
@@ -0,0 +1,10 @@
+-- AlterTable
+ALTER TABLE "InstanceStatistics" ALTER COLUMN "averageTimeSpent" SET DATA TYPE REAL;
+
+-- AlterTable
+ALTER TABLE "QuestionResponse" ALTER COLUMN "averageTimeSpent" SET DEFAULT 0,
+ALTER COLUMN "averageTimeSpent" SET DATA TYPE REAL;
+
+-- AlterTable
+ALTER TABLE "QuestionResponseDetail" ALTER COLUMN "timeSpent" SET DEFAULT 0,
+ALTER COLUMN "timeSpent" SET DATA TYPE REAL;
