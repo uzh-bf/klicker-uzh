15a05d6ee3810ff007fba4a87d549844793a2a5d
sjschlapbach <julius.schlapbach@gmx.net>
2024-11-13 13:20:55 +0100

    chore: remove default value for time spent

--- packages/prisma/src/prisma/migrations/20241113121758_average_times_float_precision/migration.sql
+++ packages/prisma/src/prisma/migrations/20241113121758_average_times_float_precision/migration.sql
@@ -6,5 +6,5 @@ ALTER TABLE "QuestionResponse" ALTER COLUMN "averageTimeSpent" DROP DEFAULT,
 ALTER COLUMN "averageTimeSpent" SET DATA TYPE REAL;
 
 -- AlterTable
-ALTER TABLE "QuestionResponseDetail" ALTER COLUMN "timeSpent" SET DEFAULT 0,
+ALTER TABLE "QuestionResponseDetail" ALTER COLUMN "timeSpent" DROP DEFAULT,
 ALTER COLUMN "timeSpent" SET DATA TYPE REAL;
