522d38300c6635fb8a3ba4e01bfd31139d13be3a
Chris <93202854+Queentaker@users.noreply.github.com>
2024-11-14 14:35:51 +0100

    prettier: update

--- apps/docs/src/pages/about.tsx
+++ apps/docs/src/pages/about.tsx
@@ -10,42 +10,44 @@ import { Prose } from '@uzh-bf/design-system'
 import TextTransition, { presets } from 'react-text-transition'
 
 const KPIs = () => {
-    const metrics = [
-        {
-            value: "1894",
-            label: "Lecturers",
-            description: "use KlickerUZH"
-        },
-        {
-            value: "26134",
-            label: "Elements",
-            description: "have been created"
-        },
-        {
-            value: "7203",
-            label: "Live Quizzes",
-            description: "were conducted"
-        }
-    ];
+  const metrics = [
+    {
+      value: '1894',
+      label: 'Lecturers',
+      description: 'use KlickerUZH',
+    },
+    {
+      value: '26134',
+      label: 'Elements',
+      description: 'have been created',
+    },
+    {
+      value: '7203',
+      label: 'Live Quizzes',
+      description: 'were conducted',
+    },
+  ]
 
-    return (
-        <>
-        <h1 className="mt-2 flex w-max flex-row gap-4 text-xl md:text-3xl" >
-            KlickerUZH in numbers
-        </h1>
-
-        <div className="mx-auto mt-12 grid max-w-[45rem] grid-cols-3 gap-8">
-            {metrics.map((metric, index) => (
-                <div key={index} className="flex flex-col items-center justify-center rounded-lg bg-uzh-blue-80 p-6 text-center text-white">
-                    <div className="text-3xl font-bold">{metric.value}</div>
-                    <div className="mt-2 text-xl font-semibold">{metric.label}</div>
-                    <div className="mt-1 text-sm">{metric.description}</div>
-                </div>
-            ))}
-        </div>
-        </>
-    );
-};
+  return (
+    <>
+      <h1 className="mt-2 flex w-max flex-row gap-4 text-xl md:text-3xl">
+        KlickerUZH in numbers
+      </h1>
+      <div className="mx-auto mt-12 max-w-[45rem] gap-8 sm:grid sm:grid-cols-3">
+        {metrics.map((metric, index) => (
+          <div
+            key={index}
+            className="bg-uzh-blue-80 flex flex-col items-center justify-center rounded-lg p-6 text-center text-white"
+          >
+            <div className="text-3xl font-bold">{metric.value}</div>
+            <div className="mt-2 text-xl font-semibold">{metric.label}</div>
+            <div className="mt-1 text-sm">{metric.description}</div>
+          </div>
+        ))}
+      </div>
+    </>
+  )
+}
 
 function Models() {
   return (
@@ -149,60 +151,66 @@ function Models() {
   )
 }
 
+const Purpose = (
+  <>
+    <h1 className="mt-2 flex w-max flex-row gap-4 md:text-5xl">
+      <div> Purpose of KlickerUZH</div>
+      <div className="flex justify-center"></div>
+    </h1>
+    <p className="text-center">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+    </p>
+  </>
+)
+
 function Catalyst() {
   return (
     <Layout>
-
-
-          <div className="flex max-w-7xl flex-col items-center text-center md:mx-auto lg:px-8">
-              <div className="px-8 py-24">
-                  <h1 className="mt-2 flex w-max flex-row gap-4 md:text-5xl">
-                      <div> Purpose of KlickerUZH</div>
-                      <div className="flex justify-center">
-                      </div>
-                  </h1>
-                  <p className="text-center">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-                  </p>
-                  <h1 className="mt-2 flex w-max flex-row gap-4 md:text-5xl">
-                      <div>KlickerUZH</div>
-                      <div className="flex justify-center">
-                          <TextTransition springConfig={presets.wobbly}>
-                              <u className="decoration-[#3353b7]">Models</u>
-                          </TextTransition>
-                      </div>
-                  </h1>
-                  {Models()}
-                  <Prose className={{root: 'prose-xl w-full max-w-3xl'}}>
-                      <p>
-                          The core components of our KlickerUZH instance are free to use for
-                          everyone. Advanced functionalities are restricted to users at UZH
-                          or sponsors ("catalysts") of the KlickerUZH open-source project.
-                      </p>
-                      <p>
-                          We offer the advanced functionalities for free to individual
-                          lecturers in small educational use cases or for piloting
-                          KlickerUZH in an external organization. For broad use across a
-                          larger organization, a sponsorship agreement is required.
-                      </p>
-                      <p>
-                          You can contribute to the project in various ways, e.g., by
-                          self-hosting and collaborating on the code base, or by sponsoring
-                          the project financially.
-                      </p>
-                      <p>
-                          To get access and for other inquiries please fill out the
-                          following{' '}
-                          <a href="https://forms.office.com/e/4AsWW1uck2" target="_blank">
-                              form
-                          </a>
-                          .
-                      </p>
-                  </Prose>
-                  {KPIs()}
-
-              </div>
-          </div>
+      <div className="flex max-w-7xl flex-col items-center text-center md:mx-auto lg:px-8">
+        <div className="px-8 py-24">
+          {Purpose}
+          <h1 className="mt-2 flex w-max flex-row gap-4 md:text-5xl">
+            <div>Team</div>
+            <div className="flex justify-center"></div>
+          </h1>
+          <h1 className="mt-2 flex w-max flex-row gap-4 md:text-5xl">
+            <div>KlickerUZH</div>
+            <div className="flex justify-center">
+              <TextTransition springConfig={presets.wobbly}>
+                <u className="decoration-[#3353b7]">Models</u>
+              </TextTransition>
+            </div>
+          </h1>
+          {Models()}
+          <Prose className={{ root: 'prose-xl w-full max-w-3xl' }}>
+            <p>
+              The core components of our KlickerUZH instance are free to use for
+              everyone. Advanced functionalities are restricted to users at UZH
+              or sponsors ("catalysts") of the KlickerUZH open-source project.
+            </p>
+            <p>
+              We offer the advanced functionalities for free to individual
+              lecturers in small educational use cases or for piloting
+              KlickerUZH in an external organization. For broad use across a
+              larger organization, a sponsorship agreement is required.
+            </p>
+            <p>
+              You can contribute to the project in various ways, e.g., by
+              self-hosting and collaborating on the code base, or by sponsoring
+              the project financially.
+            </p>
+            <p>
+              To get access and for other inquiries please fill out the
+              following{' '}
+              <a href="https://forms.office.com/e/4AsWW1uck2" target="_blank">
+                form
+              </a>
+              .
+            </p>
+          </Prose>
+          {KPIs()}
+        </div>
+      </div>
     </Layout>
   )
 }
