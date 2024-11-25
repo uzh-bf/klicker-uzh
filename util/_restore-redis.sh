1d804f2bb00e83af56bd11c234e1590f6a5ca686
Roland Schläfli <rolandschlaefli@gmail.com>
2024-11-25 17:00:20 +0100

    deps: restore necessary production deps

--- util/_restore-redis.sh
+++ util/_restore-redis.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Run doppler secrets first to ensure we have the environment loaded
-eval $(doppler secrets download --no-file --format env --config prd)
-
 # Now run the dump command with the loaded environment variables
 # Requires redis installation using, e.g., brew install redis
 redis-cli -u redis://localhost:6379 --pipe < redis.dump
