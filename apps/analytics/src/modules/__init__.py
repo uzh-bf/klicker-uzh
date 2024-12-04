4b60b7e8bf53a7ff07e281243d262a7f1c6693fa
Julius Schlapbach <80708107+sjschlapbach@users.noreply.github.com>
2024-12-04 16:07:08 +0100

    enhance(apps/analytics): add logic for computation of participant course analytics (#4387)

--- apps/analytics/src/modules/__init__.py
+++ apps/analytics/src/modules/__init__.py
@@ -1,2 +1,8 @@
 from .participant_analytics import compute_correctness, get_participant_responses
 from .aggregated_analytics import compute_aggregated_analytics
+from .participant_course_analytics import (
+    get_running_past_courses,
+    get_active_weeks,
+    compute_participant_activity,
+    save_participant_course_analytics,
+)
---
+++ apps/analytics/src/modules/participant_course_analytics/get_running_past_courses.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+import pandas as pd
+
+
+def get_running_past_courses(db):
+    curr_date = datetime.now().strftime("%Y-%m-%d")
+    courses = db.course.find_many(
+        where={
+            # Incremental scripts can add this statement to reduce the amount of required computations
+            # 'endDate': {
+            #     'gt': datetime.now().strftime('%Y-%m-%d') + 'T00:00:00.000Z'
+            # }
+            "startDate": {"lte": curr_date + "T23:59:59.999Z"},
+        },
+        include={"participations": True},
+    )
+
+    df_courses = pd.DataFrame(list(map(lambda x: x.dict(), courses)))
+    print(
+        "Found {} courses with a start date before {}".format(
+            len(df_courses), curr_date
+        )
+    )
+
+    return df_courses
