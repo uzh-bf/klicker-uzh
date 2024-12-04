1cddacd318363ccfd216fff24fbfa319381f24fd
sjschlapbach <julius.schlapbach@gmx.net>
2024-12-04 16:11:03 +0100

    wip: update database schema for activity tracking in aggregated course analytics

---
+++ packages/prisma/src/prisma/migrations/20241204150959_update_daily_activity_types/migration.sql
@@ -0,0 +1,17 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `participantCount` on the `AggregatedCourseAnalytics` table. All the data in the column will be lost.
+  - Added the required column `courseParticipantCount` to the `AggregatedCourseAnalytics` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "AggregatedCourseAnalytics" DROP COLUMN "participantCount",
+ADD COLUMN     "courseParticipantCount" INTEGER NOT NULL,
+ALTER COLUMN "activityMonday" SET DATA TYPE REAL,
+ALTER COLUMN "activityTuesday" SET DATA TYPE REAL,
+ALTER COLUMN "activityWednesday" SET DATA TYPE REAL,
+ALTER COLUMN "activityThursday" SET DATA TYPE REAL,
+ALTER COLUMN "activityFriday" SET DATA TYPE REAL,
+ALTER COLUMN "activitySaturday" SET DATA TYPE REAL,
+ALTER COLUMN "activitySunday" SET DATA TYPE REAL;
