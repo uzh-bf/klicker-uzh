ceaa10e37442a3cac26c511cec35a09a1cec64f6
sjschlapbach <julius.schlapbach@gmx.net>
2024-12-04 17:58:52 +0100

    wip: update database migration

--- packages/prisma/src/prisma/migrations/20241204165711_update_daily_activity_types/migration.sql
+++ packages/prisma/src/prisma/migrations/20241204165711_update_daily_activity_types/migration.sql
@@ -2,6 +2,7 @@
   Warnings:
 
   - You are about to drop the column `participantCount` on the `AggregatedCourseAnalytics` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[courseId]` on the table `AggregatedCourseAnalytics` will be added. If there are existing duplicate values, this will fail.
   - Added the required column `courseParticipantCount` to the `AggregatedCourseAnalytics` table without a default value. This is not possible if the table is not empty.
 
 */
@@ -15,3 +16,6 @@ ALTER COLUMN "activityThursday" SET DATA TYPE REAL,
 ALTER COLUMN "activityFriday" SET DATA TYPE REAL,
 ALTER COLUMN "activitySaturday" SET DATA TYPE REAL,
 ALTER COLUMN "activitySunday" SET DATA TYPE REAL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "AggregatedCourseAnalytics_courseId_key" ON "AggregatedCourseAnalytics"("courseId");
