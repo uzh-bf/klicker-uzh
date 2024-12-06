5d3dec5f05d3525b6df0a52c123d1eb88b78e137
sjschlapbach <julius.schlapbach@gmx.net>
2024-12-06 08:21:01 +0100

    chore: remove legacy code and update python imports

--- apps/analytics/src/modules/__init__.py
+++ apps/analytics/src/modules/__init__.py
@@ -1,21 +1,6 @@
-from .participant_analytics import compute_correctness, get_participant_responses
-from .aggregated_analytics import compute_aggregated_analytics
-from .participant_course_analytics import (
-    get_running_past_courses,
-    get_active_weeks,
-    compute_participant_activity,
-    save_participant_course_analytics,
-)
-from .aggregated_course_analytics import compute_weekday_activity
-from .participant_performance import (
-    compute_response_error_rates,
-    compute_performance_levels,
-    save_participant_performance,
-)
-from .instance_activity_performance import (
-    get_course_activities,
-    compute_instance_performance,
-    agg_activity_performance,
-    save_instance_performances,
-    save_activity_performance,
-)
+from .participant_analytics import *
+from .aggregated_analytics import *
+from .participant_course_analytics import *
+from .aggregated_course_analytics import *
+from .participant_performance import *
+from .instance_activity_performance import *
--- apps/analytics/src/modules/instance_activity_performance/save_activity_performance.py
+++ apps/analytics/src/modules/instance_activity_performance/save_activity_performance.py
@@ -35,30 +35,6 @@ def save_activity_performance(
             "Either practice_quiz_id or microlearning_id must be provided for activity performance creation/update"
         )
 
-    # data_create = {
-    #     "firstErrorRate": activity_performance.firstErrorRate,
-    #     "firstPartialRate": activity_performance.firstPartialRate,
-    #     "firstCorrectRate": activity_performance.firstCorrectRate,
-    #     "lastErrorRate": activity_performance.lastErrorRate,
-    #     "lastPartialRate": activity_performance.lastPartialRate,
-    #     "lastCorrectRate": activity_performance.lastCorrectRate,
-    #     "totalErrorRate": activity_performance.totalErrorRate,
-    #     "totalPartialRate": activity_performance.totalPartialRate,
-    #     "totalCorrectRate": activity_performance.totalCorrectRate,
-    #     "course": {"connect": {"id": course_id}},
-    # }
-
-    # if practice_quiz_id is not None:
-    #     data_create["practiceQuiz"] = {"connect": {"id": practice_quiz_id}}
-    #     where_clause = {"practiceQuizId": practice_quiz_id}
-    # elif microlearning_id is not None:
-    #     data_create["microLearning"] = {"connect": {"id": microlearning_id}}
-    #     where_clause = {"microLearningId": microlearning_id}
-    # else:
-    #     raise ValueError(
-    #         "Either practice_quiz_id or microlearning_id must be provided for activity performance creation/update"
-    #     )
-
     db.activityperformance.upsert(
         where=where_clause,
         data={"create": create_values, "update": values},
