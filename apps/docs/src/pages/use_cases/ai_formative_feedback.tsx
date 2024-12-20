6d8aa259b0940788a6cb0686c856a47854fa392a
Roland Schläfli <rolandschlaefli@gmail.com>
2024-12-20 12:26:35 +0100

    enhance(docs): add new use cases and improve website structure (#4399)

---
+++ apps/docs/src/pages/use_cases/ai_formative_feedback.tsx
@@ -0,0 +1,7 @@
+import UseCase from '@site/src/components/usecases/UseCase'
+
+function AIFormativeFeedback() {
+  return <UseCase slug="ai_formative_feedback" />
+}
+
+export default AIFormativeFeedback
