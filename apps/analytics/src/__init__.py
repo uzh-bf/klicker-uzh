593cef67c8d6674fde37f8a7fc405e21361b19a0
Julius Schlapbach <80708107+sjschlapbach@users.noreply.github.com>
2024-12-23 13:28:34 +0100

    chore(apps/analytics): create executable python scripts for initial learning analytics computation (#4410)

--- apps/analytics/src/__init__.py
+++ apps/analytics/src/__init__.py
@@ -1,2 +1,3 @@
 from .modules import *
 from .notebooks import *
+from .scripts import *
---
+++ apps/analytics/src/scripts/2_initial_aggregated_course_analytics.py
@@ -0,0 +1,38 @@
+# This script computes the aggregated course analytics
+# ! This script is a copy of the corresponding notebook content and needs to be kept in sync with it
+
+import os
+from prisma import Prisma
+import pandas as pd
+
+# set the python path correctly for module imports to work
+import sys
+
+sys.path.append("../../")
+
+from src.modules.participant_course_analytics.get_running_past_courses import (
+    get_running_past_courses,
+)
+from src.modules.aggregated_course_analytics.compute_weekday_activity import (
+    compute_weekday_activity,
+)
+
+
+db = Prisma()
+db.connect()
+
+# Script settings
+verbose = False
+
+# find all courses that started in the past
+df_courses = get_running_past_courses(db)
+
+# iterate over all courses and compute the participant course analytics
+for idx, course in df_courses.iterrows():
+    print(f"Processing course", idx, "of", len(df_courses), "with id", course["id"])
+
+    # computation of activity per weekday
+    compute_weekday_activity(db, course)
+
+# Disconnect from the database
+db.disconnect()
