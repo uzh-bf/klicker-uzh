f4c67601f1c5c8e3d435141e25079d353f874398
Julius Schlapbach <80708107+sjschlapbach@users.noreply.github.com>
2025-01-03 13:39:36 +0100

    chore(apps/docs): update team on website about page (#4429)

--- apps/docs/src/pages/about.tsx
+++ apps/docs/src/pages/about.tsx
@@ -1,92 +1,79 @@
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faInfoCircle } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Layout from '@theme/Layout'
 import { H1, H2 } from '@uzh-bf/design-system'
 
-const PEOPLE: { name: string; role: string; imageUrl?: string }[] = [
+const PEOPLE: {
+  name: string
+  role: string
+  imageUrl?: string
+  info?: string
+  github?: string
+}[] = [
   {
     name: 'Roland Schläfli',
     role: 'Project Manager',
     imageUrl:
       'https://www.df.uzh.ch/contacts/df/admin/teaching-center/rschl%C3%A4fli/photo/Schl%C3%A4fli-Roland.jpg.jpg',
+    info: 'https://www.df.uzh.ch/en/people/staff/teaching-center/roland-schlaefli.html',
+    github: 'https://github.com/rschlaefli',
   },
   {
     name: 'Julius Schlapbach',
     role: 'Project Maintainer',
     imageUrl:
       'https://www.df.uzh.ch/contacts/df/student-assistants/jschlapbach/photo/20220504_Schlapbach-Julius-019.jpg.jpg',
+    info: 'https://www.df.uzh.ch/en/people/student-assistants/julius-schlapbach.html',
+    github: 'https://github.com/sjschlapbach',
   },
   {
     name: 'Johanna Braun',
     role: 'Head Teaching Center',
     imageUrl:
       'https://www.df.uzh.ch/contacts/df/admin/teaching-center/jbraun/photo/Braun%20Johanna.jpg.jpg',
+    info: 'https://www.df.uzh.ch/en/people/staff/teaching-center/Johanna-Braun.html',
   },
   {
     name: 'Benjamin Wilding',
     role: 'Managing Director Teaching DF',
     imageUrl:
       'https://www.df.uzh.ch/contacts/df/admin/management/bwilding/photo/Benjamin%20Wilding.jpg.jpg',
+    info: 'https://www.df.uzh.ch/en/people/staff/management/benjamin-wilding.html',
   },
   {
     name: 'Walter Farkas',
     role: 'Director Teaching Center',
     imageUrl:
       'https://www.df.uzh.ch/contacts/df/professors/efarkas/photo/farkas-erich_walter.jpg.jpg',
+    info: 'https://www.df.uzh.ch/en/people/professors/erich-walter-farkas.html',
   },
   {
     name: 'Jannis Alsbach',
     role: 'Contributor',
     imageUrl:
       'https://www.df.uzh.ch/contacts/df/student-assistants/jalsbach/photo/20220504_Alsbach-Jannis-006.jpg.jpg',
+    info: 'https://www.df.uzh.ch/en/people/student-assistants/jannis-alsbach.html',
+    github: 'https://github.com/TheHummel',
   },
   {
     name: 'Julia Gut',
     role: 'Contributor (Content)',
     imageUrl:
       'https://www.df.uzh.ch/contacts/df/student-assistants/jgut/photo/Julia%20Gut.jpg.jpg',
+    info: 'https://www.df.uzh.ch/en/people/student-assistants/julia-gut.html',
+  },
+  {
+    name: "Alessio D'Andrea",
+    role: 'Contributor (Content)',
+    imageUrl:
+      'https://www.df.uzh.ch/contacts/df/student-assistants/ad-andrea/photo/Alessio%20D&%23039;Andrea.jpg.jpg',
+    info: 'https://www.df.uzh.ch/en/people/student-assistants/alessio-dandrea.html',
   },
   {
     name: 'Selina De Pizzol',
     role: 'Contributor (Content)',
-  },
-  {
-    name: 'Bulin Shaqiri',
-    role: 'Contributor (Alumnus)',
-  },
-  {
-    name: 'Alex Scheitlin',
-    role: 'Contributor (Alumnus)',
-  },
-  {
-    name: 'Lisa Liechti',
-    role: 'Project Manager (Alumnus)',
-  },
-  {
-    name: 'Felix Schelbert',
-    role: 'Contributor (Alumnus)',
-  },
-  {
-    name: 'Luca Locher',
-    role: 'Contributor (Alumnus)',
-  },
-  {
-    name: 'Florina Vogel',
-    role: 'Contributor (Alumnus)',
-  },
-  {
-    name: 'Maximilian Weber',
-    role: 'Contributor (Alumnus)',
-  },
-  {
-    name: 'Valentin Meyer',
-    role: 'Contributor (Alumnus)',
-  },
-  {
-    name: 'Christopher Narayanan',
-    role: 'Contributor (Alumnus)',
-  },
-  {
-    name: 'Jonas Gebel',
-    role: 'Contributor (Alumnus)',
+    info: 'https://www.df.uzh.ch/en/people/student-assistants/selina-de-pizzol.html',
   },
 ]
 
@@ -125,25 +112,73 @@ const About = () => {
         </div>
 
         <H2 className={{ root: 'mb-4' }}>Our Team</H2>
+        <p>
+          The following people are activtely involved in the KlickerUZH project
+          and its continued development. If you have any questions or feedback,
+          feel free to reach out to us via e-mail at{' '}
+          <span
+            onClick={() =>
+              (window.location.href =
+                'mailto:klicker@df.uzh.ch?subject=[KlickerUZH]: Website Request')
+            }
+            className="cursor-pointer text-blue-800 hover:underline"
+          >
+            klicker@df.uzh.ch
+          </span>
+          . Former contributors can be found in our{' '}
+          <span
+            onClick={() =>
+              window.open(
+                'https://github.com/uzh-bf/klicker-uzh/graphs/contributors',
+                '_blank'
+              )
+            }
+            className="cursor-pointer text-blue-800 hover:underline"
+          >
+            <FontAwesomeIcon icon={faGithub} className="mx-1" />
+            open-source GitHub repository
+          </span>
+          .
+        </p>
         <ul
           role="list"
-          className="mb-14 grid list-none gap-x-8 gap-y-10 sm:grid-cols-2 sm:gap-y-12 md:grid-cols-3 lg:grid-cols-4 xl:col-span-2"
+          className="mb-14 grid list-none grid-cols-1 gap-x-8 gap-y-10 pl-0 sm:grid-cols-2 sm:gap-y-12 md:grid-cols-3 lg:grid-cols-4 xl:col-span-2"
         >
           {PEOPLE.map((person) => (
             <li key={person.name}>
-              <div className="flex items-center gap-x-6">
+              <div className="flex items-start gap-x-6">
                 <img
                   alt={`Profile photo of ${person.name}`}
                   src={person.imageUrl ?? '/img_v3/anonymousUser.svg'}
                   className="aspect-ratio w-14 rounded-full"
                 />
-                <div className="h-max">
+                <div className="flex h-max flex-col">
                   <h3 className="mb-0 text-base/7 font-semibold tracking-tight text-gray-900">
                     {person.name}
                   </h3>
-                  <p className="text-sm/6 font-semibold text-indigo-600">
+                  <p className="mb-1 text-sm/6 font-semibold text-indigo-600">
                     {person.role}
                   </p>
+                  <div className="mt-0 flex flex-row items-center gap-3">
+                    {person.info ? (
+                      <FontAwesomeIcon
+                        icon={faInfoCircle}
+                        onClick={() => {
+                          window.open(person.info, '_blank')
+                        }}
+                        className="w-max hover:cursor-pointer"
+                      />
+                    ) : null}
+                    {person.github ? (
+                      <FontAwesomeIcon
+                        icon={faGithub}
+                        onClick={() => {
+                          window.open(person.github, '_blank')
+                        }}
+                        className="w-max hover:cursor-pointer"
+                      />
+                    ) : null}
+                  </div>
                 </div>
               </div>
             </li>
@@ -151,7 +186,7 @@ const About = () => {
         </ul>
 
         <H2>Project Sponsors</H2>
-        <div className="mb-4 flex h-12 flex-row items-start">
+        <div className="mb-4 flex h-24 flex-col items-start md:h-12 md:flex-row">
           <img src="/img/logo_swissuniversities.png" className="mr-8 h-full" />
           <img src="/img/logo_uzh.jpeg" className="h-full" />
         </div>
