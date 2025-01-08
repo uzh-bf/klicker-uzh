a000cb260fd42aa93cf70d68cc474933e3aa0bbf
sjschlapbach <julius.schlapbach@gmx.net>
2025-01-08 22:55:17 +0100

    chore: update stylesheet

--- packages/graphql/src/schema/evaluation.ts
+++ packages/graphql/src/schema/evaluation.ts
@@ -403,7 +403,6 @@ export const ContentElementResults = ContentElementResultsRef.implement({
   }),
 })
 
-// TODO: add selection instance results - potentially update the function computeSelectionEvaluation - required for activity evaluation
 // ----- ELEMENT EVALUATION INTERFACE -----
 export const ElementInstanceEvaluation = builder.unionType(
   'ElementInstanceEvaluation',
