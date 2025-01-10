d8eecbf77a1bb9119f058955bc8de1b23053c7a0
sjschlapbach <julius.schlapbach@gmx.net>
2025-01-10 18:06:47 +0100

    chore(packages/prisma): minor updates to database schema

--- packages/prisma/src/prisma/migrations/20250110170418_object_sharing_permissions/migration.sql
+++ packages/prisma/src/prisma/migrations/20250110170418_object_sharing_permissions/migration.sql
@@ -41,10 +41,10 @@ CREATE TABLE "UserGroup" (
 );
 
 -- CreateTable
-CREATE TABLE "Permissions" (
+CREATE TABLE "Permission" (
     "id" SERIAL NOT NULL,
     "accessLevel" "AccessLevel" NOT NULL,
-    "status" "PermissionStatus" NOT NULL,
+    "permissionStatus" "PermissionStatus" NOT NULL,
     "userId" UUID,
     "userGroupId" INTEGER,
     "objectOwnerId" UUID,
@@ -58,7 +58,7 @@ CREATE TABLE "Permissions" (
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "Permissions_pkey" PRIMARY KEY ("id"),
+    CONSTRAINT "Permission_pkey" PRIMARY KEY ("id"),
     CONSTRAINT "UserOrGroupRequired" CHECK ((("userId" IS NOT NULL) AND ("userGroupId" IS NULL)) OR (("userId" IS NULL) AND ("userGroupId" IS NOT NULL))),
     CONSTRAINT "ObjectRequired" CHECK (("answerCollectionId" IS NOT NULL) OR ("elementId" IS NOT NULL) OR ("courseId" IS NOT NULL) OR ("liveQuizId" IS NOT NULL) OR ("practiceQuizId" IS NOT NULL) OR ("microLearningId" IS NOT NULL) OR ("groupActivityId" IS NOT NULL))
 );
@@ -75,46 +75,46 @@ CREATE TABLE "_UserGroupMembers" (
 CREATE UNIQUE INDEX "UserGroup_ownerId_name_key" ON "UserGroup"("ownerId", "name");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Permissions_answerCollectionId_userId_key" ON "Permissions"("answerCollectionId", "userId");
+CREATE UNIQUE INDEX "Permission_answerCollectionId_userId_key" ON "Permission"("answerCollectionId", "userId");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Permissions_elementId_userId_key" ON "Permissions"("elementId", "userId");
+CREATE UNIQUE INDEX "Permission_elementId_userId_key" ON "Permission"("elementId", "userId");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Permissions_courseId_userId_key" ON "Permissions"("courseId", "userId");
+CREATE UNIQUE INDEX "Permission_courseId_userId_key" ON "Permission"("courseId", "userId");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Permissions_liveQuizId_userId_key" ON "Permissions"("liveQuizId", "userId");
+CREATE UNIQUE INDEX "Permission_liveQuizId_userId_key" ON "Permission"("liveQuizId", "userId");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Permissions_practiceQuizId_userId_key" ON "Permissions"("practiceQuizId", "userId");
+CREATE UNIQUE INDEX "Permission_practiceQuizId_userId_key" ON "Permission"("practiceQuizId", "userId");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Permissions_microLearningId_userId_key" ON "Permissions"("microLearningId", "userId");
+CREATE UNIQUE INDEX "Permission_microLearningId_userId_key" ON "Permission"("microLearningId", "userId");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Permissions_groupActivityId_userId_key" ON "Permissions"("groupActivityId", "userId");
+CREATE UNIQUE INDEX "Permission_groupActivityId_userId_key" ON "Permission"("groupActivityId", "userId");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Permissions_answerCollectionId_userGroupId_key" ON "Permissions"("answerCollectionId", "userGroupId");
+CREATE UNIQUE INDEX "Permission_answerCollectionId_userGroupId_key" ON "Permission"("answerCollectionId", "userGroupId");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Permissions_elementId_userGroupId_key" ON "Permissions"("elementId", "userGroupId");
+CREATE UNIQUE INDEX "Permission_elementId_userGroupId_key" ON "Permission"("elementId", "userGroupId");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Permissions_courseId_userGroupId_key" ON "Permissions"("courseId", "userGroupId");
+CREATE UNIQUE INDEX "Permission_courseId_userGroupId_key" ON "Permission"("courseId", "userGroupId");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Permissions_liveQuizId_userGroupId_key" ON "Permissions"("liveQuizId", "userGroupId");
+CREATE UNIQUE INDEX "Permission_liveQuizId_userGroupId_key" ON "Permission"("liveQuizId", "userGroupId");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Permissions_practiceQuizId_userGroupId_key" ON "Permissions"("practiceQuizId", "userGroupId");
+CREATE UNIQUE INDEX "Permission_practiceQuizId_userGroupId_key" ON "Permission"("practiceQuizId", "userGroupId");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Permissions_microLearningId_userGroupId_key" ON "Permissions"("microLearningId", "userGroupId");
+CREATE UNIQUE INDEX "Permission_microLearningId_userGroupId_key" ON "Permission"("microLearningId", "userGroupId");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Permissions_groupActivityId_userGroupId_key" ON "Permissions"("groupActivityId", "userGroupId");
+CREATE UNIQUE INDEX "Permission_groupActivityId_userGroupId_key" ON "Permission"("groupActivityId", "userGroupId");
 
 -- CreateIndex
 CREATE INDEX "_UserGroupMembers_B_index" ON "_UserGroupMembers"("B");
@@ -123,34 +123,34 @@ CREATE INDEX "_UserGroupMembers_B_index" ON "_UserGroupMembers"("B");
 ALTER TABLE "UserGroup" ADD CONSTRAINT "UserGroup_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Permissions" ADD CONSTRAINT "Permissions_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "Permission" ADD CONSTRAINT "Permission_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Permissions" ADD CONSTRAINT "Permissions_userGroupId_fkey" FOREIGN KEY ("userGroupId") REFERENCES "UserGroup"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "Permission" ADD CONSTRAINT "Permission_userGroupId_fkey" FOREIGN KEY ("userGroupId") REFERENCES "UserGroup"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Permissions" ADD CONSTRAINT "Permissions_objectOwnerId_fkey" FOREIGN KEY ("objectOwnerId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "Permission" ADD CONSTRAINT "Permission_objectOwnerId_fkey" FOREIGN KEY ("objectOwnerId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Permissions" ADD CONSTRAINT "Permissions_answerCollectionId_fkey" FOREIGN KEY ("answerCollectionId") REFERENCES "AnswerCollection"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "Permission" ADD CONSTRAINT "Permission_answerCollectionId_fkey" FOREIGN KEY ("answerCollectionId") REFERENCES "AnswerCollection"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Permissions" ADD CONSTRAINT "Permissions_elementId_fkey" FOREIGN KEY ("elementId") REFERENCES "Element"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "Permission" ADD CONSTRAINT "Permission_elementId_fkey" FOREIGN KEY ("elementId") REFERENCES "Element"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Permissions" ADD CONSTRAINT "Permissions_courseId_fkey" FOREIGN KEY ("courseId") REFERENCES "Course"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "Permission" ADD CONSTRAINT "Permission_courseId_fkey" FOREIGN KEY ("courseId") REFERENCES "Course"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Permissions" ADD CONSTRAINT "Permissions_liveQuizId_fkey" FOREIGN KEY ("liveQuizId") REFERENCES "LiveQuiz"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "Permission" ADD CONSTRAINT "Permission_liveQuizId_fkey" FOREIGN KEY ("liveQuizId") REFERENCES "LiveQuiz"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Permissions" ADD CONSTRAINT "Permissions_practiceQuizId_fkey" FOREIGN KEY ("practiceQuizId") REFERENCES "PracticeQuiz"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "Permission" ADD CONSTRAINT "Permission_practiceQuizId_fkey" FOREIGN KEY ("practiceQuizId") REFERENCES "PracticeQuiz"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Permissions" ADD CONSTRAINT "Permissions_microLearningId_fkey" FOREIGN KEY ("microLearningId") REFERENCES "MicroLearning"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "Permission" ADD CONSTRAINT "Permission_microLearningId_fkey" FOREIGN KEY ("microLearningId") REFERENCES "MicroLearning"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Permissions" ADD CONSTRAINT "Permissions_groupActivityId_fkey" FOREIGN KEY ("groupActivityId") REFERENCES "GroupActivity"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "Permission" ADD CONSTRAINT "Permission_groupActivityId_fkey" FOREIGN KEY ("groupActivityId") REFERENCES "GroupActivity"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "_UserGroupMembers" ADD CONSTRAINT "_UserGroupMembers_A_fkey" FOREIGN KEY ("A") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
