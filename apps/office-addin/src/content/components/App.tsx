dd258586a37a5ed4cc4d3a7b4dd0f6797319e98c
sjschlapbach <julius.schlapbach@gmx.net>
2025-01-15 10:45:29 +0100

    chore: minor content update

--- apps/office-addin/src/content/components/App.tsx
+++ apps/office-addin/src/content/components/App.tsx
@@ -56,7 +56,7 @@ export default function App({ isOfficeInitialized, newlyInserted }: AppProps) {
             <ol className="list-inside list-decimal">
               <li>Go to https://manage.klicker.uzh.ch/quizzes</li>
               <li>
-                For the quiz you want to embed, open the &ldquo;Embed
+                For the live quiz you want to embed, open the &ldquo;Embed
                 Evaluation&rdquo; dialog
               </li>
               <li>
