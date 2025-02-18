dc373494f5f6d24d383b31903ed8b780284dd693
sjschlapbach <julius.schlapbach@gmx.net>
2025-02-18 22:23:48 +0100

    fix(apps/frontend-manage): disable animation on case study evaluation scatter plots

--- apps/frontend-manage/src/components/evaluation/elements/CSTwoDimScatterPlot.tsx
+++ apps/frontend-manage/src/components/evaluation/elements/CSTwoDimScatterPlot.tsx
@@ -103,6 +103,7 @@ function CSTwoDimScatterPlot({
               shape={(props: any) => (
                 <circle cx={props.cx} cy={props.cy} r={6} fill={props.fill} />
               )}
+              isAnimationActive={false}
             >
               <LabelList
                 dataKey="itemLabel"
