a52b138fe9e17f1d2ca58816edcf2480e64982aa
sjschlapbach <julius.schlapbach@gmx.net>
2025-04-03 11:45:40 +0200

    enhance: allow lecturer to access activity preview for past microlearnings

--- apps/frontend-pwa/src/components/practiceQuiz/ElementStack.tsx
+++ apps/frontend-pwa/src/components/practiceQuiz/ElementStack.tsx
@@ -420,8 +420,11 @@ function ElementStack({
           primary
           loading={submittingResponse}
           disabled={
-            activityExpired ||
-            Object.values(studentResponse).some((response) => !response.valid)
+            !previewOnly &&
+            (activityExpired ||
+              Object.values(studentResponse).some(
+                (response) => !response.valid
+              ))
           }
           className={{ root: 'float-right mt-4' }}
           onClick={async () => {
