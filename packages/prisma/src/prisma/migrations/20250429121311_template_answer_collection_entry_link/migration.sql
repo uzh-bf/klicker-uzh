2803769914e0937ca1c0b53c7716f7a9c26c2cf7
sjschlapbach <julius.schlapbach@gmx.net>
2025-04-29 14:51:46 +0200

    wip: add link between activity template and answer collection entry in database

---
+++ packages/prisma/src/prisma/migrations/20250429121311_template_answer_collection_entry_link/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "_TemplateAnswerCollectionUsedItems" (
+    "A" UUID NOT NULL,
+    "B" INTEGER NOT NULL,
+
+    CONSTRAINT "_TemplateAnswerCollectionUsedItems_AB_pkey" PRIMARY KEY ("A","B")
+);
+
+-- CreateIndex
+CREATE INDEX "_TemplateAnswerCollectionUsedItems_B_index" ON "_TemplateAnswerCollectionUsedItems"("B");
+
+-- AddForeignKey
+ALTER TABLE "_TemplateAnswerCollectionUsedItems" ADD CONSTRAINT "_TemplateAnswerCollectionUsedItems_A_fkey" FOREIGN KEY ("A") REFERENCES "ActivityTemplate"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_TemplateAnswerCollectionUsedItems" ADD CONSTRAINT "_TemplateAnswerCollectionUsedItems_B_fkey" FOREIGN KEY ("B") REFERENCES "AnswerCollectionEntry"("id") ON DELETE CASCADE ON UPDATE CASCADE;
