ebdfe0754301d6b59ed203e58feb63ee2e35c890
sjschlapbach <julius.schlapbach@gmx.net>
2025-06-25 22:27:47 +0200

    enhance: add hatchet setup locally with full functionality to trigger tasks

--- _down_wsl.sh
+++ _down_wsl.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-docker compose down postgres redis_exec redis_cache reverse_proxy_wsl
+docker compose down \
+    postgres \
+    hatchet_postgres \
+    hatchet_rabbitmq \
+    hatchet_migration \
+    hatchet_setup_config \
+    hatchet_engine \
+    hatchet_dashboard \
+    redis_exec \
+    redis_cache \
+    reverse_proxy_wsl
