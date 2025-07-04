a9cf482969bccdf499a96aa368233df3a708107a
Roland Schläfli <rolandschlaefli@gmail.com>
2025-07-04 23:00:36 +0200

    achievement and points for business game

--- packages/prisma/src/data/seedSummerSchool.ts
+++ packages/prisma/src/data/seedSummerSchool.ts
@@ -9,14 +9,18 @@ async function seedAchievements(prisma: Prisma.PrismaClient) {
     where: { id: SWISS_QUIZ_ACHIEVEMENT_ID },
     create: {
       id: SWISS_QUIZ_ACHIEVEMENT_ID,
-      name: 'Swiss Quiz',
-      nameDE: 'Schweiz Quiz',
-      nameEN: 'Swiss Quiz',
+      name: 'Swiss Whiz',
+      nameDE: 'Swiss Whiz',
+      nameEN: 'Swiss Whiz',
       icon: 'https://sos-ch-dk-2.exo.io/klicker-prod/achievements/swiss_quiz.svg',
       type: Prisma.AchievementType.PARTICIPANT,
       scope: Prisma.AchievementScope.GLOBAL,
     },
-    update: {},
+    update: {
+      name: 'Swiss Whiz',
+      nameDE: 'Swiss Whiz',
+      nameEN: 'Swiss Whiz',
+    },
   })
 
   // seed escape room achievement
@@ -25,10 +29,29 @@ async function seedAchievements(prisma: Prisma.PrismaClient) {
     where: { id: ESCAPE_ROOM_ACHIEVEMENT_ID },
     create: {
       id: ESCAPE_ROOM_ACHIEVEMENT_ID,
-      name: 'Escape Room',
-      nameDE: 'Escape Room',
-      nameEN: 'Escape Room',
+      name: 'Escape Artist',
+      nameDE: 'Escape Artist',
+      nameEN: 'Escape Artist',
       icon: 'https://sos-ch-dk-2.exo.io/klicker-prod/achievements/uzh_escape_room.svg',
+      type: Prisma.AchievementType.PARTICIPANT,
+      scope: Prisma.AchievementScope.GLOBAL,
+    },
+    update: {
+      name: 'Escape Artist',
+      nameDE: 'Escape Artist',
+      nameEN: 'Escape Artist',
+    },
+  })
+
+  const BUSINESS_GAME_ACHIEVEMENT_ID = 20
+  const businessGameAchievement = await prisma.achievement.upsert({
+    where: { id: BUSINESS_GAME_ACHIEVEMENT_ID },
+    create: {
+      id: BUSINESS_GAME_ACHIEVEMENT_ID,
+      name: 'ChocoStrategist',
+      nameDE: 'ChocoStrategist',
+      nameEN: 'ChocoStrategist',
+      icon: 'https://sos-ch-dk-2.exo.io/klicker-prod/achievements/uzh_business_game.svg',
       type: Prisma.AchievementType.PARTICIPANT,
       scope: Prisma.AchievementScope.GLOBAL,
     },
@@ -36,76 +59,133 @@ async function seedAchievements(prisma: Prisma.PrismaClient) {
   })
 
   // achievement instances for swiss quiz
-  const swissQuizWinners = ['2pFxA1hAEm', 'Nikolina']
+  // ! AWARDED
+  // const swissQuizWinners = ['2pFxA1hAEm', 'Nikolina']
 
   // achievement instances for escape room
-  const escapeRoomWinners = ['piper', 'Belly', 'MiMi77', 'Thiago']
+  // ! AWARDED
+  // const escapeRoomWinners = ['piper', 'Belly', 'MiMi77', 'Thiago']
 
   // points collected during escape room
-  const escapeRoomPoints = {
-    '2pFxA1hAEm': 163,
-    Nikolina: 299,
-    'Sprindt&Lüngli': 100,
-    arianna: 129,
-    vihan: 367,
-    Justinmoser: 333,
-    piper: 400,
-    Yilin: 367,
-    TaylanS: 100,
-    Rheinhart: 367,
-    Devansh: 197,
-    '14860023': 163,
-    Financesis: 299,
-    Ik73oqe1: 231,
-    Helen1102: 299,
-    DobPdPv7Xf: 197,
-    Liangwen: 265,
-    OliverCourtness: 299,
-    Svanen: 129,
-    CookieMonster: 265,
-    chenjiajia1412: 231,
-    agash17: 129,
-    Joeyss: 333,
-    Boban25: 367,
-    GianLucas: 333,
-    amelievastiau: 265,
-    Karan: 265,
-    buqichen: 129,
-    Kavin: 197,
-    Yanchuan: 231,
-    Gloria: 333,
-    Ningyi: 100,
-    Belly: 400,
-    SorinaMatthey: 231,
-    MiMi77: 400,
-    Thiago: 400,
-    zoeyy: 163,
-    suKJH8dWOs: 163,
-  }
+  // ! AWARDED
+  // const escapeRoomPoints = {
+  //   '2pFxA1hAEm': 163,
+  //   Nikolina: 299,
+  //   'Sprindt&Lüngli': 100,
+  //   arianna: 129,
+  //   vihan: 367,
+  //   Justinmoser: 333,
+  //   piper: 400,
+  //   Yilin: 367,
+  //   TaylanS: 100,
+  //   Rheinhart: 367,
+  //   Devansh: 197,
+  //   '14860023': 163,
+  //   Financesis: 299,
+  //   Ik73oqe1: 231,
+  //   Helen1102: 299,
+  //   DobPdPv7Xf: 197,
+  //   Liangwen: 265,
+  //   OliverCourtness: 299,
+  //   Svanen: 129,
+  //   CookieMonster: 265,
+  //   chenjiajia1412: 231,
+  //   agash17: 129,
+  //   Joeyss: 333,
+  //   Boban25: 367,
+  //   GianLucas: 333,
+  //   amelievastiau: 265,
+  //   Karan: 265,
+  //   buqichen: 129,
+  //   Kavin: 197,
+  //   Yanchuan: 231,
+  //   Gloria: 333,
+  //   Ningyi: 100,
+  //   Belly: 400,
+  //   SorinaMatthey: 231,
+  //   MiMi77: 400,
+  //   Thiago: 400,
+  //   zoeyy: 163,
+  //   suKJH8dWOs: 163,
+  // }
 
-  // ! Grant live quiz achievements
+  // achievement instances for business game
+  // ! AWARDED
+  // const businessGameWinners = [
+  //   'Ik73oqe1',
+  //   'chenjiajia1412',
+  //   'Yanchuan',
+  //   'SorinaMatthey',
+  // ]
+
+  // points collected during business game
+  // ! AWARDED
+  // const businessGamePoints = {
+  //   Ik73oqe1: 1000,
+  //   vihan: 800,
+  //   chenjiajia1412: 1000,
+  //   Yilin: 800,
+  //   Rheinhart: 800,
+  //   Yanchuan: 1000,
+  //   piper: 700,
+  //   SorinaMatthey: 1000,
+  //   Boban25: 800,
+  //   Nikolina: 600,
+  //   arianna: 900,
+  //   Justinmoser: 700,
+  //   '2pFxA1hAEm': 700,
+  //   Belly: 700,
+  //   Svanen: 900,
+  //   Joeyss: 700,
+  //   agash17: 900,
+  //   MiMi77: 700,
+  //   GianLucas: 700,
+  //   buqichen: 900,
+  //   Thiago: 700,
+  //   Gloria: 700,
+  //   Devansh: 700,
+  //   Financesis: 600,
+  //   Helen1102: 600,
+  //   DobPdPv7Xf: 700,
+  //   OliverCourtness: 600,
+  //   '14860023': 700,
+  //   Liangwen: 600,
+  //   CookieMonster: 600,
+  //   Kavin: 700,
+  //   amelievastiau: 600,
+  //   Karan: 600,
+  //   'Sprindt&Lüngli': 600,
+  //   TaylanS: 600,
+  //   zoeyy: 700,
+  //   suKJH8dWOs: 700,
+  //   Ningyi: 600,
+  // }
+
+  // ! Grant achievements
+  // const ACHIEVEMENT_USERNAMES = businessGameWinners
+  // const ACHIEVEMENT_ID = businessGameAchievement.id
   // await prisma.$transaction(async (prisma) => {
   //   const participants = await prisma.participant.findMany({
-  //     where: { username: { in: swissQuizWinners } },
+  //     where: { username: { in: ACHIEVEMENT_USERNAMES } },
   //   })
 
-  //   if (participants.length !== swissQuizWinners.length) {
+  //   if (participants.length !== ACHIEVEMENT_USERNAMES.length) {
   //     throw new Error('Not all participants found')
   //   }
 
-  //   // upsert achievement instances for swiss quiz
+  //   // upsert achievement instances
   //   for (const participant of participants) {
   //     await prisma.participantAchievementInstance.upsert({
   //       where: {
   //         participantId_achievementId: {
   //           participantId: participant.id,
-  //           achievementId: SWISS_QUIZ_ACHIEVEMENT_ID,
+  //           achievementId: ACHIEVEMENT_ID,
   //         },
   //       },
   //       create: {
   //         participantId: participant.id,
-  //         achievementId: SWISS_QUIZ_ACHIEVEMENT_ID,
-  //         achievedAt: new Date('2023-07-01T12:00:00Z'),
+  //         achievementId: ACHIEVEMENT_ID,
+  //         achievedAt: new Date(),
   //         achievedCount: 1,
   //       },
   //       update: {},
@@ -113,75 +193,46 @@ async function seedAchievements(prisma: Prisma.PrismaClient) {
   //   }
   // })
 
-  // ! Grant escape room achievements
-  await prisma.$transaction(async (prisma) => {
-    const participants = await prisma.participant.findMany({
-      where: { username: { in: escapeRoomWinners } },
-    })
+  // ! Increment leaderboard entries with points
+  // const POINTS = businessGamePoints
+  // await prisma.$transaction(async (prisma) => {
+  //   const participants = await prisma.participant.findMany({
+  //     where: { username: { in: Object.keys(POINTS) } },
+  //   })
 
-    if (participants.length !== escapeRoomWinners.length) {
-      throw new Error('Not all participants found')
-    }
+  //   if (participants.length !== Object.keys(POINTS).length) {
+  //     throw new Error('Not all participants found')
+  //   }
 
-    // upsert achievement instances for escape room
-    for (const participant of participants) {
-      await prisma.participantAchievementInstance.upsert({
-        where: {
-          participantId_achievementId: {
-            participantId: participant.id,
-            achievementId: ESCAPE_ROOM_ACHIEVEMENT_ID,
-          },
-        },
-        create: {
-          participantId: participant.id,
-          achievementId: ESCAPE_ROOM_ACHIEVEMENT_ID,
-          achievedAt: new Date('2023-07-03T12:00:00Z'),
-          achievedCount: 1,
-        },
-        update: {},
-      })
-    }
-  })
+  //   // update leaderboard entries for all participants
+  //   for (const [username, points] of Object.entries(POINTS)) {
+  //     const participant = participants.find((p) => p.username === username)
+  //     if (!participant) {
+  //       throw new Error(`Participant not found: ${username}`)
+  //     }
 
-  // ! Increment leaderboard entries with escape room points
-  await prisma.$transaction(async (prisma) => {
-    const participants = await prisma.participant.findMany({
-      where: { username: { in: Object.keys(escapeRoomPoints) } },
-    })
-
-    if (participants.length !== Object.keys(escapeRoomPoints).length) {
-      throw new Error('Not all participants found')
-    }
-
-    // update leaderboard entries for all participants
-    for (const [username, points] of Object.entries(escapeRoomPoints)) {
-      const participant = participants.find((p) => p.username === username)
-      if (!participant) {
-        throw new Error(`Participant not found: ${username}`)
-      }
-
-      await prisma.leaderboardEntry.upsert({
-        where: {
-          type_participantId_courseId: {
-            type: Prisma.LeaderboardType.COURSE,
-            participantId: participant.id,
-            courseId: COURSE_ID,
-          },
-        },
-        create: {
-          type: Prisma.LeaderboardType.COURSE,
-          participantId: participant.id,
-          courseId: COURSE_ID,
-          score: points,
-        },
-        update: {
-          score: {
-            increment: points,
-          },
-        },
-      })
-    }
-  })
+  //     await prisma.leaderboardEntry.upsert({
+  //       where: {
+  //         type_participantId_courseId: {
+  //           type: Prisma.LeaderboardType.COURSE,
+  //           participantId: participant.id,
+  //           courseId: COURSE_ID,
+  //         },
+  //       },
+  //       create: {
+  //         type: Prisma.LeaderboardType.COURSE,
+  //         participantId: participant.id,
+  //         courseId: COURSE_ID,
+  //         score: points,
+  //       },
+  //       update: {
+  //         score: {
+  //           increment: points,
+  //         },
+  //       },
+  //     })
+  //   }
+  // })
 }
 
 const prismaClient = new Prisma.PrismaClient()
