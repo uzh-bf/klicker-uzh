8551fe61da2217208407db1636c0f1feec1828e5
sjschlapbach <julius.schlapbach@gmx.net>
2025-07-12 17:25:22 +0200

    chore: make sure all classes for font size exist in frontend-manage

--- apps/frontend-manage/src/components/evaluation/elements/QuestionCollapsible.tsx
+++ apps/frontend-manage/src/components/evaluation/elements/QuestionCollapsible.tsx
@@ -18,7 +18,7 @@ function QuestionCollapsible({
         <Markdown
           className={{
             root: twMerge(
-              'prose-p:m-0 prose-lg prose-img:m-0 content-between py-2 leading-8',
+              'prose-p:m-0 prose-2xl prose-img:m-0 content-between py-2 leading-8',
               proseSize
             ),
           }}
