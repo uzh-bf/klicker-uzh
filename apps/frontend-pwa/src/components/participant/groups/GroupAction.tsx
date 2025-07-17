101d9e2dd9b6b2b3f3bca83280648f3408bfabb2
Julius Schlapbach <80708107+sjschlapbach@users.noreply.github.com>
2025-07-17 10:48:41 +0200

    chore(apps/docs): update documentation on element types, element management, and course management (#4782)

--- apps/frontend-pwa/src/components/participant/groups/GroupAction.tsx
+++ apps/frontend-pwa/src/components/participant/groups/GroupAction.tsx
@@ -57,8 +57,8 @@ function GroupAction({
         buttonMode && 'mb-0 md:mb-0'
       )}
     >
-      <H2 className={{ root: 'm-0!' }}>{title}</H2>
-      <FontAwesomeIcon icon={icon} className="h-14 w-14 md:h-24 md:w-24" />
+      <H2 className={{ root: 'mb-2' }}>{title}</H2>
+      <FontAwesomeIcon icon={icon} size="5x" className="mb-2" />
     </div>
   )
 
