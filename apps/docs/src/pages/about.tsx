665214c987f42dcaf9a546ff4ebbf5a3e584c83c
sjschlapbach <julius.schlapbach@gmx.net>
2025-07-28 23:41:43 +0200

    enhance(apps/docs): update documentation of live quiz

--- apps/docs/src/pages/about.tsx
+++ apps/docs/src/pages/about.tsx
@@ -29,7 +29,7 @@ const PEOPLE: {
   },
   {
     name: 'Patrick Aldover',
-    role: 'Project Developer',
+    role: 'Project Maintainer',
     info: 'https://www.df.uzh.ch/en/people/staff/teaching-center/patrick-aldover.html',
     github: 'https://github.com/jabbadizzleCode',
   },
