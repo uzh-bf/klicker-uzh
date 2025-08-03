42fba74f575e7eed40e81988d313e5ae58f5effd
sjschlapbach <julius.schlapbach@gmx.net>
2025-08-03 13:42:18 +0200

    wip: add script to initialize course language with user locale

---
+++ packages/graphql/src/scripts/2025-08-03_set_course_language.ts
@@ -0,0 +1,32 @@
+import { PrismaClient } from '@klicker-uzh/prisma'
+
+async function run() {
+  const DEBUG = false
+  const prisma = new PrismaClient()
+
+  // get all courses and the associated users
+  const courses = await prisma.course.findMany({
+    include: { owner: { select: { locale: true } } },
+  })
+
+  // set the course language to the owner's locale
+  let counter = 0
+  for (const course of courses) {
+    console.log(
+      `Processing course ${++counter}/${courses.length}: ${course.name} (${course.id})`
+    )
+
+    if (DEBUG) {
+      console.log(
+        `Setting course language for course: ${course.name} (${course.id}) to ${course.owner.locale}`
+      )
+    }
+
+    await prisma.course.update({
+      where: { id: course.id },
+      data: { language: course.owner.locale },
+    })
+  }
+}
+
+await run()
--- util/_restore-db-dev.sh
+++ util/_restore-db-dev.sh
@@ -3,4 +3,4 @@
 # Get the directory where the script is located
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-PGPASSWORD="klicker" pg_restore --host="localhost" --port=5432 --user="klicker" --dbname="klicker-prod" --no-owner --format="t" "${SCRIPT_DIR}/dump.tar"
+PGPASSWORD="klicker" pg_restore --host="localhost" --port=5432 --user="klicker-prod" --dbname="klicker-prod" --no-owner --format="t" "${SCRIPT_DIR}/dump.tar"
