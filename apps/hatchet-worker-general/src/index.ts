26a926b9f7334e7e3ad4362aafad9064d1c70f53
Roland Schlaefli <rolandschlaefli@gmail.com>
2025-08-15 23:14:33 +0200

    add stub package and import in hatchet worker and backend

--- apps/hatchet-worker-general/src/index.ts
+++ apps/hatchet-worker-general/src/index.ts
@@ -1,10 +1,13 @@
+import { prepareHatchetTasks } from '@klicker-uzh/hatchet-tasks'
 import { hatchet } from './hatchet-client.js'
 
 async function main() {
+  const workflows = prepareHatchetTasks(hatchet)
+
   const worker = await hatchet.worker('hatchet-worker-general', {
-    // TODO: figure out some way to share tasks with backend (e.g., shared tasks package)
+    // TODO: figure out some way to share tasks with backend (e.g., try shared tasks package)
     // basic structure according to https://github.com/hatchet-dev/hatchet-typescript-quickstart/tree/main/monorepo
-    workflows: [],
+    workflows,
   })
 
   await worker.start()
