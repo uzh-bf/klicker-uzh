08506649463ae67064eeb345425b5c37a68f2fdc
Roland Schlaefli <rolandschlaefli@gmail.com>
2025-08-17 17:42:59 +0200

    fix: token twice

--- apps/hatchet-worker-general/src/hatchet-client.ts
+++ apps/hatchet-worker-general/src/hatchet-client.ts
@@ -2,7 +2,6 @@ import { HatchetClient } from '@hatchet-dev/typescript-sdk'
 
 export const hatchet = HatchetClient.init({
   token: process.env.HATCHET_CLIENT_TOKEN,
-  token: process.env.HATCHET_CLIENT_TOKEN,
   api_url: process.env.HATCHET_API_URL,
   tenant_id: process.env.HATCHET_TENANT_ID,
   host_port: process.env.HATCHET_HOST_PORT,
