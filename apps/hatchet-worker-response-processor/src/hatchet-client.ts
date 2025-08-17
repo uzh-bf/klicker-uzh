d1000651243b6110eb33815a56f29f37a14e98cc
Roland Schlaefli <rolandschlaefli@gmail.com>
2025-08-17 17:41:39 +0200

    add more hatchet client config options to enable in-cluster communication

--- apps/hatchet-worker-response-processor/src/hatchet-client.ts
+++ apps/hatchet-worker-response-processor/src/hatchet-client.ts
@@ -2,5 +2,8 @@ import { HatchetClient } from '@hatchet-dev/typescript-sdk'
 
 export const hatchet = HatchetClient.init({
   token: process.env.HATCHET_CLIENT_TOKEN,
+  api_url: process.env.HATCHET_API_URL,
+  tenant_id: process.env.HATCHET_TENANT_ID,
+  host_port: process.env.HATCHET_HOST_PORT,
   log_level: 'DEBUG',
 })
