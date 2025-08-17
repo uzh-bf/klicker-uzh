3be4dc10d2e25155bedfe471719052ceb802a579
sjschlapbach <julius.schlapbach@gmx.net>
2025-08-17 16:13:48 +0200

    wip: add documentation for element and activity batch operations

--- apps/docs/sidebars.js
+++ apps/docs/sidebars.js
@@ -10,6 +10,7 @@ module.exports = {
       'tutorials/course_management',
       'tutorials/element_stacks',
       'tutorials/answer_collections',
+      'tutorials/element_batch_operations',
     ],
     'Lecturer - Activities': [
       'tutorials/live_quiz',
@@ -19,6 +20,7 @@ module.exports = {
       'tutorials/participant_group_management',
       'tutorials/group_activity',
       // 'tutorials/activity_templates',
+      'tutorials/activity_batch_operations',
       'tutorials/element_updates_activities',
     ],
     'Lecturer - Sharing': [
