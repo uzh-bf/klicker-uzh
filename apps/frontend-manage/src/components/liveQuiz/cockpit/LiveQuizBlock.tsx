fc443672738e53556c95dacdfddb09e1d18ae4ac
sjschlapbach <julius.schlapbach@gmx.net>
2025-08-25 23:34:27 +0200

    enhance: show countdown on evaluation results cover for active live quiz blocks

--- apps/frontend-manage/src/components/liveQuiz/cockpit/LiveQuizBlock.tsx
+++ apps/frontend-manage/src/components/liveQuiz/cockpit/LiveQuizBlock.tsx
@@ -2,7 +2,6 @@ import {
   faCalendar,
   faCheck,
   faExternalLink,
-  faSave,
   faSync,
   faUserGroup,
 } from '@fortawesome/free-solid-svg-icons'
@@ -12,12 +11,11 @@ import {
   ElementBlock as ElementBlockType,
   ElementInstance,
 } from '@klicker-uzh/graphql/dist/ops'
-import { CycleCountdown } from '@uzh-bf/design-system'
-import dayjs from 'dayjs'
 import { useTranslations } from 'next-intl'
 import Link from 'next/link'
-import React, { useMemo, useState } from 'react'
+import React, { useState } from 'react'
 import { twMerge } from 'tailwind-merge'
+import LiveQuizCountdown from './LiveQuizCountdown'
 
 type QuizTimelineInstance = Omit<ElementInstance, 'elementData'> & {
   elementData: { elementId: number | null; name: string }
@@ -31,8 +29,6 @@ interface LiveQuizBlockProps {
   className?: string
   active: boolean
   block: QuizTimelineBlock
-  inCooldown: boolean
-  setInCooldown: (value: boolean) => void
 }
 
 const ICON_MAP = {
@@ -45,46 +41,9 @@ function LiveQuizBlock({
   className,
   active,
   block,
-  inCooldown,
-  setInCooldown,
 }: LiveQuizBlockProps): React.ReactElement {
   const t = useTranslations()
-  const [endTime, setEndTime] = useState<Date>()
-  const [timerColor, setTimerColor] = useState<string | undefined>(undefined)
-  const [totalDuration, setTotalDuration] = useState<number | undefined | null>(
-    undefined
-  )
-
-  // cooldown duration in seconds
-  const cooldownDuration = 10
-
-  // compute the time until expiration (student-visible time) and
-  // the time until the block is closed (including cooldown)
-  const { expiration: expirationTime, closure: closureTime } = useMemo(() => {
-    if (block.status === ElementBlockStatus.Executed) {
-      setTotalDuration(0)
-      return { expiration: undefined, closure: undefined }
-    }
-    const timeUntilExpiration = block.expiresAt
-      ? dayjs(block.expiresAt).diff(dayjs(), 'second')
-      : block.timeLimit
-    const timeUntilClosure = block.expiresAt
-      ? dayjs(block.expiresAt).diff(dayjs(), 'second') + cooldownDuration
-      : block.timeLimit
-
-    const expirationTime = new Date()
-    expirationTime.setSeconds(
-      expirationTime.getSeconds() + (timeUntilExpiration ?? 0) + 1
-    )
-    const closureTime = new Date()
-    closureTime.setSeconds(
-      closureTime.getSeconds() + (timeUntilClosure ?? 0) + 1
-    )
-    setTotalDuration((timeUntilExpiration ?? 0) + 1)
-    setEndTime(expirationTime)
-
-    return { expiration: expirationTime, closure: closureTime }
-  }, [block.expiresAt, block.status, block.timeLimit])
+  const [inCooldown, setInCooldown] = useState(false)
 
   return (
     <div
@@ -119,33 +78,10 @@ function LiveQuizBlock({
         )}
 
         {block.timeLimit && (
-          <CycleCountdown
-            key={`${block.id}-${endTime}-${totalDuration}`}
-            size="sm"
-            isStatic={
-              !block.expiresAt || block.status === ElementBlockStatus.Executed
-            }
-            color={timerColor}
-            expiresAt={endTime ?? new Date()}
-            strokeWidthRem={0.2}
-            totalDuration={totalDuration ?? 0}
-            terminalPercentage={100}
-            onUpdate={(timeRemaining) => {
-              if (timeRemaining < 1.5) {
-                setTimerColor('#FF4D01')
-                setEndTime(closureTime)
-                setTotalDuration((cooldownDuration ?? 0) + 1)
-                setInCooldown(true)
-              }
-            }}
-            formatter={(value) => {
-              if (inCooldown && value !== 0) {
-                return <FontAwesomeIcon icon={faSave} />
-              } else {
-                return Math.max(value - 1, 0)
-              }
-            }}
-            className={{ countdown: 'font-bold', root: 'ml-2' }}
+          <LiveQuizCountdown
+            block={block}
+            inCooldown={inCooldown}
+            setInCooldown={setInCooldown}
           />
         )}
       </div>
---
+++ apps/frontend-manage/src/components/liveQuiz/cockpit/LiveQuizCountdown.tsx
@@ -0,0 +1,96 @@
+import { faSave } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { ElementBlockStatus } from '@klicker-uzh/graphql/dist/ops'
+import { CycleCountdown } from '@uzh-bf/design-system'
+import dayjs from 'dayjs'
+import { Dispatch, SetStateAction, useEffect, useMemo } from 'react'
+import { twMerge } from 'tailwind-merge'
+import { QuizTimelineBlock } from './LiveQuizBlock'
+
+// fixed cooldown duration (including some buffer for UI updates after block closure)
+const COOLDOWN_DURATION = 12
+
+function LiveQuizCountdown({
+  block,
+  size,
+  inCooldown,
+  setInCooldown,
+  className,
+}: {
+  block: Pick<QuizTimelineBlock, 'id' | 'status' | 'expiresAt' | 'timeLimit'>
+  size?: 'sm' | 'md' | 'lg'
+  inCooldown: boolean
+  setInCooldown: Dispatch<SetStateAction<boolean>>
+  className?: string
+}) {
+  // compute the time until expiration (student-visible time) and
+  // the time until the block is closed (including cooldown)
+  const { endDuration, endTimestamp, cooldown } = useMemo(() => {
+    // if the block is already closed, return early
+    if (block.status === ElementBlockStatus.Executed) {
+      return { endDuration: 0, endTimestamp: new Date(), cooldown: false }
+    }
+
+    // compute the time until the official closure and the time until the actual closure
+    const timeUntilExpiration = block.expiresAt
+      ? dayjs(block.expiresAt).diff(dayjs(), 'second') + 1
+      : block.timeLimit
+
+    const expirationTime = new Date()
+    expirationTime.setSeconds(
+      expirationTime.getSeconds() + (timeUntilExpiration ?? 0)
+    )
+
+    if (!((timeUntilExpiration ?? 0) > 1)) {
+      const timeUntilClosure = block.expiresAt
+        ? dayjs(block.expiresAt).diff(dayjs(), 'second') + COOLDOWN_DURATION
+        : block.timeLimit
+      const closureTime = new Date()
+      closureTime.setSeconds(closureTime.getSeconds() + (timeUntilClosure ?? 0))
+
+      return {
+        endDuration: timeUntilClosure ?? 0,
+        endTimestamp: closureTime,
+        cooldown: true,
+      }
+    }
+
+    return {
+      endDuration: timeUntilExpiration ?? 0,
+      endTimestamp: expirationTime,
+      cooldown: false,
+    }
+  }, [block.expiresAt, block.status, block.timeLimit, inCooldown])
+
+  // as soon as the endTimestamp changes, we should re-evaluate if the block is in cooldown
+  useEffect(() => {
+    if (cooldown && !inCooldown) {
+      setInCooldown(cooldown)
+    }
+  }, [endTimestamp, cooldown])
+
+  return (
+    <CycleCountdown
+      key={`${block.id}-${endTimestamp}-${endDuration}-${String(inCooldown)}`}
+      size={size ?? 'sm'}
+      isStatic={
+        !block.expiresAt || block.status === ElementBlockStatus.Executed
+      }
+      color={inCooldown ? '#FF4D01' : undefined}
+      expiresAt={endTimestamp}
+      totalDuration={endDuration}
+      terminalPercentage={100}
+      onExpire={() => setInCooldown(true)}
+      formatter={(value) => {
+        if (inCooldown && value !== 0) {
+          return <FontAwesomeIcon icon={faSave} />
+        } else {
+          return Math.max(value, 0)
+        }
+      }}
+      className={{ countdown: 'font-bold', root: twMerge('ml-2', className) }}
+    />
+  )
+}
+
+export default LiveQuizCountdown
