b46e31948e0f14243e2d9ea378cdea06b68819dc
Roland Schlaefli <rolandschlaefli@gmail.com>
2025-08-31 17:42:26 +0200

    fix(ci): add cypress version of hatchet token preparation script

---
+++ util/_create_hatchet_token_cypress.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+TOKEN=$(docker compose exec -T hatchet /hatchet-admin token create --config /config --tenant-id 707d0855-80ab-4e1f-a156-f1c4546cbf52 | xargs)
+for d in apps/backend-docker; do sed "s|__HATCHET_CLIENT_TOKEN__|$TOKEN|g" "$d/.env.cypress" > "$d/.env"; done
+for d in apps/hatchet-worker-general apps/hatchet-worker-response-processor apps/response-api; do sed "s|__HATCHET_CLIENT_TOKEN__|$TOKEN|g" "$d/.env.example" > "$d/.env"; done
