220629048d1de05c2492fd8c5c5b05ccb61bd710
sjschlapbach <julius.schlapbach@gmx.net>
2025-09-04 13:18:39 +0200

    chore: add id quizid combination as primary key

--- packages/prisma/src/prisma/schema/migrations/20250904111101_temporary_leaderboard_constraints/migration.sql
+++ packages/prisma/src/prisma/schema/migrations/20250904111101_temporary_leaderboard_constraints/migration.sql
@@ -2,17 +2,14 @@
   Warnings:
 
   - The primary key for the `TemporaryLeaderboardEntry` table will be changed. If it partially fails, the table could be left without primary key constraint.
-  - A unique constraint covering the columns `[id,quizId]` on the table `TemporaryLeaderboardEntry` will be added. If there are existing duplicate values, this will fail.
 
 */
 -- AlterTable
-ALTER TABLE "public"."TemporaryLeaderboardEntry" DROP CONSTRAINT "TemporaryLeaderboardEntry_pkey";
+ALTER TABLE "public"."TemporaryLeaderboardEntry" DROP CONSTRAINT "TemporaryLeaderboardEntry_pkey",
+ADD CONSTRAINT "TemporaryLeaderboardEntry_pkey" PRIMARY KEY ("id", "quizId");
 
 -- CreateIndex
 CREATE INDEX "TemporaryLeaderboardEntry_id_idx" ON "public"."TemporaryLeaderboardEntry"("id");
 
 -- CreateIndex
 CREATE INDEX "TemporaryLeaderboardEntry_quizId_idx" ON "public"."TemporaryLeaderboardEntry"("quizId");
-
--- CreateIndex
-CREATE UNIQUE INDEX "TemporaryLeaderboardEntry_id_quizId_key" ON "public"."TemporaryLeaderboardEntry"("id", "quizId");
