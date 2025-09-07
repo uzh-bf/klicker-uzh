54c467e7d1cca87b67d507de9a45b192189781f9
sjschlapbach <julius.schlapbach@gmx.net>
2025-09-07 21:45:57 +0200

    chore: update restart script

--- deploy/env-prod-v3/_restart.sh
+++ deploy/env-prod-v3/_restart.sh
@@ -6,4 +6,7 @@ kubectl rollout restart -n klicker-v2-prod deployment klicker-v2-prod-klicker-uz
 kubectl rollout restart -n klicker-v2-prod deployment klicker-v2-prod-klicker-uzh-v2-auth
 kubectl rollout restart -n klicker-v2-prod deployment klicker-v2-prod-klicker-uzh-v2-lti
 kubectl rollout restart -n klicker-v2-prod deployment klicker-v2-prod-klicker-uzh-v2-olat-api
+kubectl rollout restart -n klicker-v2-prod deployment klicker-v2-prod-klicker-uzh-v2-hatchet-worker-general
+kubectl rollout restart -n klicker-v2-prod deployment klicker-v2-prod-klicker-uzh-v2-hatchet-worker-response
+kubectl rollout restart -n klicker-v2-prod deployment klicker-v2-prod-klicker-uzh-v2-response-api
 kubectl get pods -n klicker-v2-prod
