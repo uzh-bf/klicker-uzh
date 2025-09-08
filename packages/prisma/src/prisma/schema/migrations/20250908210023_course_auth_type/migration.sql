01941c7851d007a7994e20eaf65ee9a0f8f15d79
Roland Schlaefli <rolandschlaefli@gmail.com>
2025-09-09 00:16:34 +0200

    remove migration warning

--- packages/prisma/src/prisma/schema/migrations/20250908210023_course_auth_type/migration.sql
+++ packages/prisma/src/prisma/schema/migrations/20250908210023_course_auth_type/migration.sql
@@ -1,9 +1,3 @@
-/*
-  Warnings:
-
-  - Added the required column `authType` to the `Course` table without a default value. This is not possible if the table is not empty.
-
-*/
 -- CreateEnum
 CREATE TYPE "public"."CourseAuthType" AS ENUM ('SSO', 'PIN');
 
