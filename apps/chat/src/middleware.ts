70a7a7c4f7778fd3df1379814db75ae429f545b5
TheHummel <jannisalsbach@gmail.com>
2025-09-10 12:22:15 +0200

    add middleware

---
+++ apps/chat/src/middleware.ts
@@ -0,0 +1,38 @@
+// nextjs middleware that redirects to login if no participant_token cookie is set
+import { JWTPayload, jwtVerify } from 'jose'
+import type { NextRequest } from 'next/server'
+import { NextResponse } from 'next/server'
+
+export async function middleware(request: NextRequest) {
+  const participantToken = request.cookies.get('participant_token')?.value
+
+  if (!participantToken) {
+    return NextResponse.redirect('https://pwa.klicker.com', 302)
+  }
+
+  let participantData: JWTPayload
+
+  // verify with jose that the token is valid
+  // if not valid, redirect to pwa.klicker.com
+  // if valid, continue
+  try {
+    const jwtPayload = await jwtVerify(
+      participantToken || '',
+      new TextEncoder().encode(process.env.APP_SECRET || '')
+    )
+    participantData = jwtPayload.payload
+    console.log('Participant data:', participantData)
+  } catch (error) {
+    console.error('Invalid participant token:', error)
+    return NextResponse.redirect(process.env.NEXT_PUBLIC_PWA_URL as string, 302)
+  }
+
+  // TODO: relay participant data to api routes or similar
+
+  return NextResponse.next()
+}
+
+// Paths that should be protected by this middleware
+export const config = {
+  matcher: ['/:path*'],
+}
