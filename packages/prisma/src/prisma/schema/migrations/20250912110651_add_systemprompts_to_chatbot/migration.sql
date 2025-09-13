54423e6f285b8d2c8f973c0e4417a28cbf42feaa
TheHummel <jannisalsbach@gmail.com>
2025-09-13 14:31:27 +0200

    replace mirgations with new one

--- packages/prisma/src/prisma/schema/migrations/20250912110651_add_systemprompts_to_chatbot/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."Chatbot" ADD COLUMN     "systemPrompts" JSONB;
---
+++ packages/prisma/src/prisma/schema/migrations/20250913122149_add_chatbot_related_tables/migration.sql
@@ -0,0 +1,90 @@
+-- AlterTable
+ALTER TABLE "public"."Account" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "public"."Session" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+-- CreateTable
+CREATE TABLE "public"."ChatUsageCredits" (
+    "id" UUID NOT NULL,
+    "total" INTEGER NOT NULL DEFAULT 0,
+    "current" INTEGER NOT NULL DEFAULT 0,
+    "participantId" UUID NOT NULL,
+    "chatbotId" UUID NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "ChatUsageCredits_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."ChatThread" (
+    "id" UUID NOT NULL,
+    "title" TEXT,
+    "participantId" UUID NOT NULL,
+    "chatbotId" UUID NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "ChatThread_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."ChatMessage" (
+    "id" UUID NOT NULL,
+    "threadId" UUID NOT NULL,
+    "parentId" UUID,
+    "role" TEXT NOT NULL,
+    "content" JSONB NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "ChatMessage_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Chatbot" (
+    "id" UUID NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "avatar" TEXT,
+    "systemPrompts" JSONB,
+    "ownerId" UUID NOT NULL,
+    "courseId" UUID NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Chatbot_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ChatUsageCredits_participantId_chatbotId_key" ON "public"."ChatUsageCredits"("participantId", "chatbotId");
+
+-- CreateIndex
+CREATE INDEX "ChatMessage_threadId_idx" ON "public"."ChatMessage"("threadId");
+
+-- CreateIndex
+CREATE INDEX "ChatMessage_parentId_idx" ON "public"."ChatMessage"("parentId");
+
+-- AddForeignKey
+ALTER TABLE "public"."ChatUsageCredits" ADD CONSTRAINT "ChatUsageCredits_participantId_fkey" FOREIGN KEY ("participantId") REFERENCES "public"."Participant"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."ChatUsageCredits" ADD CONSTRAINT "ChatUsageCredits_chatbotId_fkey" FOREIGN KEY ("chatbotId") REFERENCES "public"."Chatbot"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."ChatThread" ADD CONSTRAINT "ChatThread_participantId_fkey" FOREIGN KEY ("participantId") REFERENCES "public"."Participant"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."ChatThread" ADD CONSTRAINT "ChatThread_chatbotId_fkey" FOREIGN KEY ("chatbotId") REFERENCES "public"."Chatbot"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."ChatMessage" ADD CONSTRAINT "ChatMessage_threadId_fkey" FOREIGN KEY ("threadId") REFERENCES "public"."ChatThread"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Chatbot" ADD CONSTRAINT "Chatbot_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "public"."User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Chatbot" ADD CONSTRAINT "Chatbot_courseId_fkey" FOREIGN KEY ("courseId") REFERENCES "public"."Course"("id") ON DELETE CASCADE ON UPDATE CASCADE;
