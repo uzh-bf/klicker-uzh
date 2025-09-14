85d42d29563dd311bb41d0f259766570445dbaf5
sjschlapbach <julius.schlapbach@gmx.net>
2025-09-15 01:06:52 +0200

    chore: update env variable that should be verified with jwt

--- apps/response-api/src/index.ts
+++ apps/response-api/src/index.ts
@@ -236,7 +236,7 @@ async function handleAddAssessmentResponse(
       ? await verifyJWT(
           parsedCookies['next-auth.participant-session-token'],
           process.env.APP_SECRET as string,
-          { issuer: process.env.APP_ORIGIN_AUTH }
+          { issuer: process.env.APP_ORIGIN_API }
         )
       : null
   } catch (err) {
