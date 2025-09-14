4261e851e53a28689b4d3cfa3d15b6ecb9fbf194
Roland Schlaefli <rolandschlaefli@gmail.com>
2025-09-14 18:46:29 +0200

    fix hatchet script

--- util/_create_hatchet_token_cypress.sh
+++ util/_create_hatchet_token_cypress.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-CONTAINER_NAME=$(docker ps -a --filter "ancestor=ghcr.io/hatchet-dev/hatchet/hatchet-lite:latest" --format "{{.Names}}" | head -n1)
+CONTAINER_NAME=$(docker ps -a --filter "ancestor=ghcr.io/hatchet-dev/hatchet/hatchet-lite:v0.73.1" --format "{{.Names}}" | head -n1)
 TOKEN=$(docker exec $CONTAINER_NAME /hatchet-admin token create --config /config --tenant-id 707d0855-80ab-4e1f-a156-f1c4546cbf52 | xargs)
 for d in apps/backend-docker; do sed "s|__HATCHET_CLIENT_TOKEN__|$TOKEN|g" "$d/.env.cypress" > "$d/.env"; done
 for d in apps/hatchet-worker-general apps/hatchet-worker-response-processor apps/response-api; do sed "s|__HATCHET_CLIENT_TOKEN__|$TOKEN|g" "$d/.env.example" > "$d/.env"; done
