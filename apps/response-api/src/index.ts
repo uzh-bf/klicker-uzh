7180b2758207156d94bc21e6ca7e98d1d3842334
sjschlapbach <julius.schlapbach@gmx.net>
2025-09-15 02:13:09 +0200

    chore: fix redis connection in response-api

--- apps/response-api/src/index.ts
+++ apps/response-api/src/index.ts
@@ -10,7 +10,7 @@ const redis = new Redis({
   family: 4,
   host: process.env.REDIS_HOST,
   password: process.env.REDIS_PASS ?? '',
-  port: Number(process.env.REDIS_PORT) ?? 6379,
+  port: Number(process.env.REDIS_PORT ?? 6379),
   tls: process.env.REDIS_TLS ? {} : undefined,
 })
 
