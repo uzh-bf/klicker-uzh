67aedd126e6a98631b3f973d672459f26ab7e743
Roland Schlaefli <rolandschlaefli@gmail.com>
2025-09-22 22:26:00 +0200

    update prisma schema and regenerate migration

--- packages/prisma/src/prisma/schema/migrations/20250922202537_chatbot_disclaimers/migration.sql
+++ packages/prisma/src/prisma/schema/migrations/20250922202537_chatbot_disclaimers/migration.sql
@@ -15,7 +15,7 @@ CREATE TABLE "public"."ChatbotDisclaimer" (
     "introText" TEXT,
     "mediaUrl" TEXT,
     "mediaType" TEXT,
-    "createdById" UUID NOT NULL,
+    "ownerId" UUID NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
@@ -26,4 +26,4 @@ CREATE TABLE "public"."ChatbotDisclaimer" (
 ALTER TABLE "public"."Chatbot" ADD CONSTRAINT "Chatbot_disclaimerId_fkey" FOREIGN KEY ("disclaimerId") REFERENCES "public"."ChatbotDisclaimer"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "public"."ChatbotDisclaimer" ADD CONSTRAINT "ChatbotDisclaimer_createdById_fkey" FOREIGN KEY ("createdById") REFERENCES "public"."User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "public"."ChatbotDisclaimer" ADD CONSTRAINT "ChatbotDisclaimer_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "public"."User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
