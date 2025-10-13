3425054b00ca49213c110117715a8dc267dd9304
Patrick Louis Aldover <patrick.aldover@df.uzh.ch>
2025-10-13 10:01:37 +0200

    chore(packages/shared-components): handle undefined input immediately when calling formatText

--- packages/shared-components/src/utils/slateMdConversion.ts
+++ packages/shared-components/src/utils/slateMdConversion.ts
@@ -187,12 +187,12 @@ export const convertToSlate = (mdObj?: string | null) => {
 }
 
 const formatText = (input: any) => {
-  if (!input || input.text === '') {
+  if (!input || !input.text || input.text === '') {
     return ''
   }
   return serialize({
     type: 'paragraph',
-    text: input.text || '',
+    text: input.text,
     bold: input.bold || false,
     italic: input.italic || false,
     code: input.code || false,
