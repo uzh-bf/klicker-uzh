c91d35e342ef7e49e457cd69ac52c2b39d09b01f
Patrick Louis Aldover <patrick.aldover@df.uzh.ch>
2026-07-20 22:02:20 +0200

    feat(kg): add shared graph contracts

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -15,6 +15,9 @@ import type {
 // ----- HATCHET (WORKER/TASK) TYPES -----
 export * from './hatchet.js'
 
+// ----- KNOWLEDGE GRAPH TYPES -----
+export * from './knowledgeGraph.js'
+
 // ----- ACTIVITY LOG TYPES -----
 // #region
 export enum ActivityLogModificationFieldType {
