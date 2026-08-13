12161f80a50944a0f50131a5c1ab2a1cdd268e20
Roland Schlaefli <rolandschlaefli@gmail.com>
2026-08-13 16:08:55 +0200

    fix(frontend-manage): prevent activity wizard overlap

--- apps/frontend-manage/src/components/activities/ActivityCreation.tsx
+++ apps/frontend-manage/src/components/activities/ActivityCreation.tsx
@@ -216,7 +216,7 @@ function ActivityCreation({
   }
 
   return (
-    <div className="print-hidden md:h-73 md:min-h-73 mb-3 flex flex-col justify-center">
+    <div className="print-hidden md:min-h-73 mb-3 flex shrink-0 flex-col justify-center md:h-auto">
       <div className="h-full w-full">
         {creationMode === ActivityType.LiveQuiz && (
           <LiveQuizWizard
