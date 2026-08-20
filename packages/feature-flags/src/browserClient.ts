1f0cfe42dbdd838e5aa4d6c6e796ba912deb9cec
Roland Schlaefli <rolandschlaefli@gmail.com>
2026-08-20 17:36:10 +0200

    fix(feature-flags): expose sanitized browser attributes

--- packages/feature-flags/src/browserClient.ts
+++ packages/feature-flags/src/browserClient.ts
@@ -1,5 +1,8 @@
 import { GrowthBook } from '@growthbook/growthbook'
-import { normalizeFeatureFlagEnvironment } from './contracts.js'
+import {
+  normalizeFeatureFlagEnvironment,
+  sanitizeFeatureFlagAttributes,
+} from './contracts.js'
 
 export type BrowserFeatureFlagConfig = {
   apiHost?: string
@@ -46,5 +49,10 @@ export function createBrowserFeatureFlagClient<
     return initializePromise
   }
 
-  return { environment, growthbook, initialize }
+  const setAttributes = (attributes: unknown) =>
+    growthbook.setAttributes(
+      sanitizeFeatureFlagAttributes(attributes, environment)
+    )
+
+  return { environment, growthbook, initialize, setAttributes }
 }
