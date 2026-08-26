c8e1c9b22c96716288469d18dca85aecb1b3d990
Roland Schläfli <rolandschlaefli@gmail.com>
2026-08-26 19:53:10 +0200

    fix(deploy): open basic KB ingestion in staging (#5580)

--- util/check-kb-ingestion-stg.mjs
+++ util/check-kb-ingestion-stg.mjs
@@ -8,15 +8,16 @@ const expectedWorkerData = {
   KB_INGESTION_API_URL:
     'http://ingestion-resource-api.stg-ingestion.svc.cluster.local:8000',
   KB_INGESTION_PROJECT_ID: 'klicker-course-materials',
-  KB_INGESTION_WORKER_DISABLED: 'true',
   KB_SOURCE_GATEWAY_URL:
     'http://klicker-backend.stg-klicker.svc.cluster.local:3000',
 }
 const expectedBackendData = {
   KB_GRAPH_DISABLED: 'true',
-  KB_INGESTION_DISABLED: 'true',
 }
-const workerOnlyKeys = Object.keys(expectedWorkerData)
+const workerOnlyKeys = [
+  ...Object.keys(expectedWorkerData),
+  'KB_INGESTION_WORKER_DISABLED',
+]
 const secretKeys = [
   'KB_INGESTION_API_KEY',
   'KB_SOURCE_GATEWAY_KEY',
@@ -77,6 +78,14 @@ function requireData(configMap, expected) {
   }
 }
 
+function requireAbsent(configMap, keys) {
+  for (const key of keys) {
+    if (key in (configMap.data ?? {})) {
+      fail(`${configMap.metadata.name} must not render ${key}`)
+    }
+  }
+}
+
 const configMaps = renderConfigMaps()
 const backend = findConfigMap(configMaps, '-config-backend-graphql')
 const generalWorker = findConfigMap(
@@ -89,6 +98,8 @@ const responseProcessors = configMaps.filter((value) =>
 
 requireData(backend, expectedBackendData)
 requireData(generalWorker, expectedWorkerData)
+requireAbsent(backend, ['KB_INGESTION_DISABLED'])
+requireAbsent(generalWorker, ['KB_INGESTION_WORKER_DISABLED'])
 if (responseProcessors.length !== 2) {
   fail(
     `expected two response-processor ConfigMaps, received ${responseProcessors.length}`
