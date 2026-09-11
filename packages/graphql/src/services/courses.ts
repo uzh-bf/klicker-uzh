f88fa65c889688e32390b56a7470a88f86180f91
Patrick Louis Aldover <patrick.aldover@df.uzh.ch>
2026-09-11 18:45:39 +0200

    fix: allow twenty minutes for course deletion

--- packages/graphql/src/services/courses.ts
+++ packages/graphql/src/services/courses.ts
@@ -3350,7 +3350,7 @@ export async function deleteCourse(
 
       return { deleted, deletionCancelled: false }
     },
-    { timeout: 300000 }
+    { timeout: 1200000 }
   )
 
   if (!deletedCourse.deleted) {
