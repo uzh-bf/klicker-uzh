d2c23125cc44ac8b81a8ffaddd01030e56bcbe27
Roland Schlaefli <rolandschlaefli@gmail.com>
2026-09-20 21:57:07 +0200

    fix(frontend-pwa): keep SSR request logging alongside the data-use flow in the v3-audit sync

--- apps/frontend-pwa/src/lib/apollo.ts
+++ apps/frontend-pwa/src/lib/apollo.ts
@@ -13,6 +13,10 @@ import { createPersistedQueryLink } from '@apollo/client/link/persisted-queries'
 import { RetryLink } from '@apollo/client/link/retry'
 import { GraphQLWsLink } from '@apollo/client/link/subscriptions'
 import hashes from '@klicker-uzh/graphql/dist/client.json'
+import {
+  propagationHeaders,
+  type RequestContext,
+} from '@klicker-uzh/logging/request'
 import { getStoredAuthToken } from '@klicker-uzh/util/client-auth'
 import merge from 'deepmerge'
 import { getOperationAST } from 'graphql'
@@ -37,7 +41,10 @@ export const APOLLO_STATE_PROP_NAME = '__APOLLO_STATE__'
 
 let apolloClient: ApolloClient<NormalizedCacheObject>
 
-function createIsomorphLink(ctx?: GetServerSidePropsContext) {
+function createIsomorphLink(
+  ctx?: GetServerSidePropsContext,
+  requestContext?: RequestContext
+) {
   const isBrowser = typeof window !== 'undefined'
 
   const persistedLink =
@@ -85,12 +92,13 @@ function createIsomorphLink(ctx?: GetServerSidePropsContext) {
         ...(ctx?.req?.headers?.cookie
           ? { cookie: ctx.req.headers.cookie }
           : {}),
+        ...(requestContext ? propagationHeaders(requestContext) : {}),
       },
     }
   })
 
   const errorLink = onError(({ graphQLErrors, networkError }) => {
-    if (graphQLErrors)
+    if (isBrowser && graphQLErrors)
       graphQLErrors.forEach(({ message, locations, path, extensions }) => {
         console.log(
           `[GraphQL error]: Message: ${message}, Locations: ${util.inspect(
@@ -132,7 +140,7 @@ function createIsomorphLink(ctx?: GetServerSidePropsContext) {
           )
         }
       })
-    if (networkError) console.log(`[Network error]`, networkError)
+    if (isBrowser && networkError) console.log(`[Network error]`, networkError)
   })
 
   let link: ApolloLink = new HttpLink({
@@ -196,7 +204,10 @@ function createIsomorphLink(ctx?: GetServerSidePropsContext) {
 }
 
 // TODO: use the schema link when working on the server?
-function createApolloClient(ctx?: GetServerSidePropsContext) {
+function createApolloClient(
+  ctx?: GetServerSidePropsContext,
+  requestContext?: RequestContext
+) {
   // TODO: switch to yoga link
   // const yogaLink = new YogaLink({
   //   endpoint: publicRuntimeConfig.API_URL,
@@ -205,7 +216,7 @@ function createApolloClient(ctx?: GetServerSidePropsContext) {
 
   return new ApolloClient({
     ssrMode: typeof window === 'undefined',
-    link: createIsomorphLink(ctx),
+    link: createIsomorphLink(ctx, requestContext),
     cache: new InMemoryCache(),
     connectToDevTools: process.env.NODE_ENV === 'development',
   })
@@ -213,9 +224,10 @@ function createApolloClient(ctx?: GetServerSidePropsContext) {
 
 export function initializeApollo(
   initialState?: NormalizedCacheObject,
-  ctx?: GetServerSidePropsContext
+  ctx?: GetServerSidePropsContext,
+  requestContext?: RequestContext
 ): ApolloClient<NormalizedCacheObject> {
-  const _apolloClient = apolloClient ?? createApolloClient(ctx)
+  const _apolloClient = apolloClient ?? createApolloClient(ctx, requestContext)
 
   // If your page has Next.js data fetching methods that use Apollo Client, the initial state
   // gets hydrated here
--- packages/graphql/test/accountLtiLinking.test.ts
+++ packages/graphql/test/accountLtiLinking.test.ts
@@ -1,4 +1,5 @@
 import { prisma as prismaClient } from '@klicker-uzh/prisma'
+import { createLogger } from '@klicker-uzh/logging/node'
 import { CourseAuthType, PrismaClient } from '@klicker-uzh/prisma/client'
 import {
   PARTICIPANT_DATA_USE_DISCLOSURE_VERSION,
@@ -51,6 +52,14 @@ function createCtx(): Context {
     emitter: new EventEmitter(),
     hatchet: {} as any,
     tasks: {} as any,
+    requestContext: {
+      requestId: 'account-lti-test-request',
+      correlationId: 'account-lti-test-request',
+    },
+    log: createLogger({
+      service: 'graphql-account-lti-test',
+      environment: 'test',
+    }),
   } as Context
 }
 
